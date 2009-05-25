0a28f5adb67531ba7fdc22e4a8f3a69a7a140f36
Greg Wilkins <greg@webtide.com>
2009-05-25 23:43:56 +0000

    include
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@281 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-webapp/src/main/java/org/eclipse/jetty/webapp/FragmentConfiguration.java
+++ jetty-webapp/src/main/java/org/eclipse/jetty/webapp/FragmentConfiguration.java
@@ -14,13 +14,9 @@
 
 package org.eclipse.jetty.webapp;
 
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.jar.JarEntry;
 import java.util.regex.Pattern;
 
-import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.resource.Resource;
 
 /**
