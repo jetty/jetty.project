7c8f5442c1f6dceaf42643ff5dd7dbe7b3b5e2ef
Greg Wilkins <greg@webtide.com>
2009-05-28 08:16:10 +0000

    async-supported
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/branches/jetty-8@315 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-webapp/src/main/java/org/eclipse/jetty/webapp/WebXmlProcessor.java
+++ jetty-webapp/src/main/java/org/eclipse/jetty/webapp/WebXmlProcessor.java
@@ -646,7 +646,7 @@ public class WebXmlProcessor
                 holder.setRunAsRole(roleName);
         }
 
-        String async=node.getString("async-support",false,true);
+        String async=node.getString("async-supported",false,true);
         if (async!=null)
             holder.setAsyncSupported(async.length()==0||Boolean.valueOf(async));
 
