659cde6d29dbb0a96522dadc239a90080fcf2a0d
Jesse McConnell <jesse.mcconnell@gmail.com>
2009-06-30 21:34:46 +0000

    [BUG-27751] update headers and remove extra file
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk/jetty-policy@448 7e9141cc-0065-0410-87d8-b60c137991c4

--- src/main/java/org/eclipse/jetty/policy/PolicyContext.java
+++ src/main/java/org/eclipse/jetty/policy/PolicyContext.java
@@ -1,4 +1,19 @@
 package org.eclipse.jetty.policy;
+//========================================================================
+//Copyright (c) Webtide LLC
+//------------------------------------------------------------------------
+//All rights reserved. This program and the accompanying materials
+//are made available under the terms of the Eclipse Public License v1.0
+//and Apache License v2.0 which accompanies this distribution.
+//
+//The Eclipse Public License is available at
+//http://www.eclipse.org/legal/epl-v10.html
+//
+//The Apache License v2.0 is available at
+//http://www.apache.org/licenses/LICENSE-2.0.txt
+//
+//You may elect to redistribute this code under either of these licenses.
+//========================================================================
 
 import java.security.KeyStore;
 import java.security.Principal;
--- src/main/java/org/eclipse/jetty/policy/loader/PolicyFileScanner.java
+++ src/main/java/org/eclipse/jetty/policy/loader/PolicyFileScanner.java
@@ -1,35 +1,22 @@
 package org.eclipse.jetty.policy.loader;
 
-/*
- *  Licensed to the Apache Software Foundation (ASF) under one or more
- *  contributor license agreements.  See the NOTICE file distributed with
- *  this work for additional information regarding copyright ownership.
- *  The ASF licenses this file to You under the Apache License, Version 2.0
- *  (the "License"); you may not use this file except in compliance with
- *  the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */ 
-
 //========================================================================
+//Copyright (c) Webtide LLC
+//------------------------------------------------------------------------
 //All rights reserved. This program and the accompanying materials
 //are made available under the terms of the Eclipse Public License v1.0
 //and Apache License v2.0 which accompanies this distribution.
-//The Eclipse Public License is available at 
+//
+//The Eclipse Public License is available at
 //http://www.eclipse.org/legal/epl-v10.html
+//
 //The Apache License v2.0 is available at
-//http://www.opensource.org/licenses/apache2.0.php
-//You may elect to redistribute this code under either of these licenses. 
-// 
+//http://www.apache.org/licenses/LICENSE-2.0.txt
+//
+//You may elect to redistribute this code under either of these licenses.
+//========================================================================
 // This file adapted for use from Apache Harmony code by written and contributed 
 // to that project by Alexey V. Varlamov under the ASL-2.0
-//
 // See CQ3380
 //========================================================================
 
