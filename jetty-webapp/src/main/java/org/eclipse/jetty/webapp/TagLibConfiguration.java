87884156c6ad0d09df0b0b390b354c6753c82aad
Jan Bartel <jan@webtide.com>
2009-07-06 06:13:17 +0000

    Add comments, take out old code.
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/branches/jetty-8@484 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-webapp/src/main/java/org/eclipse/jetty/webapp/TagLibConfiguration.java
+++ jetty-webapp/src/main/java/org/eclipse/jetty/webapp/TagLibConfiguration.java
@@ -53,9 +53,6 @@ public class TagLibConfiguration implements Configuration
 {
     public static final String TLD_RESOURCES = "org.eclipse.jetty.tlds";
     
-    // TODO support patterns
-    private static final String __web_inf_pattern = "org.eclipse.jetty.webapp.WebInfIncludeTLDJarPattern";
-    private static final String __container_pattern = "org.eclipse.jetty.server.webapp.ContainerIncludeTLDJarPattern";
     
     public class TldProcessor
     {
@@ -243,7 +240,9 @@ public class TagLibConfiguration implements Configuration
         }
         
     
-        // Add in tlds found in META-INF of jars
+        // Add in tlds found in META-INF of jars. The jars that will be scanned are controlled by
+        // the patterns defined in the context attributes: org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern,
+        // and org.eclipse.jetty.server.webapp.WebInfIncludeJarPattern
         Collection<Resource> tld_resources=(Collection<Resource>)context.getAttribute(TLD_RESOURCES);
         if (tld_resources!=null)
             tlds.addAll(tld_resources);
