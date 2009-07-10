17b3aac3708b68b3b9b33c15a7ea35e66360b995
Jan Bartel <jan@webtide.com>
2009-07-10 02:20:22 +0000

    Put in accordance with email from Jan Luehe 10 July 2009 regarding precedence of web.xml wrt @WebServlet annotation.
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/branches/jetty-8@537 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-annotations/src/main/java/org/eclipse/jetty/annotations/WebServletAnnotationHandler.java
+++ jetty-annotations/src/main/java/org/eclipse/jetty/annotations/WebServletAnnotationHandler.java
@@ -14,7 +14,9 @@
 package org.eclipse.jetty.annotations;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.ListIterator;
 
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebInitParam;
@@ -25,6 +27,7 @@ import org.eclipse.jetty.annotations.AnnotationParser.AnnotationHandler;
 import org.eclipse.jetty.annotations.AnnotationParser.Value;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.servlet.ServletMapping;
+import org.eclipse.jetty.util.LazyList;
 import org.eclipse.jetty.util.Loader;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.webapp.WebAppContext;
@@ -47,10 +50,19 @@ public class WebServletAnnotationHandler implements AnnotationHandler
     
     
     /** 
-     * TODO: ensure that web.xml takes precedence:
-     *  - if servlet of same name and same class exists in web.xml, then values from annotation cannot override it?
-     *  
-     *  - if servlet is different name NEED CLARIFICATION FROM JSR. For now, we assume it is a different servlet.
+     * Handle a WebServlet annotation.
+     * 
+     * If web.xml does not define a servlet of the same name, then this is an entirely
+     * new servlet definition.
+     * 
+     * Otherwise, the values from web.xml override the values from the annotation except for:
+     * 
+     * <ul>
+     * <li>init-params: if the annotation contains a different init-param name, then it is added to the
+     *     effective init-params for the servlet<li>
+     * <li>url-patterns: if the annotation contains a different url-pattern, then it is added to the
+     *     effective url-patterns for the servlet</li>
+     * </ul>
      *  
      * @see org.eclipse.jetty.annotations.AnnotationParser.AnnotationHandler#handleClass(java.lang.String, int, int, java.lang.String, java.lang.String, java.lang.String[], java.lang.String, java.util.List)
      */
@@ -98,60 +110,76 @@ public class WebServletAnnotationHandler implements AnnotationHandler
             Log.warn(clazz.getName()+ " defines neither @WebServlet.value nor @WebServlet.urlPatterns");
             return;
         }
-
+        //canonicalize the patterns
+        ArrayList<String> urlPatternList = new ArrayList<String>();
+        for (String p : urlPatterns)
+            urlPatternList.add(Util.normalizePattern(p));
+        
         String servletName = (annotation.name().equals("")?clazz.getName():annotation.name());
 
         //Find out if a <servlet> from web.xml of this type already exists with this name
         ServletHolder[] holders = _wac.getServletHandler().getServlets();
+        boolean isNew = true;
         ServletHolder holder = null;
         for (ServletHolder h : holders)
         {
             if (h.getClassName().equals(clazz.getName()) && h.getName().equals(servletName))
             {
                 holder = h;
+                isNew = false;
                 break;
             }
         }
-        
-        //A <servlet> with matching name already exists in web.xml, ignore
-        //the annotation
-        //TODO - Get confirmation from JSR!
-        if (holder != null)
-            return;
-        
-        // If no <servlet> with matching name was found, we need to make one
-        if (holder == null)
+
+        if (isNew)
+        {
             holder = new ServletHolder(clazz);
-        
-        holder.setName(servletName);
-        holder.setInitOrder(annotation.loadOnStartup());
-
-        for (WebInitParam ip:annotation.initParams())
+            _wac.getServletHandler().addServlet(holder);
+            holder.setName(servletName);
+            holder.setInitOrder(annotation.loadOnStartup());
+            holder.setAsyncSupported(annotation.asyncSupported());
+            for (WebInitParam ip:annotation.initParams())
+            {
+                holder.setInitParameter(ip.name(), ip.value());
+            }
+            MultipartConfig multipart = (MultipartConfig)clazz.getAnnotation(MultipartConfig.class);
+            if (multipart != null)
+            {
+                //TODO set multipart on ServletHolder
+            }
+            ServletMapping mapping = new ServletMapping();  
+            mapping.setServletName(holder.getName());
+            mapping.setPathSpecs( LazyList.toStringArray(urlPatternList));
+            _wac.getServletHandler().addServletMapping(mapping);
+        }
+        else
         {
-            holder.setInitParameter(ip.name(), ip.value());
+            //check if the existing servlet has each init-param from the annotation
+            //if not, add it
+            for (WebInitParam ip:annotation.initParams())
+            {
+                if (holder.getInitParameter(ip.name()) == null)
+                    holder.setInitParameter(ip.name(), ip.value());
+            }
+            
+            //check the url-patterns, if there annotation has a new one, add it
+            ServletMapping[] mappings = _wac.getServletHandler().getServletMappings();
+            
+            //find which patterns aren't already in web.xml
+            if (mappings != null)
+            {
+                for (ServletMapping mapping : mappings)
+                {   
+                    List<String> specs = LazyList.array2List(mapping.getPathSpecs());
+                    urlPatternList.removeAll(specs);
+                }
+            }
+            //add the remaining new ones in as mapping patterns
+            ServletMapping mapping = new ServletMapping();
+            mapping.setServletName(servletName);
+            mapping.setPathSpecs(LazyList.toStringArray(urlPatternList));
+            _wac.getServletHandler().addServletMapping(mapping); 
         }
-
-        ArrayList paths = new ArrayList();
-        ServletMapping mapping = new ServletMapping();
-        mapping.setServletName(holder.getName());
-        for (String s:urlPatterns)
-        {    
-            paths.add(Util.normalizePattern(s)); 
-        }
-        mapping.setPathSpecs((String[])paths.toArray(new String[paths.size()]));     
-        holder.setAsyncSupported(annotation.asyncSupported());
-        
-        //MultipartConfig annotation handled separately
-        MultipartConfig multipart = (MultipartConfig)clazz.getAnnotation(MultipartConfig.class);
-        if (multipart != null)
-        {
-            //TODO handle in here
-        }
-        
-        //If the ServletHolder did not already exist, then add it
-         _wac.getServletHandler().addServlet(holder);
-        //Add the mappings for it 
-        _wac.getServletHandler().addServletMapping(mapping);
     }
 
     public void handleField(String className, String fieldName, int access, String fieldType, String signature, Object value, String annotation,
