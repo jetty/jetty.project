42016bd3f18a6a195ab7c40dfc23c676c25db0ae
Jesse McConnell <jesse.mcconnell@gmail.com>
2009-08-13 19:20:26 +0000

    update the jetty.policy
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@714 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-policy/src/test/java/org/eclipse/jetty/policy/JettyPolicyTest.java
+++ jetty-policy/src/test/java/org/eclipse/jetty/policy/JettyPolicyTest.java
@@ -40,8 +40,9 @@ public class JettyPolicyTest extends TestCase
         throws Exception
     {
         super.setUp();
-        
-        evaluator.put( "jetty.home", getWorkingDirectory() );
+
+        evaluator.put("jetty.home",MavenTestingUtils.getBasedir().getAbsolutePath());
+        evaluator.put("basedir",MavenTestingUtils.getBasedir().getAbsolutePath());
     }
 
     public void testGlobalAllPermissionLoader()
@@ -100,6 +101,8 @@ public class JettyPolicyTest extends TestCase
 
         ap.refresh();
         
+        // ap.dump(System.out);
+
         URL url = new URL( "file:///bar.jar" ); 
         CodeSource cs = new CodeSource( url, new Certificate[0]);
         
@@ -123,8 +126,17 @@ public class JettyPolicyTest extends TestCase
                 + "/src/test/resources/multiple-codebase-mixed-permission.policy" ), evaluator );
 
         ap.refresh();
+
+        // ap.dump(System.out);
     }
     
+    public void testSCLoader() throws Exception
+    {
+        JettyPolicy ap = new JettyPolicy(Collections.singleton(getWorkingDirectory() + "/src/main/config/lib/security/jetty.policy"),evaluator);
+
+        ap.refresh();
+        ap.dump(System.out);
+    }
 
     public void testMultipleFilePermissionLoader()
         throws Exception
