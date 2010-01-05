acb98b34be4ca0f8a586ed5b3f4aac03c8b96eda
Greg Wilkins <greg@webtide.com>
2010-01-05 00:59:52 +0000

    work in progress for OSGi boot to use Deployment manager
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@1178 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-osgi/jetty-osgi-boot/src/main/java/org/eclipse/jetty/osgi/boot/OSGiAppProvider.java
+++ jetty-osgi/jetty-osgi-boot/src/main/java/org/eclipse/jetty/osgi/boot/OSGiAppProvider.java
@@ -1,5 +1,10 @@
 package org.eclipse.jetty.osgi.boot;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.eclipse.jetty.deploy.App;
 import org.eclipse.jetty.deploy.AppProvider;
 import org.eclipse.jetty.deploy.DeploymentManager;
@@ -9,10 +14,12 @@ import org.eclipse.jetty.util.component.AbstractLifeCycle;
 public class OSGiAppProvider extends AbstractLifeCycle implements AppProvider
 {
     DeploymentManager _manager;
+    Map<ContextHandler,OSGiApp> _apps = new ConcurrentHashMap<ContextHandler, OSGiApp>();
     
     public ContextHandler createContextHandler(App app) throws Exception
     {
-        return null;
+        // return pre-created Context
+        return ((OSGiApp)app)._context;
     }
 
     public void setDeploymentManager(DeploymentManager deploymentManager)
@@ -20,4 +27,42 @@ public class OSGiAppProvider extends AbstractLifeCycle implements AppProvider
         _manager=deploymentManager;
     }
 
+    public void addContext(ContextHandler context) throws Exception
+    {
+        // TODO apply configuration specific to this provider
+        
+        // wrap context as an App
+        OSGiApp app = new OSGiApp(_manager,this,context.getDisplayName(),null,context);
+        _apps.put(context,app);
+        _manager.addApp(app);
+    }
+
+    public void addContext(String contextxml) throws Exception
+    {
+        // TODO apply configuration specific to this provider
+        // TODO construct ContextHandler
+        ContextHandler context=null;
+        // wrap context as an App
+        OSGiApp app = new OSGiApp(_manager,this,context.getDisplayName(),null,context);
+        _apps.put(context,app);
+        _manager.addApp(app);
+    }
+    
+    public void removeContext(ContextHandler context) throws Exception
+    {
+        // wrap context as an App
+        OSGiApp app = _apps.get(context);
+        
+    }
+    
+    
+    class OSGiApp extends App
+    {
+        final ContextHandler _context;
+        public OSGiApp(DeploymentManager manager, AppProvider provider, String originId, File archivePath, ContextHandler context)
+        {
+            super(manager,provider,originId,archivePath);
+            _context=context;
+        }
+    }
 }
