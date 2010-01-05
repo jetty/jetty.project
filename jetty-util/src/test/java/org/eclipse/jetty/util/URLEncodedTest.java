cbd9ed8386a7a7ee3ba80c21db236f254d402eb4
Greg Wilkins <greg@webtide.com>
2010-01-05 05:09:31 +0000

    JETTY-1151 JETTY-1098 allow UTF-8 with 0 carry bits
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@1179 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-util/src/test/java/org/eclipse/jetty/util/URLEncodedTest.java
+++ jetty-util/src/test/java/org/eclipse/jetty/util/URLEncodedTest.java
@@ -183,5 +183,16 @@ public class URLEncodedTest extends junit.framework.TestCase
         else
             assertTrue("Charset Shift_JIS not supported by jvm", true);
     }
-    
+
+    /* -------------------------------------------------------------- */
+    public void testUtf8()
+        throws Exception
+    {
+        UrlEncoded url_encoded = new UrlEncoded();
+        assertEquals("Empty",0, url_encoded.size());
+
+        url_encoded.clear();
+        url_encoded.decode("text=%E0%B8%9F%E0%B8%AB%E0%B8%81%E0%B8%A7%E0%B8%94%E0%B8%B2%E0%B9%88%E0%B8%81%E0%B8%9F%E0%B8%A7%E0%B8%AB%E0%B8%AA%E0%B8%94%E0%B8%B2%E0%B9%88%E0%B8%AB%E0%B8%9F%E0%B8%81%E0%B8%A7%E0%B8%94%E0%B8%AA%E0%B8%B2%E0%B8%9F%E0%B8%81%E0%B8%AB%E0%B8%A3%E0%B8%94%E0%B9%89%E0%B8%9F%E0%B8%AB%E0%B8%99%E0%B8%81%E0%B8%A3%E0%B8%94%E0%B8%B5&Action=Submit");
+        assertEquals("ฟหกวดา่กฟวหสดา่หฟกวดสาฟกหรด้ฟหนกรดี",url_encoded.get("text"));
+    }
 }
