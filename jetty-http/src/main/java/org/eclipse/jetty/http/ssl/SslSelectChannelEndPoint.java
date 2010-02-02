77f019b9f9979fc6234321b844e0ccbc4bbc43c4
Greg Wilkins <greg@webtide.com>
2010-02-02 22:52:51 +0000

    roll back attempt to handle ssl engine buffer overflow in IBM JVM
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@1233 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-http/src/main/java/org/eclipse/jetty/http/ssl/SslSelectChannelEndPoint.java
+++ jetty-http/src/main/java/org/eclipse/jetty/http/ssl/SslSelectChannelEndPoint.java
@@ -681,10 +681,7 @@ public class SslSelectChannelEndPoint extends SelectChannelEndPoint
         switch(_result.getStatus())
         {
             case BUFFER_OVERFLOW:
-                if (buffer.position()==0) // if this buffer can't have more space
-                    throw new IllegalStateException(_result.toString()+" "+buffer.limit());
-                // return true is some bytes somewhere were moved about.
-                return total_filled>0 ||_result.bytesConsumed()>0 || _result.bytesProduced()>0;
+                throw new IllegalStateException(_result.toString()+" "+buffer.position()+" "+buffer.limit());
                 
             case BUFFER_UNDERFLOW:
                 // Not enough data, 
@@ -704,7 +701,6 @@ public class SslSelectChannelEndPoint extends SelectChannelEndPoint
                 _closing=true;
                 // return true is some bytes somewhere were moved about.
                 return total_filled>0 ||_result.bytesConsumed()>0 || _result.bytesProduced()>0;
-                
             case OK:
                 // return true is some bytes somewhere were moved about.
                 return total_filled>0 ||_result.bytesConsumed()>0 || _result.bytesProduced()>0;
