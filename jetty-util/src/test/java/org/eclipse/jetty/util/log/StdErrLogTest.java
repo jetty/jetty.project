48a474dee1e01fe8f8b95490a5513543914b615f
Simone Bordet <simone.bordet@gmail.com>
2010-04-27 11:22:27 +0000

    Fixes 310603 (Make Logger interface consistent).
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@1572 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-util/src/test/java/org/eclipse/jetty/util/log/StdErrLogTest.java
+++ jetty-util/src/test/java/org/eclipse/jetty/util/log/StdErrLogTest.java
@@ -4,7 +4,7 @@
 // All rights reserved. This program and the accompanying materials
 // are made available under the terms of the Eclipse Public License v1.0
 // and Apache License v2.0 which accompanies this distribution.
-// The Eclipse Public License is available at 
+// The Eclipse Public License is available at
 // http://www.eclipse.org/legal/epl-v10.html
 // The Apache License v2.0 is available at
 // http://www.opensource.org/licenses/apache2.0.php
@@ -18,37 +18,37 @@ import junit.framework.TestCase;
 /**
  * @author mgorovoy
  * */
-public class StdErrLogTest extends TestCase 
+public class StdErrLogTest extends TestCase
 {
     public void testNullValues()
     {
         StdErrLog log = new StdErrLog();
         log.setDebugEnabled(true);
         log.setHideStacks(true);
-        
+
         try {
             log.info("Testing info(msg,null,null) - {} {}",null,null);
             log.info("Testing info(msg,null,null) - {}",null,null);
             log.info("Testing info(msg,null,null)",null,null);
             log.info(null,"- Testing","info(null,arg0,arg1)");
             log.info(null,null,null);
-            
+
             log.debug("Testing debug(msg,null,null) - {} {}",null,null);
             log.debug("Testing debug(msg,null,null) - {}",null,null);
             log.debug("Testing debug(msg,null,null)",null,null);
             log.debug(null,"- Testing","debug(null,arg0,arg1)");
             log.debug(null,null,null);
-            
-            log.debug("Testing debug(msg,null)",null);
+
+            log.debug("Testing debug(msg,null)");
             log.debug(null,new Throwable("IGNORE::Testing debug(null,thrw)").fillInStackTrace());
-            
+
             log.warn("Testing warn(msg,null,null) - {} {}",null,null);
             log.warn("Testing warn(msg,null,null) - {}",null,null);
             log.warn("Testing warn(msg,null,null)",null,null);
             log.warn(null,"- Testing","warn(msg,arg0,arg1)");
             log.warn(null,null,null);
-                       
-            log.warn("Testing warn(msg,null)",null);
+
+            log.warn("Testing warn(msg,null)");
             log.warn(null,new Throwable("IGNORE::Testing warn(msg,thrw)").fillInStackTrace());
         }
         catch (NullPointerException npe)
