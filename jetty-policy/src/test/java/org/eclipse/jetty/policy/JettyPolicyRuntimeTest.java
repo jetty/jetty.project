8f6bde3ccd0be902a7bfaf00c9049077544e67ce
Simone Bordet <simone.bordet@gmail.com>
2010-05-11 15:27:33 +0000

    Fix for #308859 (Update test suite to JUnit4 - Module jetty-policy).
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@1753 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-policy/src/test/java/org/eclipse/jetty/policy/JettyPolicyRuntimeTest.java
+++ jetty-policy/src/test/java/org/eclipse/jetty/policy/JettyPolicyRuntimeTest.java
@@ -1,17 +1,17 @@
-package org.eclipse.jetty.policy;
 //========================================================================
 //Copyright (c) Webtide LLC
 //------------------------------------------------------------------------
 //All rights reserved. This program and the accompanying materials
 //are made available under the terms of the Eclipse Public License v1.0
 //and Apache License v2.0 which accompanies this distribution.
-//The Eclipse Public License is available at 
+//The Eclipse Public License is available at
 //http://www.eclipse.org/legal/epl-v10.html
 //The Apache License v2.0 is available at
 //http://www.opensource.org/licenses/apache2.0.php
-//You may elect to redistribute this code under either of these licenses. 
+//You may elect to redistribute this code under either of these licenses.
 //========================================================================
 
+package org.eclipse.jetty.policy;
 
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
@@ -24,65 +24,61 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Set;
 
-import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class JettyPolicyRuntimeTest extends TestCase
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class JettyPolicyRuntimeTest
 {
-    HashMap<String, String> evaluator = new HashMap<String, String>();
-    
+    private HashMap<String, String> evaluator = new HashMap<String, String>();
     private boolean _runningOnWindows;
-    
-    
-    @Override
-    protected void setUp() throws Exception
+
+    @Before
+    public void init() throws Exception
     {
         System.setSecurityManager(null);
         Policy.setPolicy(null);
 
         _runningOnWindows = System.getProperty( "os.name" ).startsWith( "Windows" );
-        
-        super.setUp();
-        
+
         evaluator.put("jetty.home",MavenTestingUtils.getBaseURI().toASCIIString());
         evaluator.put("basedir",MavenTestingUtils.getBaseURI().toASCIIString());
     }
 
-    
-    @Override
-    protected void tearDown() throws Exception
+    @After
+    public void destroy() throws Exception
     {
-        super.tearDown();
-        
-        System.setSecurityManager( null );
-        Policy.setPolicy( null );
+        System.setSecurityManager(null);
+        Policy.setPolicy(null);
     }
 
+    @Test
     public void testSimplePolicyReplacement() throws Exception
-    {   
-        
-        JettyPolicy ap = new JettyPolicy(getSinglePolicy("global-all-permission.policy"),evaluator);
-
+    {
+        JettyPolicy ap = new JettyPolicy(getSinglePolicy("global-all-permission.policy"), evaluator);
         ap.refresh();
-        
-        Policy.setPolicy( ap );       
-        System.setSecurityManager( new SecurityManager() );
-        
-        File test = new File( "/tmp" );
-        
-        assertTrue ( test.canRead() );
-         
-    }
-    
-    public void testRepeatedPolicyReplacement() throws Exception
-    {       
-        JettyPolicy ap = new JettyPolicy(getSinglePolicy("global-all-permission.policy"),evaluator);
 
-        ap.refresh();
-        
         Policy.setPolicy( ap );
-        
         System.setSecurityManager( new SecurityManager() );
-        
+
+        File test = new File( "/tmp" );
+
+        assertTrue( test.canRead() );
+    }
+
+    @Test
+    public void testRepeatedPolicyReplacement() throws Exception
+    {
+        JettyPolicy ap = new JettyPolicy(getSinglePolicy("global-all-permission.policy"),evaluator);
+        ap.refresh();
+
+        Policy.setPolicy( ap );
+        System.setSecurityManager( new SecurityManager() );
+
         // Test that the all permission policy allows us to do this
         try
         {
@@ -95,11 +91,10 @@ public class JettyPolicyRuntimeTest extends TestCase
             ace.printStackTrace(System.err);
             fail("Should NOT have thrown an AccessControlException");
         }
-        
+
         JettyPolicy ap2 = new JettyPolicy(getSinglePolicy("global-file-read-only-tmp-permission.policy"),evaluator);
-        
         ap2.refresh();
-        
+
         Policy.setPolicy( ap2 );
 
         // Test that the new policy does replace the old one and we are now not allowed
@@ -114,9 +109,9 @@ public class JettyPolicyRuntimeTest extends TestCase
         {
             // Expected Path
         }
-              
     }
 
+    @Test
     public void testPolicyRestrictive() throws Exception
     {
         // TODO - temporary, create alternate file to load for windows
@@ -127,20 +122,18 @@ public class JettyPolicyRuntimeTest extends TestCase
         }
 
         JettyPolicy ap = new JettyPolicy(getSinglePolicy("global-file-read-only-tmp-permission.policy"),evaluator);
-        
         ap.refresh();
-        
+
         Policy.setPolicy( ap );
-        
         System.setSecurityManager( new SecurityManager() );
-       
+
         File test = new File( "/tmp" );
-        
+
         assertTrue ( test.canRead() );
-        
+
         File test2 = new File( "/tmp/foo" );
         assertTrue ( test2.canRead() );
-        
+
         try
         {
             File test3 = new File("/tmp/foo/bar/do");
@@ -153,9 +146,9 @@ public class JettyPolicyRuntimeTest extends TestCase
             // Expected Path
         }
     }
-    
-    public void testCertificateLoader()
-    throws Exception
+
+    @Test
+    public void testCertificateLoader() throws Exception
     {
         // TODO - temporary, create alternate file to load for windows
         if (_runningOnWindows)
@@ -165,51 +158,47 @@ public class JettyPolicyRuntimeTest extends TestCase
         }
 
         JettyPolicy ap = new JettyPolicy(getSinglePolicy("jetty-certificate.policy"),evaluator);
-
         ap.refresh();
-        
+
         Policy.setPolicy( ap );
-        
         System.setSecurityManager( new SecurityManager() );
-     
+
         URL url = MavenTestingUtils.toTargetURL("test-policy/jetty-test-policy.jar");
-        
+
         URLClassLoader loader ;
         if (Thread.currentThread().getContextClassLoader() != null )
         {
-            loader = new URLClassLoader( new URL[]{ url }, Thread.currentThread().getContextClassLoader() );    
+            loader = new URLClassLoader( new URL[]{ url }, Thread.currentThread().getContextClassLoader() );
         }
         else
         {
-            loader = new URLClassLoader( new URL[]{ url }, ClassLoader.getSystemClassLoader() );           
+            loader = new URLClassLoader( new URL[]{ url }, ClassLoader.getSystemClassLoader() );
         }
-        
+
         Thread.currentThread().setContextClassLoader(loader);
-        
+
         ap.refresh();
-        
+
         Class<?> clazz = loader.loadClass("org.eclipse.jetty.toolchain.test.policy.Tester");
 
         Method m = clazz.getMethod("testEcho",new Class[]
         { String.class });
 
-        String foo = (String)m.invoke(clazz.newInstance(),new Object[]
-        { "foo" });
+        String foo = (String)m.invoke(clazz.newInstance(), "foo");
 
         assertEquals("foo",foo);
 
         Method m2 = clazz.getMethod("testReadSystemProperty",new Class[]
         { String.class });
 
-        m2.invoke(clazz.newInstance(),new Object[]
-        { "foo" });
+        m2.invoke(clazz.newInstance(), "foo");
 
         assertTrue("system property access was granted",true);
 
         // ap.dump(System.out);
     }
-    
-    
+
+    @Test
     public void testBadCertificateLoader()
     throws Exception
     {
@@ -221,43 +210,40 @@ public class JettyPolicyRuntimeTest extends TestCase
         }
 
         JettyPolicy ap = new JettyPolicy(getSinglePolicy("jetty-bad-certificate.policy"),evaluator);
-
         ap.refresh();
-        
+
         Policy.setPolicy( ap );
-        
         System.setSecurityManager( new SecurityManager() );
-     
+
         URL url = MavenTestingUtils.toTargetURL("test-policy/jetty-test-policy.jar");
-        
+
         URLClassLoader loader ;
         if (Thread.currentThread().getContextClassLoader() != null )
         {
-            loader = new URLClassLoader( new URL[]{ url }, Thread.currentThread().getContextClassLoader() );    
+            loader = new URLClassLoader( new URL[]{ url }, Thread.currentThread().getContextClassLoader() );
         }
         else
         {
-            loader = new URLClassLoader( new URL[]{ url }, ClassLoader.getSystemClassLoader() );           
+            loader = new URLClassLoader( new URL[]{ url }, ClassLoader.getSystemClassLoader() );
         }
-        
+
         Thread.currentThread().setContextClassLoader(loader);
-        
+
         ap.refresh();
-        
+
         try
         {
             Class<?> clazz = loader.loadClass("org.eclipse.jetty.toolchain.test.policy.Tester");
-            
+
             Method m = clazz.getMethod( "testEcho", new Class[] {String.class} );
-            
-            String foo = (String)m.invoke( clazz.newInstance(), new Object[] {"foo"} );                    
-            
+
+            String foo = (String)m.invoke( clazz.newInstance(), "foo");
+
             assertEquals("foo", foo );
-            
+
             Method m2 = clazz.getMethod( "testReadSystemProperty", new Class[] {String.class} );
-            
-            m2.invoke(clazz.newInstance(),new Object[]
-            { "foobar" });
+
+            m2.invoke(clazz.newInstance(), "foobar");
 
             fail("Should have thrown an InvocationTargetException");
         }
