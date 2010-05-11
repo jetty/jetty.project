bd32fd7f084d4c27945411e873118508b5428679
Simone Bordet <simone.bordet@gmail.com>
2010-05-11 15:27:33 +0000

    Fix for #308859 (Update test suite to JUnit4 - Module jetty-policy).
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk/jetty-policy@1753 7e9141cc-0065-0410-87d8-b60c137991c4

--- src/test/java/org/eclipse/jetty/policy/PolicyContextTest.java
+++ src/test/java/org/eclipse/jetty/policy/PolicyContextTest.java
@@ -1,4 +1,3 @@
-package org.eclipse.jetty.policy;
 //========================================================================
 //Copyright (c) Webtide LLC
 //------------------------------------------------------------------------
@@ -15,120 +14,110 @@ package org.eclipse.jetty.policy;
 //You may elect to redistribute this code under either of these licenses.
 //========================================================================
 
+package org.eclipse.jetty.policy;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.security.Permission;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-
-import junit.framework.TestCase;
 
 import org.eclipse.jetty.policy.entry.GrantEntry;
 import org.eclipse.jetty.policy.entry.KeystoreEntry;
 import org.eclipse.jetty.policy.loader.PolicyFileScanner;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PolicyContextTest
-    extends TestCase
 {
     public static final String __PRINCIPAL = "javax.security.auth.x500.X500Principal \"CN=Jetty Policy,OU=Artifact,O=Jetty Project,L=Earth,ST=Internet,C=US\"";
-    
     private boolean _runningOnWindows;
-    
-    
-    @Override
-    protected void setUp()
-        throws Exception
+
+    @Before
+    public void init() throws Exception
     {
         _runningOnWindows = System.getProperty( "os.name" ).startsWith( "Windows" );
-        
         System.setProperty( "basedir", MavenTestingUtils.getBaseURI().toASCIIString() );
-        
-        super.setUp();
     }
 
+    @Test
     public void testSelfPropertyExpansion() throws Exception
     {
-        
-        PolicyContext context = new PolicyContext();       
-        PolicyFileScanner loader = new PolicyFileScanner();       
+        PolicyContext context = new PolicyContext();
+        PolicyFileScanner loader = new PolicyFileScanner();
         List<GrantEntry> grantEntries = new ArrayList<GrantEntry>();
         List<KeystoreEntry> keystoreEntries = new ArrayList<KeystoreEntry>();
-        
-        File policyFile = new File( getWorkingDirectory() + "/src/test/resources/context/jetty-certificate.policy" );              
-   
+
+        File policyFile = new File( getWorkingDirectory() + "/src/test/resources/context/jetty-certificate.policy" );
+
         loader.scanStream( new InputStreamReader( new FileInputStream( policyFile ) ), grantEntries, keystoreEntries );
-        
+
         if ( !_runningOnWindows ) //temporary, create alternate file to load for windows
         {
-        
-        for ( Iterator<KeystoreEntry> i = keystoreEntries.iterator(); i.hasNext();)
-        {
-            KeystoreEntry node = i.next();
-            node.expand( context );
-            
-            context.setKeystore( node.toKeyStore() );
-        }    
-        
-        GrantEntry grant = grantEntries.get( 0 );      
-        grant.expand( context );
- 
-        Permission perm = grant.getPermissions().elements().nextElement();
-        
-        
-        assertEquals( __PRINCIPAL, perm.getName() );
+            for (KeystoreEntry node : keystoreEntries)
+            {
+                node.expand(context);
+
+                context.setKeystore(node.toKeyStore());
+            }
+
+            GrantEntry grant = grantEntries.get( 0 );
+            grant.expand( context );
+
+            Permission perm = grant.getPermissions().elements().nextElement();
+
+            assertEquals( __PRINCIPAL, perm.getName() );
         }
     }
-    
+
+    @Test
     public void testAliasPropertyExpansion() throws Exception
     {
-        
-        PolicyContext context = new PolicyContext();       
-        PolicyFileScanner loader = new PolicyFileScanner();       
+        PolicyContext context = new PolicyContext();
+        PolicyFileScanner loader = new PolicyFileScanner();
         List<GrantEntry> grantEntries = new ArrayList<GrantEntry>();
         List<KeystoreEntry> keystoreEntries = new ArrayList<KeystoreEntry>();
-        
-        File policyFile = new File( getWorkingDirectory() + "/src/test/resources/context/jetty-certificate-alias.policy" );              
-   
+
+        File policyFile = new File( getWorkingDirectory() + "/src/test/resources/context/jetty-certificate-alias.policy" );
+
         loader.scanStream( new InputStreamReader( new FileInputStream( policyFile ) ), grantEntries, keystoreEntries );
-        
+
         if ( !_runningOnWindows ) //temporary, create alternate file to load for windows
         {
-       
-        for ( Iterator<KeystoreEntry> i = keystoreEntries.iterator(); i.hasNext();)
-        {
-            KeystoreEntry node = i.next();
-            node.expand( context );
-            
-            context.setKeystore( node.toKeyStore() );
-        }    
-        
-        GrantEntry grant = grantEntries.get( 0 );      
-        grant.expand( context );
- 
-        Permission perm = grant.getPermissions().elements().nextElement();
-        
-        assertEquals( __PRINCIPAL, perm.getName() );
+            for (KeystoreEntry node : keystoreEntries)
+            {
+                node.expand(context);
+
+                context.setKeystore(node.toKeyStore());
+            }
+
+            GrantEntry grant = grantEntries.get( 0 );
+            grant.expand( context );
+
+            Permission perm = grant.getPermissions().elements().nextElement();
+
+            assertEquals( __PRINCIPAL, perm.getName() );
         }
     }
-    
+
+    @Test
     public void testFileSeparatorExpansion() throws Exception
     {
-        PolicyContext context = new PolicyContext();  
+        PolicyContext context = new PolicyContext();
         context.addProperty( "foo", "bar" );
-        
+
         assertEquals(File.separator, context.evaluate( "${/}" ) );
 
         assertEquals(File.separator + "bar" + File.separator, context.evaluate( "${/}${foo}${/}" ) );
-        
 
         assertEquals(File.separator + File.separator, context.evaluate( "${/}${/}" ) );
     }
-    
+
     private String getWorkingDirectory()
     {
         return MavenTestingUtils.getBasedir().getAbsolutePath();
     }
-
 }
