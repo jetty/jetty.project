8d9ecd32685b02137739d71b19546942aaa4b2ef
Simone Bordet <simone.bordet@gmail.com>
2010-05-12 08:39:06 +0000

    Fix for #308855 (Update test suite to JUnit4 - Module jetty-io).
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@1762 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-io/src/test/java/org/eclipse/jetty/io/BufferUtilTest.java
+++ jetty-io/src/test/java/org/eclipse/jetty/io/BufferUtilTest.java
@@ -4,43 +4,29 @@
 // All rights reserved. This program and the accompanying materials
 // are made available under the terms of the Eclipse Public License v1.0
 // and Apache License v2.0 which accompanies this distribution.
-// The Eclipse Public License is available at 
+// The Eclipse Public License is available at
 // http://www.eclipse.org/legal/epl-v10.html
 // The Apache License v2.0 is available at
 // http://www.opensource.org/licenses/apache2.0.php
-// You may elect to redistribute this code under either of these licenses. 
+// You may elect to redistribute this code under either of these licenses.
 // ========================================================================
 
 package org.eclipse.jetty.io;
 
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-/* ------------------------------------------------------------------------------- */
+import static org.junit.Assert.assertEquals;
+
 /**
- * 
+ *
  */
-public class BufferUtilTest extends TestCase
+public class BufferUtilTest
 {
-
-    /**
-     * Constructor for BufferUtilTest.
-     * @param arg0
-     */
-    public BufferUtilTest(String arg0)
+    @Test
+    public void testToInt() throws Exception
     {
-        super(arg0);
-    }
-
-    public static void main(String[] args)
-    {
-        junit.textui.TestRunner.run(BufferUtilTest.class);
-    }
-
-    public void testToInt()
-        throws Exception
-    {
-        Buffer buf[] = 
+        Buffer buf[] =
         {
             new ByteArrayBuffer("0"),
             new ByteArrayBuffer(" 42 "),
@@ -50,29 +36,29 @@ public class BufferUtilTest extends TestCase
             new ByteArrayBuffer("-2147483648"),
             new ByteArrayBuffer("2147483647"),
         };
-        
+
         int val[] =
         {
             0,42,43,-44,-45,-2147483648,2147483647
         };
-        
+
         for (int i=0;i<buf.length;i++)
             assertEquals("t"+i, val[i], BufferUtil.toInt(buf[i]));
     }
 
-    public void testPutInt()
-        throws Exception
+    @Test
+    public void testPutInt() throws Exception
     {
         int val[] =
         {
             0,42,43,-44,-45,Integer.MIN_VALUE,Integer.MAX_VALUE
         };
-        
+
         String str[] =
         {
             "0","42","43","-44","-45",""+Integer.MIN_VALUE,""+Integer.MAX_VALUE
         };
-        
+
         Buffer buffer = new ByteArrayBuffer(12);
 
         for (int i=0;i<val.length;i++)
@@ -80,11 +66,11 @@ public class BufferUtilTest extends TestCase
             buffer.clear();
             BufferUtil.putDecInt(buffer,val[i]);
             assertEquals("t"+i,str[i],BufferUtil.to8859_1_String(buffer));
-        }       
+        }
     }
-    
-    public void testPutLong()
-    throws Exception
+
+    @Test
+    public void testPutLong() throws Exception
     {
         long val[] =
         {
@@ -103,22 +89,22 @@ public class BufferUtilTest extends TestCase
             buffer.clear();
             BufferUtil.putDecLong(buffer,val[i]);
             assertEquals("t"+i,str[i],BufferUtil.to8859_1_String(buffer));
-        }       
+        }
     }
 
-    public void testPutHexInt()
-        throws Exception
+    @Test
+    public void testPutHexInt() throws Exception
     {
         int val[] =
         {
             0,42,43,-44,-45,-2147483648,2147483647
         };
-        
+
         String str[] =
         {
             "0","2A","2B","-2C","-2D","-80000000","7FFFFFFF"
         };
-        
+
         Buffer buffer = new ByteArrayBuffer(12);
 
         for (int i=0;i<val.length;i++)
@@ -126,6 +112,6 @@ public class BufferUtilTest extends TestCase
             buffer.clear();
             BufferUtil.putHexInt(buffer,val[i]);
             assertEquals("t"+i,str[i],BufferUtil.to8859_1_String(buffer));
-        }       
+        }
     }
 }
