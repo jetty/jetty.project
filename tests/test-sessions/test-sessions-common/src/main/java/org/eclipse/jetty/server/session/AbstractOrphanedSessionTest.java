f5559e3f3a2090d15f81d3a566ff282514de0be9
Greg Wilkins <greg@webtide.com>
2010-05-17 20:02:09 +0000

    313196 randomly allocate ports for session test.
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@1815 7e9141cc-0065-0410-87d8-b60c137991c4

--- tests/test-sessions/test-sessions-common/src/main/java/org/eclipse/jetty/server/session/AbstractOrphanedSessionTest.java
+++ tests/test-sessions/test-sessions-common/src/main/java/org/eclipse/jetty/server/session/AbstractOrphanedSessionTest.java
@@ -51,18 +51,18 @@ public abstract class AbstractOrphanedSessionTest
         // Disable scavenging for the first server, so that we simulate its "crash".
         String contextPath = "";
         String servletMapping = "/server";
-        int port1 = random.nextInt(50000) + 10000;
         int inactivePeriod = 5;
-        AbstractTestServer server1 = createServer(port1, inactivePeriod, -1);
+        AbstractTestServer server1 = createServer(0, inactivePeriod, -1);
         server1.addContext(contextPath).addServlet(TestServlet.class, servletMapping);
         server1.start();
+        int port1 = server1.getPort();
         try
         {
-            int port2 = random.nextInt(50000) + 10000;
             int scavengePeriod = 2;
-            AbstractTestServer server2 = createServer(port2, inactivePeriod, scavengePeriod);
+            AbstractTestServer server2 = createServer(0, inactivePeriod, scavengePeriod);
             server2.addContext(contextPath).addServlet(TestServlet.class, servletMapping);
             server2.start();
+            int port2 = server2.getPort();
             try
             {
                 HttpClient client = new HttpClient();
