81b24d2d7ad36f778972af88f4fe655dc3ff46df
Michael Gorovoy <michael@webtide.com>
2010-05-27 17:17:23 +0000

    294212 Can not customize session cookie path
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@1905 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-server/src/main/java/org/eclipse/jetty/server/session/AbstractSessionManager.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/session/AbstractSessionManager.java
@@ -337,12 +337,14 @@ public abstract class AbstractSessionManager extends AbstractLifeCycle implement
     {
         if (isUsingCookies())
         {
+            String sessionPath = (_sessionPath==null) ? contextPath : _sessionPath;
+            sessionPath = (sessionPath==null||sessionPath.length()==0) ? "/" : sessionPath;
             String id = getNodeId(session);
             HttpCookie cookie=new HttpCookie(
                     _sessionCookie,
                     id,
                     _sessionDomain,
-                    (contextPath==null||contextPath.length()==0)?"/":contextPath,
+                    sessionPath,
                     getMaxCookieAge(),
                     getHttpOnly(),
                     requestIsSecure&&getSecureCookies());      
