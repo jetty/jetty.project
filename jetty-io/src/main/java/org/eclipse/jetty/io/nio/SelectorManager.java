31a6622d3ad5be27dfd55fe77305a054baa35415
Greg Wilkins <greg@webtide.com>
2010-06-09 00:16:52 +0000

    JVM bugs info to debug
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@1946 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-io/src/main/java/org/eclipse/jetty/io/nio/SelectorManager.java
+++ jetty-io/src/main/java/org/eclipse/jetty/io/nio/SelectorManager.java
@@ -49,7 +49,7 @@ public abstract class SelectorManager extends AbstractLifeCycle
     // TODO Tune these by approx system speed.
     private static final int __JVMBUG_THRESHHOLD=Integer.getInteger("org.mortbay.io.nio.JVMBUG_THRESHHOLD",512).intValue();
     private static final int __MONITOR_PERIOD=Integer.getInteger("org.mortbay.io.nio.MONITOR_PERIOD",1000).intValue();
-    private static final int __MAX_SELECTS=Integer.getInteger("org.mortbay.io.nio.MAX_SELECTS",15000).intValue();
+    private static final int __MAX_SELECTS=Integer.getInteger("org.mortbay.io.nio.MAX_SELECTS",25000).intValue();
     private static final int __BUSY_PAUSE=Integer.getInteger("org.mortbay.io.nio.BUSY_PAUSE",50).intValue();
     private static final int __BUSY_KEY=Integer.getInteger("org.mortbay.io.nio.BUSY_KEY",-1).intValue();
     
@@ -503,16 +503,16 @@ public abstract class SelectorManager extends AbstractLifeCycle
                     if (now>_log)
                     {
                         if (_paused>0)  
-                            Log.info(this+" Busy selector - injecting delay "+_paused+" times");
+                            Log.debug(this+" Busy selector - injecting delay "+_paused+" times");
 
                         if (_jvmFix2>0)
-                            Log.info(this+" JVM BUG(s) - injecting delay"+_jvmFix2+" times");
+                            Log.debug(this+" JVM BUG(s) - injecting delay"+_jvmFix2+" times");
 
                         if (_jvmFix1>0)
-                            Log.info(this+" JVM BUG(s) - recreating selector "+_jvmFix1+" times, cancelled keys "+_jvmFix0+" times");
+                            Log.debug(this+" JVM BUG(s) - recreating selector "+_jvmFix1+" times, cancelled keys "+_jvmFix0+" times");
 
                         else if(Log.isDebugEnabled() && _jvmFix0>0)
-                            Log.info(this+" JVM BUG(s) - cancelled keys "+_jvmFix0+" times");
+                            Log.debug(this+" JVM BUG(s) - cancelled keys "+_jvmFix0+" times");
                         _paused=0;
                         _jvmFix2=0;
                         _jvmFix1=0;
