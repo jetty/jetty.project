de2e13a9e3f2e6adce9c84c423068833fde17d37
Hugues Malphettes <hmalphettes@intalio.com>
2010-07-27 22:08:29 +0000

    commit test-jetty-osgi
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@2185 7e9141cc-0065-0410-87d8-b60c137991c4

---
+++ jetty-osgi/test-jetty-osgi/src/test/java/org/eclipse/jetty/osgi/boot/TestJettyOSGiBootCore.java
@@ -0,0 +1,192 @@
+// ========================================================================
+// Copyright (c) 2010 Intalio, Inc.
+// ------------------------------------------------------------------------
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// and Apache License v2.0 which accompanies this distribution.
+// The Eclipse Public License is available at 
+// http://www.eclipse.org/legal/epl-v10.html
+// The Apache License v2.0 is available at
+// http://www.opensource.org/licenses/apache2.0.php
+// You may elect to redistribute this code under either of these licenses. 
+// Contributors:
+//    Hugues Malphettes - initial API and implementation
+// ========================================================================
+package org.eclipse.jetty.osgi.boot;
+
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import junit.framework.Assert;
+
+import org.eclipse.jetty.client.ContentExchange;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.http.HttpMethods;
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.osgi.boot.internal.serverfactory.DefaultJettyAtJettyHomeHelper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.CoreOptions;
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.container.def.PaxRunnerOptions;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+
+
+/**
+ * Pax-Exam to make sure the jetty-osgi-boot can be started along with the httpservice web-bundle.
+ * Then make sure we can deploy an OSGi service on the top of this.
+ */
+@RunWith( JUnit4TestRunner.class )
+public class TestJettyOSGiBootCore
+{
+    /**
+     * Jetty-osgi including webapp support and also jetty-client.
+     * Sets the system property jetty.home.bunde=org.eclipse.jetty.osgi.boot
+     * to use the jetty server configuration embedded in 
+     * @return
+     */
+    public static List<Option> provisionCoreJetty()
+    {
+        return Arrays.asList(options(
+                //get the jetty home config from the osgi boot bundle.
+                PaxRunnerOptions.vmOptions("-D" + DefaultJettyAtJettyHomeHelper.SYS_PROP_JETTY_HOME_BUNDLE + "=org.eclipse.jetty.osgi.boot"),
+                
+               // CoreOptions.equinox(),
+                
+                mavenBundle().groupId( "org.mortbay.jetty" ).artifactId( "servlet-api" ).versionAsInProject().noStart(),
+                mavenBundle().groupId( "org.eclipse" ).artifactId( "osgi" ).versionAsInProject().noStart(),
+                mavenBundle().groupId( "org.eclipse.osgi" ).artifactId( "services" ).versionAsInProject().noStart(),
+
+                mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-deploy" ).versionAsInProject().noStart(),   
+                mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-server" ).versionAsInProject().noStart(),   
+                mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-servlet" ).versionAsInProject().noStart(),  
+                mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-util" ).versionAsInProject().noStart(), 
+                mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-http" ).versionAsInProject().noStart(), 
+                mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-xml" ).versionAsInProject().noStart(),  
+                mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-webapp" ).versionAsInProject().noStart(),
+                mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-io" ).versionAsInProject().noStart(),
+                mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-continuation" ).versionAsInProject().noStart(),
+                mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-security" ).versionAsInProject().noStart(),
+                
+                mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-client" ).versionAsInProject().noStart()
+        ));
+        
+    }
+    
+    @Inject
+    BundleContext bundleContext = null;
+
+
+    @Configuration
+    public static Option[] configure()
+    {
+        ArrayList<Option> options = new ArrayList<Option>();
+        options.addAll(provisionCoreJetty());
+        options.addAll(Arrays.asList(options(
+            // install log service using pax runners profile abstraction (there are more profiles, like DS)
+            //logProfile(),
+            // this is how you set the default log level when using pax logging (logProfile)
+            //systemProperty( "org.ops4j.pax.logging.DefaultServiceLog.level" ).value( "INFO" ),
+            
+        //	CoreOptions.equinox(), CoreOptions.felix(),//.version("3.0.0"),
+        		
+            mavenBundle().groupId( "org.eclipse.jetty.osgi" ).artifactId( "jetty-osgi-boot" ).versionAsInProject().start(),
+            mavenBundle().groupId( "org.eclipse.jetty.osgi" ).artifactId( "jetty-httpservice" ).versionAsInProject().start(),
+            
+            mavenBundle().groupId( "org.eclipse.equinox.http" ).artifactId( "servlet" ).versionAsInProject().start()
+        )));
+        return options.toArray(new Option[options.size()]);
+    }
+
+    /**
+     * You will get a list of bundles installed by default
+     * plus your testcase, wrapped into a bundle called pax-exam-probe
+     */
+    @Test
+    public void testHttpService() throws Exception
+    {
+//      ServletContextHandler sch = null;
+//      sch.addServlet("className", "pathSpec").setInitOrder("0");
+        
+        
+        Map<String,Bundle> bundlesIndexedBySymbolicName = new HashMap<String, Bundle>();
+        for( Bundle b : bundleContext.getBundles() )
+        {
+            bundlesIndexedBySymbolicName.put(b.getSymbolicName(), b);
+          System.err.println("got " + b.getSymbolicName());
+        }
+        Bundle osgiBoot = bundlesIndexedBySymbolicName.get("org.eclipse.jetty.osgi.boot");
+        Assert.assertNotNull("Could not find the org.eclipse.jetty.osgi.boot bundle", osgiBoot);
+        Assert.assertTrue(osgiBoot.getState() == Bundle.ACTIVE);
+
+        Bundle httpServiceOSGiBundle = bundlesIndexedBySymbolicName.get("org.eclipse.jetty.osgi.httpservice");
+        Assert.assertNotNull(httpServiceOSGiBundle);
+        Assert.assertTrue(httpServiceOSGiBundle.getState() == Bundle.ACTIVE);
+
+        Bundle equinoxServlet = bundlesIndexedBySymbolicName.get("org.eclipse.equinox.http.servlet");
+        Assert.assertNotNull(equinoxServlet);
+        //interestingly with equinox the bundle is not started. probably a difference in pax-exam and
+        //the way the bundles are activated. the rest of the test goes fine.
+        Assert.assertTrue(equinoxServlet.getState() == Bundle.ACTIVE);
+        
+        //in the OSGi world this would be bad code and we should use a bundle tracker.
+        //here we purposely want to make sure that the httpService is actually ready.
+        ServiceReference sr  =  bundleContext.getServiceReference(HttpService.class.getName());
+        Assert.assertNotNull("The httpServiceOSGiBundle is started and should have deployed a service reference for HttpService" ,sr);
+        HttpService http = (HttpService)bundleContext.getService(sr);
+        http.registerServlet("/greetings", new HttpServlet() {
+            private static final long serialVersionUID = 1L;
+            protected void doGet(HttpServletRequest req,
+                    HttpServletResponse resp) throws ServletException,
+                    IOException {
+                resp.getWriter().append("Hello");
+            }
+        }, null, null);
+        
+        //now test the servlet
+        HttpClient client = new HttpClient();
+        client.setConnectorType(HttpClient.CONNECTOR_SELECT_CHANNEL);
+        try
+        {
+            client.start();
+            
+            ContentExchange getExchange = new ContentExchange();
+            getExchange.setURL("http://localhost:8080/greetings");
+            getExchange.setMethod(HttpMethods.GET);
+     
+            client.send(getExchange);
+            int state = getExchange.waitForDone();
+     
+            String content = null;
+            int responseStatus = getExchange.getResponseStatus();
+            Assert.assertEquals(HttpStatus.OK_200, responseStatus);
+            if (responseStatus == HttpStatus.OK_200) {
+                content = getExchange.getResponseContent();
+            }
+            Assert.assertEquals("Hello", content);
+        }
+        finally
+        {
+            client.stop();
+        }
+    }
+    
+}
---
+++ jetty-osgi/test-jetty-osgi/src/test/java/org/eclipse/jetty/osgi/boot/TestJettyOSGiBootWithJsp.java
@@ -0,0 +1,144 @@
+// ========================================================================
+// Copyright (c) 2010 Intalio, Inc.
+// ------------------------------------------------------------------------
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// and Apache License v2.0 which accompanies this distribution.
+// The Eclipse Public License is available at 
+// http://www.eclipse.org/legal/epl-v10.html
+// The Apache License v2.0 is available at
+// http://www.opensource.org/licenses/apache2.0.php
+// You may elect to redistribute this code under either of these licenses. 
+// Contributors:
+//    Hugues Malphettes - initial API and implementation
+// ========================================================================
+package org.eclipse.jetty.osgi.boot;
+
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.eclipse.jetty.client.ContentExchange;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.http.HttpMethods;
+import org.eclipse.jetty.http.HttpStatus;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.CoreOptions;
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.container.def.PaxRunnerOptions;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Pax-Exam to make sure the jetty-osgi-boot can be started along with the httpservice web-bundle.
+ * Then make sure we can deploy an OSGi service on the top of this.
+ */
+@RunWith( JUnit4TestRunner.class )
+public class TestJettyOSGiBootWithJsp
+{
+    @Inject
+    BundleContext bundleContext = null;
+
+
+    @Configuration
+    public static Option[] configure()
+    {
+    	File testrealm = new File("src/test/config/etc/jetty-testrealm.xml");
+    	
+    	ArrayList<Option> options = new ArrayList<Option>();
+    	options.addAll(TestJettyOSGiBootCore.provisionCoreJetty());
+    	options.addAll(Arrays.asList(options(
+            // install log service using pax runners profile abstraction (there are more profiles, like DS)
+            //logProfile(),
+            // this is how you set the default log level when using pax logging (logProfile)
+            //systemProperty( "org.ops4j.pax.logging.DefaultServiceLog.level" ).value( "INFO" ),
+            	
+                // this just adds all what you write here to java vm argumenents of the (new) osgi process.
+//            PaxRunnerOptions.vmOption( "-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5006" ),
+    			
+    		PaxRunnerOptions.vmOption("-D" + OSGiServerConstants.MANAGED_JETTY_XML_CONFIG_URLS + 
+    				"=etc/jetty.xml;" + testrealm.getAbsolutePath()),
+
+    		mavenBundle().groupId( "org.eclipse.jetty.osgi" ).artifactId( "jetty-osgi-boot" ).versionAsInProject(),
+            mavenBundle().groupId( "org.eclipse.jetty.osgi" ).artifactId( "jetty-osgi-boot-jsp" ).versionAsInProject(),
+
+            mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-websocket" ).versionAsInProject(),
+            mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-servlets" ).versionAsInProject(),  
+            mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "test-jetty-webapp" ).classifier("webbundle").versionAsInProject(),
+            //the jsp bundles downloaded from eclipse orbit website and the ecj jar.
+//            CoreOptions.provision( PaxRunnerOptions.scanDir( "jasper" ) ),
+            CoreOptions.provision( PaxRunnerOptions.scanDir( "target/distribution/lib/jsp" ) )
+
+        )));
+    	return options.toArray(new Option[options.size()]);
+    }
+
+    /**
+     * You will get a list of bundles installed by default
+     * plus your testcase, wrapped into a bundle called pax-exam-probe
+     */
+    @Test
+    public void listBundles() throws Exception
+    {
+    	Map<String,Bundle> bundlesIndexedBySymbolicName = new HashMap<String, Bundle>();
+        for( Bundle b : bundleContext.getBundles() )
+        {
+        	bundlesIndexedBySymbolicName.put(b.getSymbolicName(), b);
+        	//System.err.println("Got " + b.getSymbolicName());
+        }
+        
+        Bundle osgiBoot = bundlesIndexedBySymbolicName.get("org.eclipse.jetty.osgi.boot");
+        Assert.assertNotNull("Could not find the org.eclipse.jetty.osgi.boot bundle", osgiBoot);
+        Assert.assertTrue(osgiBoot.getState() == Bundle.ACTIVE);
+        
+        Bundle osgiBootJsp = bundlesIndexedBySymbolicName.get("org.eclipse.jetty.osgi.boot.jsp");
+        Assert.assertNotNull("Could not find the org.eclipse.jetty.osgi.boot.jsp bundle", osgiBootJsp);
+        Assert.assertTrue("The fragment jsp is not correctly resolved", osgiBootJsp.getState() == Bundle.RESOLVED);
+        
+        Bundle testWebBundle = bundlesIndexedBySymbolicName.get("org.eclipse.jetty.test-jetty-webapp");
+        Assert.assertNotNull("Could not find the org.eclipse.jetty.osgi.boot.jsp bundle", osgiBootJsp);
+        Assert.assertTrue("The fragment jsp is not correctly resolved", testWebBundle.getState() == Bundle.ACTIVE);
+        
+        //now test the jsp/dump.jsp
+        HttpClient client = new HttpClient();
+        client.setConnectorType(HttpClient.CONNECTOR_SELECT_CHANNEL);
+        try
+        {
+            client.start();
+            
+            ContentExchange getExchange = new ContentExchange();
+            getExchange.setURL("http://localhost:8080/jsp/dump.jsp");
+            getExchange.setMethod(HttpMethods.GET);
+     
+            client.send(getExchange);
+            int state = getExchange.waitForDone();
+     
+            String content = null;
+            int responseStatus = getExchange.getResponseStatus();
+            Assert.assertEquals(HttpStatus.OK_200, responseStatus);
+            if (responseStatus == HttpStatus.OK_200) {
+                content = getExchange.getResponseContent();
+            }
+            //System.err.println("content: " + content);
+            Assert.assertTrue(content.indexOf("<tr><th>ServletPath:</th><td>/jsp/dump.jsp</td></tr>") != -1);
+        }
+        finally
+        {
+            client.stop();
+        }
+        
+    }
+
+	
+}
