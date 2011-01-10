0109d0cbe6c46ebbb9724ec2d7d68de7fe2ef82f
Jan Bartel <jan@webtide.com>
2011-01-10 18:49:34 +0000

    JETTY-1259 further update
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@2647 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-server/src/main/java/org/eclipse/jetty/server/session/JDBCSessionIdManager.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/session/JDBCSessionIdManager.java
@@ -675,11 +675,15 @@ public class JDBCSessionIdManager extends AbstractSessionIdManager
                 Handler[] contexts = _server.getChildHandlersByClass(ContextHandler.class);
                 for (int i=0; contexts!=null && i<contexts.length; i++)
                 {
-                    SessionManager manager = ((SessionHandler)((ContextHandler)contexts[i]).getChildHandlerByClass(SessionHandler.class)).getSessionManager();
-                            
-                    if (manager instanceof JDBCSessionManager)
-                    {
-                        ((JDBCSessionManager)manager).expire(expiredSessionIds);
+
+                    SessionHandler sessionHandler = (SessionHandler)((ContextHandler)contexts[i]).getChildHandlerByClass(SessionHandler.class);
+                    if (sessionHandler != null) 
+                    { 
+                        SessionManager manager = sessionHandler.getSessionManager();
+                        if (manager != null && manager instanceof JDBCSessionManager)
+                        {
+                            ((JDBCSessionManager)manager).expire(expiredSessionIds);
+                        }
                     }
                 }
 
