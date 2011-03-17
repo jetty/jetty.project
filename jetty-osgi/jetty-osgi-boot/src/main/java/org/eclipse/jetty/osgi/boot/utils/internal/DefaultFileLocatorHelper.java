f85710711bb3a929a559b283ed57510ceab4b513
Hugues Malphettes <hmalphettes@intalio.com>
2011-03-17 09:40:48 +0000

    Don't use URL#toURI to transform a URL into a File; that does not work when spaces and other characters appear on the path.
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/branches/jetty-8@2901 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-osgi/jetty-osgi-boot/src/main/java/org/eclipse/jetty/osgi/boot/utils/internal/DefaultFileLocatorHelper.java
+++ jetty-osgi/jetty-osgi-boot/src/main/java/org/eclipse/jetty/osgi/boot/utils/internal/DefaultFileLocatorHelper.java
@@ -24,6 +24,7 @@ import java.util.Enumeration;
 import java.util.zip.ZipFile;
 
 import org.eclipse.jetty.osgi.boot.utils.BundleFileLocatorHelper;
+import org.eclipse.jetty.util.resource.FileResource;
 import org.eclipse.jetty.util.URIUtil;
 import org.osgi.framework.Bundle;
 
