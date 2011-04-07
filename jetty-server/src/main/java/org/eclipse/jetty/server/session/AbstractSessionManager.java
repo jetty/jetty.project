94ddc21289d024401a7d59a9833aebe9596a79ff
Greg Wilkins <greg@webtide.com>
2011-04-07 00:23:40 +0000

    force merge with jetty 8
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@2987 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-server/src/main/java/org/eclipse/jetty/server/session/AbstractSessionManager.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/session/AbstractSessionManager.java
@@ -566,6 +566,9 @@ public abstract class AbstractSessionManager extends AbstractLifeCycle implement
     }
 
     /* ------------------------------------------------------------ */
+    /**
+     * @see org.eclipse.jetty.server.SessionManager#setSessionPath(java.lang.String)
+     */
     public void setSessionPath(String path)
     {
         _sessionPath=path;
