25a7cc4ab7618ce2f421eddc40c45f2a042af4b3
Hugues Malphettes <hmalphettes@intalio.com>
2011-05-03 07:34:18 +0000

    bug 344529 Ability to customize the error handling of the OSGi HttpService
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@3065 7e9141cc-0065-0410-87d8-b60c137991c4

---
+++ jetty-osgi/jetty-osgi-httpservice/src/main/java/org/eclipse/jetty/osgi/httpservice/HttpServiceErrorPageErrorHandler.java
@@ -0,0 +1,67 @@
+// ========================================================================
+// Copyright (c) 2010-2011 Mort Bay Consulting Pty. Ltd.
+// ------------------------------------------------------------------------
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// and Apache License v2.0 which accompanies this distribution.
+// The Eclipse Public License is available at 
+// http://www.eclipse.org/legal/epl-v10.html
+// The Apache License v2.0 is available at
+// http://www.opensource.org/licenses/apache2.0.php
+// You may elect to redistribute this code under either of these licenses. 
+// ========================================================================
+package org.eclipse.jetty.osgi.httpservice;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.servlet.ErrorPageErrorHandler;
+
+/**
+ * Extended error page handler.
+ * Makes it easy to plug a servlet to handle errors thrown by the HttpService or
+ * to use Jetty's ErrorPageErrorHandler API to plug custom error pages.
+ */
+public class HttpServiceErrorPageErrorHandler extends ErrorPageErrorHandler
+{
+
+	private static HttpServiceErrorPageErrorHandler INSTANCE;
+	
+	public static HttpServiceErrorPageErrorHandler getInstance()
+	{
+		return INSTANCE;
+	}
+	
+	public HttpServiceErrorPageErrorHandler()
+	{
+		INSTANCE = this;
+	}
+
+	@Override
+	public void handle(String target, Request baseRequest,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		if (HttpServiceErrorHandlerHelper.getCustomErrorHandler() != null)
+		{
+			try
+			{
+				HttpServiceErrorHandlerHelper.getCustomErrorHandler().service(request, response);
+			}
+			catch (ServletException e)
+			{
+				
+			}
+		}
+		if (!response.isCommitted())
+		{
+			super.handle(target, baseRequest, request, response);
+		}
+	}
+	
+	
+	
+}
