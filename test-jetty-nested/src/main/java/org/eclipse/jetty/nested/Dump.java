5dc33c8446104b54beb7d299d57deff299655705
Hugues Malphettes <hmalphettes@intalio.com>
2011-05-09 03:35:30 +0000

    request parameter to debug the system properties in the dump servlet
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@3094 7e9141cc-0065-0410-87d8-b60c137991c4

--- test-jetty-nested/src/main/java/org/eclipse/jetty/nested/Dump.java
+++ test-jetty-nested/src/main/java/org/eclipse/jetty/nested/Dump.java
@@ -25,6 +25,7 @@ import java.lang.reflect.Field;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.Locale;
+import java.util.Map.Entry;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
