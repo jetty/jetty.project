8f257c6fd8a6355ffd3c457bdfc12a27476fd064
Jesse McConnell <jesse.mcconnell@gmail.com>
2011-05-18 21:43:13 +0000

    Bug 345729 add license info and comment
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.jetty/jetty/trunk@3211 7e9141cc-0065-0410-87d8-b60c137991c4

--- jetty-deploy/src/main/java/org/eclipse/jetty/deploy/bindings/GlobalWebappConfigBinding.java
+++ jetty-deploy/src/main/java/org/eclipse/jetty/deploy/bindings/GlobalWebappConfigBinding.java
@@ -1,4 +1,19 @@
 package org.eclipse.jetty.deploy.bindings;
+//========================================================================
+//Copyright (c) Webtide LLC
+//------------------------------------------------------------------------
+//All rights reserved. This program and the accompanying materials
+//are made available under the terms of the Eclipse Public License v1.0
+//and Apache License v2.0 which accompanies this distribution.
+//
+//The Eclipse Public License is available at 
+//http://www.eclipse.org/legal/epl-v10.html
+//
+//The Apache License v2.0 is available at
+//http://www.apache.org/licenses/LICENSE-2.0.txt
+//
+//You may elect to redistribute this code under either of these licenses. 
+//========================================================================
 
 import java.net.URL;
 
@@ -12,6 +27,20 @@ import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.eclipse.jetty.xml.XmlConfiguration;
 
+/**
+ * Provides a way of globally setting various aspects of webapp contexts.
+ * 
+ * Adding this binding will allow the user to arbitrarily apply a file of 
+ * jetty-web.xml like settings to a webapp context.
+ * 
+ * Example usage would be:
+ * - adding a server or system class setting to all webapp contexts
+ * - adding an override descriptor 
+ * 
+ * Note: Currently properties from startup will not be available for 
+ * reference.
+ *
+ */
 public class GlobalWebappConfigBinding implements AppLifeCycle.Binding
 {
 
