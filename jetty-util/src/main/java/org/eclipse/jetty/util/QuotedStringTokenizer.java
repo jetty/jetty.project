449db20d7de097f737740e00341cff7578a1dcfd
Simone Bordet <simone.bordet@gmail.com>
2011-08-04 14:44:21 +0200

    Fixes #353862 (Improve performance of QuotedStringTokenizer.quote()). Corrected various implementation mistakes (thanks test suite).

--- jetty-util/src/main/java/org/eclipse/jetty/util/QuotedStringTokenizer.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/QuotedStringTokenizer.java
@@ -316,10 +316,10 @@ public class QuotedStringTokenizer
 
     }
 
-    private static final char[] escapes = new char[31];
+    private static final char[] escapes = new char[32];
     static
     {
-        Arrays.fill(escapes, (char)-1);
+        Arrays.fill(escapes, (char)0xFFFF);
         escapes['\b'] = 'b';
         escapes['\t'] = 't';
         escapes['\n'] = 'n';
@@ -350,10 +350,10 @@ public class QuotedStringTokenizer
                 else
                 {
                     char escape = escapes[c];
-                    if (escape == -1)
+                    if (escape == 0xFFFF)
                     {
                         // Unicode escape
-                        buffer.append('\\').append('0').append('0');
+                        buffer.append('\\').append('u').append('0').append('0');
                         if (c < 0x10)
                             buffer.append('0');
                         buffer.append(Integer.toString(c, 16));
