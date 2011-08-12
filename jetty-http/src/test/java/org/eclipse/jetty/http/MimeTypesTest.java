06e78135395e1915bc9901fe8d90e617dbaa5e3a
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2011-08-12 14:04:33 -0700

    Testing some perceived odd behavior with MimeTypes object.

---
+++ jetty-http/src/test/java/org/eclipse/jetty/http/MimeTypesTest.java
@@ -0,0 +1,50 @@
+package org.eclipse.jetty.http;
+
+import org.eclipse.jetty.io.Buffer;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MimeTypesTest
+{
+    @Test
+    public void testGetMimeByExtension_Gzip()
+    {
+        assertMimeTypeByExtension("application/gzip","test.gz");
+    }
+
+    @Test
+    public void testGetMimeByExtension_Png()
+    {
+        assertMimeTypeByExtension("image/png","test.png");
+        assertMimeTypeByExtension("image/png","TEST.PNG");
+        assertMimeTypeByExtension("image/png","Test.Png");
+    }
+
+    @Test
+    public void testGetMimeByExtension_Png_MultiDot()
+    {
+        assertMimeTypeByExtension("image/png","org.eclipse.jetty.Logo.png");
+    }
+
+    @Test
+    public void testGetMimeByExtension_Png_DeepPath()
+    {
+        assertMimeTypeByExtension("image/png","/org/eclipse/jetty/Logo.png");
+    }
+
+    @Test
+    public void testGetMimeByExtension_Text()
+    {
+        assertMimeTypeByExtension("text/plain","test.txt");
+        assertMimeTypeByExtension("text/plain","TEST.TXT");
+    }
+
+    private void assertMimeTypeByExtension(String expectedMimeType, String filename)
+    {
+        MimeTypes mimetypes = new MimeTypes();
+        Buffer contentType = mimetypes.getMimeByExtension(filename);
+        String prefix = "MimeTypes.getMimeByExtension(" + filename + ")";
+        Assert.assertNotNull(prefix,contentType);
+        Assert.assertEquals(prefix,expectedMimeType,contentType.toString());
+    }
+}
