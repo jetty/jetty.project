1f1bf28cd68e7217c03fef412b84fb787fe55144
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2011-08-12 16:04:46 -0700

    Adding some code to test for gzip no-recompress logic

--- jetty-servlets/src/test/java/org/eclipse/jetty/servlets/GzipFilterDefaultNoRecompressTest.java
+++ jetty-servlets/src/test/java/org/eclipse/jetty/servlets/GzipFilterDefaultNoRecompressTest.java
@@ -8,10 +8,10 @@ import java.util.List;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlets.gzip.GzipTester;
+import org.eclipse.jetty.servlets.gzip.TestStaticMimeTypeServlet;
 import org.eclipse.jetty.toolchain.test.IO;
 import org.eclipse.jetty.toolchain.test.MavenTestingUtils;
 import org.eclipse.jetty.toolchain.test.TestingDir;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,9 +19,8 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 /**
- * Tests {@link GzipFilter} in combination with {@link DefaultServlet} for 
- * ability to configure {@link GzipFilter} to ignore recompress situations
- * from upstream.
+ * Tests {@link GzipFilter} in combination with {@link DefaultServlet} for ability to configure {@link GzipFilter} to
+ * ignore recompress situations from upstream.
  */
 @RunWith(Parameterized.class)
 public class GzipFilterDefaultNoRecompressTest
@@ -32,59 +31,52 @@ public class GzipFilterDefaultNoRecompressTest
         return Arrays.asList(new Object[][]
         {
                 // Some already compressed files
-                { "test_quotes.gz" },
-                { "test_quotes.bz2" },
-                { "test_quotes.zip" },
-                { "test_quotes.rar" },
+                { "test_quotes.gz", "application/gzip" },
+                { "test_quotes.bz2", "application/bzip2" },
+                { "test_quotes.zip", "application/zip" },
+                { "test_quotes.rar", "application/octet-stream" },
                 // Some images (common first)
-                { "jetty_logo.png" },
-                { "jetty_logo.gif" },
-                { "jetty_logo.jpeg" },
-                { "jetty_logo.jpg" },
+                { "jetty_logo.png", "image/png" },
+                { "jetty_logo.gif", "image/gif" },
+                { "jetty_logo.jpeg", "image/jpeg" },
+                { "jetty_logo.jpg", "image/jpeg" },
                 // Lesser encountered images (usually found being requested from non-browser clients)
-                { "jetty_logo.bmp" },
-                { "jetty_logo.tga" },
-                { "jetty_logo.tif" },
-                { "jetty_logo.tiff" },
-                { "jetty_logo.xcf" },
-                { "jetty_logo.jp2" } });
+                { "jetty_logo.bmp", "image/bmp" },
+                { "jetty_logo.tga", "application/tga" },
+                { "jetty_logo.tif", "image/tiff" },
+                { "jetty_logo.tiff", "image/tiff" },
+                { "jetty_logo.xcf", "image/xcf" },
+                { "jetty_logo.jp2", "image/jpeg2000" } });
     }
 
     @Rule
     public TestingDir testingdir = new TestingDir();
-    
+
     private String alreadyCompressedFilename;
-    
-    public GzipFilterDefaultNoRecompressTest(String testFilename) {
+    private String expectedContentType;
+
+    public GzipFilterDefaultNoRecompressTest(String testFilename, String expectedContentType)
+    {
         this.alreadyCompressedFilename = testFilename;
+        this.expectedContentType = expectedContentType;
     }
 
     @Test
-    @Ignore("Cannot find a configuration that would allow this to pass")
     public void testNotGzipFiltered_Default_AlreadyCompressed() throws Exception
     {
         GzipTester tester = new GzipTester(testingdir);
 
         copyTestFileToServer(alreadyCompressedFilename);
 
-        // Using DefaultServlet, with default GzipFilter setup
-        FilterHolder holder = tester.setContentServlet(DefaultServlet.class);
-        // TODO: find a configuration of the GzipFilter to allow
-        //       each of these test cases to pass.
-        
+        FilterHolder holder = tester.setContentServlet(TestStaticMimeTypeServlet.class);
         StringBuilder mimeTypes = new StringBuilder();
-        mimeTypes.append("images/png");
-        mimeTypes.append(",images/jpeg");
-        mimeTypes.append(",images/gif");
-        mimeTypes.append(",images/jp2");
-        
-        holder.setInitParameter("mimeTypes", mimeTypes.toString());
+        mimeTypes.append("text/plain");
+        holder.setInitParameter("mimeTypes",mimeTypes.toString());
 
         try
         {
             tester.start();
-            tester.assertIsResponseNotGzipFiltered(alreadyCompressedFilename,
-                    alreadyCompressedFilename + ".sha1");
+            tester.assertIsResponseNotGzipFiltered(alreadyCompressedFilename,alreadyCompressedFilename + ".sha1",expectedContentType);
         }
         finally
         {
