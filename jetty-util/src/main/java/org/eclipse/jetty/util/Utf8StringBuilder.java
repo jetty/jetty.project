b3e6ebf416221f649b168bdf83b21aa91f97d34c
Thomas Becker <tbecker@intalio.com>
2011-09-21 11:47:38 +0200

    Fix for #358121 (Utf8Appendable refactored to use Bjoern Hoehrmann's decoder).
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-util/src/main/java/org/eclipse/jetty/util/Utf8StringBuilder.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/Utf8StringBuilder.java
@@ -4,70 +4,74 @@
 // All rights reserved. This program and the accompanying materials
 // are made available under the terms of the Eclipse Public License v1.0
 // and Apache License v2.0 which accompanies this distribution.
-// The Eclipse Public License is available at 
+// The Eclipse Public License is available at
 // http://www.eclipse.org/legal/epl-v10.html
 // The Apache License v2.0 is available at
 // http://www.opensource.org/licenses/apache2.0.php
-// You may elect to redistribute this code under either of these licenses. 
+// You may elect to redistribute this code under either of these licenses.
 // ========================================================================
 
 package org.eclipse.jetty.util;
 
-import java.io.IOException;
 
 /* ------------------------------------------------------------ */
 /** UTF-8 StringBuilder.
  *
- * This class wraps a standard {@link java.lang.StringBuffer} and provides methods to append 
+ * This class wraps a standard {@link java.lang.StringBuilder} and provides methods to append
  * UTF-8 encoded bytes, that are converted into characters.
- * 
- * This class is stateful and up to 6  calls to {@link #append(byte)} may be needed before 
+ *
+ * This class is stateful and up to 4 calls to {@link #append(byte)} may be needed before
  * state a character is appended to the string buffer.
- * 
+ *
  * The UTF-8 decoding is done by this class and no additional buffers or Readers are used.
- * The UTF-8 code was inspired by http://javolution.org
- * 
+ * The UTF-8 code was inspired by http://bjoern.hoehrmann.de/utf-8/decoder/dfa/
+ *
  */
-public class Utf8StringBuilder extends Utf8Appendable 
+public class Utf8StringBuilder extends Utf8Appendable
 {
     final StringBuilder _buffer;
-    
+
     public Utf8StringBuilder()
     {
         super(new StringBuilder());
         _buffer=(StringBuilder)_appendable;
     }
-    
+
     public Utf8StringBuilder(int capacity)
     {
         super(new StringBuilder(capacity));
         _buffer=(StringBuilder)_appendable;
     }
-    
+
+    @Override
     public int length()
     {
         return _buffer.length();
     }
-    
+
+    @Override
     public void reset()
     {
+        super.reset();
         _buffer.setLength(0);
-        _expectedContinuationBytes=0;
-        _codePoint=0;
     }
-    
+
     public StringBuilder getStringBuilder()
     {
-        if (_expectedContinuationBytes!=0)
-            throw new NotUtf8Exception();
+        checkState();
         return _buffer;
     }
-    
+
     @Override
     public String toString()
     {
-        if (_expectedContinuationBytes!=0)
-            throw new NotUtf8Exception();
+        checkState();
         return _buffer.toString();
+    }
+
+    private void checkState()
+    {
+        if (!isUtf8SequenceComplete())
+            throw new IllegalArgumentException("Tried to read incomplete UTF8 decoded String");
     }
 }
--- jetty-util/src/test/java/org/eclipse/jetty/util/Utf8StringBuilderTest.java
+++ jetty-util/src/test/java/org/eclipse/jetty/util/Utf8StringBuilderTest.java
@@ -4,142 +4,102 @@
 // All rights reserved. This program and the accompanying materials
 // are made available under the terms of the Eclipse Public License v1.0
 // and Apache License v2.0 which accompanies this distribution.
-// The Eclipse Public License is available at 
+// The Eclipse Public License is available at
 // http://www.eclipse.org/legal/epl-v10.html
 // The Apache License v2.0 is available at
 // http://www.opensource.org/licenses/apache2.0.php
-// You may elect to redistribute this code under either of these licenses. 
+// You may elect to redistribute this code under either of these licenses.
 // ========================================================================
 
 package org.eclipse.jetty.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Utf8StringBuilderTest
 {
     @Test
-    public void testInvalid()
-        throws Exception
+    public void testInvalid() throws Exception
     {
-        String[] invalids = {
-                "c0af",
-                "EDA080",
-                "f08080af",
-                "f8808080af",
-                "e080af",
-                "F4908080",
-                "fbbfbfbfbf"
-        };
-        
+        String[] invalids =
+        { "c0af", "EDA080", "f08080af", "f8808080af", "e080af", "F4908080", "fbbfbfbfbf", "10FFFF" };
+
         for (String i : invalids)
         {
             byte[] bytes = TypeUtil.fromHexString(i);
-
-            /* Test what JVM does
-            try
-            {
-                String s = new String(bytes,0,bytes.length,"UTF-8");
-                System.err.println(i+": "+s);
-            }
-            catch(Exception e)
-            {
-                System.err.println(i+": "+e);
-            }
-            */
-            
             try
             {
                 Utf8StringBuilder buffer = new Utf8StringBuilder();
                 buffer.append(bytes,0,bytes.length);
-                
+
                 assertEquals(i,"not expected",buffer.toString());
             }
-            catch(IllegalArgumentException e)
+            catch (Utf8Appendable.NotUtf8Exception e)
             {
                 assertTrue(i,true);
             }
         }
     }
-    
+
     @Test
-    public void testUtfStringBuilder()
-        throws Exception
+    public void testUtfStringBuilder() throws Exception
     {
-        String source="abcd012345\n\r\u0000\u00a4\u10fb\ufffdjetty";
+        String source = "abcd012345\n\r\u0000\u00a4\u10fb\ufffdjetty";
         byte[] bytes = source.getBytes(StringUtil.__UTF8);
         Utf8StringBuilder buffer = new Utf8StringBuilder();
-        for (int i=0;i<bytes.length;i++)
-            buffer.append(bytes[i]);
-        assertEquals(source, buffer.toString());
-        assertTrue(buffer.toString().endsWith("jetty")); 
+        for (byte aByte : bytes)
+            buffer.append(aByte);
+        assertEquals(source,buffer.toString());
+        assertTrue(buffer.toString().endsWith("jetty"));
     }
-    
-    
-    
-    @Test
-    public void testShort()
-    throws Exception
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testShort() throws Exception
     {
-        String source="abc\u10fb";
+        String source = "abc\u10fb";
         byte[] bytes = source.getBytes(StringUtil.__UTF8);
         Utf8StringBuilder buffer = new Utf8StringBuilder();
-        for (int i=0;i<bytes.length-1;i++)
+        for (int i = 0; i < bytes.length - 1; i++)
             buffer.append(bytes[i]);
-        try
-        {
-            buffer.toString();
-            assertTrue(false);
-        }
-        catch(Utf8Appendable.NotUtf8Exception e)
-        {
-            assertTrue(e.toString().indexOf("!UTF-8")>=0);
-        }
+        buffer.toString();
     }
-    
+
     @Test
-    public void testLong()
-    throws Exception
+    public void testLong() throws Exception
     {
-        String source="abcXX";
+        String source = "abcXX";
         byte[] bytes = source.getBytes(StringUtil.__UTF8);
-        bytes[3]=(byte)0xc0;
-        bytes[4]=(byte)0x00;
+        bytes[3] = (byte)0xc0;
+        bytes[4] = (byte)0x00;
 
         Utf8StringBuilder buffer = new Utf8StringBuilder();
         try
         {
-            for (int i = 0; i < bytes.length; i++)
-                buffer.append(bytes[i]);
+            for (byte aByte : bytes)
+                buffer.append(aByte);
             assertTrue(false);
         }
-        catch(Utf8Appendable.NotUtf8Exception e)
+        catch (IllegalArgumentException e)
         {
             assertTrue(true);
         }
-        assertEquals("abc\ufffd", buffer.toString());
+        assertEquals("abc\ufffd",buffer.toString());
     }
 
-    
-    @Test 
-    public void testUTF32codes()
-    throws Exception
+    @Test
+    public void testUTF32codes() throws Exception
     {
-        String source="\uD842\uDF9F";
-        byte[] bytes=source.getBytes("UTF-8");
-        
-        // System.err.println(TypeUtil.toHexString(bytes));
+        String source = "\uD842\uDF9F";
+        byte[] bytes = source.getBytes("UTF-8");
+
         String jvmcheck = new String(bytes,0,bytes.length,"UTF-8");
         assertEquals(source,jvmcheck);
-        
+
         Utf8StringBuilder buffer = new Utf8StringBuilder();
         buffer.append(bytes,0,bytes.length);
-        String result=buffer.toString();
+        String result = buffer.toString();
         assertEquals(source,result);
     }
-    
-
 }
