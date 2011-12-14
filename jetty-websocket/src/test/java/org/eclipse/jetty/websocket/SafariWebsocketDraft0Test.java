6b42a1c45d6c62f9eedd72e547b62f93bac7a253
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2011-12-14 10:23:40 -0700

    JETTY-1463 - WebSockets with Safari gets messages stuck as if in a buffer that needs to be flushed.
    + Implementation of Safari WebSocket Draft-0 behavior in a unit test.
      (Test fails, and is currently set as @Ignore)

--- jetty-websocket/src/test/java/org/eclipse/jetty/websocket/SafariWebsocketDraft0Test.java
+++ jetty-websocket/src/test/java/org/eclipse/jetty/websocket/SafariWebsocketDraft0Test.java
@@ -11,18 +11,16 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.log.StdErrLog;
 import org.eclipse.jetty.websocket.helper.CaptureSocket;
-import org.eclipse.jetty.websocket.helper.MessageSender;
+import org.eclipse.jetty.websocket.helper.SafariD00;
 import org.eclipse.jetty.websocket.helper.WebSocketCaptureServlet;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
-/**
- * WebSocketCommTest - to test reported undelivered messages in bug <a
- * href="https://jira.codehaus.org/browse/JETTY-1463">JETTY-1463</a>
- */
-public class WebSocketCommTest
+public class SafariWebsocketDraft0Test
 {
     private Server server;
     private WebSocketCaptureServlet servlet;
@@ -63,27 +61,21 @@ public class WebSocketCommTest
         serverUri = new URI(String.format("ws://%s:%d/",host,port));
         System.out.printf("Server URI: %s%n",serverUri);
     }
-
+    
     @Test
+    @Ignore
     public void testSendTextMessages() throws Exception
     {
-        WebSocketClientFactory clientFactory = new WebSocketClientFactory();
-        clientFactory.start();
-
-        WebSocketClient wsc = clientFactory.newWebSocketClient();
-        MessageSender sender = new MessageSender();
-        wsc.open(serverUri,sender);
+        SafariD00 safari = new SafariD00(serverUri);
 
         try
         {
-            sender.awaitConnect();
+            safari.connect();
+            safari.issueHandshake();
 
-            // Send 5 short messages
-            for (int i = 0; i < 5; i++)
-            {
-                System.out.printf("Sending msg-%d%n",i);
-                sender.sendMessage("msg-%d",i);
-            }
+            // Send 5 short messages, using technique seen in Safari.
+            safari.sendMessage("aa-0"); // single msg
+            safari.sendMessage("aa-1", "aa-2", "aa-3", "aa-4");
 
             // Servlet should show only 1 connection.
             Assert.assertThat("Servlet.captureSockets.size",servlet.captures.size(),is(1));
@@ -93,20 +85,26 @@ public class WebSocketCommTest
             Assert.assertThat("CaptureSocket.isConnected", socket.isConnected(), is(true));
 
             // Give servlet 500 millisecond to process messages
-            threadSleep(500,TimeUnit.MILLISECONDS);
+            threadSleep(1,TimeUnit.SECONDS);
             // Should have captured 5 messages.
             Assert.assertThat("CaptureSocket.messages.size",socket.messages.size(),is(5));
         }
         finally
         {
             System.out.println("Closing client socket");
-            sender.close();
+            safari.disconnect();
         }
     }
-
+    
     public static void threadSleep(int dur, TimeUnit unit) throws InterruptedException
     {
         long ms = TimeUnit.MILLISECONDS.convert(dur,unit);
         Thread.sleep(ms);
+    }
+    
+    @After
+    public void stopServer() throws Exception
+    {
+        server.stop();
     }
 }
