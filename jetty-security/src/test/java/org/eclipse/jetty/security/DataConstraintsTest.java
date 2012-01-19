de16f7672518ecf4dd61bf8957ecee49fa612fad
Thomas Becker <tbecker@intalio.com>
2012-01-19 11:20:37 +0100

    369048: one more test
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- jetty-security/src/test/java/org/eclipse/jetty/security/DataConstraintsTest.java
+++ jetty-security/src/test/java/org/eclipse/jetty/security/DataConstraintsTest.java
@@ -331,6 +331,39 @@ public class DataConstraintsTest
         constraint0.setName("restricted");
         ConstraintMapping mapping0 = new ConstraintMapping();
         mapping0.setPathSpec("/restricted/*");
+        mapping0.setConstraint(constraint0);
+
+        _security.setConstraintMappings(Arrays.asList(new ConstraintMapping[]
+                                                                            {
+                mapping0
+                                                                            }));
+        _server.start();
+
+        String response;
+
+        response = _connector.getResponses("GET /ctx/restricted/info HTTP/1.0\r\n\r\n");
+        assertThat(response, containsString("HTTP/1.1 403 Forbidden"));
+
+        response = _connectorS.getResponses("GET /ctx/restricted/info HTTP/1.0\r\n\r\n");
+        assertThat(response, containsString("HTTP/1.1 403 Forbidden"));
+
+        response = _connector.getResponses("GET /ctx/restricted/info HTTP/1.0\r\n Authorization: Basic YWRtaW46cGFzc3dvcmQ=\r\n\r\n");
+        assertThat(response, containsString("HTTP/1.1 403 Forbidden"));
+
+        response = _connectorS.getResponses("GET /ctx/restricted/info HTTP/1.0\r\n Authorization: Basic YWRtaW46cGFzc3dvcmQ=\r\n\r\n");
+        assertThat(response, containsString("HTTP/1.1 403 Forbidden"));
+
+    }
+
+    @Test
+    public void testRestrictedWithoutAuthenticatorAndMethod() throws Exception
+    {
+        Constraint constraint0 = new Constraint();
+        constraint0.setAuthenticate(true);
+        constraint0.setRoles(new String[] { "admin" } );
+        constraint0.setName("restricted");
+        ConstraintMapping mapping0 = new ConstraintMapping();
+        mapping0.setPathSpec("/restricted/*");
         mapping0.setMethod("GET");
         mapping0.setConstraint(constraint0);
 
