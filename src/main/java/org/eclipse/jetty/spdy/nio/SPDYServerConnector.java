000b635a12759a4749f2e744cea35cca4a7558ba
Simone Bordet <simone.bordet@gmail.com>
2012-01-28 16:04:38 +0100

    Corrected server-side plumbing of endpoints and connections.

--- src/main/java/org/eclipse/jetty/spdy/nio/SPDYServerConnector.java
+++ src/main/java/org/eclipse/jetty/spdy/nio/SPDYServerConnector.java
@@ -39,29 +39,39 @@ public class SPDYServerConnector extends SelectChannelConnector
     @Override
     protected AsyncConnection newConnection(SocketChannel channel, AsyncEndPoint endPoint)
     {
+        CompressionFactory compressionFactory = newCompressionFactory();
+        Parser parser = newParser(compressionFactory.newDecompressor());
+        Generator generator = newGenerator(compressionFactory.newCompressor());
+
+        AsyncConnection result;
+        ISession.Controller controller;
         if (sslContextFactory != null)
         {
             SSLEngine engine = newSSLEngine(sslContextFactory, channel);
             SslConnection sslConnection = new SslConnection(engine, endPoint);
             endPoint.setConnection(sslConnection);
-            endPoint = sslConnection.getSslEndPoint();
+            AsyncEndPoint sslEndPoint = sslConnection.getSslEndPoint();
+            AsyncSPDYConnection connection = new AsyncSPDYConnection(sslEndPoint, parser);
+            sslEndPoint.setConnection(connection);
+            result = sslConnection;
+            controller = connection;
+        }
+        else
+        {
+            AsyncSPDYConnection connection = new AsyncSPDYConnection(endPoint, parser);
+            endPoint.setConnection(connection);
+            result = connection;
+            controller = connection;
         }
 
-        CompressionFactory compressionFactory = newCompressionFactory();
-        Parser parser = newParser(compressionFactory.newDecompressor());
-        Generator generator = newGenerator(compressionFactory.newCompressor());
-
-        AsyncSPDYConnection connection = new AsyncSPDYConnection(endPoint, parser);
-        endPoint.setConnection(connection);
-
-        Session session = newSession(connection, listener, parser, generator);
+        Session session = newSession(controller, listener, parser, generator);
 
         // TODO: this is called in the selector thread, which is not good
         // NPE guard to support tests
         if (listener != null)
             listener.onConnect(session);
 
-        return connection;
+        return result;
     }
 
     protected SSLEngine newSSLEngine(SslContextFactory sslContextFactory, SocketChannel channel)
