a90c3201e8632b65f2bff626f3c838071211feb2
Jesse McConnell <jesse.mcconnell@gmail.com>
2012-02-21 10:34:11 -0600

    [Bug #371635] and  [Bug #371649] add missing attributes (non-optimal) and resolve async flow issue with scoping

--- jetty-server/src/main/java/org/eclipse/jetty/server/handler/ContextHandler.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/handler/ContextHandler.java
@@ -971,6 +971,7 @@ public class ContextHandler extends ScopedHandler implements Attributes, Server.
         }
         finally
         {
+
             if (old_context != _scontext)
             {
                 // reset the classloader
@@ -983,8 +984,13 @@ public class ContextHandler extends ScopedHandler implements Attributes, Server.
                 baseRequest.setContext(old_context);
                 __context.set(old_context);
                 baseRequest.setContextPath(old_context_path);
-                baseRequest.setServletPath(old_servlet_path);
-                baseRequest.setPathInfo(old_path_info);
+                
+                // #371649 if we have started async then we need to protect this state
+                if (!baseRequest.getAsyncContinuation().isAsyncStarted())
+                {
+                    baseRequest.setServletPath(old_servlet_path);
+                    baseRequest.setPathInfo(old_path_info);
+                }
             }
         }
     }
