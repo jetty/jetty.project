708f8ccaebb2d36112c4e6c44a198e74d72c3bf4
Greg Wilkins <gregw@webtide.com>
2012-03-14 11:48:06 +1100

    374158: do not asyncDispatch from SSL if suspended

--- jetty-io/src/main/java/org/eclipse/jetty/io/nio/SslConnection.java
+++ jetty-io/src/main/java/org/eclipse/jetty/io/nio/SslConnection.java
@@ -408,7 +408,7 @@ public class SslConnection extends AbstractConnection implements AsyncConnection
             }
 
             // If we are reading into the temp buffer and it has some content, then we should be dispatched.
-            if (toFill==_unwrapBuf && _unwrapBuf.hasContent())
+            if (toFill==_unwrapBuf && _unwrapBuf.hasContent() && !_connection.isSuspended())
                 _aEndp.asyncDispatch();
         }
         finally
@@ -550,7 +550,7 @@ public class SslConnection extends AbstractConnection implements AsyncConnection
                 break;
 
             case BUFFER_OVERFLOW:
-                _logger.debug("{} unwrap {} {}->{}",_session,result.getStatus(),_inbound.toDetailString(),buffer.toDetailString());
+                if (_logger.isDebugEnabled()) _logger.debug("{} unwrap {} {}->{}",_session,result.getStatus(),_inbound.toDetailString(),buffer.toDetailString());
                 break;
 
             case OK:
