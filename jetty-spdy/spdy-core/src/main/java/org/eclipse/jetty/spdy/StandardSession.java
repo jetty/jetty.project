bb66cfc8774800b41d0d0616a1e33ca159116191
Simone Bordet <simone.bordet@gmail.com>
2012-03-22 17:08:11 +0100

    375083 - Flow control should take in account window size changes from concurrent SETTINGS

--- jetty-spdy/spdy-core/src/main/java/org/eclipse/jetty/spdy/StandardSession.java
+++ jetty-spdy/spdy-core/src/main/java/org/eclipse/jetty/spdy/StandardSession.java
@@ -18,10 +18,11 @@ package org.eclipse.jetty.spdy;
 
 import java.nio.ByteBuffer;
 import java.nio.channels.InterruptedByTimeoutException;
-import java.util.ArrayList;
 import java.util.Deque;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -253,9 +254,9 @@ public class StandardSession implements ISession, Parser.Listener, Handler<Stand
     }
 
     @Override
-    public List<Stream> getStreams()
+    public Set<Stream> getStreams()
     {
-        List<Stream> result = new ArrayList<>();
+        Set<Stream> result = new HashSet<>();
         result.addAll(streams.values());
         return result;
     }
@@ -540,7 +541,10 @@ public class StandardSession implements ISession, Parser.Listener, Handler<Stand
         Settings.Setting windowSizeSetting = frame.getSettings().get(Settings.ID.INITIAL_WINDOW_SIZE);
         if (windowSizeSetting != null)
         {
+            int prevWindowSize = windowSize;
             windowSize = windowSizeSetting.value();
+            for (IStream stream : streams.values())
+                stream.updateWindowSize(windowSize - prevWindowSize);
             logger.debug("Updated window size to {}", windowSize);
         }
 
@@ -772,12 +776,6 @@ public class StandardSession implements ISession, Parser.Listener, Handler<Stand
     private void execute(Runnable task)
     {
         threadPool.execute(task);
-    }
-
-    @Override
-    public int getWindowSize()
-    {
-        return windowSize;
     }
 
     @Override
