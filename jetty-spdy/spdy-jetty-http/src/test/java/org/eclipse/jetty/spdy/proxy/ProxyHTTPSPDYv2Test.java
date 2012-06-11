bee03fb89e80f641dbe6ef7d3a7a16a49ac6a4b6
Simone Bordet <simone.bordet@gmail.com>
2012-06-11 20:46:43 +0200

    Second pass at the implementation of a reverse SPDY proxy.

--- jetty-spdy/spdy-jetty-http/src/test/java/org/eclipse/jetty/spdy/proxy/ProxyHTTPSPDYv2Test.java
+++ jetty-spdy/spdy-jetty-http/src/test/java/org/eclipse/jetty/spdy/proxy/ProxyHTTPSPDYv2Test.java
@@ -17,6 +17,7 @@
 package org.eclipse.jetty.spdy.proxy;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -32,13 +33,17 @@ import org.eclipse.jetty.spdy.ServerSPDYAsyncConnectionFactory;
 import org.eclipse.jetty.spdy.api.BytesDataInfo;
 import org.eclipse.jetty.spdy.api.DataInfo;
 import org.eclipse.jetty.spdy.api.GoAwayInfo;
+import org.eclipse.jetty.spdy.api.Handler;
 import org.eclipse.jetty.spdy.api.Headers;
+import org.eclipse.jetty.spdy.api.PingInfo;
 import org.eclipse.jetty.spdy.api.ReplyInfo;
+import org.eclipse.jetty.spdy.api.RstInfo;
 import org.eclipse.jetty.spdy.api.SPDY;
 import org.eclipse.jetty.spdy.api.Session;
 import org.eclipse.jetty.spdy.api.SessionFrameListener;
 import org.eclipse.jetty.spdy.api.Stream;
 import org.eclipse.jetty.spdy.api.StreamFrameListener;
+import org.eclipse.jetty.spdy.api.StreamStatus;
 import org.eclipse.jetty.spdy.api.SynInfo;
 import org.eclipse.jetty.spdy.api.server.ServerSessionFrameListener;
 import org.eclipse.jetty.spdy.http.HTTPSPDYHeader;
@@ -550,6 +555,300 @@ public class ProxyHTTPSPDYv2Test
         });
 
         Assert.assertTrue(replyLatch.await(5, TimeUnit.SECONDS));
+
+        client.goAway().get(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testSYNThenREPLYAndDATA() throws Exception
+    {
+        final byte[] data = "0123456789ABCDEF".getBytes("UTF-8");
+        final String header = "foo";
+        InetSocketAddress proxyAddress = startProxy(startServer(new ServerSessionFrameListener.Adapter()
+        {
+            @Override
+            public StreamFrameListener onSyn(Stream stream, SynInfo synInfo)
+            {
+                Headers requestHeaders = synInfo.getHeaders();
+                Assert.assertNotNull(requestHeaders.get("via"));
+                Assert.assertNotNull(requestHeaders.get(header));
+
+                Headers responseHeaders = new Headers();
+                responseHeaders.put(header, "baz");
+                stream.reply(new ReplyInfo(responseHeaders, false));
+                stream.data(new BytesDataInfo(data, true));
+                return null;
+            }
+        }));
+        proxyConnector.setDefaultAsyncConnectionFactory(proxyConnector.getAsyncConnectionFactory("spdy/" + version()));
+
+        Session client = factory.newSPDYClient(version()).connect(proxyAddress, null).get(5, TimeUnit.SECONDS);
+
+        final CountDownLatch replyLatch = new CountDownLatch(1);
+        final CountDownLatch dataLatch = new CountDownLatch(1);
+        Headers headers = new Headers();
+        headers.put(HTTPSPDYHeader.HOST.name(version()), "localhost:" + proxyAddress.getPort());
+        headers.put(header, "bar");
+        client.syn(new SynInfo(headers, true), new StreamFrameListener.Adapter()
+        {
+            private final ByteArrayOutputStream result = new ByteArrayOutputStream();
+
+            @Override
+            public void onReply(Stream stream, ReplyInfo replyInfo)
+            {
+                Headers headers = replyInfo.getHeaders();
+                Assert.assertNotNull(headers.get(header));
+                replyLatch.countDown();
+            }
+
+            @Override
+            public void onData(Stream stream, DataInfo dataInfo)
+            {
+                result.write(dataInfo.asBytes(true), 0, dataInfo.length());
+                if (dataInfo.isClose())
+                {
+                    Assert.assertArrayEquals(data, result.toByteArray());
+                    dataLatch.countDown();
+                }
+            }
+        });
+
+        Assert.assertTrue(replyLatch.await(5, TimeUnit.SECONDS));
+        Assert.assertTrue(dataLatch.await(5, TimeUnit.SECONDS));
+
+        client.goAway().get(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testGETThenSPDYPushIsIgnored() throws Exception
+    {
+        final byte[] data = "0123456789ABCDEF".getBytes("UTF-8");
+        InetSocketAddress proxyAddress = startProxy(startServer(new ServerSessionFrameListener.Adapter()
+        {
+            @Override
+            public StreamFrameListener onSyn(Stream stream, SynInfo synInfo)
+            {
+                Headers responseHeaders = new Headers();
+                responseHeaders.put(HTTPSPDYHeader.VERSION.name(version()), "HTTP/1.1");
+                responseHeaders.put(HTTPSPDYHeader.STATUS.name(version()), "200 OK");
+
+                Headers pushHeaders = new Headers();
+                pushHeaders.put(HTTPSPDYHeader.URI.name(version()), "/push");
+                stream.syn(new SynInfo(pushHeaders, false), 5, TimeUnit.SECONDS, new Handler.Adapter<Stream>()
+                {
+                    @Override
+                    public void completed(Stream pushStream)
+                    {
+                        pushStream.data(new BytesDataInfo(data, true));
+                    }
+                });
+
+                stream.reply(new ReplyInfo(responseHeaders, true));
+                return null;
+            }
+        }));
+
+        Socket client = new Socket();
+        client.connect(proxyAddress);
+        OutputStream output = client.getOutputStream();
+
+        String request = "" +
+                "GET / HTTP/1.1\r\n" +
+                "Host: localhost:" + proxyAddress.getPort() + "\r\n" +
+                "\r\n";
+        output.write(request.getBytes("UTF-8"));
+        output.flush();
+
+        client.setSoTimeout(1000);
+        InputStream input = client.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"));
+        String line = reader.readLine();
+        Assert.assertTrue(line.contains(" 200"));
+        while (line.length() > 0)
+            line = reader.readLine();
+        Assert.assertFalse(reader.ready());
+
+        client.close();
+    }
+
+    @Test
+    public void testSYNThenSPDYPushIsReceived() throws Exception
+    {
+        final byte[] data = "0123456789ABCDEF".getBytes("UTF-8");
+        InetSocketAddress proxyAddress = startProxy(startServer(new ServerSessionFrameListener.Adapter()
+        {
+            @Override
+            public StreamFrameListener onSyn(Stream stream, SynInfo synInfo)
+            {
+                Headers responseHeaders = new Headers();
+                responseHeaders.put(HTTPSPDYHeader.VERSION.name(version()), "HTTP/1.1");
+                responseHeaders.put(HTTPSPDYHeader.STATUS.name(version()), "200 OK");
+                stream.reply(new ReplyInfo(responseHeaders, false));
+
+                Headers pushHeaders = new Headers();
+                pushHeaders.put(HTTPSPDYHeader.URI.name(version()), "/push");
+                stream.syn(new SynInfo(pushHeaders, false), 5, TimeUnit.SECONDS, new Handler.Adapter<Stream>()
+                {
+                    @Override
+                    public void completed(Stream pushStream)
+                    {
+                        pushStream.data(new BytesDataInfo(data, true));
+                    }
+                });
+
+                stream.data(new BytesDataInfo(data, true));
+
+                return null;
+            }
+        }));
+        proxyConnector.setDefaultAsyncConnectionFactory(proxyConnector.getAsyncConnectionFactory("spdy/" + version()));
+
+        final CountDownLatch pushSynLatch = new CountDownLatch(1);
+        final CountDownLatch pushDataLatch = new CountDownLatch(1);
+        Session client = factory.newSPDYClient(version()).connect(proxyAddress, new SessionFrameListener.Adapter()
+        {
+            @Override
+            public StreamFrameListener onSyn(Stream stream, SynInfo synInfo)
+            {
+                pushSynLatch.countDown();
+                return new StreamFrameListener.Adapter()
+                {
+                    @Override
+                    public void onData(Stream stream, DataInfo dataInfo)
+                    {
+                        dataInfo.consume(dataInfo.length());
+                        if (dataInfo.isClose())
+                            pushDataLatch.countDown();
+                    }
+                };
+            }
+        }).get(5, TimeUnit.SECONDS);
+
+        Headers headers = new Headers();
+        headers.put(HTTPSPDYHeader.HOST.name(version()), "localhost:" + proxyAddress.getPort());
+        final CountDownLatch replyLatch = new CountDownLatch(1);
+        final CountDownLatch dataLatch = new CountDownLatch(1);
+        client.syn(new SynInfo(headers, true), new StreamFrameListener.Adapter()
+        {
+            @Override
+            public void onReply(Stream stream, ReplyInfo replyInfo)
+            {
+                replyLatch.countDown();
+            }
+
+            @Override
+            public void onData(Stream stream, DataInfo dataInfo)
+            {
+                dataInfo.consume(dataInfo.length());
+                if (dataInfo.isClose())
+                    dataLatch.countDown();
+            }
+        });
+
+        Assert.assertTrue(replyLatch.await(5, TimeUnit.SECONDS));
+        Assert.assertTrue(pushSynLatch.await(5, TimeUnit.SECONDS));
+        Assert.assertTrue(pushDataLatch.await(5, TimeUnit.SECONDS));
+        Assert.assertTrue(dataLatch.await(5, TimeUnit.SECONDS));
+
+        client.goAway().get(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testPING() throws Exception
+    {
+        // PING is per hop, and it does not carry the information to which server to ping to
+        // We just verify that it works
+
+        InetSocketAddress proxyAddress = startProxy(startServer(new ServerSessionFrameListener.Adapter()));
+        proxyConnector.setDefaultAsyncConnectionFactory(proxyConnector.getAsyncConnectionFactory("spdy/" + version()));
+
+        final CountDownLatch pingLatch = new CountDownLatch(1);
+        Session client = factory.newSPDYClient(version()).connect(proxyAddress, new SessionFrameListener.Adapter()
+        {
+            @Override
+            public void onPing(Session session, PingInfo pingInfo)
+            {
+                pingLatch.countDown();
+            }
+        }).get(5, TimeUnit.SECONDS);
+
+        client.ping().get(5, TimeUnit.SECONDS);
+
+        Assert.assertTrue(pingLatch.await(5, TimeUnit.SECONDS));
+
+        client.goAway().get(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testGETThenReset() throws Exception
+    {
+        InetSocketAddress proxyAddress = startProxy(startServer(new ServerSessionFrameListener.Adapter()
+        {
+            @Override
+            public StreamFrameListener onSyn(Stream stream, SynInfo synInfo)
+            {
+                Assert.assertTrue(synInfo.isClose());
+                Headers requestHeaders = synInfo.getHeaders();
+                Assert.assertNotNull(requestHeaders.get("via"));
+
+                stream.getSession().rst(new RstInfo(stream.getId(), StreamStatus.REFUSED_STREAM));
+
+                return null;
+            }
+        }));
+
+        Socket client = new Socket();
+        client.connect(proxyAddress);
+        OutputStream output = client.getOutputStream();
+
+        String request = "" +
+                "GET / HTTP/1.1\r\n" +
+                "Host: localhost:" + proxyAddress.getPort() + "\r\n" +
+                "\r\n";
+        output.write(request.getBytes("UTF-8"));
+        output.flush();
+
+        InputStream input = client.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"));
+        Assert.assertNull(reader.readLine());
+
+        client.close();
+    }
+
+    @Test
+    public void testSYNThenReset() throws Exception
+    {
+        InetSocketAddress proxyAddress = startProxy(startServer(new ServerSessionFrameListener.Adapter()
+        {
+            @Override
+            public StreamFrameListener onSyn(Stream stream, SynInfo synInfo)
+            {
+                Assert.assertTrue(synInfo.isClose());
+                Headers requestHeaders = synInfo.getHeaders();
+                Assert.assertNotNull(requestHeaders.get("via"));
+
+                stream.getSession().rst(new RstInfo(stream.getId(), StreamStatus.REFUSED_STREAM));
+
+                return null;
+            }
+        }));
+        proxyConnector.setDefaultAsyncConnectionFactory(proxyConnector.getAsyncConnectionFactory("spdy/" + version()));
+
+        final CountDownLatch resetLatch = new CountDownLatch(1);
+        Session client = factory.newSPDYClient(version()).connect(proxyAddress, new SessionFrameListener.Adapter()
+        {
+            @Override
+            public void onRst(Session session, RstInfo rstInfo)
+            {
+                resetLatch.countDown();
+            }
+        }).get(5, TimeUnit.SECONDS);
+
+        Headers headers = new Headers();
+        headers.put(HTTPSPDYHeader.HOST.name(version()), "localhost:" + proxyAddress.getPort());
+        client.syn(new SynInfo(headers, true), null);
+
+        Assert.assertTrue(resetLatch.await(5, TimeUnit.SECONDS));
 
         client.goAway().get(5, TimeUnit.SECONDS);
     }
