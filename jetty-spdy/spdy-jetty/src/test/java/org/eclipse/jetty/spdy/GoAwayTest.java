3c0e13a7fe6450deead1c98fea86d6a6d42a8fdc
Simone Bordet <simone.bordet@gmail.com>
2012-06-12 14:07:18 +0200

    Fixed test after changes to the semantic of last-good-stream-id.

--- jetty-spdy/spdy-jetty/src/test/java/org/eclipse/jetty/spdy/GoAwayTest.java
+++ jetty-spdy/spdy-jetty/src/test/java/org/eclipse/jetty/spdy/GoAwayTest.java
@@ -219,10 +219,10 @@ public class GoAwayTest extends AbstractTest
             Assert.assertThat(x.getCause(), CoreMatchers.instanceOf(ClosedChannelException.class));
         }
 
-        // Be sure the last good stream is the first
+        // The last good stream is the second, because it was received by the server
         Assert.assertTrue(goAwayLatch.await(5, TimeUnit.SECONDS));
         GoAwayInfo goAway = goAwayRef.get();
         Assert.assertNotNull(goAway);
-        Assert.assertEquals(stream1.getId(), goAway.getLastStreamId());
+        Assert.assertEquals(stream2.getId(), goAway.getLastStreamId());
     }
 }
