6679c9436ee9a364a613c5d49a8b72015281baa0
Jesse McConnell <jesse.mcconnell@gmail.com>
2012-06-18 14:03:22 -0500

    minor structural updates with joakim's latest

---
+++ jetty-websocket/websocket-core/src/main/java/org/eclipse/jetty/websocket/generator/PingFrameGenerator.java
@@ -0,0 +1,17 @@
+package org.eclipse.jetty.websocket.generator;
+
+import java.nio.ByteBuffer;
+
+import org.eclipse.jetty.websocket.frames.ControlFrame;
+
+public class PingFrameGenerator extends ControlFrameGenerator
+{
+
+    @Override
+    public ByteBuffer generate(ControlFrame frame)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
