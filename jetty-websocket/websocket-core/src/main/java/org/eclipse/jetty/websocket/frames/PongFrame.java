993c3788b9e4a16bdcd43aec909f477a2e6a3607
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2012-06-19 10:22:39 -0700

    Fixing up PongFrame to match PingFrame

--- jetty-websocket/websocket-core/src/main/java/org/eclipse/jetty/websocket/frames/PongFrame.java
+++ jetty-websocket/websocket-core/src/main/java/org/eclipse/jetty/websocket/frames/PongFrame.java
@@ -1,28 +1,73 @@
 package org.eclipse.jetty.websocket.frames;
 
+import java.nio.ByteBuffer;
+
 import org.eclipse.jetty.websocket.api.OpCode;
+import org.eclipse.jetty.websocket.api.WebSocketException;
 
 /**
  * Representation of a <a href="https://tools.ietf.org/html/rfc6455#section-5.5.3">Pong Frame (0x0A)</a>.
  */
 public class PongFrame extends ControlFrame
 {
-    private final int pingId;
+    private ByteBuffer payload;
 
-    public PongFrame(int pingId)
+    /**
+     * Default contructor
+     */
+    public PongFrame()
     {
         super(OpCode.PONG);
-        this.pingId = pingId;
     }
 
-    public int getPingId()
+    /**
+     * Construct Pong frame from known byteBuffer
+     * 
+     * @param payload
+     */
+    public PongFrame(ByteBuffer payload)
     {
-        return pingId;
+        this();
+        setPayload(payload);
+    }
+
+    /**
+     * Construct appropriate PongFrame from PingFrame
+     * @param ping the ping frame to base pong from
+     */
+    public PongFrame(PingFrame ping)
+    {
+        this();
+        // TODO: set appropriate pong from ping frame payload + masking
+    }
+
+    @Override
+    public OpCode getOpCode()
+    {
+        return OpCode.PONG;
+    }
+
+    public ByteBuffer getPayload()
+    {
+        return payload;
+    }
+
+    public void setPayload(ByteBuffer payload)
+    {
+        if (payload.array().length >= 126)
+        {
+            this.payload = payload;
+            setPayloadLength(payload.array().length);
+        }
+        else
+        {
+            throw new WebSocketException("too long, catch this better");
+        }
     }
 
     @Override
     public String toString()
     {
-        return String.format("%s pong=%d",super.toString(),getPingId());
+        return String.format("%s pong",super.toString());
     }
 }
