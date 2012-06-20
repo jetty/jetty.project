ad6eee39d60e0b7bf7846f7abdbb9fb5b5820b08
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2012-06-20 11:30:04 -0700

    Minor formatting update

--- jetty-websocket/websocket-core/src/main/java/org/eclipse/jetty/websocket/api/WebSocketException.java
+++ jetty-websocket/websocket-core/src/main/java/org/eclipse/jetty/websocket/api/WebSocketException.java
@@ -4,20 +4,25 @@ package org.eclipse.jetty.websocket.api;
  * A recoverable exception within the websocket framework.
  */
 @SuppressWarnings("serial")
-public class WebSocketException extends RuntimeException {
-	public WebSocketException() {
-		super();
-	}
+public class WebSocketException extends RuntimeException
+{
+    public WebSocketException()
+    {
+        super();
+    }
 
-	public WebSocketException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    public WebSocketException(String message)
+    {
+        super(message);
+    }
 
-	public WebSocketException(String message) {
-		super(message);
-	}
+    public WebSocketException(String message, Throwable cause)
+    {
+        super(message,cause);
+    }
 
-	public WebSocketException(Throwable cause) {
-		super(cause);
-	}
+    public WebSocketException(Throwable cause)
+    {
+        super(cause);
+    }
 }
