8893b9c96a0b21ca4005176b4c3d75d9662214a3
Jesse McConnell <jesse.mcconnell@gmail.com>
2012-06-20 16:34:45 -0500

    add text and binary frame generators

---
+++ jetty-websocket/websocket-core/src/main/java/org/eclipse/jetty/websocket/generator/BinaryFrameGenerator.java
@@ -0,0 +1,21 @@
+package org.eclipse.jetty.websocket.generator;
+
+import java.nio.ByteBuffer;
+
+import org.eclipse.jetty.io.ByteBufferPool;
+import org.eclipse.jetty.websocket.api.WebSocketPolicy;
+import org.eclipse.jetty.websocket.frames.BinaryFrame;
+
+public class BinaryFrameGenerator extends FrameGenerator<BinaryFrame>
+{    
+    public BinaryFrameGenerator(ByteBufferPool bufferPool, WebSocketPolicy policy)
+    {
+        super(bufferPool, policy);
+    }
+
+    @Override
+    public ByteBuffer payload(BinaryFrame binary)
+    {
+        return binary.getData();
+    }
+}
