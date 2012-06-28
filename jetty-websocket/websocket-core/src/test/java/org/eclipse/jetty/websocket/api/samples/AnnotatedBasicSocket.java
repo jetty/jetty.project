70b9f7245e8f9141f91e49c7db4f710842e25b1e
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2012-06-28 14:27:51 -0700

    ByteBuffer vs byte[] array tests

---
+++ jetty-websocket/websocket-core/src/test/java/org/eclipse/jetty/websocket/api/samples/AnnotatedBasicSocket.java
@@ -0,0 +1,35 @@
+package org.eclipse.jetty.websocket.api.samples;
+
+import java.nio.ByteBuffer;
+
+import org.eclipse.jetty.websocket.annotations.OnWebSocketBinary;
+import org.eclipse.jetty.websocket.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.annotations.WebSocket;
+import org.eclipse.jetty.websocket.api.EventCapture;
+import org.eclipse.jetty.websocket.api.WebSocketConnection;
+
+@WebSocket
+public class AnnotatedBasicSocket
+{
+    public EventCapture capture = new EventCapture();
+
+    @OnWebSocketBinary
+    public void onBinary(ByteBuffer payload)
+    {
+        capture.add("onBinary(%s)",payload);
+    }
+
+    @OnWebSocketClose
+    public void onClose(int statusCode, String reason)
+    {
+        capture.add("onClose(%d, %s)",statusCode,capture.q(reason));
+    }
+
+    @OnWebSocketConnect
+    public void onConnect(WebSocketConnection conn)
+    {
+        capture.add("onConnect(%s)", conn);
+    }
+
+}
