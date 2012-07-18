f1d4d4699fe84b465f468c669150d0d04968cfbc
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2012-07-18 10:32:07 -0700

    Moving tests into jetty-util/src/test/java/org/eclipse/jetty/util/BufferUtilTest.java

--- jetty-util/src/test/java/org/eclipse/jetty/util/BufferUtilTest.java
+++ jetty-util/src/test/java/org/eclipse/jetty/util/BufferUtilTest.java
@@ -14,15 +14,12 @@
 package org.eclipse.jetty.util;
 
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.nio.ByteBuffer;
 
 import org.junit.Test;
 
-/**
- *
- */
 public class BufferUtilTest
 {
     @Test
@@ -119,4 +116,43 @@ public class BufferUtilTest
             assertEquals("t"+i,str[i],BufferUtil.toString(buffer));
         }
     }
+    
+    @Test
+    public void testPut() throws Exception
+    {
+        ByteBuffer to = BufferUtil.allocate(10);
+        ByteBuffer from=BufferUtil.toBuffer("12345");
+        
+        BufferUtil.clear(to);
+        assertEquals(5,BufferUtil.append(from,to));
+        assertTrue(BufferUtil.isEmpty(from));
+        assertEquals("12345",BufferUtil.toString(to));
+        
+        from=BufferUtil.toBuffer("XX67890ZZ");
+        from.position(2);
+
+        assertEquals(5,BufferUtil.append(from,to));
+        assertEquals(2,from.remaining());
+        assertEquals("1234567890",BufferUtil.toString(to));
+    }   
+
+    @Test
+    public void testPutDirect() throws Exception
+    {
+        ByteBuffer to = BufferUtil.allocateDirect(10);
+        ByteBuffer from=BufferUtil.toBuffer("12345");
+        
+        BufferUtil.clear(to);
+        assertEquals(5,BufferUtil.append(from,to));
+        assertTrue(BufferUtil.isEmpty(from));
+        assertEquals("12345",BufferUtil.toString(to));
+        
+        from=BufferUtil.toBuffer("XX67890ZZ");
+        from.position(2);
+
+        assertEquals(5,BufferUtil.append(from,to));
+        assertEquals(2,from.remaining());
+        assertEquals("1234567890",BufferUtil.toString(to));
+    }
+   
 }
