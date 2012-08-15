c1530f6be44744285923d82805d385db530426c5
Jesse McConnell <jesse.mcconnell@gmail.com>
2012-08-15 13:17:00 -0500

    add example of file server using one url and two directories for resources

---
+++ example-jetty-embedded/src/main/java/org/eclipse/jetty/embedded/SplitFileServer.java
@@ -0,0 +1,66 @@
+// ========================================================================
+// Copyright (c) 2006-2009 Mort Bay Consulting Pty. Ltd.
+// ------------------------------------------------------------------------
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// and Apache License v2.0 which accompanies this distribution.
+// The Eclipse Public License is available at 
+// http://www.eclipse.org/legal/epl-v10.html
+// The Apache License v2.0 is available at
+// http://www.opensource.org/licenses/apache2.0.php
+// You may elect to redistribute this code under either of these licenses. 
+// ========================================================================
+
+package org.eclipse.jetty.embedded;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.SelectChannelConnector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.toolchain.test.MavenTestingUtils;
+import org.eclipse.jetty.util.resource.Resource;
+
+/* ------------------------------------------------------------ */
+/**
+ * A {@link ContextHandlerCollection} handler may be used to direct a request to
+ * a specific Context. The URI path prefix and optional virtual host is used to
+ * select the context.
+ * 
+ */
+public class SplitFileServer
+{
+        
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server();
+        SelectChannelConnector connector = new SelectChannelConnector(server);
+        connector.setPort(8090);
+        server.setConnectors(new Connector[]
+        { connector });
+
+        ContextHandler context0 = new ContextHandler();
+        context0.setContextPath("/");
+        ResourceHandler rh0 = new ResourceHandler();
+        rh0.setBaseResource( Resource.newResource(MavenTestingUtils.getTestResourceDir("dir0")));
+        context0.setHandler(rh0);
+
+        ContextHandler context1 = new ContextHandler();
+        context1.setContextPath("/");   
+        ResourceHandler rh1 = new ResourceHandler();
+        rh1.setBaseResource( Resource.newResource(MavenTestingUtils.getTestResourceDir("dir1")));
+        context1.setHandler(rh1);
+      
+        ContextHandlerCollection contexts = new ContextHandlerCollection();
+        contexts.setHandlers(new Handler[]
+        { context0, context1 });
+
+        server.setHandler(contexts);
+
+        server.start();
+        System.err.println(server.dump());
+        server.join();
+    }
+}
