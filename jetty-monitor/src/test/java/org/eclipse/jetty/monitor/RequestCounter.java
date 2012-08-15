e1daf7bcbd0fed01c4686c7642c4cbff39d5749b
Jesse McConnell <jesse.mcconnell@gmail.com>
2012-08-15 14:20:15 -0500

    switch monitor test to annotations as well

--- jetty-monitor/src/test/java/org/eclipse/jetty/monitor/RequestCounter.java
+++ jetty-monitor/src/test/java/org/eclipse/jetty/monitor/RequestCounter.java
@@ -13,16 +13,22 @@
 
 package org.eclipse.jetty.monitor;
 
+import org.eclipse.jetty.util.annotation.ManagedAttribute;
+import org.eclipse.jetty.util.annotation.ManagedObject;
+import org.eclipse.jetty.util.annotation.ManagedOperation;
 
+@ManagedObject("Request Counter")
 public class RequestCounter
 {  
     public long _counter;
     
+    @ManagedAttribute("current value of the counter")
     public synchronized long getCounter()
     {
         return _counter;
     }
     
+    @ManagedOperation("increment the counter")
     public synchronized void increment()
     {
         _counter++;
