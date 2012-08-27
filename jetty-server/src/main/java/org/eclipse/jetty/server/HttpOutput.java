5e4a2508b16d49d42f7b5f9206a02ada892a13df
Simone Bordet <simone.bordet@gmail.com>
2012-08-27 17:34:46 +0200

    Jetty9 - Removed System.err.println().

--- jetty-server/src/main/java/org/eclipse/jetty/server/HttpOutput.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/HttpOutput.java
@@ -183,7 +183,6 @@ public class HttpOutput extends ServletOutputStream
         if (_aggregate == null)
             _aggregate = _channel.getByteBufferPool().acquire(getBufferSize(), false);
 
-        System.err.println(BufferUtil.toDetailString(_aggregate));
         BufferUtil.append(_aggregate, (byte)b);
         _written++;
 
