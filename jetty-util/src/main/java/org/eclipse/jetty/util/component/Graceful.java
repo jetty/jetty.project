acb89e0d2f2b7d044eb0e1150cbee99c124fdf83
Simone Bordet <simone.bordet@gmail.com>
2012-08-28 21:55:44 +0200

    Jetty9 - Added copyright header.

--- jetty-util/src/main/java/org/eclipse/jetty/util/component/Graceful.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/component/Graceful.java
@@ -1,3 +1,21 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2012 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
 package org.eclipse.jetty.util.component;
 
 import java.util.concurrent.Future;
@@ -5,7 +23,7 @@ import java.util.concurrent.Future;
 /* ------------------------------------------------------------ */
 /* A Lifecycle that can be gracefully shutdown.
  */
-public interface Graceful 
+public interface Graceful
 {
     public <C> Future<C> shutdown(C c);
 }
