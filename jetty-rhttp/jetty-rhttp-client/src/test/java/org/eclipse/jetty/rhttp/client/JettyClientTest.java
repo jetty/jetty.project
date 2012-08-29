d8e51cca72c855483df9c6900c6678a553fc3f16
Jesse McConnell <jesse.mcconnell@gmail.com>
2012-08-29 11:49:16 -0500

    add jetty-rhttp from codehaus to jetty-9

---
+++ jetty-rhttp/jetty-rhttp-client/src/test/java/org/eclipse/jetty/rhttp/client/JettyClientTest.java
@@ -0,0 +1,52 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2012 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.rhttp.client;
+
+import org.eclipse.jetty.client.Address;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.rhttp.client.JettyClient;
+import org.eclipse.jetty.rhttp.client.RHTTPClient;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.StdErrLog;
+
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class JettyClientTest extends ClientTest
+{
+    {
+        ((StdErrLog)Log.getLog()).setHideStacks(!Log.getLog().isDebugEnabled());
+    }
+    
+    private HttpClient httpClient;
+
+    protected RHTTPClient createClient(int port, String targetId) throws Exception
+    {
+        ((StdErrLog)Log.getLog()).setSource(true);
+        httpClient = new HttpClient();
+        httpClient.start();
+        return new JettyClient(httpClient, new Address("localhost", port), "", targetId);
+    }
+
+    protected void destroyClient(RHTTPClient client) throws Exception
+    {
+        httpClient.stop();
+    }
+}
