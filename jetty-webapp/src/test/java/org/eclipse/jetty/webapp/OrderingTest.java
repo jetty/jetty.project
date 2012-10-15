6d26b45afed18dd5fe1211ccfbac7eb2587aa9a9
Jan Bartel <janb@intalio.com>
2012-10-15 12:12:53 +1100

    391877 org.eclipse.jetty.webapp.FragmentDescriptor incorrectly reporting duplicate others for after ordering

--- jetty-webapp/src/test/java/org/eclipse/jetty/webapp/OrderingTest.java
+++ jetty-webapp/src/test/java/org/eclipse/jetty/webapp/OrderingTest.java
@@ -853,6 +853,44 @@ public class OrderingTest
     }
     
     @Test
+    public void testRelativeOrderingWithPlainJars2 ()
+    throws Exception
+    {
+        //web.xml has no ordering, jar A has fragment after others, jar B is plain, jar C is plain
+        List<Resource> resources = new ArrayList<Resource>();
+        WebAppContext wac = new WebAppContext();
+        MetaData metaData = new MetaData();
+        metaData._ordering = new RelativeOrdering(metaData);
+        
+        //A has after others
+        TestResource jar1 = new TestResource("A");
+        resources.add(jar1);
+        TestResource r1 = new TestResource("A/web-fragment.xml");
+        FragmentDescriptor f1 = new FragmentDescriptor(r1);
+        f1._name = "A";
+        metaData._webFragmentNameMap.put(f1._name, f1);
+        metaData._webFragmentResourceMap.put(jar1, f1);
+        f1._otherType = FragmentDescriptor.OtherType.After;
+        
+        //No fragment jar B
+        TestResource r4 = new TestResource("plainB");
+        resources.add(r4);
+        
+        //No fragment jar C
+        TestResource r5 = new TestResource("plainC");
+        resources.add(r5);
+        
+        List<Resource> orderedList = metaData._ordering.order(resources);
+        String[] outcomes = {"plainBplainCA"};
+        String result = "";
+        for (Resource r:orderedList)
+           result+=(((TestResource)r)._name);
+        
+        if (!checkResult(result, outcomes))
+            fail ("No outcome matched "+result);
+    }
+    
+    @Test
     public void testAbsoluteOrderingWithPlainJars()
     throws Exception
     {
