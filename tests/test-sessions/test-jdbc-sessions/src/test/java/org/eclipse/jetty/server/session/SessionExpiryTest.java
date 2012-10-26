4913c1eb9b5a5ef4815072cb47f4d0d481211ac3
Greg Wilkins <gregw@intalio.com>
2012-10-26 14:35:53 +1100

    jetty-9 spaces appear to help with header detection

--- tests/test-sessions/test-jdbc-sessions/src/test/java/org/eclipse/jetty/server/session/SessionExpiryTest.java
+++ tests/test-sessions/test-jdbc-sessions/src/test/java/org/eclipse/jetty/server/session/SessionExpiryTest.java
@@ -15,6 +15,7 @@
 //  You may elect to redistribute this code under either of these licenses.
 //  ========================================================================
 //
+
 package org.eclipse.jetty.server.session;
 
 import org.junit.Test;
