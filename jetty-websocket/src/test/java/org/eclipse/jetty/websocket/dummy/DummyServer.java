949996931a089589de6005b2e0bc2f4c44aa1820
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2012-11-01 21:25:25 -0700

    Fixing license (for check plugin)

--- jetty-websocket/src/test/java/org/eclipse/jetty/websocket/dummy/DummyServer.java
+++ jetty-websocket/src/test/java/org/eclipse/jetty/websocket/dummy/DummyServer.java
@@ -15,6 +15,7 @@
 //  You may elect to redistribute this code under either of these licenses.
 //  ========================================================================
 //
+
 package org.eclipse.jetty.websocket.dummy;
 
 import static org.hamcrest.Matchers.*;
