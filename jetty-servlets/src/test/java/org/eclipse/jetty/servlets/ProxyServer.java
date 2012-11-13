56cf2c2d3dc7977fb82b4f70dc46f1c21722e2e7
Simone Bordet <simone.bordet@gmail.com>
2012-11-13 19:31:41 +0100

    Restored ProxyServer class to be used as a proxy for browsers.

--- jetty-servlets/src/test/java/org/eclipse/jetty/servlets/ProxyServer.java
+++ jetty-servlets/src/test/java/org/eclipse/jetty/servlets/ProxyServer.java
@@ -20,7 +20,7 @@ package org.eclipse.jetty.servlets;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.server.handler.ConnectHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
@@ -30,25 +30,21 @@ public class ProxyServer
     {
         Server server = new Server();
         ServerConnector connector = new ServerConnector(server);
-        connector.setPort(8080);
+        connector.setPort(8888);
         server.addConnector(connector);
 
-        HandlerCollection handlers = new HandlerCollection();
-        server.setHandler(handlers);
+        // Setup proxy handler to handle CONNECT methods
+        ConnectHandler proxy = new ConnectHandler();
+//        proxy.setWhite(new String[]{"mail.google.com"});
+//        proxy.addWhitelistHost("www.google.com");
+        server.setHandler(proxy);
 
         // Setup proxy servlet
-        ServletContextHandler context = new ServletContextHandler(handlers, "/", ServletContextHandler.SESSIONS);
+        ServletContextHandler context = new ServletContextHandler(proxy, "/", ServletContextHandler.SESSIONS);
         ServletHolder proxyServlet = new ServletHolder(ProxyServlet.class);
 //        proxyServlet.setInitParameter("whiteList", "google.com, www.eclipse.org, localhost");
 //        proxyServlet.setInitParameter("blackList", "google.com/calendar/*, www.eclipse.org/committers/");
         context.addServlet(proxyServlet, "/*");
-
-
-        // Setup proxy handler to handle CONNECT methods
-//        ConnectHandler proxy = new ConnectHandler();
-//        proxy.setWhite(new String[]{"mail.google.com"});
-//        proxy.addWhitelistHost("www.google.com");
-//        handlers.addHandler(proxy);
 
         server.start();
     }
