0ff769e633d5d7c07374897ca76272a7d09872b0
Simone Bordet <simone.bordet@gmail.com>
2012-12-12 18:35:10 +0100

    Introduced HttpCookieStore.Empty to disable cookies in HttpClient and in the websocket client.

--- jetty-util/src/main/java/org/eclipse/jetty/util/HttpCookieStore.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/HttpCookieStore.java
@@ -22,6 +22,7 @@ import java.net.CookieManager;
 import java.net.CookieStore;
 import java.net.HttpCookie;
 import java.net.URI;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -71,5 +72,43 @@ public class HttpCookieStore implements CookieStore
     public boolean removeAll()
     {
         return delegate.removeAll();
+    }
+
+    public static class Empty implements CookieStore
+    {
+        @Override
+        public void add(URI uri, HttpCookie cookie)
+        {
+        }
+
+        @Override
+        public List<HttpCookie> get(URI uri)
+        {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public List<HttpCookie> getCookies()
+        {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public List<URI> getURIs()
+        {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public boolean remove(URI uri, HttpCookie cookie)
+        {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll()
+        {
+            return false;
+        }
     }
 }
