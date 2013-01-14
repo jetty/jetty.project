3a52ceca5077364a5dba99e49816f8fa70c0d37b
Greg Wilkins <gregw@intalio.com>
2013-01-14 18:04:18 +1100

    jetty-9 fixed year in header

--- jetty-util/src/main/java/org/eclipse/jetty/util/ArrayTernaryTrie.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/ArrayTernaryTrie.java
@@ -1,6 +1,6 @@
 //
 //  ========================================================================
-//  Copyright (c) 1995-2012 Mort Bay Consulting Pty. Ltd.
+//  Copyright (c) 1995-2013 Mort Bay Consulting Pty. Ltd.
 //  ------------------------------------------------------------------------
 //  All rights reserved. This program and the accompanying materials
 //  are made available under the terms of the Eclipse Public License v1.0
@@ -18,7 +18,6 @@
 
 package org.eclipse.jetty.util;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.HashSet;
 import java.util.Set;
