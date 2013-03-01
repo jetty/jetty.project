2feafb9a9726dbac73e0f42852573b82c988ad6a
Simone Bordet <simone.bordet@gmail.com>
2013-03-01 14:30:51 +0100

    402090 - httpsender PendingState cause uncertain data send to server.
    WriteFlusher was storing consumed buffers that may have been reused,
    and when the write was being completed those consumed buffer may have
    contained new content that was being written too.
    
    Fixed by compacting the buffers at the moment of creation of the
    WriteFlusher.PendingState (and not at the moment of the completeWrite() like
    it was in historically done, see 4e9460161944b5ccfa4bf1b5494d96ef395a0e76).

--- jetty-io/src/test/java/org/eclipse/jetty/io/WriteFlusherTest.java
+++ jetty-io/src/test/java/org/eclipse/jetty/io/WriteFlusherTest.java
@@ -18,19 +18,11 @@
 
 package org.eclipse.jetty.io;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
-
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.WritePendingException;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
@@ -54,16 +46,23 @@ import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
 @RunWith(MockitoJUnitRunner.class)
 public class WriteFlusherTest
 {
-    @Mock
-    private EndPoint _endPointMock;
-
-    private WriteFlusher _flusher;
-
     private final AtomicBoolean _flushIncomplete = new AtomicBoolean(false);
     private final ExecutorService executor = Executors.newFixedThreadPool(16);
+    @Mock
+    private EndPoint _endPointMock;
+    private WriteFlusher _flusher;
     private ByteArrayEndPoint _endp;
 
     @Before
@@ -398,6 +397,34 @@ public class WriteFlusherTest
             assertThat("callback failed", callback.isFailed(), is(true));
         }
         assertThat("callback completed", callback.isDone(), is(true));
+    }
+
+    @Test
+    public void testPendingWriteDoesNotStoreConsumedBuffers() throws Exception
+    {
+        int toWrite = _endp.getOutput().capacity();
+        byte[] chunk1 = new byte[toWrite / 2];
+        Arrays.fill(chunk1, (byte)1);
+        ByteBuffer buffer1 = ByteBuffer.wrap(chunk1);
+        byte[] chunk2 = new byte[toWrite];
+        Arrays.fill(chunk1, (byte)2);
+        ByteBuffer buffer2 = ByteBuffer.wrap(chunk2);
+
+        _flusher.write(new Callback.Adapter(), buffer1, buffer2);
+        assertTrue(_flushIncomplete.get());
+        assertFalse(buffer1.hasRemaining());
+
+        // Reuse buffer1
+        buffer1.clear();
+        Arrays.fill(chunk1, (byte)3);
+        int remaining1 = buffer1.remaining();
+
+        // Complete the write
+        _endp.takeOutput();
+        _flusher.completeWrite();
+
+        // Make sure buffer1 is unchanged
+        assertEquals(remaining1, buffer1.remaining());
     }
 
     private class ExposingStateCallback extends FutureCallback
