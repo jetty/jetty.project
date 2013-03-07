4b58eae39b2b5b96217a7875af62003671f4ad88
Greg Wilkins <gregw@intalio.com>
2013-03-08 09:48:58 +1100

    399967 Destroyables destroyed on undeploy and shutdown hook

--- jetty-util/src/main/java/org/eclipse/jetty/util/component/ContainerLifeCycle.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/component/ContainerLifeCycle.java
@@ -452,7 +452,6 @@ public class ContainerLifeCycle extends AbstractLifeCycle implements Container, 
     {
         if (_beans.remove(bean))
         {
-            boolean managed=bean.isManaged();
             unmanage(bean);
 
             for (Container.Listener l:_listeners)
@@ -471,18 +470,6 @@ public class ContainerLifeCycle extends AbstractLifeCycle implements Container, 
                         if (listener instanceof InheritedListener && b.isManaged() && b._bean instanceof Container)
                             ((Container)b._bean).removeBean(listener);
                     }
-                }
-            }
-            
-            if (managed && bean._bean instanceof Destroyable)
-            {
-                try
-                {
-                    ((Destroyable)bean._bean).destroy();
-                }
-                catch(Exception e)
-                {
-                    LOG.warn(e);
                 }
             }
             
