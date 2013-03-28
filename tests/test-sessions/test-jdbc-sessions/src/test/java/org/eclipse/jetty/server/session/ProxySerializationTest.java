af5f8aac33414c7a1713e818f4b72cc0576a2eba
Jan Bartel <janb@intalio.com>
2013-03-28 15:01:59 +1100

    403122  Session replication fails with ClassNotFoundException when session attribute is Java dynamic proxy

---
+++ tests/test-sessions/test-jdbc-sessions/src/test/java/org/eclipse/jetty/server/session/ProxySerializationTest.java
@@ -0,0 +1,58 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2013 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+
+package org.eclipse.jetty.server.session;
+
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.junit.Test;
+
+/**
+ * ProxySerializationTest
+ *
+ *
+ */
+public class ProxySerializationTest extends AbstractProxySerializationTest
+{
+
+    /** 
+     * @see org.eclipse.jetty.server.session.AbstractProxySerializationTest#createServer(int, int, int)
+     */
+    @Override
+    public AbstractTestServer createServer(int port, int max, int scavenge)
+    {
+        return new JdbcTestServer(port, max, scavenge);
+    }
+
+    /** 
+     * @see org.eclipse.jetty.server.session.AbstractProxySerializationTest#customizeContext(org.eclipse.jetty.servlet.ServletContextHandler)
+     */
+    @Override
+    public void customizeContext(ServletContextHandler c)
+    {
+    }
+    
+    
+
+    @Test
+    public void testProxySerialization() throws Exception
+    {
+        super.testProxySerialization();
+    }
+
+}
