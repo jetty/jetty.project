120b8c98396f8176bf80798ed86db5a6568381c8
Simone Bordet <simone.bordet@gmail.com>
2013-04-05 23:20:04 +0200

    405044 - Query parameters lost for non GET or POST.

--- jetty-client/src/test/java/org/eclipse/jetty/client/HttpClientTest.java
+++ jetty-client/src/test/java/org/eclipse/jetty/client/HttpClientTest.java
@@ -255,6 +255,38 @@ public class HttpClientTest extends AbstractHttpClientServerTest
     }
 
     @Test
+    public void test_PUT_WithParameters() throws Exception
+    {
+        final String paramName = "a";
+        final String paramValue = "\u20AC";
+        start(new AbstractHandler()
+        {
+            @Override
+            public void handle(String target, org.eclipse.jetty.server.Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+            {
+                baseRequest.setHandled(true);
+                String value = request.getParameter(paramName);
+                if (paramValue.equals(value))
+                {
+                    response.setCharacterEncoding("UTF-8");
+                    response.setContentType("text/plain");
+                    response.getOutputStream().print(value);
+                }
+            }
+        });
+
+        URI uri = URI.create(scheme + "://localhost:" + connector.getLocalPort() + "/path?" + paramName + "=" + paramValue);
+        ContentResponse response = client.newRequest(uri)
+                .method(HttpMethod.PUT)
+                .timeout(5, TimeUnit.SECONDS)
+                .send();
+
+        Assert.assertNotNull(response);
+        Assert.assertEquals(200, response.getStatus());
+        Assert.assertEquals(paramValue, new String(response.getContent(), "UTF-8"));
+    }
+
+    @Test
     public void test_POST_WithParameters_WithContent() throws Exception
     {
         final byte[] content = {0, 1, 2, 3};
