4c04f5dd40106d4b40402161beb0d5f92b5ecbb8
Greg Wilkins <gregw@intalio.com>
2013-04-11 11:45:53 +1000

    403591 do not use the ConcurrentArrayBlockingQueue for thread pool, selector and async request log

--- jetty-util/src/main/java/org/eclipse/jetty/util/thread/QueuedThreadPool.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/thread/QueuedThreadPool.java
@@ -31,6 +31,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.eclipse.jetty.util.BlockingArrayQueue;
+import org.eclipse.jetty.util.ConcurrentArrayBlockingQueue;
 import org.eclipse.jetty.util.StringUtil;
 import org.eclipse.jetty.util.annotation.ManagedAttribute;
 import org.eclipse.jetty.util.annotation.ManagedObject;
@@ -91,7 +92,8 @@ public class QueuedThreadPool extends AbstractLifeCycle implements SizedThreadPo
         setStopTimeout(5000);
         
         if (queue==null)
-            queue=new BlockingArrayQueue<Runnable>(_minThreads, _minThreads);// TODO ConcurrentArrayBlockingQueue.Unbounded<Runnable>();
+            queue=new ConcurrentArrayBlockingQueue.Unbounded<Runnable>();
+            // queue=new BlockingArrayQueue<Runnable>(_minThreads, _minThreads);
         _jobs=queue;
 
     }
