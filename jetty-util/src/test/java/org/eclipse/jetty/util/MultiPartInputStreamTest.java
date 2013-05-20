053408254efaf1d654bb977937d9476bcb8b6d2d
Jan Bartel <janb@intalio.com>
2013-05-20 15:39:30 +1000

    408446  Multipart parsing issue with boundry and charset in ContentType header

--- jetty-util/src/test/java/org/eclipse/jetty/util/MultiPartInputStreamTest.java
+++ jetty-util/src/test/java/org/eclipse/jetty/util/MultiPartInputStreamTest.java
@@ -530,6 +530,28 @@ public class MultiPartInputStreamTest
     }
     
     @Test
+    public void testCharsetEncoding () throws Exception
+    {
+        String contentType = "multipart/form-data; boundary=TheBoundary; charset=ISO-8859-1";
+        String str = "--TheBoundary\r"+
+                "content-disposition: form-data; name=\"field1\"\r"+
+                "\r"+
+                "\nJoe Blow\n"+ 
+                "\r"+
+                "--TheBoundary--\r";
+        
+        MultipartConfigElement config = new MultipartConfigElement(_dirname, 1024, 3072, 50);
+        MultiPartInputStreamParser mpis = new MultiPartInputStreamParser(new ByteArrayInputStream(str.getBytes()),
+                                                                         contentType,
+                                                                         config,
+                                                                         _tmpDir);
+        mpis.setDeleteOnExit(true);
+        Collection<Part> parts = mpis.getParts();
+        //assertThat(parts.size(), is(1));
+    }
+    
+    
+    @Test
     public void testBadlyEncodedFilename() throws Exception
     {
         
