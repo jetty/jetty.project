ba9f9b4195418e86ccd8d612e26dd9179d94c792
Thomas Becker <tbecker@intalio.com>
2013-06-06 16:46:29 +0200

    406759 supressed stacktrace in ReferrerPushStrategyTest

--- jetty-spdy/spdy-http-server/src/test/java/org/eclipse/jetty/spdy/server/http/ReferrerPushStrategyTest.java
+++ jetty-spdy/spdy-http-server/src/test/java/org/eclipse/jetty/spdy/server/http/ReferrerPushStrategyTest.java
@@ -57,6 +57,7 @@ import org.eclipse.jetty.util.Fields;
 import org.eclipse.jetty.util.Promise;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
+import org.eclipse.jetty.util.log.StdErrLog;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -181,6 +182,8 @@ public class ReferrerPushStrategyTest extends AbstractHTTPSPDYTest
         SettingsInfo settingsInfo = new SettingsInfo(settings);
         session.settings(settingsInfo);
 
+        ((StdErrLog)Log.getLogger("org.eclipse.jetty.spdy.server.http" +
+                        ".HttpTransportOverSPDY$PushResourceCoordinator$1")).setHideStacks(true);
         session.syn(new SynInfo(mainRequestHeaders, true), new StreamFrameListener.Adapter()
         {
             @Override
@@ -192,6 +195,8 @@ public class ReferrerPushStrategyTest extends AbstractHTTPSPDYTest
         });
 
         assertThat("No push stream is received", pushReceivedLatch.await(1, TimeUnit.SECONDS), is(false));
+        ((StdErrLog)Log.getLogger("org.eclipse.jetty.spdy.server.http" +
+                                ".HttpTransportOverSPDY$PushResourceCoordinator$1")).setHideStacks(false);
     }
 
     @Test
