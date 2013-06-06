d3b96328f9e3d30e7c4318fad2b949cad2425675
Thomas Becker <tbecker@intalio.com>
2013-06-06 12:59:18 +0200

    409953 More efficient implementation of BufferUtil.writeTo(...)

--- jetty-util/src/test/java/org/eclipse/jetty/util/BufferUtilTest.java
+++ jetty-util/src/test/java/org/eclipse/jetty/util/BufferUtilTest.java
@@ -241,24 +241,30 @@ public class BufferUtilTest
             "ignored.")
     public void testWriteToMicrobenchmark() throws IOException
     {
-        int capacity = 1024 * 1024;
-        int iterations = 30;
+        int capacity = 1024 * 128;
+        int iterations = 100;
+        int testRuns = 10;
         byte[] bytes = new byte[capacity];
         new Random().nextBytes(bytes);
         ByteBuffer buffer = BufferUtil.allocate(capacity);
         BufferUtil.append(buffer, bytes, 0, capacity);
-        long start = System.nanoTime();
-        for (int i = 0; i < iterations; i++)
+        long startTest = System.nanoTime();
+        for (int i = 0; i < testRuns; i++)
         {
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            long startRun = System.nanoTime();
-            BufferUtil.writeTo(buffer.asReadOnlyBuffer(), out);
-            long elapsedRun = System.nanoTime() - startRun;
-            LOG.warn("run elapsed={}ms", elapsedRun / 1000);
-            assertThat("Bytes in out equal bytes in buffer", Arrays.equals(bytes, out.toByteArray()), is(true));
+            long start = System.nanoTime();
+            for (int j = 0; j < iterations; j++)
+            {
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                long startRun = System.nanoTime();
+                BufferUtil.writeTo(buffer.asReadOnlyBuffer(), out);
+                long elapsedRun = System.nanoTime() - startRun;
+//                LOG.warn("run elapsed={}ms", elapsedRun / 1000);
+                assertThat("Bytes in out equal bytes in buffer", Arrays.equals(bytes, out.toByteArray()), is(true));
+            }
+            long elapsed = System.nanoTime() - start;
+            LOG.warn("elapsed={}ms average={}ms", elapsed / 1000, elapsed/iterations/1000);
         }
-        long elapsed = System.nanoTime() - start;
-        LOG.warn("elapsed={}ms average={}ms", elapsed / 1000, elapsed/iterations/1000);
+        LOG.warn("overall average: {}ms", (System.nanoTime() - startTest) / testRuns / iterations / 1000);
     }
 
     @Test
