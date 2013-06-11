2d661f18ddcb62dfbda474d77667c32ef2cabcbb
Greg Wilkins <gregw@intalio.com>
2013-06-11 15:30:40 +1000

    410405 Avoid NPE for requestDispatcher(../)
    Test double encoded dot dot

--- jetty-servlet/src/test/java/org/eclipse/jetty/servlet/DispatcherTest.java
+++ jetty-servlet/src/test/java/org/eclipse/jetty/servlet/DispatcherTest.java
@@ -232,7 +232,20 @@ public class DispatcherTest
         _contextHandler.addServlet(DispatchServletServlet.class, "/dispatch/*");
         _contextHandler.addServlet(RogerThatServlet.class, "/roger/that");
 
-        String requests="GET /context/dispatch/test?forward=%2e%2e/roger/that HTTP/1.0\n" + "Host: localhost\n\n";
+        String requests="GET /context/dispatch/test?forward=/%2e%2e/roger/that HTTP/1.0\n" + "Host: localhost\n\n";
+
+        String responses = _connector.getResponses(requests);
+
+        assertThat(responses,startsWith("HTTP/1.1 404 "));
+    }
+    
+    @Test
+    public void testServletForwardEncodedDotDot() throws Exception
+    {
+        _contextHandler.addServlet(DispatchServletServlet.class, "/dispatch/*");
+        _contextHandler.addServlet(RogerThatServlet.class, "/roger/that");
+
+        String requests="GET /context/dispatch/test?forward=/%252e%252e/roger/that HTTP/1.0\n" + "Host: localhost\n\n";
 
         String responses = _connector.getResponses(requests);
 
