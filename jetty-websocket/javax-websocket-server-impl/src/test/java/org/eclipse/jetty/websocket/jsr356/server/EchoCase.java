70a57f223fcd70652b1d676dd839788f24b708e8
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2013-07-23 08:05:20 -0700

    Expanding EchoCase.toString

--- jetty-websocket/javax-websocket-server-impl/src/test/java/org/eclipse/jetty/websocket/jsr356/server/EchoCase.java
+++ jetty-websocket/javax-websocket-server-impl/src/test/java/org/eclipse/jetty/websocket/jsr356/server/EchoCase.java
@@ -76,6 +76,26 @@ public class EchoCase
     @Override
     public String toString()
     {
-        return String.format("EchoCase['%s',%s,messages=%d]",path,serverPojo.getName(),messages.size());
+        StringBuilder str = new StringBuilder();
+        str.append("EchoCase['");
+        str.append(path);
+        str.append("',").append(serverPojo.getName());
+        str.append(",messages[").append(messages.size());
+        str.append("]=");
+        boolean delim = false;
+        for(Object msg: messages) {
+            if(delim) {
+                str.append(",");
+            }
+            if(msg instanceof String) {
+                str.append("'").append(msg).append("'");
+            } else {
+                str.append("(").append(msg.getClass().getName()).append(")");
+                str.append(msg);
+            }
+            delim = true;
+        }
+        str.append("]");
+        return str.toString();
     }
 }
