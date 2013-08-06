2354ad4c3dddfab9c1c48fe26d44aa0fc4fc9ada
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2013-08-06 09:19:49 -0700

    Cleaning up javadoc

--- jetty-websocket/websocket-common/src/main/java/org/eclipse/jetty/websocket/common/io/WriteCallbackWrapper.java
+++ jetty-websocket/websocket-common/src/main/java/org/eclipse/jetty/websocket/common/io/WriteCallbackWrapper.java
@@ -22,7 +22,7 @@ import org.eclipse.jetty.util.Callback;
 import org.eclipse.jetty.websocket.api.WriteCallback;
 
 /**
- * Wraps the exposed {@link WriteCallback} API with a Jetty {@link Callback}.
+ * Wraps the exposed {@link WriteCallback} WebSocket API with a Jetty {@link Callback}.
  * <p>
  * We don't expose the jetty {@link Callback} object to the webapp, as that makes things complicated for the WebAppContext's Classloader.
  */
