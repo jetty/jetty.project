67bdc29b9b666fd32beba59413715115f505e56e
Jan Bartel <janb@intalio.com>
2013-09-09 12:29:36 +1000

    416787 StringIndexOutOfBounds with a pathMap of ""

--- jetty-http/src/main/java/org/eclipse/jetty/http/PathMap.java
+++ jetty-http/src/main/java/org/eclipse/jetty/http/PathMap.java
@@ -401,6 +401,9 @@ public class PathMap<O> extends HashMap<String,O>
     public static boolean match(String pathSpec, String path, boolean noDefault)
         throws IllegalArgumentException
     {
+        if (pathSpec.length()==0)
+            return "/".equals(path);
+            
         char c = pathSpec.charAt(0);
         if (c=='/')
         {
