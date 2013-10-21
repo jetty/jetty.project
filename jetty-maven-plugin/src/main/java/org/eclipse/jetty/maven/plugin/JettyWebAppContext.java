3f852cf3db6f423a4b6d59e401cfd8c60ccddabd
Jan Bartel <janb@intalio.com>
2013-10-21 16:44:33 +1100

    416597  Allow classes and jars on the webappcontext extraclasspath to be scanned for annotations by jetty-maven-plugin

--- jetty-maven-plugin/src/main/java/org/eclipse/jetty/maven/plugin/JettyWebAppContext.java
+++ jetty-maven-plugin/src/main/java/org/eclipse/jetty/maven/plugin/JettyWebAppContext.java
@@ -29,6 +29,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
 import org.eclipse.jetty.plus.webapp.EnvConfiguration;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.FilterMapping;
@@ -110,7 +111,7 @@ public class JettyWebAppContext extends WebAppContext
                 new FragmentConfiguration(),
                 _envConfig = new EnvConfiguration(),
                 new org.eclipse.jetty.plus.webapp.PlusConfiguration(),
-                new MavenAnnotationConfiguration(),
+                new AnnotationConfiguration(),
                 new JettyWebXmlConfiguration()
         });
         // Turn off copyWebInf option as it is not applicable for plugin.
