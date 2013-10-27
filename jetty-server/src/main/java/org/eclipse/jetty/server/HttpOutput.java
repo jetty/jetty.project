960c03b8acfd52083ba16632e59523e605aa12cf
Greg Wilkins <gregw@intalio.com>
2013-10-28 10:14:12 +1100

    419350 Do not borrow space from passed arrays

--- jetty-server/src/main/java/org/eclipse/jetty/server/HttpOutput.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/HttpOutput.java
@@ -300,12 +300,14 @@ public class HttpOutput extends ServletOutputStream
 
     /* ------------------------------------------------------------ */
     /** Blocking send of content.
-     * @param content The content to send
+     * @param content The content to send.
      * @throws IOException
      */
     public void sendContent(ByteBuffer content) throws IOException
     {
         final BlockingCallback callback =_channel.getWriteBlockingCallback();
+        if (content.hasArray()&&content.limit()<content.capacity())
+            content=content.asReadOnlyBuffer();
         _channel.write(content,true,callback);
         callback.block();
     }
@@ -347,7 +349,6 @@ public class HttpOutput extends ServletOutputStream
         callback.block();
     }
    
-
     /* ------------------------------------------------------------ */
     /** Asynchronous send of content.
      * @param content The content to send
@@ -355,6 +356,8 @@ public class HttpOutput extends ServletOutputStream
      */
     public void sendContent(ByteBuffer content, final Callback callback)
     {
+        if (content.hasArray()&&content.limit()<content.capacity())
+            content=content.asReadOnlyBuffer();
         _channel.write(content,true,new Callback()
         {
             @Override
