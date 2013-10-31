37ea88d299b1220dff4902b46500415046e962cc
Greg Wilkins <gregw@intalio.com>
2013-11-01 08:09:26 +1100

    fixed bad merge

--- jetty-servlet/src/test/java/org/eclipse/jetty/servlet/AsyncContextTest.java
+++ jetty-servlet/src/test/java/org/eclipse/jetty/servlet/AsyncContextTest.java
@@ -135,7 +135,7 @@ public class AsyncContextTest
 
         Assert.assertEquals("error servlet","ERROR: /error",br.readLine());
         Assert.assertEquals("error servlet","PathInfo= /IOE",br.readLine());
-        Assert.assertEquals("error servlet","EXCEPTION: java.io.IOException: Test",br.readLine());
+        Assert.assertEquals("error servlet","EXCEPTION: org.eclipse.jetty.server.QuietServletException: java.io.IOException: Test",br.readLine());
     }
     
     @Test
@@ -350,7 +350,7 @@ public class AsyncContextTest
 
         Assert.assertEquals("error servlet","ERROR: /error",br.readLine());
         Assert.assertEquals("error servlet","PathInfo= /500",br.readLine());
-        Assert.assertEquals("error servlet","EXCEPTION: java.io.IOException: TEST",br.readLine());
+        Assert.assertEquals("error servlet","EXCEPTION: java.lang.RuntimeException: TEST",br.readLine());
     }
 
     private class DispatchingRunnable implements Runnable
@@ -424,7 +424,7 @@ public class AsyncContextTest
                     @Override
                     public void onTimeout(AsyncEvent event) throws IOException
                     {
-                        throw new IOException("TEST");
+                        throw new RuntimeException("TEST");
                     }
                     
                     @Override
