ffddaeceec322ba8b226432fb649f0e4ceab0544
Greg Wilkins <gregw@intalio.com>
2013-11-25 11:47:15 +1100

    421775 CookiePatternRule only sets cookie if not set already

--- jetty-rewrite/src/main/java/org/eclipse/jetty/rewrite/handler/CookiePatternRule.java
+++ jetty-rewrite/src/main/java/org/eclipse/jetty/rewrite/handler/CookiePatternRule.java
@@ -73,6 +73,18 @@ public class CookiePatternRule extends PatternRule
     @Override
     public String apply(String target, HttpServletRequest request, HttpServletResponse response) throws IOException
     {
+        // Check that cookie is not already set
+        Cookie[] cookies = request.getCookies();
+        if (cookies!=null)
+        {
+            for (Cookie cookie:cookies)
+            {
+                if (_name.equals(cookie.getName()) && _value.equals(cookie.getValue()))
+                    return target;
+            }
+        }
+        
+        // set it
         response.addCookie(new Cookie(_name, _value));
         return target;
     }
