232ca134fc29fbe80fcc12159f09b89960acbfcd
Greg Wilkins <gregw@intalio.com>
2013-12-02 18:49:23 +1100

    removed debug print from test

--- jetty-servlet/src/test/java/org/eclipse/jetty/servlet/AsyncServletIOTest.java
+++ jetty-servlet/src/test/java/org/eclipse/jetty/servlet/AsyncServletIOTest.java
@@ -240,7 +240,7 @@ public class AsyncServletIOTest
         int w=0;
         for (String line : list)
         {
-            System.err.println(line);
+            // System.err.println(line);
             if ("-".equals(line))
                 continue;
             assertEquals(writes[w],line.length());
