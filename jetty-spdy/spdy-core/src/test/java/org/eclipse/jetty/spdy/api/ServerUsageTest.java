f4a5f68bdd00f4378c2dab5b7232b5f063b5e9b8
Simone Bordet <simone.bordet@gmail.com>
2013-12-13 13:24:33 +0100

    Using org.junit.Assert rather than junit.framework.Assert.

--- jetty-spdy/spdy-core/src/test/java/org/eclipse/jetty/spdy/api/ServerUsageTest.java
+++ jetty-spdy/spdy-core/src/test/java/org/eclipse/jetty/spdy/api/ServerUsageTest.java
@@ -18,12 +18,11 @@
 
 package org.eclipse.jetty.spdy.api;
 
-import junit.framework.Assert;
-
 import org.eclipse.jetty.spdy.api.server.ServerSessionFrameListener;
 import org.eclipse.jetty.util.Callback;
 import org.eclipse.jetty.util.Fields;
 import org.eclipse.jetty.util.Promise;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -55,7 +54,7 @@ public class ServerUsageTest
                 return null;
             }
         };
-        Assert.assertTrue(ssfl!=null);
+        Assert.assertNotNull(ssfl);
     }
 
     @Test
@@ -90,7 +89,7 @@ public class ServerUsageTest
                 });
             }
         };
-        Assert.assertTrue(ssfl!=null);
+        Assert.assertNotNull(ssfl);
     }
 
     @Test
@@ -117,6 +116,6 @@ public class ServerUsageTest
                 return null;
             }
         };
-        Assert.assertTrue(ssfl!=null);
+        Assert.assertNotNull(ssfl);
     }
 }
