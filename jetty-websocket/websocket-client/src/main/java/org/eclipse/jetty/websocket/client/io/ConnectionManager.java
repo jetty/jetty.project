259c6245581410c3e7568e0ec9b2d73ef457cd1d
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2014-01-07 13:19:01 -0700

    425038 - WebSocketClient leaks file handles when exceptions are thrown from open()
    + Close connection on open() exception

--- jetty-websocket/websocket-client/src/main/java/org/eclipse/jetty/websocket/client/io/ConnectionManager.java
+++ jetty-websocket/websocket-client/src/main/java/org/eclipse/jetty/websocket/client/io/ConnectionManager.java
@@ -18,6 +18,7 @@
 
 package org.eclipse.jetty.websocket.client.io;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.net.URI;
@@ -56,9 +57,10 @@ public class ConnectionManager extends ContainerLifeCycle
         @Override
         public void run()
         {
+            SocketChannel channel = null;
             try
             {
-                SocketChannel channel = SocketChannel.open();
+                channel = SocketChannel.open();
                 if (bindAddress != null)
                 {
                     channel.bind(bindAddress);
@@ -76,6 +78,20 @@ public class ConnectionManager extends ContainerLifeCycle
             }
             catch (Throwable t)
             {
+                // close the socket channel
+                if (channel != null)
+                {
+                    try
+                    {
+                        channel.close();
+                    }
+                    catch (IOException ignore)
+                    {
+                        LOG.ignore(ignore);
+                    }
+                }
+                
+                // notify the future
                 failed(t);
             }
         }
