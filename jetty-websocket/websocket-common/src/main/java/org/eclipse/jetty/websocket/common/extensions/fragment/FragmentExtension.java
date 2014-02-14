a2a90c5c875988fea19ca432efe1c27dad6a2c78
Simone Bordet <simone.bordet@gmail.com>
2014-02-14 22:04:51 +0100

    427700 - Outgoing extensions that create multiple frames should flush them in order and atomically.
    Modified FragmentExtension to use a Queue and IteratingCallback to
    make sure that frames are iteratively fragmented.
    The creation of the next fragment only happens when there is a callback
    from the next outgoing layer.

--- jetty-websocket/websocket-common/src/main/java/org/eclipse/jetty/websocket/common/extensions/fragment/FragmentExtension.java
+++ jetty-websocket/websocket-common/src/main/java/org/eclipse/jetty/websocket/common/extensions/fragment/FragmentExtension.java
@@ -20,7 +20,12 @@ package org.eclipse.jetty.websocket.common.extensions.fragment;
 
 
 import java.nio.ByteBuffer;
+import java.util.Queue;
 
+import org.eclipse.jetty.util.ConcurrentArrayQueue;
+import org.eclipse.jetty.util.IteratingCallback;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.websocket.api.WriteCallback;
 import org.eclipse.jetty.websocket.api.extensions.ExtensionConfig;
 import org.eclipse.jetty.websocket.api.extensions.Frame;
@@ -33,8 +38,12 @@ import org.eclipse.jetty.websocket.common.frames.DataFrame;
  */
 public class FragmentExtension extends AbstractExtension
 {
-    private int maxLength = -1;
-    
+    private static final Logger LOG = Log.getLogger(FragmentExtension.class);
+
+    private final Queue<FrameEntry> entries = new ConcurrentArrayQueue<>();
+    private final IteratingCallback flusher = new Flusher();
+    private int maxLength;
+
     @Override
     public String getName()
     {
@@ -42,76 +51,152 @@ public class FragmentExtension extends AbstractExtension
     }
 
     @Override
-    public void incomingError(Throwable e)
-    {
-        // Pass thru
-        nextIncomingError(e);
-    }
-
-    @Override
     public void incomingFrame(Frame frame)
     {
-        // Pass thru
         nextIncomingFrame(frame);
     }
 
     @Override
     public void outgoingFrame(Frame frame, WriteCallback callback)
     {
-        if (OpCode.isControlFrame(frame.getOpCode()))
+        ByteBuffer payload = frame.getPayload();
+        int length = payload != null ? payload.remaining() : 0;
+        if (OpCode.isControlFrame(frame.getOpCode()) || maxLength <= 0 || length <= maxLength)
         {
-            // Cannot fragment Control Frames
-            nextOutgoingFrame(frame,callback);
+            nextOutgoingFrame(frame, callback);
             return;
         }
 
-        int length = frame.getPayloadLength();
-
-        ByteBuffer payload = frame.getPayload().slice();
-        int originalLimit = payload.limit();
-        int currentPosition = payload.position();
-
-        if (maxLength <= 0)
-        {
-            // output original frame
-            nextOutgoingFrame(frame,callback);
-            return;
-        }
-
-        boolean continuation = false;
-
-        // break apart payload based on maxLength rules
-        while (length > maxLength)
-        {
-            DataFrame frag = new DataFrame(frame,continuation);
-            frag.setFin(false); // always false here
-            payload.position(currentPosition);
-            payload.limit(Math.min(payload.position() + maxLength,originalLimit));
-            frag.setPayload(payload);
-
-            // no callback for beginning and middle parts
-            nextOutgoingFrame(frag,null);
-
-            length -= maxLength;
-            continuation = true;
-            currentPosition = payload.limit();
-        }
-
-        // write remaining
-        DataFrame frag = new DataFrame(frame,continuation);
-        frag.setFin(frame.isFin()); // use original fin
-        payload.position(currentPosition);
-        payload.limit(originalLimit);
-        frag.setPayload(payload);
-
-        nextOutgoingFrame(frag,callback);
+        FrameEntry entry = new FrameEntry(frame, callback);
+        LOG.debug("Queuing {}", entry);
+        entries.offer(entry);
+        flusher.iterate();
     }
 
     @Override
     public void setConfig(ExtensionConfig config)
     {
         super.setConfig(config);
+        maxLength = config.getParameter("maxLength", -1);
+    }
 
-        maxLength = config.getParameter("maxLength",maxLength);
+    private static class FrameEntry
+    {
+        private final Frame frame;
+        private final WriteCallback callback;
+
+        private FrameEntry(Frame frame, WriteCallback callback)
+        {
+            this.frame = frame;
+            this.callback = callback;
+        }
+
+        @Override
+        public String toString()
+        {
+            return frame.toString();
+        }
+    }
+
+    private class Flusher extends IteratingCallback implements WriteCallback
+    {
+        private FrameEntry current;
+        private boolean finished = true;
+
+        @Override
+        protected Action process() throws Exception
+        {
+            if (finished)
+            {
+                current = entries.poll();
+                LOG.debug("Processing {}", current);
+                if (current == null)
+                    return Action.IDLE;
+                fragment(current, true);
+            }
+            else
+            {
+                fragment(current, false);
+            }
+            return Action.SCHEDULED;
+        }
+
+        private void fragment(FrameEntry entry, boolean first)
+        {
+            Frame frame = entry.frame;
+            ByteBuffer payload = frame.getPayload();
+            int remaining = payload.remaining();
+            int length = Math.min(remaining, maxLength);
+            finished = length == remaining;
+
+            boolean continuation = frame.getType().isContinuation() || !first;
+            DataFrame fragment = new DataFrame(frame, continuation);
+            boolean fin = frame.isFin() && finished;
+            fragment.setFin(fin);
+
+            int limit = payload.limit();
+            int newLimit = payload.position() + length;
+            payload.limit(newLimit);
+            ByteBuffer payloadFragment = payload.slice();
+            payload.limit(limit);
+            fragment.setPayload(payloadFragment);
+            LOG.debug("Fragmented {}->{}", frame, fragment);
+            payload.position(newLimit);
+
+            nextOutgoingFrame(fragment, this);
+        }
+
+        @Override
+        protected void completed()
+        {
+            // This IteratingCallback never completes.
+        }
+
+        @Override
+        public void writeSuccess()
+        {
+            // Notify first then call succeeded(), otherwise
+            // write callbacks may be invoked out of order.
+            notifyCallbackSuccess(current.callback);
+            succeeded();
+        }
+
+        @Override
+        public void writeFailed(Throwable x)
+        {
+            // Notify first, the call succeeded() to drain the queue.
+            // We don't want to call failed(x) because that will put
+            // this flusher into a final state that cannot be exited,
+            // and the failure of a frame may not mean that the whole
+            // connection is now invalid.
+            notifyCallbackFailure(current.callback, x);
+            succeeded();
+        }
+
+        private void notifyCallbackSuccess(WriteCallback callback)
+        {
+            try
+            {
+                if (callback != null)
+                    callback.writeSuccess();
+            }
+            catch (Throwable x)
+            {
+                LOG.debug("Exception while notifying success of callback " + callback, x);
+            }
+        }
+
+        private void notifyCallbackFailure(WriteCallback callback, Throwable failure)
+        {
+            try
+            {
+                if (callback != null)
+                    callback.writeFailed(failure);
+            }
+            catch (Throwable x)
+            {
+                LOG.debug("Exception while notifying failure of callback " + callback, x);
+            }
+        }
     }
 }
