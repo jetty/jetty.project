2d52280179a7d9f8b2449c49bcb0847e02115082
Greg Wilkins <gregw@intalio.com>
2014-07-17 14:29:58 +1000

    436987 limited range of default acceptors and selectors

--- jetty-server/src/main/java/org/eclipse/jetty/server/ServerConnector.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/ServerConnector.java
@@ -205,7 +205,8 @@ public class ServerConnector extends AbstractNetworkConnector
         @Name("factories") ConnectionFactory... factories)
     {
         super(server,executor,scheduler,bufferPool,acceptors,factories);
-        _manager = new ServerConnectorManager(getExecutor(), getScheduler(), selectors > 0 ? selectors : Runtime.getRuntime().availableProcessors());
+        _manager = new ServerConnectorManager(getExecutor(), getScheduler(), 
+            selectors>0?selectors:Math.max(1,Math.min(4,Runtime.getRuntime().availableProcessors()/2)));
         addBean(_manager, true);
     }
 
