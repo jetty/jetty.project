d60a61288a4932acbcd8e9b195ed008d419e25c5
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2014-09-18 15:10:59 -0700

    Restoring ErrorHandler / ErrorPageErrorHandler logic

--- jetty-servlet/src/main/java/org/eclipse/jetty/servlet/ServletHolder.java
+++ jetty-servlet/src/main/java/org/eclipse/jetty/servlet/ServletHolder.java
@@ -786,10 +786,6 @@ public class ServletHolder extends Holder<Servlet> implements UserIdentity.Scope
             makeUnavailable(e);
             throw _unavailableEx;
         }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         finally
         {
             baseRequest.setAsyncSupported(suspendable);
