d17613982bae95caef12b0736f141502de0a3363
Jesse McConnell <jesse.mcconnell@gmail.com>
2014-10-27 15:43:30 -0500

    [448761] ContextHandler returns incorrect servlet minor version

--- jetty-server/src/main/java/org/eclipse/jetty/server/handler/ContextHandler.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/handler/ContextHandler.java
@@ -105,7 +105,7 @@ import org.eclipse.jetty.util.resource.Resource;
 public class ContextHandler extends ScopedHandler implements Attributes, Graceful
 {
     public final static int SERVLET_MAJOR_VERSION=3;
-    public final static int SERVLET_MINOR_VERSION=0;
+    public final static int SERVLET_MINOR_VERSION=1;
     public static final Class<?>[] SERVLET_LISTENER_TYPES = new Class[] {ServletContextListener.class,
                                                                       ServletContextAttributeListener.class,
                                                                       ServletRequestListener.class,
