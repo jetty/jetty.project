733cc71791eefc3db26b2e40dc09a417177f4c47
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2014-10-29 15:31:14 -0700

    Reworking jetty.home warning to be a module instead.
    + This will cause it to be logged using the configured logger as well.

---
+++ jetty-server/src/main/java/org/eclipse/jetty/server/HomeBaseWarning.java
@@ -0,0 +1,78 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2014 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.server;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.eclipse.jetty.util.StringUtil;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
+
+/**
+ * Display an optional Warning Message if the {jetty.home} and {jetty.base} are the same directory.
+ * <p>
+ * This is to warn about not recommended approach to setting up the Jetty Distribution.
+ */
+public class HomeBaseWarning
+{
+    private static final Logger LOG = Log.getLogger(HomeBaseWarning.class);
+
+    public HomeBaseWarning()
+    {
+        boolean showWarn = false;
+        
+        String home = System.getProperty("jetty.home");
+        String base = System.getProperty("jetty.base");
+
+        if (StringUtil.isBlank(base))
+        {
+            // no base defined? then we are likely running
+            // via direct command line.
+            return;
+        }
+
+        Path homePath = new File(home).toPath();
+        Path basePath = new File(base).toPath();
+
+        try
+        {
+            showWarn = Files.isSameFile(homePath,basePath);
+        }
+        catch (IOException e)
+        {
+            LOG.ignore(e);
+            // Can't definitively determine this state
+            return;
+        }
+
+        if (showWarn)
+        {
+            StringBuilder warn = new StringBuilder();
+            warn.append("This instance of Jetty is running directly from {jetty.home} at ");
+            warn.append(homePath.toAbsolutePath().toString());
+            warn.append(", this is not recommended.").append(System.lineSeparator());
+            warn.append("Please setup a separate {jetty.base} directory to manage your configuration.").append(System.lineSeparator());
+            warn.append("See documentation at http://www.eclipse.org/jetty/documentation/current/startup.html");
+            LOG.warn("{}",warn.toString());
+        }
+    }
+}
