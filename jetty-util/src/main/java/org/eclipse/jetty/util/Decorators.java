fb88bc4c1921f4804d638d91d715f68a433d436f
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2014-11-21 12:01:32 -0700

    Making ServletContextHandler use util.Decorators
    + Deprecating methods that are just awkward now.
       ServletContextHandler.getDecorators() - as it expects the
         ServletContextHandler.Decorator version

--- jetty-util/src/main/java/org/eclipse/jetty/util/Decorators.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/Decorators.java
@@ -20,36 +20,51 @@ package org.eclipse.jetty.util;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * Represents a collection of {@link Decorator} instances that apply to a known
  * state, such as a WebAppContext, WebSocketServerFactory, or WebSocketClient.
  * <p>
- * Consistent single location for all Decorator behavior, with equal behavior in
- * a ServletContext and also for a stand alone client.
+ * Consistent single location for all Decorator behavior, with equal behavior in a ServletContext and also for a stand
+ * alone client.
  */
-public class Decorators
+public class Decorators implements Iterable<Decorator>
 {
     private List<Decorator> decorators = new ArrayList<>();
-
-    public List<Decorator> getDecorators()
-    {
-        return Collections.unmodifiableList(decorators);
-    }
-
-    public void setDecorators(List<Decorator> decorators)
-    {
-        this.decorators.clear();
-        if (decorators != null)
-        {
-            this.decorators.addAll(decorators);
-        }
-    }
 
     public void addDecorator(Decorator decorator)
     {
         this.decorators.add(decorator);
+    }
+
+    public void clear()
+    {
+        this.decorators.clear();
+    }
+
+    public <T> T createDecoratedInstance(Class<T> clazz) throws Exception
+    {
+        T o = clazz.newInstance();
+        return decorate(o);
+    }
+
+    public <T> T createInstance(Class<T> clazz) throws Exception
+    {
+        T o = clazz.newInstance();
+        return o;
+    }
+
+    public <T> T decorate(T obj)
+    {
+        T f = obj;
+        // Decorate is always backwards
+        for (int i = decorators.size() - 1; i >= 0; i--)
+        {
+            f = decorators.get(i).decorate(f);
+        }
+        return f;
     }
 
     public void destroy(Object obj)
@@ -60,19 +75,23 @@ public class Decorators
         }
     }
 
-    public <T> T decorate(T obj)
+    public List<Decorator> getDecorators()
     {
-        T f = obj;
-        for (Decorator decorator : this.decorators)
-        {
-            f = decorator.decorate(f);
-        }
-        return f;
+        return Collections.unmodifiableList(decorators);
     }
 
-    public <T> T createInstance(Class<T> clazz) throws Exception
+    @Override
+    public Iterator<Decorator> iterator()
     {
-        T o = clazz.newInstance();
-        return o;
+        return this.decorators.iterator();
+    }
+
+    public void setDecorators(List<? extends Decorator> decorators)
+    {
+        this.decorators.clear();
+        if (decorators != null)
+        {
+            this.decorators.addAll(decorators);
+        }
     }
 }
