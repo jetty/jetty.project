5a63d765f792d3130b9b7f962887c8b136d9cb94
Simone Bordet <simone.bordet@gmail.com>
2015-01-02 19:01:37 +0100

    Removed exceptions thrown for code that needed review.

--- jetty-spdy/spdy-http-client-transport/src/test/java/org/eclipse/jetty/spdy/client/http/HttpClientCustomProxyTest.java
+++ jetty-spdy/spdy-http-client-transport/src/test/java/org/eclipse/jetty/spdy/client/http/HttpClientCustomProxyTest.java
@@ -24,7 +24,6 @@ import java.nio.ByteBuffer;
 import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -172,7 +171,6 @@ public class HttpClientCustomProxyTest
             super(endPoint, executor);
             this.connectionFactory = connectionFactory;
             this.context = context;
-            throw new IllegalStateException("This was calling super dispatchIO=true. Needs to be reviewed");
         }
 
         @Override
@@ -231,8 +229,6 @@ public class HttpClientCustomProxyTest
         {
             super(endPoint, connector.getExecutor());
             this.connectionFactory = connectionFactory;
-
-            throw new IllegalStateException("This was calling super dispatchIO=true. Needs to be reviewed");
         }
 
         @Override
