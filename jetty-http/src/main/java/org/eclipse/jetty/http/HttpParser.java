19570a753065f804b14523392a60822507e5292d
Simone Bordet <simone.bordet@gmail.com>
2015-01-05 13:31:48 +0100

    Removed printStackTrace() call.

--- jetty-http/src/main/java/org/eclipse/jetty/http/HttpParser.java
+++ jetty-http/src/main/java/org/eclipse/jetty/http/HttpParser.java
@@ -1226,7 +1226,6 @@ public class HttpParser
         }
         catch(NumberFormatException|IllegalStateException e)
         {
-            e.printStackTrace();
             BufferUtil.clear(buffer);
             LOG.warn("parse exception: {} in {} for {}",e.toString(),_state,_handler);
             if (DEBUG)
