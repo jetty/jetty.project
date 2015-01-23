afdc522d204ced92b152f0ad1fd898f54b4be5df
Greg Wilkins <gregw@intalio.com>
2015-01-23 12:15:28 +0100

    fixed merge

--- jetty-http/src/test/java/org/eclipse/jetty/http/HttpParserTest.java
+++ jetty-http/src/test/java/org/eclipse/jetty/http/HttpParserTest.java
@@ -32,6 +32,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -82,8 +83,6 @@ public class HttpParserTest
         
         BufferUtil.append(b,BufferUtil.toBuffer(" "));
         assertEquals(HttpMethod.GET,HttpMethod.lookAheadGet(b));
-        
-        
     }
     
     
