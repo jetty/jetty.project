bb8b5f9b76d6decdd416da782209c1182568173a
Greg Wilkins <gregw@intalio.com>
2015-02-19 15:39:36 +1100

    better toString

--- jetty-io/src/main/java/org/eclipse/jetty/io/ManagedSelector.java
+++ jetty-io/src/main/java/org/eclipse/jetty/io/ManagedSelector.java
@@ -454,8 +454,9 @@ public class ManagedSelector extends AbstractLifeCycle implements Runnable, Dump
     public String toString()
     {
         Selector selector = _selector;
-        return String.format("%s keys=%d selected=%d",
+        return String.format("%s id=%s keys=%d selected=%d",
                 super.toString(),
+                _id,
                 selector != null && selector.isOpen() ? selector.keys().size() : -1,
                 selector != null && selector.isOpen() ? selector.selectedKeys().size() : -1);
     }
