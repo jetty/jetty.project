7c915bcba27d958febe18be658dab39e8fd1f063
Simone Bordet <simone.bordet@gmail.com>
2015-03-07 23:13:21 +0100

    461643 - HttpContent.advance() race.
    Fixed by correctly synchronizing on ContentProvider.iterator.hasNext()
    and ContentProvider.iterator.next() for those ContentProviders that
    require it, such as DeferredContentProvider.

---
+++ jetty-client/src/main/java/org/eclipse/jetty/client/Synchronizable.java
@@ -0,0 +1,45 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2015 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.client;
+
+/**
+ * <p>Implementations of this interface expose a lock object
+ * via {@link #getLock()} so that callers can synchronize
+ * externally on that lock:</p>
+ * <pre>
+ * if (iterator instanceof Synchronizable)
+ * {
+ *     Object element = null;
+ *     synchronized (((Synchronizable)iterator).getLock())
+ *     {
+ *         if (iterator.hasNext())
+ *             element = iterator.next();
+ *     }
+ * }
+ * </pre>
+ * <p>In the example above, the calls to {@code hasNext()} and
+ * {@code next()} are performed "atomically".</p>
+ */
+public interface Synchronizable
+{
+    /**
+     * @return the lock object to synchronize on
+     */
+    public Object getLock();
+}
--- jetty-proxy/src/test/java/org/eclipse/jetty/proxy/AsyncProxyServletLoadTest.java
+++ jetty-proxy/src/test/java/org/eclipse/jetty/proxy/AsyncProxyServletLoadTest.java
@@ -19,15 +19,11 @@
 package org.eclipse.jetty.proxy;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -38,8 +34,6 @@ import org.eclipse.jetty.client.HttpProxy;
 import org.eclipse.jetty.client.api.ContentResponse;
 import org.eclipse.jetty.client.util.BytesContentProvider;
 import org.eclipse.jetty.http.HttpMethod;
-import org.eclipse.jetty.proxy.AbstractProxyServlet;
-import org.eclipse.jetty.proxy.AsyncProxyServlet;
 import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
@@ -50,36 +44,37 @@ import org.eclipse.jetty.util.IO;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-public class Async502Loop
+@RunWith(Parameterized.class)
+public class AsyncProxyServletLoadTest
 {
-    private static final Logger LOG = Log.getLogger(Async502Loop.class);
-    private static final String PROXIED_HEADER = "X-Proxied";
-
-    public static void main(String[] args)
+    @Parameterized.Parameters(name = "{0}")
+    public static Iterable<Object[]> data()
     {
-        try
-        {
-            new Async502Loop().loop();
-        }
-        catch (Throwable t)
-        {
-            t.printStackTrace(System.err);
-        }
+        return Arrays.asList(new Object[][]{
+                {AsyncProxyServlet.class},
+                {AsyncMiddleManServlet.class}
+        });
     }
+
+    private static final Logger LOG = Log.getLogger(AsyncProxyServletLoadTest.class);
+    private static final String PROXIED_HEADER = "X-Proxied";
 
     private HttpClient client;
     private Server proxy;
     private ServerConnector proxyConnector;
-    private ServletContextHandler proxyContext;
     private AbstractProxyServlet proxyServlet;
     private Server server;
     private ServerConnector serverConnector;
 
-    public Async502Loop()
+    public AsyncProxyServletLoadTest(Class<?> proxyServletClass) throws Exception
     {
-        proxyServlet = new AsyncProxyServlet();
-        // proxyServlet = new AsyncMiddleManServlet();
+        proxyServlet = (AbstractProxyServlet)proxyServletClass.newInstance();
     }
 
     private void startServer(HttpServlet servlet) throws Exception
@@ -90,19 +85,14 @@ public class Async502Loop
         serverConnector = new ServerConnector(server);
         server.addConnector(serverConnector);
 
-        ServletContextHandler appCtx = new ServletContextHandler(server,"/",true,false);
+        ServletContextHandler appCtx = new ServletContextHandler(server, "/", true, false);
         ServletHolder appServletHolder = new ServletHolder(servlet);
-        appCtx.addServlet(appServletHolder,"/*");
+        appCtx.addServlet(appServletHolder, "/*");
 
         server.start();
     }
 
     private void startProxy() throws Exception
-    {
-        startProxy(new HashMap<String, String>());
-    }
-
-    private void startProxy(Map<String, String> initParams) throws Exception
     {
         QueuedThreadPool proxyPool = new QueuedThreadPool();
         proxyPool.setName("proxy");
@@ -111,36 +101,28 @@ public class Async502Loop
         HttpConfiguration configuration = new HttpConfiguration();
         configuration.setSendDateHeader(false);
         configuration.setSendServerVersion(false);
-        String value = initParams.get("outputBufferSize");
-        if (value != null)
-            configuration.setOutputBufferSize(Integer.valueOf(value));
-        proxyConnector = new ServerConnector(proxy,new HttpConnectionFactory(configuration));
+        proxyConnector = new ServerConnector(proxy, new HttpConnectionFactory(configuration));
         proxy.addConnector(proxyConnector);
 
-        proxyContext = new ServletContextHandler(proxy,"/",true,false);
+        ServletContextHandler proxyContext = new ServletContextHandler(proxy, "/", true, false);
         ServletHolder proxyServletHolder = new ServletHolder(proxyServlet);
-        proxyServletHolder.setInitParameters(initParams);
-        proxyContext.addServlet(proxyServletHolder,"/*");
+        proxyContext.addServlet(proxyServletHolder, "/*");
 
         proxy.start();
     }
 
     private void startClient() throws Exception
     {
-        client = prepareClient();
-    }
-
-    private HttpClient prepareClient() throws Exception
-    {
         QueuedThreadPool clientPool = new QueuedThreadPool();
         clientPool.setName("client");
         HttpClient result = new HttpClient();
         result.setExecutor(clientPool);
-        result.getProxyConfiguration().getProxies().add(new HttpProxy("localhost",proxyConnector.getLocalPort()));
+        result.getProxyConfiguration().getProxies().add(new HttpProxy("localhost", proxyConnector.getLocalPort()));
         result.start();
-        return result;
+        client = result;
     }
 
+    @After
     public void dispose() throws Exception
     {
         client.stop();
@@ -148,60 +130,8 @@ public class Async502Loop
         server.stop();
     }
 
-    private static class ClientLoop implements Runnable
-    {
-        private final CountDownLatch active;
-        private final AtomicBoolean ok;
-        private final HttpClient client;
-        private final String host;
-        private final int port;
-
-        public ClientLoop(CountDownLatch activeClientLatch, AtomicBoolean atomicOk, HttpClient client, String serverHost, int serverPort)
-        {
-            this.active = activeClientLatch;
-            this.ok = atomicOk;
-            this.client = client;
-            this.host = serverHost;
-            this.port = serverPort;
-        }
-
-        @Override
-        public void run()
-        {
-            String threadName = Thread.currentThread().getName();
-            LOG.info("Starting thread {}",threadName);
-            try
-            {
-                while (ok.get())
-                {
-                    byte[] content = new byte[1024];
-                    new Random().nextBytes(content);
-                    ContentResponse response = client.newRequest(host,port).method(HttpMethod.POST).content(new BytesContentProvider(content))
-                            .timeout(5,TimeUnit.SECONDS).send();
-
-                    if (response.getStatus() != 200)
-                    {
-                        LOG.warn("Got response <{}>, expecting <{}>",response.getStatus(),200);
-                        // allow all ClientLoops to finish
-                        ok.set(false);
-                    }
-                }
-            }
-            catch (InterruptedException | TimeoutException | ExecutionException e)
-            {
-                LOG.warn("Error processing request",e);
-                ok.set(false);
-            }
-            finally
-            {
-                LOG.info("Shutting down thread {}",threadName);
-                active.countDown();
-            }
-        }
-    }
-
-    @SuppressWarnings("serial")
-    private void loop() throws Exception
+    @Test
+    public void test() throws Exception
     {
         startServer(new HttpServlet()
         {
@@ -209,33 +139,98 @@ public class Async502Loop
             protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
             {
                 if (req.getHeader("Via") != null)
-                    resp.addHeader(PROXIED_HEADER,"true");
-                IO.copy(req.getInputStream(),resp.getOutputStream());
+                    resp.addHeader(PROXIED_HEADER, "true");
+                IO.copy(req.getInputStream(), resp.getOutputStream());
             }
         });
         startProxy();
         startClient();
 
         // Number of clients to simulate
-        int clientCount = 5;
+        int clientCount = Runtime.getRuntime().availableProcessors();
+
         // Latch for number of clients still active (used to terminate test)
         final CountDownLatch activeClientLatch = new CountDownLatch(clientCount);
+
         // Atomic Boolean to track that its OK to still continue looping.
         // When this goes false, that means one of the client threads has
         // encountered an error condition, and should allow all remaining
         // client threads to finish cleanly.
-        final AtomicBoolean atomicOk = new AtomicBoolean(true);
+        final AtomicBoolean success = new AtomicBoolean(true);
+
+        int iterations = 1000;
 
         // Start clients
         for (int i = 0; i < clientCount; i++)
         {
-            ClientLoop r = new ClientLoop(activeClientLatch,atomicOk,client,"localhost",serverConnector.getLocalPort());
+            ClientLoop r = new ClientLoop(activeClientLatch, success, client, "localhost", serverConnector.getLocalPort(), iterations);
             String name = "client-" + i;
-            Thread thread = new Thread(r,name);
+            Thread thread = new Thread(r, name);
             thread.start();
         }
 
-        activeClientLatch.await();
-        dispose();
+        Assert.assertTrue(activeClientLatch.await(clientCount * iterations * 10, TimeUnit.MILLISECONDS));
+        Assert.assertTrue(success.get());
+    }
+
+    private static class ClientLoop implements Runnable
+    {
+        private final CountDownLatch active;
+        private final AtomicBoolean success;
+        private final HttpClient client;
+        private final String host;
+        private final int port;
+        private int iterations;
+
+        public ClientLoop(CountDownLatch activeClientLatch, AtomicBoolean success, HttpClient client, String serverHost, int serverPort, int iterations)
+        {
+            this.active = activeClientLatch;
+            this.success = success;
+            this.client = client;
+            this.host = serverHost;
+            this.port = serverPort;
+            this.iterations = iterations;
+        }
+
+        @Override
+        public void run()
+        {
+            String threadName = Thread.currentThread().getName();
+            LOG.info("Starting thread {}", threadName);
+            try
+            {
+                while (success.get())
+                {
+                    --iterations;
+
+                    byte[] content = new byte[1024];
+                    new Random().nextBytes(content);
+                    ContentResponse response = client.newRequest(host, port).method(HttpMethod.POST).content(new BytesContentProvider(content))
+                            .timeout(5, TimeUnit.SECONDS).send();
+
+                    if (response.getStatus() != 200)
+                    {
+                        LOG.warn("Got response <{}>, expecting <{}>", response.getStatus(), 200);
+                        // allow all ClientLoops to finish
+                        success.set(false);
+                    }
+                    else
+                    {
+                        if (iterations == 0)
+                            break;
+                    }
+                }
+            }
+            catch (Throwable x)
+            {
+                LOG.warn("Error processing request", x);
+                success.set(false);
+            }
+            finally
+            {
+                LOG.info("Shutting down thread {}", threadName);
+                active.countDown();
+            }
+        }
     }
 }
