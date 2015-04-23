184c4d4af830a95d93f5de3fd57139c96db5be1a
Jan Bartel <janb@intalio.com>
2015-04-23 14:25:34 +1000

    464564 NoSql sessions created inside a forward not persisted correctly

--- jetty-server/src/main/java/org/eclipse/jetty/server/session/SessionHandler.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/session/SessionHandler.java
@@ -158,7 +158,7 @@ public class SessionHandler extends ScopedHandler
                 session = baseRequest.getSession(false);
                 if (session != null)
                 {
-                    if (session != old_session)
+                    if ((session != old_session) && (request.getDispatcherType() == DispatcherType.ASYNC || request.getDispatcherType() == DispatcherType.REQUEST))
                     {
                         access = session;
                         HttpCookie cookie = _sessionManager.access(session,request.isSecure());
@@ -192,11 +192,14 @@ public class SessionHandler extends ScopedHandler
         }
         finally
         {
+            //if we accessed an existing session entering this context, then complete it
             if (access != null)
                 _sessionManager.complete(access);
 
+            
+            //if there is a session that was created during handling this context, then complete it
             HttpSession session = baseRequest.getSession(false);
-            if (session != null && old_session == null && session != access)
+            if ((session != null && old_session == null && session != access) && (request.getDispatcherType() == DispatcherType.ASYNC || request.getDispatcherType() == DispatcherType.REQUEST))
                 _sessionManager.complete(session);
 
             if (old_session_manager != null && old_session_manager != _sessionManager)
