7e7b2e2950a8edc304b9ee9a3b555f0f2c56f320
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2015-05-06 10:15:48 -0700

    420678 - Add WebSocketPartialListener to support receiving partial WebSocket TEXT/BINARY messages
    + Reworking WebSocketListener to be based on new
      WebSocketConnectionListener
    + Adding new WebSocketPartialListener and example

--- jetty-websocket/websocket-api/src/main/java/org/eclipse/jetty/websocket/api/WebSocketListener.java
+++ jetty-websocket/websocket-api/src/main/java/org/eclipse/jetty/websocket/api/WebSocketListener.java
@@ -19,9 +19,9 @@
 package org.eclipse.jetty.websocket.api;
 
 /**
- * Basic WebSocket Listener interface for incoming WebSocket events.
+ * Basic WebSocket Listener interface for incoming WebSocket message events.
  */
-public interface WebSocketListener
+public interface WebSocketListener extends WebSocketConnectionListener
 {
     /**
      * A WebSocket binary frame has been received.
@@ -34,42 +34,6 @@ public interface WebSocketListener
      *            the length of bytes in the payload
      */
     void onWebSocketBinary(byte payload[], int offset, int len);
-
-    /**
-     * A Close Event was received.
-     * <p>
-     * The underlying Connection will be considered closed at this point.
-     * 
-     * @param statusCode
-     *            the close status code. (See {@link StatusCode})
-     * @param reason
-     *            the optional reason for the close.
-     */
-    void onWebSocketClose(int statusCode, String reason);
-
-    /**
-     * A WebSocket {@link Session} has connected successfully and is ready to be used.
-     * <p>
-     * Note: It is a good idea to track this session as a field in your object so that you can write messages back via the {@link RemoteEndpoint}
-     * 
-     * @param session
-     *            the websocket session.
-     */
-    void onWebSocketConnect(Session session);
-
-    /**
-     * A WebSocket exception has occurred.
-     * <p>
-     * This is a way for the internal implementation to notify of exceptions occured during the processing of websocket.
-     * <p>
-     * Usually this occurs from bad / malformed incoming packets. (example: bad UTF8 data, frames that are too big, violations of the spec)
-     * <p>
-     * This will result in the {@link Session} being closed by the implementing side.
-     * 
-     * @param cause
-     *            the error that occurred.
-     */
-    void onWebSocketError(Throwable cause);
 
     /**
      * A WebSocket Text frame was received.
