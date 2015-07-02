a9dd6ec676f67c91108e0a7dcb63514966aab267
Greg Wilkins <gregw@webtide.com>
2015-07-02 17:24:37 +1000

    fixed header

--- jetty-server/src/main/java/org/eclipse/jetty/server/RequestLogCollection.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/RequestLogCollection.java
@@ -1,7 +1,7 @@
 //
 //  ========================================================================
 //  Copyright (c) 1995-2015 Mort Bay Consulting Pty. Ltd.
-//  ========================================================================
+//  ------------------------------------------------------------------------
 //  All rights reserved. This program and the accompanying materials
 //  are made available under the terms of the Eclipse Public License v1.0
 //  and Apache License v2.0 which accompanies this distribution.
