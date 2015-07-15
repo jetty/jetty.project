c5b36c6353d113b925fccf78f8d1972e4e44546b
Greg Wilkins <gregw@webtide.com>
2015-07-16 09:08:00 +1000

    Bug 472601: org.eclipse.jetty.util.log.Log.setLog(Logger) fixed

--- jetty-util/src/main/java/org/eclipse/jetty/util/log/Log.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/log/Log.java
@@ -169,8 +169,8 @@ public class Log
 
             try
             {
-                Class<?> log_class = Loader.loadClass(Log.class, __logClass);
-                if (LOG == null || !LOG.getClass().equals(log_class))
+                Class<?> log_class = __logClass==null?null:Loader.loadClass(Log.class, __logClass);
+                if (LOG == null || (log_class!=null && !LOG.getClass().equals(log_class)))
                 {
                     LOG = (Logger)log_class.newInstance();
                     LOG.debug("Logging to {} via {}", LOG, log_class.getName());
@@ -212,6 +212,7 @@ public class Log
     public static void setLog(Logger log)
     {
         Log.LOG = log;
+        __logClass=null;
     }
 
     /**
