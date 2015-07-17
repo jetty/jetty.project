54995452d6c1e433abac347da800034ca22feda2
Greg Wilkins <gregw@webtide.com>
2015-07-17 23:50:39 +1000

    472931 - HttpConfiguration copy constructor incomplete

--- jetty-server/src/main/java/org/eclipse/jetty/server/HttpConfiguration.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/HttpConfiguration.java
@@ -103,16 +103,21 @@ public class HttpConfiguration
     public HttpConfiguration(HttpConfiguration config)
     {
         _customizers.addAll(config._customizers);
+        for (String s:config._formEncodedMethods.keySet())
+            _formEncodedMethods.put(s,Boolean.TRUE);
         _outputBufferSize=config._outputBufferSize;
         _outputAggregationSize=config._outputAggregationSize;
         _requestHeaderSize=config._requestHeaderSize;
         _responseHeaderSize=config._responseHeaderSize;
-        _securePort=config._securePort;
+        _headerCacheSize=config._headerCacheSize;
         _secureScheme=config._secureScheme;
+        _securePort=config._securePort;
+        _blockingTimeout=config._blockingTimeout;
         _sendDateHeader=config._sendDateHeader;
         _sendServerVersion=config._sendServerVersion;
-        _headerCacheSize=config._headerCacheSize;
-        _blockingTimeout=config._blockingTimeout;
+        _sendXPoweredBy=config._sendXPoweredBy;
+        _delayDispatchUntilContent=config._delayDispatchUntilContent;
+        _persistentConnectionsEnabled=config._persistentConnectionsEnabled;
     }
     
     /* ------------------------------------------------------------ */
