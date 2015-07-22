7bf6cec74e6d1eb1f7caff628b5d59b5047384b2
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2015-07-22 13:29:10 -0700

    473309 - Add special (non-replacement) Terminating rules to jetty-rewrite
    + Adding TerminatingPatternRule (and test)
    + Adding TerminatingRegexRule (and test)

---
+++ jetty-rewrite/src/main/java/org/eclipse/jetty/rewrite/handler/TerminatingPatternRule.java
@@ -0,0 +1,51 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2015 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.rewrite.handler;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * If this rule matches, terminate the processing of other rules.
+ * Allowing the request to be processed by the handlers after the rewrite rules.
+ */
+public class TerminatingPatternRule extends PatternRule
+{
+    public TerminatingPatternRule()
+    {
+        super.setTerminating(true);
+    }
+
+    @Override
+    public void setTerminating(boolean terminating)
+    {
+        if (!terminating)
+        {
+            throw new RuntimeException("Not allowed to disable terminating on a " + TerminatingPatternRule.class.getName());
+        }
+    }
+
+    @Override
+    protected String apply(String target, HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        return target;
+    }
+}
---
+++ jetty-rewrite/src/main/java/org/eclipse/jetty/rewrite/handler/TerminatingRegexRule.java
@@ -0,0 +1,52 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2015 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.rewrite.handler;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * If this rule matches, terminate the processing of other rules.
+ * Allowing the request to be processed by the handlers after the rewrite rules.
+ */
+public class TerminatingRegexRule extends RegexRule
+{
+    public TerminatingRegexRule()
+    {
+        super.setTerminating(true);
+    }
+
+    @Override
+    public void setTerminating(boolean terminating)
+    {
+        if (!terminating)
+        {
+            throw new RuntimeException("Not allowed to disable terminating on a " + TerminatingRegexRule.class.getName());
+        }
+    }
+
+    @Override
+    public String apply(String target, HttpServletRequest request, HttpServletResponse response, Matcher matcher) throws IOException
+    {
+        return target;
+    }
+}
