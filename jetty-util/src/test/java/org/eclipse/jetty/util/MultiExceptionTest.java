30a99ddcf403e0dc20e2d723b6cc71359691c517
Greg Wilkins <gregw@webtide.com>
2015-07-23 09:02:52 +1000

    473266 - init MultiException cause

--- jetty-util/src/test/java/org/eclipse/jetty/util/MultiExceptionTest.java
+++ jetty-util/src/test/java/org/eclipse/jetty/util/MultiExceptionTest.java
@@ -148,4 +148,17 @@ public class MultiExceptionTest
             assertTrue(e.getCause()==me);
         }
     }
+    
+    @Test
+    public void testCause() throws Exception
+    {
+        MultiException me = new MultiException();
+        IOException io = new IOException("one");
+        RuntimeException run = new RuntimeException("two");
+        me.add(io);
+        me.add(run);
+
+        assertEquals(2,me.size());
+        assertEquals(io,me.getCause());        
+    }
 }
