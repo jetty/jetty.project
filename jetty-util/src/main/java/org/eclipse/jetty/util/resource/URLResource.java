232b4a46803ae2efbceafb01699e51d70390ecb8
Greg Wilkins <gregw@webtide.com>
2015-07-22 13:01:21 +1000

    473006 - Encode addPath in URLResource

--- jetty-util/src/main/java/org/eclipse/jetty/util/resource/URLResource.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/resource/URLResource.java
@@ -301,7 +301,7 @@ public class URLResource extends Resource
 
         path = URIUtil.canonicalPath(path);
 
-        return newResource(URIUtil.addPaths(_url.toExternalForm(),path), _useCaches);
+        return newResource(URIUtil.addPaths(_url.toExternalForm(),URIUtil.encodePath(path)), _useCaches);
     }
 
     /* ------------------------------------------------------------ */
