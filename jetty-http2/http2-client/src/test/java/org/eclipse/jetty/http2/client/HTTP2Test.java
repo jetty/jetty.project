25ed10d205619dd7b7fbf7cd9a3f37281adff38b
Simone Bordet <simone.bordet@gmail.com>
2015-07-28 12:26:53 +0200

    473118 - HTTP/2 server does not retrieve Host header from client.

--- jetty-http2/http2-client/src/test/java/org/eclipse/jetty/http2/client/HTTP2Test.java
+++ jetty-http2/http2-client/src/test/java/org/eclipse/jetty/http2/client/HTTP2Test.java
@@ -30,7 +30,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jetty.http.HostPortHttpField;
 import org.eclipse.jetty.http.HttpFields;
+import org.eclipse.jetty.http.HttpScheme;
+import org.eclipse.jetty.http.HttpVersion;
 import org.eclipse.jetty.http.MetaData;
 import org.eclipse.jetty.http2.api.Session;
 import org.eclipse.jetty.http2.api.Stream;
@@ -192,6 +195,43 @@ public class HTTP2Test extends AbstractTest
             {
                 MetaData.Response response = (MetaData.Response)frame.getMetaData();
                 Assert.assertEquals(status, response.getStatus());
+                if (frame.isEndStream())
+                    latch.countDown();
+            }
+        });
+
+        Assert.assertTrue(latch.await(5, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void testHostHeader() throws Exception
+    {
+        final String host = "fooBar";
+        final int port = 1313;
+        final String authority = host + ":" + port;
+        start(new HttpServlet()
+        {
+            @Override
+            protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+            {
+                Assert.assertEquals(host, request.getServerName());
+                Assert.assertEquals(port, request.getServerPort());
+                Assert.assertEquals(authority, request.getHeader("Host"));
+            }
+        });
+
+        Session session = newClient(new Session.Listener.Adapter());
+        HostPortHttpField hostHeader = new HostPortHttpField(authority);
+        MetaData.Request metaData = new MetaData.Request("GET", HttpScheme.HTTP, hostHeader, servletPath, HttpVersion.HTTP_2, new HttpFields());
+        HeadersFrame frame = new HeadersFrame(1, metaData, null, true);
+        final CountDownLatch latch = new CountDownLatch(1);
+        session.newStream(frame, new Promise.Adapter<>(), new Stream.Listener.Adapter()
+        {
+            @Override
+            public void onHeaders(Stream stream, HeadersFrame frame)
+            {
+                MetaData.Response response = (MetaData.Response)frame.getMetaData();
+                Assert.assertEquals(200, response.getStatus());
                 if (frame.isEndStream())
                     latch.countDown();
             }
