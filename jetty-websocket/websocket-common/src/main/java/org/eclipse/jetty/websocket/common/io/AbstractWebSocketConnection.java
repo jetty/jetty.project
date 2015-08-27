9c855bee76188982d2b58aab90086ab22c153f02
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2015-08-26 17:07:01 -0700

    474936 - WebSocketSessions are not always cleaned out from openSessions
    + Adding testcase
    + Enabling Connection.onClose() -> ioState.onDisconnected()
    
    Conflicts:
    	jetty-websocket/websocket-server/src/test/java/org/eclipse/jetty/websocket/server/WebSocketCloseTest.java

--- jetty-websocket/websocket-common/src/main/java/org/eclipse/jetty/websocket/common/io/AbstractWebSocketConnection.java
+++ jetty-websocket/websocket-common/src/main/java/org/eclipse/jetty/websocket/common/io/AbstractWebSocketConnection.java
@@ -412,7 +412,7 @@ public abstract class AbstractWebSocketConnection extends AbstractConnection imp
         if (LOG.isDebugEnabled())
             LOG.debug("{} onClose()",policy.getBehavior());
         super.onClose();
-        // ioState.onDisconnected();
+        ioState.onDisconnected();
         flusher.close();
     }
 
