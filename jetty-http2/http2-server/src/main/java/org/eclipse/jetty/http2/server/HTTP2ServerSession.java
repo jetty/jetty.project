ccbd626cb2b8b691609aded7e9342116935bf8c9
Simone Bordet <simone.bordet@gmail.com>
2015-10-12 17:50:09 +0200

    479537 - Server preface sent after client preface reply.
    Fixed by anticipating the onPreface() callback on server before
    processing the client preface's SETTINGS frame.

--- jetty-http2/http2-server/src/main/java/org/eclipse/jetty/http2/server/HTTP2ServerSession.java
+++ jetty-http2/http2-server/src/main/java/org/eclipse/jetty/http2/server/HTTP2ServerSession.java
@@ -112,6 +112,9 @@ public class HTTP2ServerSession extends HTTP2Session implements ServerParser.Lis
     {
         switch (frame.getType())
         {
+            case PREFACE:
+                onPreface();
+                break;
             case SETTINGS:
                 // SPEC: the required reply to this SETTINGS frame is the 101 response.
                 onSettings((SettingsFrame)frame, false);
