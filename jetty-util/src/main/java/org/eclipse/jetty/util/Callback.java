8ed67c009fd02bfb2bccb63fc106ce355de85179
Simone Bordet <simone.bordet@gmail.com>
2015-11-09 12:35:58 +0100

    Removed usages of deprecated Callback.Adapter class.

--- jetty-util/src/main/java/org/eclipse/jetty/util/Callback.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/Callback.java
@@ -37,7 +37,7 @@ package org.eclipse.jetty.util;
 /**
  * <p>A callback abstraction that handles completed/failed events of asynchronous operations.</p>
  *
- * <p>Semantically this is equivalent to an optimise Promise&lt;Void&gt;, but callback is a more meaningful 
+ * <p>Semantically this is equivalent to an optimise Promise&lt;Void&gt;, but callback is a more meaningful
  * name than EmptyPromise</p>
  */
 public interface Callback
@@ -46,8 +46,7 @@ public interface Callback
      * Instance of Adapter that can be used when the callback methods need an empty
      * implementation without incurring in the cost of allocating a new Adapter object.
      */
-    static Callback NOOP = new Callback(){};
-
+    Callback NOOP = new Callback(){};
 
     /**
      * <p>Callback invoked when the operation completes.</p>
@@ -71,25 +70,16 @@ public interface Callback
     {
         return false;
     }
-    
-    
+
     /**
      * Callback interface that declares itself as non-blocking
      */
     interface NonBlocking extends Callback
     {
         @Override
-        public default boolean isNonBlocking()
+        default boolean isNonBlocking()
         {
             return true;
         }
     }
-    
-    
-    /**
-     * <p>Empty implementation of {@link Callback}</p>
-     */
-    @Deprecated
-    static class Adapter implements Callback
-    {}
 }
