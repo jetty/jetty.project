6978eeb5c4fc5d7194d9554cc6497c203d377e77
Greg Wilkins <gregw@webtide.com>
2015-12-02 12:41:51 +1100

    483422 - Empty chunked body in 304 Response
    test harness only

--- jetty-server/src/test/java/org/eclipse/jetty/server/AbstractHttpTest.java
+++ jetty-server/src/test/java/org/eclipse/jetty/server/AbstractHttpTest.java
@@ -26,6 +26,10 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -46,6 +50,8 @@ import static org.junit.Assert.assertThat;
 
 public abstract class AbstractHttpTest
 {
+    private final static Set<String> __noBodyCodes = new HashSet<>(Arrays.asList(new String[]{"100","101","102","204","304"}));
+    
     @Rule
     public TestTracker tracker = new TestTracker();
 
@@ -91,8 +97,10 @@ public abstract class AbstractHttpTest
         writer.flush();
 
         SimpleHttpResponse response = httpParser.readResponse(reader);
-        if ("HTTP/1.1".equals(httpVersion) && response.getHeaders().get("content-length") == null && response
-                .getHeaders().get("transfer-encoding") == null)
+        if ("HTTP/1.1".equals(httpVersion) 
+            && response.getHeaders().get("content-length") == null 
+            && response.getHeaders().get("transfer-encoding") == null
+            && !__noBodyCodes.contains(response.getCode()))
             assertThat("If HTTP/1.1 response doesn't contain transfer-encoding or content-length headers, " +
                     "it should contain connection:close", response.getHeaders().get("connection"), is("close"));
         return response;
