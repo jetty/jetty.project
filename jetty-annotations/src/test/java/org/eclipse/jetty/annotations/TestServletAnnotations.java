66e596511dba86225e30281cfa2e920c646b6fcc
Jan Bartel <janb@webtide.com>
2015-12-04 15:58:07 +1100

    483620 Servlet annotation mapping to "/" should override webdefault.xml mapping

--- jetty-annotations/src/test/java/org/eclipse/jetty/annotations/TestServletAnnotations.java
+++ jetty-annotations/src/test/java/org/eclipse/jetty/annotations/TestServletAnnotations.java
@@ -107,7 +107,196 @@ public class TestServletAnnotations
         assertNotNull(paths);
         assertEquals(2, paths.length);
     }
+    
+    
+    @Test
+    public void testWebServletAnnotationOverrideDefault () throws Exception
+    {
+        //if the existing servlet mapping TO A DIFFERENT SERVLET IS from a default descriptor we
+        //DO allow the annotation to replace the mapping.
+        
+        WebAppContext wac = new WebAppContext();
+        ServletHolder defaultServlet = new ServletHolder();
+        defaultServlet.setClassName("org.eclipse.jetty.servlet.DefaultServlet");
+        defaultServlet.setName("default");      
+        wac.getServletHandler().addServlet(defaultServlet);
+        
+        ServletMapping m = new ServletMapping();
+        m.setPathSpec("/");
+        m.setServletName("default");
+        m.setDefault(true);  //this mapping will be from a default descriptor
+        wac.getServletHandler().addServletMapping(m);
+        
+        WebServletAnnotation annotation = new WebServletAnnotation(wac, "org.eclipse.jetty.annotations.ServletD", null);
+        annotation.apply();
+        
+        //test that as the original servlet mapping had only 1 pathspec, then the whole
+        //servlet mapping should be deleted as that pathspec will be remapped to the DServlet
+        ServletMapping[] resultMappings = wac.getServletHandler().getServletMappings();
+        assertNotNull(resultMappings);    
+        assertEquals(1, resultMappings.length);
+        assertEquals(2, resultMappings[0].getPathSpecs().length);
+        resultMappings[0].getServletName().equals("DServlet");
+        for (String s:resultMappings[0].getPathSpecs())
+        {
+            assertTrue (s.equals("/") || s.equals("/bah/*"));
+        }
+    }
+    
+    
+    
+    @Test
+    public void testWebServletAnnotationReplaceDefault () throws Exception
+    {
+        //if the existing servlet mapping TO A DIFFERENT SERVLET IS from a default descriptor we
+        //DO allow the annotation to replace the mapping.    
+        WebAppContext wac = new WebAppContext();
+        ServletHolder defaultServlet = new ServletHolder();
+        defaultServlet.setClassName("org.eclipse.jetty.servlet.DefaultServlet");
+        defaultServlet.setName("default");      
+        wac.getServletHandler().addServlet(defaultServlet);
+        
+        ServletMapping m = new ServletMapping();
+        m.setPathSpec("/");
+        m.setServletName("default");
+        m.setDefault(true);  //this mapping will be from a default descriptor
+        wac.getServletHandler().addServletMapping(m);
+        
+        ServletMapping m2 = new ServletMapping();
+        m2.setPathSpec("/other");
+        m2.setServletName("default");
+        m2.setDefault(true);  //this mapping will be from a default descriptor
+        wac.getServletHandler().addServletMapping(m2);
+        
+        WebServletAnnotation annotation = new WebServletAnnotation(wac, "org.eclipse.jetty.annotations.ServletD", null);
+        annotation.apply();
+        
+        //test that only the mapping for "/" was removed from the mappings to the default servlet
+        ServletMapping[] resultMappings = wac.getServletHandler().getServletMappings();
+        assertNotNull(resultMappings);    
+        assertEquals(2, resultMappings.length);
+        for (ServletMapping r:resultMappings)
+        {
+           if (r.getServletName().equals("default"))
+           {
+               assertEquals(1,r.getPathSpecs().length);
+               assertEquals("/other", r.getPathSpecs()[0]);
+           }
+           else if (r.getServletName().equals("DServlet"))
+           {
+               assertEquals(2,r.getPathSpecs().length);
+               for (String p:r.getPathSpecs())
+               {
+                   if (!p.equals("/") && !p.equals("/bah/*"))
+                       fail("Unexpected path");
+               }
+           }
+           else
+               fail("Unexpected servlet mapping");
+        }
+        
+    }
+    
+    
+    @Test
+    public void testWebServletAnnotationNotOverride () throws Exception
+    {
+        //if the existing servlet mapping TO A DIFFERENT SERVLET IS NOT from a default descriptor we
+        //DO NOT allow the annotation to replace the mapping
+        WebAppContext wac = new WebAppContext();
+        ServletHolder servlet = new ServletHolder();
+        servlet.setClassName("org.eclipse.jetty.servlet.FooServlet");
+        servlet.setName("foo");
+        wac.getServletHandler().addServlet(servlet);
+        ServletMapping m = new ServletMapping();
+        m.setPathSpec("/");
+        m.setServletName("foo");
+        wac.getServletHandler().addServletMapping(m);
 
+        WebServletAnnotation annotation = new WebServletAnnotation(wac, "org.eclipse.jetty.annotations.ServletD", null);
+        annotation.apply();
+
+        ServletMapping[] resultMappings = wac.getServletHandler().getServletMappings();
+        assertEquals(2, resultMappings.length);
+        for (ServletMapping r:resultMappings)
+        {
+            if (r.getServletName().equals("DServlet"))
+            {
+                assertEquals(2, r.getPathSpecs().length);
+            }
+            else if (r.getServletName().equals("foo"))
+            {
+                assertEquals(1, r.getPathSpecs().length);
+            }
+            else
+                fail("Unexpected servlet name");
+        }
+    }
+    
+    @Test
+    public void testWebServletAnnotationIgnore () throws Exception
+    {
+        //an existing servlet OF THE SAME NAME has even 1 non-default mapping we can't use
+        //any of the url mappings in the annotation
+        WebAppContext wac = new WebAppContext();
+        ServletHolder servlet = new ServletHolder();
+        servlet.setClassName("org.eclipse.jetty.servlet.OtherDServlet");
+        servlet.setName("DServlet");
+        wac.getServletHandler().addServlet(servlet);
+        
+        ServletMapping m = new ServletMapping();
+        m.setPathSpec("/default");
+        m.setDefault(true);
+        m.setServletName("DServlet");
+        wac.getServletHandler().addServletMapping(m);
+        
+        ServletMapping m2 = new ServletMapping();
+        m2.setPathSpec("/other");
+        m2.setServletName("DServlet");
+        wac.getServletHandler().addServletMapping(m2);
+        
+        WebServletAnnotation annotation = new WebServletAnnotation(wac, "org.eclipse.jetty.annotations.ServletD", null);
+        annotation.apply();
+
+        ServletMapping[] resultMappings = wac.getServletHandler().getServletMappings();
+        assertEquals(2, resultMappings.length);
+        
+        for (ServletMapping r:resultMappings)
+        {
+            assertEquals(1, r.getPathSpecs().length);
+            if (!r.getPathSpecs()[0].equals("/default") && !r.getPathSpecs()[0].equals("/other"))
+                fail("Unexpected path in mapping");
+        }
+        
+    }
+    
+    @Test
+    public void testWebServletAnnotationNoMappings () throws Exception
+    {
+        //an existing servlet OF THE SAME NAME has no mappings, therefore all mappings in the annotation
+        //should be accepted
+        WebAppContext wac = new WebAppContext();
+        ServletHolder servlet = new ServletHolder();
+        servlet.setName("foo");
+        wac.getServletHandler().addServlet(servlet);
+
+
+        WebServletAnnotation annotation = new WebServletAnnotation(wac, "org.eclipse.jetty.annotations.ServletD", null);
+        annotation.apply();
+
+        ServletMapping[] resultMappings = wac.getServletHandler().getServletMappings();
+        assertEquals(1, resultMappings.length);
+        assertEquals(2, resultMappings[0].getPathSpecs().length);
+        for (String s:resultMappings[0].getPathSpecs())
+        {
+            if (!s.equals("/") && !s.equals("/bah/*"))
+                fail("Unexpected path mapping");
+        }
+    }
+    
+    
+
+    @Test
     public void testDeclareRoles ()
     throws Exception
     {
