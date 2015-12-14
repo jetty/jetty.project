abe5d090bbbd359b579cdf19dd52cf98b150d0d1
Matt Gilman <matt.c.gilman@gmail.com>
2015-12-14 15:03:31 -0500

    [447816] - ServletHolder#compareTo not transitive
    Updating compareTo to properly order when one _className is null and the other is not.
    
    Signed-off-by: Matt Gilman <matt.c.gilman@gmail.com>

--- jetty-servlet/src/main/java/org/eclipse/jetty/servlet/ServletHolder.java
+++ jetty-servlet/src/main/java/org/eclipse/jetty/servlet/ServletHolder.java
@@ -208,15 +208,29 @@ public class ServletHolder extends Holder<Servlet> implements UserIdentity.Scope
     {
         if (sh==this)
             return 0;
+
         if (sh._initOrder<_initOrder)
             return 1;
+
         if (sh._initOrder>_initOrder)
             return -1;
 
-        int c=(_className!=null && sh._className!=null)?_className.compareTo(sh._className):0;
+        // consider _className, need to position properly when one is configured but not the other
+        int c;
+        if (_className==null && sh._className==null)
+            c=0;
+        else if (_className==null)
+            c=-1;
+        else if (sh._className==null)
+            c=1;
+        else
+            c=_className.compareTo(sh._className);
+
+        // if _initOrder and _className are the same, consider the _name
         if (c==0)
             c=_name.compareTo(sh._name);
-            return c;
+
+        return c;
     }
 
     /* ------------------------------------------------------------ */
