bf9f39dc17fb36c97eada2a67bd208ae75016e93
Simone Bordet <simone.bordet@gmail.com>
2015-12-14 15:00:19 +0100

    Improved exception reporting.

--- jetty-client/src/main/java/org/eclipse/jetty/client/MultiplexHttpDestination.java
+++ jetty-client/src/main/java/org/eclipse/jetty/client/MultiplexHttpDestination.java
@@ -93,7 +93,7 @@ public abstract class MultiplexHttpDestination<C extends Connection> extends Htt
         else
         {
             connection.close();
-            failed(new IllegalStateException());
+            failed(new IllegalStateException("Invalid connection state " + connect));
         }
     }
 
