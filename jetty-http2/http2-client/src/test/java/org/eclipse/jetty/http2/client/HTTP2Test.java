717fc7819d8a41cb21acfe2d5f6c82d105aac30a
Simone Bordet <simone.bordet@gmail.com>
2015-12-15 15:35:04 +0100

    484262 - Race condition between GOAWAY disconnect and ability to make new request.
    Fixed by making sure that when a peer received a GOAWAY frame, it
    does not also notify the onFailure() callback.

--- jetty-http2/http2-client/src/test/java/org/eclipse/jetty/http2/client/HTTP2Test.java
+++ jetty-http2/http2-client/src/test/java/org/eclipse/jetty/http2/client/HTTP2Test.java
@@ -38,6 +38,7 @@ import org.eclipse.jetty.http.HttpScheme;
 import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.http.HttpVersion;
 import org.eclipse.jetty.http.MetaData;
+import org.eclipse.jetty.http2.ErrorCode;
 import org.eclipse.jetty.http2.api.Session;
 import org.eclipse.jetty.http2.api.Stream;
 import org.eclipse.jetty.http2.api.server.ServerSessionListener;
@@ -415,5 +416,47 @@ public class HTTP2Test extends AbstractTest
         });
         Assert.assertTrue(exchangeLatch2.await(5, TimeUnit.SECONDS));
         Assert.assertEquals(0, session.getStreams().size());
+    }
+
+    @Test
+    public void testCleanGoAwayDoesNotTriggerFailureNotification() throws Exception
+    {
+        start(new ServerSessionListener.Adapter()
+        {
+            @Override
+            public Stream.Listener onNewStream(Stream stream, HeadersFrame frame)
+            {
+                MetaData.Response metaData = new MetaData.Response(HttpVersion.HTTP_2, HttpStatus.OK_200, new HttpFields());
+                HeadersFrame response = new HeadersFrame(stream.getId(), metaData, null, true);
+                stream.headers(response, Callback.NOOP);
+                // Close cleanly.
+                stream.getSession().close(ErrorCode.NO_ERROR.code, null, Callback.NOOP);
+                return null;
+            }
+        });
+
+        CountDownLatch closeLatch = new CountDownLatch(1);
+        CountDownLatch failureLatch = new CountDownLatch(1);
+        Session session = newClient(new Session.Listener.Adapter()
+        {
+            @Override
+            public void onClose(Session session, GoAwayFrame frame)
+            {
+                closeLatch.countDown();
+            }
+
+            @Override
+            public void onFailure(Session session, Throwable failure)
+            {
+                failureLatch.countDown();
+            }
+        });
+        MetaData.Request metaData = newRequest("GET", new HttpFields());
+        HeadersFrame request = new HeadersFrame(metaData, null, true);
+        session.newStream(request, new Promise.Adapter<>(), new Stream.Listener.Adapter());
+
+        // Make sure onClose() is called.
+        Assert.assertTrue(closeLatch.await(5, TimeUnit.SECONDS));
+        Assert.assertFalse(failureLatch.await(1, TimeUnit.SECONDS));
     }
 }
