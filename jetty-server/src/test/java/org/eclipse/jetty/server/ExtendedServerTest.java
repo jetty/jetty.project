55eb54799f812a39bb29e09d5429594e8ee350f0
Greg Wilkins <gregw@webtide.com>
2016-02-19 11:39:08 +0100

    Issue #346 HttpParser RFC2616 Compliance mode
    Added HttpParser.Compliance field to HttpConnectionFactory

--- jetty-server/src/test/java/org/eclipse/jetty/server/ExtendedServerTest.java
+++ jetty-server/src/test/java/org/eclipse/jetty/server/ExtendedServerTest.java
@@ -29,6 +29,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jetty.http.HttpParser;
 import org.eclipse.jetty.http.HttpVersion;
 import org.eclipse.jetty.io.Connection;
 import org.eclipse.jetty.io.EndPoint;
@@ -54,7 +55,7 @@ public class ExtendedServerTest extends HttpServerTestBase
             @Override
             public Connection newConnection(Connector connector, EndPoint endPoint)
             {
-                return configure(new ExtendedHttpConnection(getHttpConfiguration(), connector, endPoint), connector, endPoint);
+                return configure(new ExtendedHttpConnection(getHttpConfiguration(), connector, endPoint,getHttpCompliance()), connector, endPoint);
             }
         })
         {
@@ -92,9 +93,9 @@ public class ExtendedServerTest extends HttpServerTestBase
 
     private static class ExtendedHttpConnection extends HttpConnection
     {
-        public ExtendedHttpConnection(HttpConfiguration config, Connector connector, EndPoint endPoint)
+        public ExtendedHttpConnection(HttpConfiguration config, Connector connector, EndPoint endPoint, HttpParser.Compliance compliance)
         {
-            super(config,connector,endPoint);
+            super(config,connector,endPoint,compliance);
         }
 
         @Override
