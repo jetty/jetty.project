b3dc23f2c03025ff23dcc3c0185b72e7b5215515
Jesse McConnell <jesse.mcconnell@gmail.com>
2016-02-26 07:30:14 -0600

    Update ExampleServerXml.java

--- examples/embedded/src/main/java/org/eclipse/jetty/embedded/ExampleServerXml.java
+++ examples/embedded/src/main/java/org/eclipse/jetty/embedded/ExampleServerXml.java
@@ -25,7 +25,7 @@ import org.eclipse.jetty.xml.XmlConfiguration;
  * Configures and Starts a Jetty server from an XML declaration.
  * <p>
  * See <a href=
- * "http://git.eclipse.org/c/jetty/org.eclipse.jetty.project.git/tree/examples/embedded/src/main/resources/exampleserver.xml"
+ * "https://github.com/eclipse/jetty.project/blob/master/examples/embedded/src/main/resources/exampleserver.xml"
  * >exampleserver.xml</a>
  * </p>
  */
