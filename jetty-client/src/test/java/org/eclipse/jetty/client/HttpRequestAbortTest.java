24a203f9760bdc280814042dace4f4c446db8b13
Simone Bordet <simone.bordet@gmail.com>
2016-03-01 15:58:32 +0100

    Fixed compilation issue.

--- jetty-client/src/test/java/org/eclipse/jetty/client/HttpRequestAbortTest.java
+++ jetty-client/src/test/java/org/eclipse/jetty/client/HttpRequestAbortTest.java
@@ -68,7 +68,7 @@ public class HttpRequestAbortTest extends AbstractHttpClientServerTest
             Assert.assertSame(failure, x.getCause());
             // Make sure the pool is in a sane state.
             HttpDestinationOverHTTP destination = (HttpDestinationOverHTTP)client.getDestination(scheme, "localhost", connector.getLocalPort());
-            ConnectionPool connectionPool = destination.getConnectionPool();
+            DuplexConnectionPool connectionPool = destination.getConnectionPool();
             Assert.assertEquals(1, connectionPool.getConnectionCount());
             Assert.assertEquals(0, connectionPool.getActiveConnections().size());
             Assert.assertEquals(1, connectionPool.getIdleConnections().size());
