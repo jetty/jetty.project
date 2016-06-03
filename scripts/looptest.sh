5e3a534d5d63a5bfad9db2cc6705053a63dcf016
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2016-06-03 14:17:28 -0700

    More looptest experimentation

--- scripts/looptest.sh
+++ scripts/looptest.sh
@@ -10,7 +10,7 @@ cd $MODULE
 
 let count=0
 
-while mvn test $TESTNAME 2>&1 > target/lastbuild.log
+while mvn test -Dtest=$TESTNAME 2>&1 > target/lastbuild.log
 do
     now=`date`
     echo "Test Run $count - $now" >> target/looptest.log
