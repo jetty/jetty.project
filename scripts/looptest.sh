58f8d44ae24ec34db95da4f7bd2395ad9b6e4a86
Simone Bordet <simone.bordet@gmail.com>
2016-06-03 23:14:18 +0200

    Improved looptest.sh.
    Fixed reference to test class.
    Added DEBUG logging.

--- scripts/looptest.sh
+++ scripts/looptest.sh
@@ -8,10 +8,9 @@ cd $MODULE
 
 let count=0
 
-while mvn test $TESTNAME 2>&1 > target/lastbuild.log
+while mvn test -Dtest=$TESTNAME -Dorg.eclipse.jetty.LEVEL=DEBUG 2>&1 > target/lastbuild.log
 do
     now=`date`
     echo "Test Run $count - $now" >> target/looptest.log
     let count=$count+1
 done
-
