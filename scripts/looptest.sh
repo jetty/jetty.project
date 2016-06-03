f814c3d46cd4ddd8db62d62109f6187330b8c31d
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2016-06-03 10:14:09 -0700

    Adding looptest for help with CI only flappy test failures

---
+++ scripts/looptest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git fetch origin
+
+git reset HEAD
+
+git checkout $BRANCH
+
+mvn clean install -DskipTests
+
+cd $MODULE
+
+let count=0
+
+while mvn test $TESTNAME 2>&1 > target/lastbuild.log
+do
+    now=`date`
+    echo "Test Run $count - $now" >> target/looptest.log
+    let count=$count+1
+done
+
