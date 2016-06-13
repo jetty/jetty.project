2b539b6383d1ca0c4a4c0cf4e0ef4f42562a5b71
Simone Bordet <simone.bordet@gmail.com>
2016-06-13 17:54:41 +0200

    Fixes #638 - ConnectHandler responses should have Content-Length.
    Added Content-Length: 0 to the responses.

--- jetty-proxy/src/main/java/org/eclipse/jetty/proxy/ConnectHandler.java
+++ jetty-proxy/src/main/java/org/eclipse/jetty/proxy/ConnectHandler.java
@@ -362,6 +362,7 @@ public class ConnectHandler extends HandlerWrapper
         try
         {
             response.setStatus(statusCode);
+            response.setContentLength(0);
             if (statusCode != HttpServletResponse.SC_OK)
                 response.setHeader(HttpHeader.CONNECTION.asString(), HttpHeaderValue.CLOSE.asString());
             response.getOutputStream().close();
