297918f71923c4bc7782d41c250dc7a830c6b7da
Jan Bartel <janb@webtide.com>
2016-06-15 13:12:37 +1000

    Issue  #641
    Use Mongo createIndex instead of deprecated ensureIndex.

--- jetty-nosql/src/main/java/org/eclipse/jetty/nosql/mongodb/MongoSessionIdManager.java
+++ jetty-nosql/src/main/java/org/eclipse/jetty/nosql/mongodb/MongoSessionIdManager.java
@@ -175,12 +175,23 @@ public class MongoSessionIdManager extends AbstractSessionIdManager
         _server = server;
         _sessions = sessions;
 
-        _sessions.ensureIndex(
-                BasicDBObjectBuilder.start().add("id",1).get(),
-                BasicDBObjectBuilder.start().add("unique",true).add("sparse",false).get());
-        _sessions.ensureIndex(
-                BasicDBObjectBuilder.start().add("id",1).add("version",1).get(),
-                BasicDBObjectBuilder.start().add("unique",true).add("sparse",false).get());
+        DBObject idKey = BasicDBObjectBuilder.start().add("id", 1).get();        
+        _sessions.createIndex(idKey,
+                              BasicDBObjectBuilder.start()
+                              .add("name", "id_1")
+                              .add("ns", _sessions.getFullName())
+                              .add("sparse", false)
+                              .add("unique", true)
+                              .get());
+
+        DBObject versionKey = BasicDBObjectBuilder.start().add("id", 1).add("version", 1).get();       
+        _sessions.createIndex(versionKey, BasicDBObjectBuilder.start()
+                              .add("name", "id_1_version_1")
+                              .add("ns", _sessions.getFullName())
+                              .add("sparse", false)
+                              .add("unique", true)
+                              .get());
+                            
 
     }
  
