72393bc6665a3ff237d53c1741d2c11772a15b17
Simone Bordet <simone.bordet@gmail.com>
2016-06-22 11:27:13 +0200

    Fixes #659 - CONNECT request fails spuriously.
    Fixed by properly returning true from messageComplete() in case of
    a 101 response or a 200 CONNECT response.

--- jetty-client/src/main/java/org/eclipse/jetty/client/HttpProxy.java
+++ jetty-client/src/main/java/org/eclipse/jetty/client/HttpProxy.java
@@ -206,6 +206,9 @@ public class HttpProxy extends ProxyConfiguration.Proxy
                         new SslClientConnectionFactory(client.getSslContextFactory(), client.getByteBufferPool(), client.getExecutor(), connectionFactory);
                 HttpConnectionOverHTTP oldConnection = (HttpConnectionOverHTTP)endPoint.getConnection();
                 org.eclipse.jetty.io.Connection newConnection = sslConnectionFactory.newConnection(endPoint, context);
+                // Creating the connection will link the new Connection the EndPoint,
+                // but we need the old Connection linked for the upgrade to do its job.
+                endPoint.setConnection(oldConnection);
                 endPoint.upgrade(newConnection);
                 if (LOG.isDebugEnabled())
                     LOG.debug("HTTP tunnel established: {} over {}", oldConnection, newConnection);
--- jetty-client/src/test/java/org/eclipse/jetty/client/HttpClientTest.java
+++ jetty-client/src/test/java/org/eclipse/jetty/client/HttpClientTest.java
@@ -77,6 +77,7 @@ import org.eclipse.jetty.http.HttpHeader;
 import org.eclipse.jetty.http.HttpHeaderValue;
 import org.eclipse.jetty.http.HttpMethod;
 import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.io.AbstractConnection;
 import org.eclipse.jetty.io.EndPoint;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.toolchain.test.TestingDir;
@@ -1508,6 +1509,11 @@ public class HttpClientTest extends AbstractHttpClientServerTest
 
                 ContentResponse response = listener.get(5, TimeUnit.SECONDS);
                 Assert.assertEquals(200, response.getStatus());
+
+                // Because the tunnel was successful, this connection will be
+                // upgraded to an SslConnection, so it will not be fill interested.
+                // This test doesn't upgrade, so it needs to restore the fill interest.
+                ((AbstractConnection)connection).fillInterested();
 
                 // Test that I can send another request on the same connection.
                 request = client.newRequest(host, port);
