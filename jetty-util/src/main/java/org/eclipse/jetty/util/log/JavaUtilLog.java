dbc8352aa681db0b5c5a9b1a8445fd4577e831ea
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2016-06-29 15:07:43 -0700

    Fixes #676 - JavaUtilLog.ignore() logs at FINEST level now

--- jetty-util/src/main/java/org/eclipse/jetty/util/log/JavaUtilLog.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/log/JavaUtilLog.java
@@ -255,8 +255,8 @@ public class JavaUtilLog extends AbstractLogger
 
     public void ignore(Throwable ignored)
     {
-        if (_logger.isLoggable(Level.ALL))
-            log(Level.WARNING,Log.IGNORED,ignored);
+        if (_logger.isLoggable(Level.FINEST))
+            log(Level.FINEST,Log.IGNORED,ignored);
     }
 
     private String format(String msg, Object... args)
