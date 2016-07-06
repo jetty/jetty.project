5af61cfc389987ac420e8b5af1b4cbafa4de0b1e
Greg Wilkins <gregw@webtide.com>
2016-07-06 11:55:41 +1000

    fix #685 SecureRequestCustomizer SslSession attribute

--- jetty-server/src/main/java/org/eclipse/jetty/server/SecureRequestCustomizer.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/SecureRequestCustomizer.java
@@ -266,7 +266,7 @@ public class SecureRequestCustomizer implements HttpConfiguration.Customizer
             request.setAttribute("javax.servlet.request.key_size",keySize);
             request.setAttribute("javax.servlet.request.ssl_session_id", idStr);
             String sessionAttribute = getSslSessionAttribute();
-            if (sessionAttribute != null && sessionAttribute.isEmpty())
+            if (sessionAttribute != null && !sessionAttribute.isEmpty())
                 request.setAttribute(sessionAttribute, sslSession);
         }
         catch (Exception e)
