5afbcd4c6a3c44daf1093a348c9171c3d9710cde
Greg Wilkins <gregw@webtide.com>
2016-07-20 09:56:25 +1000

    Allow setCharacterEncoding after getOutputStream #733

--- jetty-server/src/test/java/org/eclipse/jetty/server/ResponseTest.java
+++ jetty-server/src/test/java/org/eclipse/jetty/server/ResponseTest.java
@@ -355,6 +355,17 @@ public class ResponseTest
         assertEquals("text/xml;charset=utf-8", response.getContentType());
         response.setCharacterEncoding("iso-8859-1");
         assertEquals("text/xml;charset=utf-8", response.getContentType());
+        
+        response.recycle();
+        response.setCharacterEncoding("utf-16");
+        response.setContentType("foo/bar");
+        assertEquals("foo/bar;charset=utf-16", response.getContentType());
+        response.getOutputStream();
+        response.setCharacterEncoding("utf-8");
+        assertEquals("foo/bar;charset=utf-8", response.getContentType());
+        response.flushBuffer();
+        response.setCharacterEncoding("utf-16");
+        assertEquals("foo/bar;charset=utf-8", response.getContentType());
     }
 
     @Test
