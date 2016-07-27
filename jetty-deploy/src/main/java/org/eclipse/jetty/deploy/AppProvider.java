8e3af8ad4fa0d1fb906d491d7802bd147a74319c
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2016-07-27 00:29:23 -0400

    Reverted one automated migration from b27e7a8d9b1bda7fc92f213148ed3f9e45b265ce.
    One import crossed maven module boundaries where there is no dependencies between the two modules.
    
    Signed-off-by: Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>

--- jetty-deploy/src/main/java/org/eclipse/jetty/deploy/AppProvider.java
+++ jetty-deploy/src/main/java/org/eclipse/jetty/deploy/AppProvider.java
@@ -20,7 +20,6 @@ package org.eclipse.jetty.deploy;
 
 import java.io.IOException;
 
-import org.eclipse.jetty.osgi.boot.AbstractContextProvider.OSGiApp;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.util.component.LifeCycle;
 
@@ -45,14 +44,5 @@ public interface AppProvider extends LifeCycle
      * @throws IOException if unable to create context
      * @throws Exception if unable to create context
      */
-    default ContextHandler createContextHandler(App app) throws Exception {
-	    if (app == null)
-	        return null;
-	    if (!(app instanceof OSGiApp))
-	        throw new IllegalStateException(app+" is not a BundleApp");
-	    
-	    //Create a ContextHandler suitable to deploy in OSGi
-	    ContextHandler h = ((OSGiApp)app).createContextHandler();           
-	    return h;
-	}
+    ContextHandler createContextHandler(App app) throws Exception;
 }
