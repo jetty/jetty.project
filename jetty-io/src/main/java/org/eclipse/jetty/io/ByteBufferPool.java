7e4b9f1d3b46ea1ff234287e236d52003368f764
Priyanshu Singh <priyanshuid@gmail.com>
2016-08-08 17:25:58 +0400

    Removed white spaces
    Signed-off-by: Priyanshu Singh <priyanshuid@gmail.com>

--- jetty-io/src/main/java/org/eclipse/jetty/io/ByteBufferPool.java
+++ jetty-io/src/main/java/org/eclipse/jetty/io/ByteBufferPool.java
@@ -67,7 +67,6 @@ public interface ByteBufferPool
             this.byteBufferPool = byteBufferPool;
             this.buffers = new ArrayList<>();
             this.recycles = new ArrayList<>();
-            
         }
 
         public ByteBuffer acquire(int capacity, boolean direct)
