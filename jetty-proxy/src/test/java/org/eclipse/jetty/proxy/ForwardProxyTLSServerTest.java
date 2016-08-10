2eec2251ee11d41506b99ab18b99eeb90f2e535f
Simone Bordet <simone.bordet@gmail.com>
2016-08-10 18:36:16 +0200

    Fixes #827 - HTTPClient fails connecting to HTTPS host through an HTTP proxy w/authentication.
    Only successful (200) responses to a CONNECT behave like HEAD and
    implicitly have no body.

--- jetty-proxy/src/test/java/org/eclipse/jetty/proxy/ForwardProxyTLSServerTest.java
+++ jetty-proxy/src/test/java/org/eclipse/jetty/proxy/ForwardProxyTLSServerTest.java
@@ -482,6 +482,29 @@ public class ForwardProxyTLSServerTest
     }
 
     @Test
+    public void testProxyAuthenticationWithResponseContent() throws Exception
+    {
+        final String realm = "test-realm";
+        testProxyAuthentication(realm, new ConnectHandler()
+        {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+            {
+                String proxyAuth = request.getHeader(HttpHeader.PROXY_AUTHORIZATION.asString());
+                if (proxyAuth == null)
+                {
+                    baseRequest.setHandled(true);
+                    response.setStatus(HttpStatus.PROXY_AUTHENTICATION_REQUIRED_407);
+                    response.setHeader(HttpHeader.PROXY_AUTHENTICATE.asString(), "Basic realm=\"" + realm + "\"");
+                    response.getOutputStream().write(new byte[4096]);
+                    return;
+                }
+                super.handle(target, baseRequest, request, response);
+            }
+        });
+    }
+
+    @Test
     public void testProxyAuthenticationClosesConnection() throws Exception
     {
         final String realm = "test-realm";
