f493e0f52823200e8b5604b374ff1f51e9e3c1a0
Greg Wilkins <gregw@webtide.com>
2016-08-10 16:26:43 +1000

    remove println

--- jetty-server/src/test/java/org/eclipse/jetty/server/handler/BufferedResponseHandlerTest.java
+++ jetty-server/src/test/java/org/eclipse/jetty/server/handler/BufferedResponseHandlerTest.java
@@ -212,7 +212,6 @@ public class BufferedResponseHandlerTest
         _test._close=false;
         _test._content = new byte[0];
         String response = _local.getResponse("GET /ctx/include/path HTTP/1.1\r\nHost: localhost\r\n\r\n");
-        System.err.println(response);
         assertThat(response,containsString(" 200 OK"));
         assertThat(response,containsString("Content-Length: "));
         assertThat(response,containsString("Write: 0"));
