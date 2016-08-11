55babbe37718c41aacbfd64dcafb75a5eec7ece5
olivier lamy <olamy@apache.org>
2016-08-11 12:25:58 +1000

    add source header

--- tests/jetty-load-generator/src/main/java/org/eclipse/jetty/load/generator/LoadGeneratorRunner.java
+++ tests/jetty-load-generator/src/main/java/org/eclipse/jetty/load/generator/LoadGeneratorRunner.java
@@ -1,7 +1,24 @@
+//
+//  ========================================================================
+//  Copyright (c) Webtide LLC
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
 package org.eclipse.jetty.load.generator;
 
 import org.eclipse.jetty.client.HttpClient;
-import org.eclipse.jetty.client.api.ContentProvider;
 import org.eclipse.jetty.client.api.Request;
 import org.eclipse.jetty.client.api.Response;
 import org.eclipse.jetty.client.api.Result;
@@ -88,7 +105,7 @@ public class LoadGeneratorRunner
                     .cookie( HTTP_COOKIE ) //
                     .header( "X-Download", Integer.toString( loadGenerator.getResponseSize() ) ) //
                     .method( loadGenerator.getMethod() );
-                
+
 
                 if (loadGenerator.getPayloadSize() > 0) {
                     request.content( new BytesContentProvider(new byte[loadGenerator.getPayloadSize()]) );
