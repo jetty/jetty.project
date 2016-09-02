1f7c5a5b204d395a0eec609e66a2f0c4c626c575
Greg Wilkins <gregw@webtide.com>
2016-09-02 14:54:42 +1000

    Issue #898
    Added addCSV method to HttpFields to more efficiently add values to a CSV field without duplicates.
    Improved usage of QuotedCSV and removed older parsing
    
    Used new method in GazipHttpOutPutInterceptor to avoid duplicate Vary fields

--- jetty-server/src/main/java/org/eclipse/jetty/server/handler/gzip/GzipHttpOutputInterceptor.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/handler/gzip/GzipHttpOutputInterceptor.java
@@ -30,6 +30,7 @@ import org.eclipse.jetty.http.HttpFields;
 import org.eclipse.jetty.http.HttpHeader;
 import org.eclipse.jetty.http.MimeTypes;
 import org.eclipse.jetty.http.PreEncodedHttpField;
+import org.eclipse.jetty.http.QuotedCSV;
 import org.eclipse.jetty.server.HttpChannel;
 import org.eclipse.jetty.server.HttpOutput;
 import org.eclipse.jetty.server.Response;
@@ -189,7 +190,8 @@ public class GzipHttpOutputInterceptor implements HttpOutput.Interceptor
         }
 
         // Has the Content-Encoding header already been set?
-        String ce=response.getHeader("Content-Encoding");
+        HttpFields fields = response.getHttpFields();
+        String ce=fields.get(HttpHeader.CONTENT_ENCODING);
         if (ce != null)
         {
             LOG.debug("{} exclude by content-encoding {}",this,ce);
@@ -202,9 +204,13 @@ public class GzipHttpOutputInterceptor implements HttpOutput.Interceptor
         if (_state.compareAndSet(GZState.MIGHT_COMPRESS,GZState.COMMITTING))
         {
             // We are varying the response due to accept encoding header.
-            HttpFields fields = response.getHttpFields();
             if (_vary != null)
-                fields.add(_vary);
+            {
+                if (fields.contains(HttpHeader.VARY))
+                    fields.addCSV(HttpHeader.VARY,_vary.getValues());
+                else
+                    fields.add(_vary);
+            }
 
             long content_length = response.getContentLength();
             if (content_length<0 && complete)
