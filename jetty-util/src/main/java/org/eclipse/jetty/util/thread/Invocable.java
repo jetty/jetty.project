3cd52fda04607422a4372c95edb7f912a7fefdbd
Greg Wilkins <gregw@webtide.com>
2016-09-09 14:11:16 +1000

    Issue #572 Javadoc

--- jetty-util/src/main/java/org/eclipse/jetty/util/thread/Invocable.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/thread/Invocable.java
@@ -28,6 +28,12 @@ import java.util.concurrent.Callable;
  * <li>non-blocking, the invocation will certainly <strong>not</strong> block</li>
  * <li>either, the invocation <em>may</em> block</li>
  * </ul>
+ * 
+ * <p>
+ * Static methods and are provided that allow the current thread to be tagged 
+ * with a {@link ThreadLocal} to indicate if it has a blocking invocation type.
+ * </p>
+ * 
  */
 public interface Invocable
 {
@@ -45,11 +51,21 @@ public interface Invocable
         }
     };
 
+    /**
+     * Test if the current thread has been tagged as non blocking
+     * @return True if the task the current thread is running has
+     * indicated that it will not block.
+     */
     public static boolean isNonBlockingInvocation()
     {
         return __nonBlocking.get();
     }
 
+    /**
+     * Invoke a task with the calling thread, tagged to indicate
+     * that it will not block.
+     * @param task The task to invoke.
+     */
     public static void invokeNonBlocking(Runnable task)
     {
         // a Choice exists, so we must indicate NonBlocking
@@ -65,6 +81,13 @@ public interface Invocable
         }
     }
 
+    /**
+     * Invoke a task with the calling thread.
+     * If the task is an {@link Invocable} of {@link InvocationType#EITHER}
+     * then it is invoked with {@link #invokeNonBlocking(Runnable)}, to 
+     * indicate the type of invocation that has been assumed.
+     * @param task The task to invoke.
+     */
     public static void invokePreferNonBlocking(Runnable task)
     {
         switch (getInvocationType(task))
@@ -81,6 +104,15 @@ public interface Invocable
         }
     }
 
+    /**
+     * Invoke a task with the calling thread.
+     * If the task is an {@link Invocable} of {@link InvocationType#EITHER}
+     * and the preferredInvocationType is not {@link InvocationType#BLOCKING}
+     * then it is invoked with {@link #invokeNonBlocking(Runnable)}.
+     * @param task The task to invoke.
+     * @param preferredInvocationType The invocation type to use if the task
+     * does not indicate a preference.
+     */
     public static void invokePreferred(Runnable task, InvocationType preferredInvocationType)
     {
         switch (getInvocationType(task))
@@ -99,6 +131,17 @@ public interface Invocable
         }
     }
 
+    /**
+     * wrap a task with the to indicate invocation type.
+     * If the task is an {@link Invocable} of {@link InvocationType#EITHER}
+     * and the preferredInvocationType is not {@link InvocationType#BLOCKING}
+     * then it is wrapped with an invocation of {@link #invokeNonBlocking(Runnable)}.
+     * otherwise the task itself is returned.
+     * @param task The task to invoke.
+     * @param preferredInvocationType The invocation type to use if the task
+     * does not indicate a preference.
+     * @return A Runnable that invokes the task in the declared or preferred type.
+     */
     public static Runnable asPreferred(Runnable task, InvocationType preferredInvocationType)
     {
         switch (getInvocationType(task))
@@ -116,6 +159,12 @@ public interface Invocable
         return task;
     }
 
+    /**
+     * Get the invocation type of an Object.
+     * @param o The object to check the invocation type of.
+     * @return If the object is a {@link Invocable}, it is coerced and the {@link #getInvocationType()}
+     * used, otherwise {@link InvocationType#BLOCKING} is returned.
+     */
     public static InvocationType getInvocationType(Object o)
     {
         if (o instanceof Invocable)
@@ -123,6 +172,9 @@ public interface Invocable
         return InvocationType.BLOCKING;
     }
 
+    /**
+     * @return The InvocationType of this object
+     */
     default InvocationType getInvocationType()
     {
         return InvocationType.BLOCKING;
