eeeafe8ee2ee79df2c48e7d6c1bf6b1ef2a792b3
fvrolijken <friso.vrolijken@qiy.nl>
2016-09-22 16:27:05 +0200

    Adding proper header comments
    Signed-off-by: fvrolijken <friso.vrolijken@qiy.nl>

--- jetty-nosql/src/main/java/org/eclipse/jetty/nosql/jedis/JedisSession.java
+++ jetty-nosql/src/main/java/org/eclipse/jetty/nosql/jedis/JedisSession.java
@@ -1,4 +1,21 @@
-/* Copyright 2016 Digital-Me */
+//
+//  ========================================================================
+//  Copyright (c) 1995-2016 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
 package org.eclipse.jetty.nosql.jedis;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -389,6 +406,8 @@ class JedisSession
         {
             return new HashMap<>();
         }
+        // else
+
         try (ByteArrayInputStream bais = new ByteArrayInputStream(input); ObjectInputStream ois = new ObjectInputStream(bais))
         {
             Map<String, Object> result = (Map<String, Object>)ois.readObject();
