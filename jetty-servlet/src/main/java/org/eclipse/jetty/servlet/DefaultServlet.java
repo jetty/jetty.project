8eb56edc8a3c5ce9d3019143d9a6cf69c7ccc563
Denis Bardadym <bardadymchik@gmail.com>
2016-10-14 06:18:16 +0300

    Fix typo in DefaultServlet (#988)
    Fixes #952
    
    Signed-off-by: Denis Bardadym <bardadymchik@gmail.com>

--- jetty-servlet/src/main/java/org/eclipse/jetty/servlet/DefaultServlet.java
+++ jetty-servlet/src/main/java/org/eclipse/jetty/servlet/DefaultServlet.java
@@ -767,7 +767,7 @@ public class DefaultServlet extends HttpServlet implements ResourceFactory
                     if (ifnm!=null && etag!=null)
                     {
                         // Handle special case of exact match OR gzip exact match
-                        if (etag.equals(ifnm) || ifnm.endsWith(ETAG_GZIP_QUOTE) && ifnm.indexOf(',')<0 && etag.equals(removeGzipFromETag(etag)))
+                        if (etag.equals(ifnm) || ifnm.endsWith(ETAG_GZIP_QUOTE) && ifnm.indexOf(',')<0 && etag.equals(removeGzipFromETag(ifnm)))
                         {
                             response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
                             response.setHeader(HttpHeader.ETAG.asString(),ifnm);
