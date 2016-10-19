0e56cd4eb2af1c9fdab6f93d4076a71aa5b14dd4
Simone Bordet <simone.bordet@gmail.com>
2016-10-19 11:36:21 +0200

    Fixed message formatting.

--- jetty-start/src/main/java/org/eclipse/jetty/start/Modules.java
+++ jetty-start/src/main/java/org/eclipse/jetty/start/Modules.java
@@ -277,7 +277,7 @@ public class Modules implements Iterable<Module>
     {
         Module module = get(name);
         if (module==null)
-            throw new UsageException(UsageException.ERR_UNKNOWN,"Unknown module='%s. List available with --list-modules",name);
+            throw new UsageException(UsageException.ERR_UNKNOWN,"Unknown module='%s'. List available with --list-modules",name);
 
         Set<String> enabled = new HashSet<>();
         enable(enabled,module,enabledFrom,false);
