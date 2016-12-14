05f397caa43ea11d5bd05ff8495ac538a16fa1df
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2016-12-14 13:20:28 -0700

    Issue #1114 - Adding initial stop/start test logic

--- jetty-websocket/websocket-server/src/test/java/org/eclipse/jetty/websocket/server/WebSocketUpgradeFilterTest.java
+++ jetty-websocket/websocket-server/src/test/java/org/eclipse/jetty/websocket/server/WebSocketUpgradeFilterTest.java
@@ -291,7 +291,7 @@ public class WebSocketUpgradeFilterTest
     }
     
     @Test
-    public void testConfiguration() throws Exception
+    public void testNormalConfiguration() throws Exception
     {
         URI destUri = serverUri.resolve("/info/");
         
@@ -306,6 +306,47 @@ public class WebSocketUpgradeFilterTest
             EventQueue<WebSocketFrame> frames = client.readFrames(1, 1000, TimeUnit.MILLISECONDS);
             String payload = frames.poll().getPayloadAsUTF8();
             
+            // If we can connect and send a text message, we know that the endpoint was
+            // added properly, and the response will help us verify the policy configuration too
+            assertThat("payload", payload, containsString("session.maxTextMessageSize=" + (10 * 1024 * 1024)));
+        }
+    }
+    
+    @Test
+    public void testStopStartOfHandler() throws Exception
+    {
+        URI destUri = serverUri.resolve("/info/");
+        
+        try (BlockheadClient client = new BlockheadClient(destUri))
+        {
+            client.connect();
+            client.sendStandardRequest();
+            client.expectUpgradeResponse();
+            
+            client.write(new TextFrame().setPayload("hello 1"));
+            
+            EventQueue<WebSocketFrame> frames = client.readFrames(1, 1000, TimeUnit.MILLISECONDS);
+            String payload = frames.poll().getPayloadAsUTF8();
+            
+            // If we can connect and send a text message, we know that the endpoint was
+            // added properly, and the response will help us verify the policy configuration too
+            assertThat("payload", payload, containsString("session.maxTextMessageSize=" + (10 * 1024 * 1024)));
+        }
+        
+        server.getHandler().stop();
+        server.getHandler().start();
+        
+        try (BlockheadClient client = new BlockheadClient(destUri))
+        {
+            client.connect();
+            client.sendStandardRequest();
+            client.expectUpgradeResponse();
+        
+            client.write(new TextFrame().setPayload("hello 2"));
+        
+            EventQueue<WebSocketFrame> frames = client.readFrames(1, 1000, TimeUnit.MILLISECONDS);
+            String payload = frames.poll().getPayloadAsUTF8();
+        
             // If we can connect and send a text message, we know that the endpoint was
             // added properly, and the response will help us verify the policy configuration too
             assertThat("payload", payload, containsString("session.maxTextMessageSize=" + (10 * 1024 * 1024)));
