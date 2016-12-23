b950a2e2e46395207f27990663120d365417f762
Greg Wilkins <gregw@webtide.com>
2016-12-23 13:44:19 +1100

    Fixes #1201

--- jetty-server/src/test/java/org/eclipse/jetty/server/ForwardedRequestCustomizerTest.java
+++ jetty-server/src/test/java/org/eclipse/jetty/server/ForwardedRequestCustomizerTest.java
@@ -203,6 +203,22 @@ public class ForwardedRequestCustomizerTest
     }
 
     @Test
+    public void testFor() throws Exception
+    {
+        String response=_connector.getResponse(
+            "GET / HTTP/1.1\n"+
+             "Host: myhost\n"+
+             "X-Forwarded-For: 10.9.8.7,6.5.4.3\n"+
+            "\n");
+        assertThat(response, Matchers.containsString("200 OK"));
+        assertEquals("http",_results.poll());
+        assertEquals("myhost",_results.poll());
+        assertEquals("80",_results.poll());
+        assertEquals("10.9.8.7",_results.poll());
+        assertEquals("0",_results.poll());
+    }
+
+    @Test
     public void testLegacyProto() throws Exception
     {
         String response=_connector.getResponse(
