e215b065dfb44ec501a6cfb13721c5d0f8f97c5c
Simone Bordet <simone.bordet@gmail.com>
2016-12-30 22:01:33 +0100

    Improved error reporting.

--- jetty-server/src/main/java/org/eclipse/jetty/server/Request.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/Request.java
@@ -275,7 +275,7 @@ public class Request implements HttpServletRequest
     public PushBuilder getPushBuilder()
     {
         if (!isPushSupported())
-            throw new IllegalStateException(String.format("%s,push=%b", this, isPush()));
+            throw new IllegalStateException(String.format("%s,push=%b,channel=%s", this, isPush(), getHttpChannel()));
 
         HttpFields fields = new HttpFields(getHttpFields().size()+5);
         boolean conditional=false;
