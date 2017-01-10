d2b898024353030c2840141ad7417dffe22bdce6
Greg Wilkins <gregw@webtide.com>
2017-01-10 16:19:40 +1100

    Issue #612
    Added support to parse and ignore HTTP/1.1 chunking trailers.
    Handle an early EOF within the trailer gracefully rather than with earlyEOF.

--- jetty-http/src/test/java/org/eclipse/jetty/http/HttpParserTest.java
+++ jetty-http/src/test/java/org/eclipse/jetty/http/HttpParserTest.java
@@ -780,6 +780,101 @@ public class HttpParserTest
     }
 
     @Test
+    public void testChunkParseTrailer() throws Exception
+    {
+        ByteBuffer buffer = BufferUtil.toBuffer(
+                "GET /chunk HTTP/1.0\r\n"
+                        + "Header1: value1\r\n"
+                        + "Transfer-Encoding: chunked\r\n"
+                        + "\r\n"
+                        + "a;\r\n"
+                        + "0123456789\r\n"
+                        + "1a\r\n"
+                        + "ABCDEFGHIJKLMNOPQRSTUVWXYZ\r\n"
+                        + "0\r\n"
+                        + "Trailer: value\r\n"
+                        + "\r\n");
+        HttpParser.RequestHandler handler = new Handler();
+        HttpParser parser = new HttpParser(handler);
+        parseAll(parser, buffer);
+
+        Assert.assertEquals("GET", _methodOrVersion);
+        Assert.assertEquals("/chunk", _uriOrStatus);
+        Assert.assertEquals("HTTP/1.0", _versionOrReason);
+        Assert.assertEquals(1, _headers);
+        Assert.assertEquals("Header1", _hdr[0]);
+        Assert.assertEquals("value1", _val[0]);
+        Assert.assertEquals("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", _content);
+
+        Assert.assertTrue(_headerCompleted);
+        Assert.assertTrue(_messageCompleted);
+    }
+
+    @Test
+    public void testChunkParseBadTrailer() throws Exception
+    {
+        ByteBuffer buffer = BufferUtil.toBuffer(
+                "GET /chunk HTTP/1.0\r\n"
+                        + "Header1: value1\r\n"
+                        + "Transfer-Encoding: chunked\r\n"
+                        + "\r\n"
+                        + "a;\r\n"
+                        + "0123456789\r\n"
+                        + "1a\r\n"
+                        + "ABCDEFGHIJKLMNOPQRSTUVWXYZ\r\n"
+                        + "0\r\n"
+                        + "Trailer: value");
+        HttpParser.RequestHandler handler = new Handler();
+        HttpParser parser = new HttpParser(handler);
+        parseAll(parser, buffer);
+        parser.atEOF();
+        parser.parseNext(BufferUtil.EMPTY_BUFFER);
+
+        Assert.assertEquals("GET", _methodOrVersion);
+        Assert.assertEquals("/chunk", _uriOrStatus);
+        Assert.assertEquals("HTTP/1.0", _versionOrReason);
+        Assert.assertEquals(1, _headers);
+        Assert.assertEquals("Header1", _hdr[0]);
+        Assert.assertEquals("value1", _val[0]);
+        Assert.assertEquals("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", _content);
+
+        Assert.assertTrue(_headerCompleted);
+        Assert.assertTrue(_early);
+    }
+
+
+    @Test
+    public void testChunkParseNoTrailer() throws Exception
+    {
+        ByteBuffer buffer = BufferUtil.toBuffer(
+                "GET /chunk HTTP/1.0\r\n"
+                        + "Header1: value1\r\n"
+                        + "Transfer-Encoding: chunked\r\n"
+                        + "\r\n"
+                        + "a;\r\n"
+                        + "0123456789\r\n"
+                        + "1a\r\n"
+                        + "ABCDEFGHIJKLMNOPQRSTUVWXYZ\r\n"
+                        + "0\r\n");
+        HttpParser.RequestHandler handler = new Handler();
+        HttpParser parser = new HttpParser(handler);
+        parseAll(parser, buffer);
+        parser.atEOF();
+        parser.parseNext(BufferUtil.EMPTY_BUFFER);
+
+        Assert.assertEquals("GET", _methodOrVersion);
+        Assert.assertEquals("/chunk", _uriOrStatus);
+        Assert.assertEquals("HTTP/1.0", _versionOrReason);
+        Assert.assertEquals(1, _headers);
+        Assert.assertEquals("Header1", _hdr[0]);
+        Assert.assertEquals("value1", _val[0]);
+        Assert.assertEquals("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", _content);
+
+        Assert.assertTrue(_headerCompleted);
+        Assert.assertTrue(_messageCompleted);
+    }
+
+    @Test
     public void testStartEOF() throws Exception
     {
         HttpParser.RequestHandler handler = new Handler();
