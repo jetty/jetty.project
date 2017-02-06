0f19cb7e97a84d50cdc826a99626a34b5447b538
Jens Reimann <jreimann@redhat.com>
2017-02-06 16:15:36 +0100

    Allow to externally provide 'daemon' parameter
    This change allows to provide the 'daemon' parameter using a property
    using "-Djetty.daemon" in addition.
    
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- jetty-maven-plugin/src/main/java/org/eclipse/jetty/maven/plugin/JettyDeployWar.java
+++ jetty-maven-plugin/src/main/java/org/eclipse/jetty/maven/plugin/JettyDeployWar.java
@@ -1,6 +1,6 @@
 //
 //  ========================================================================
-//  Copyright (c) 1995-2017 Mort Bay Consulting Pty. Ltd.
+//  Copyright (c) 1995-2017 Mort Bay Consulting Pty. Ltd. and others
 //  ------------------------------------------------------------------------
 //  All rights reserved. This program and the accompanying materials
 //  are made available under the terms of the Eclipse Public License v1.0
@@ -53,7 +53,7 @@ public class JettyDeployWar extends JettyRunWarMojo
      * cntrl-c to stop it.
      * 
      * 
-     * @parameter  default-value="true"
+     * @parameter property="jetty.daemon" default-value="true"
      */
     protected boolean daemon = true;
     
