633b68dc7245f40fa17d6965aa22e06c19f78343
Greg Wilkins <gregw@webtide.com>
2017-02-09 16:19:55 +1100

    Issue #1313 Send 500 if insufficient bytes not committed

--- jetty-server/src/test/java/org/eclipse/jetty/server/HttpManyWaysToCommitTest.java
+++ jetty-server/src/test/java/org/eclipse/jetty/server/HttpManyWaysToCommitTest.java
@@ -20,6 +20,7 @@ package org.eclipse.jetty.server;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -442,9 +443,21 @@ public class HttpManyWaysToCommitTest extends AbstractHttpTest
         server.start();
 
         HttpTester.Response response = executeRequest();
-        assertThat("response has no status", response.getStatus(), is(0));
+        assertThat("response is error", response.getStatus(), is(500));
+        assertFalse("response not eof", response.isEarlyEOF());
+    }
+
+    @Test
+    public void testSetContentLengthAndFlushWriteInsufficientBytes() throws Exception
+    {
+        server.setHandler(new SetContentLengthAndWriteInsufficientBytesHandler(true));
+        server.start();
+
+        HttpTester.Response response = executeRequest();
+        assertThat("response has no status", response.getStatus(), is(200));
         assertTrue("response eof", response.isEarlyEOF());
     }
+    
     
     @Test
     public void testSetContentLengthAndWriteExactlyThatAmountOfBytes() throws Exception
