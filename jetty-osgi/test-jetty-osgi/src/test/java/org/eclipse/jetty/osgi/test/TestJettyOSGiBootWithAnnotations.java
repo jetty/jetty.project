5933f70ae6204971b9e4cfead063e16723c59998
Jan Bartel <janb@webtide.com>
2017-03-10 11:57:45 +1100

    Issue #1049
    Ensure file paths for osgi config files are correct urls

--- jetty-osgi/test-jetty-osgi/src/test/java/org/eclipse/jetty/osgi/test/TestJettyOSGiBootWithAnnotations.java
+++ jetty-osgi/test-jetty-osgi/src/test/java/org/eclipse/jetty/osgi/test/TestJettyOSGiBootWithAnnotations.java
@@ -80,29 +80,26 @@ public class TestJettyOSGiBootWithAnnotations
 
     public static List<Option> configureJettyHomeAndPort(String jettySelectorFileName)
     {
-        File etcFolder = new File("src/test/config/etc");
-        String etc = "file://" + etcFolder.getAbsolutePath();
+        File etc = new File("src/test/config/etc");
+      
         List<Option> options = new ArrayList<Option>();
-        String xmlConfigs = etc     + "/jetty.xml;"
-                + etc
-                + "/"
-                + jettySelectorFileName
-                + ";"
-                + etc
-                + "/jetty-ssl.xml;"
-                + etc
-                + "/jetty-https.xml;"
-                + etc
-                + "/jetty-deployer.xml;"
-                + etc
-                + "/jetty-testrealm.xml";
-
-        options.add(systemProperty(OSGiServerConstants.MANAGED_JETTY_XML_CONFIG_URLS).value(xmlConfigs));
+        StringBuffer xmlConfigs = new StringBuffer();
+        xmlConfigs.append(new File(etc, "jetty.xml").toURI());
+        xmlConfigs.append(";");
+        xmlConfigs.append(new File(etc,jettySelectorFileName).toURI());
+        xmlConfigs.append(";");
+        xmlConfigs.append(new File(etc, "jetty-ssl.xml").toURI());
+        xmlConfigs.append(";");
+        xmlConfigs.append(new File(etc, "jetty-https.xml").toURI());
+        xmlConfigs.append(";");
+        xmlConfigs.append(new File(etc, "jetty-deployer.xml").toURI());
+        xmlConfigs.append(";");
+        xmlConfigs.append(new File(etc, "jetty-testrealm.xml").toURI());
+        
+        options.add(systemProperty(OSGiServerConstants.MANAGED_JETTY_XML_CONFIG_URLS).value(xmlConfigs.toString()));
         options.add(systemProperty("jetty.http.port").value(String.valueOf(TestJettyOSGiBootCore.DEFAULT_HTTP_PORT)));
         options.add(systemProperty("jetty.ssl.port").value(String.valueOf(TestJettyOSGiBootCore.DEFAULT_SSL_PORT)));
-        options.add(systemProperty("jetty.home").value(etcFolder.getParentFile().getAbsolutePath()));
-
-        
+        options.add(systemProperty("jetty.home").value(etc.getParentFile().getAbsolutePath()));
         return options;
     }
 
