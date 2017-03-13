f293f637c8d5dc9bcda3c7dea4f378919f2759bb
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2017-03-13 11:10:10 -0700

    Issue #1394 - fix default character encoding for surefire testing

--- jetty-client/src/test/java/org/eclipse/jetty/client/HttpClientURITest.java
+++ jetty-client/src/test/java/org/eclipse/jetty/client/HttpClientURITest.java
@@ -93,7 +93,8 @@ public class HttpClientURITest extends AbstractHttpClientServerTest
     public void testIDNRedirect() throws Exception
     {
         // Internationalized Domain Name.
-        String exampleHost = scheme + "://пример.рф";
+        // String exampleHost = scheme + "://пример.рф";
+        String exampleHost = scheme + "://\uD0BF\uD180\uD0B8\uD0BC\uD0B5\uD180.\uD180\uD184";
         String incorrectlyDecoded = new String(exampleHost.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
 
         // Simple server that only parses clear-text HTTP/1.1.
