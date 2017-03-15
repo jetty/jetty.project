d64218f28572d01ca8e54442bbedf73b5703e762
Greg Wilkins <gregw@webtide.com>
2017-03-15 18:11:18 +1100

    Issue #1399

--- jetty-server/src/test/java/org/eclipse/jetty/server/ssl/SlowClientsTest.java
+++ jetty-server/src/test/java/org/eclipse/jetty/server/ssl/SlowClientsTest.java
@@ -44,8 +44,10 @@ import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.junit.Ignore;
 import org.junit.Test;
 
+@Ignore
 public class SlowClientsTest
 {
     private Logger logger = Log.getLogger(getClass());
