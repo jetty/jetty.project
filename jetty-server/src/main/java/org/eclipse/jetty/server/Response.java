77bd0a66b8e2483fc33edef56d3e385c623ab68f
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2017-03-15 16:26:39 -0700

    Issue #1396 - Correcting control character range

--- jetty-server/src/main/java/org/eclipse/jetty/server/Response.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/Response.java
@@ -347,7 +347,10 @@ public class Response implements HttpServletResponse
         for(; i<valueLen; i++)
         {
             char c = value.charAt(i);
-            if (c <= 0x1F)
+            
+            // 0x00 - 0x1F are low order control characters
+            // 0x7F is the DEL control character
+            if ((c <= 0x1F) || (c == 0x7F))
                 throw new IllegalArgumentException("Control characters not allowed in RFC6265 Cookie value");
             if ((c == ' ' /* 0x20 */) ||
                 (c == '"' /* 0x2C */) ||
@@ -356,7 +359,7 @@ public class Response implements HttpServletResponse
             {
                 throw new IllegalArgumentException("RFC6265 Cookie value may not contain character: [" + c + "]");
             }
-            if (c >= 0x7F)
+            if (c >= 0x80)
                 throw new IllegalArgumentException("RFC6265 Cookie value characters restricted to US-ASCII range: 0x" + Integer.toHexString(c));
         }
     }
