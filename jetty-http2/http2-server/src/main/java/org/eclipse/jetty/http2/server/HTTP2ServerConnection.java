690d1fcf41603f1231870db46b3b4e5c8473bfe5
John Gardiner Myers <jgmyers@proofpoint.com>
2017-03-23 11:36:20 -0700

    Allow for easier subclassing of the HttpChannelOverHTTP2.
    Signed-off-by: John Gardiner Myers <jgmyers@proofpoint.com>

--- jetty-http2/http2-server/src/main/java/org/eclipse/jetty/http2/server/HTTP2ServerConnection.java
+++ jetty-http2/http2-server/src/main/java/org/eclipse/jetty/http2/server/HTTP2ServerConnection.java
@@ -213,12 +213,17 @@ public class HTTP2ServerConnection extends HTTP2Connection implements Connection
         {
             HttpTransportOverHTTP2 transport = new HttpTransportOverHTTP2(connector, this);
             transport.setStream(stream);
-            channel = new ServerHttpChannelOverHTTP2(connector, httpConfig, getEndPoint(), transport);
+            channel = newServerHttpChannelOverHTTP2(connector, httpConfig, transport);
             if (LOG.isDebugEnabled())
                 LOG.debug("Creating channel {} for {}", channel, this);
         }
         stream.setAttribute(IStream.CHANNEL_ATTRIBUTE, channel);
         return channel;
+    }
+
+    protected ServerHttpChannelOverHTTP2 newServerHttpChannelOverHTTP2(Connector connector, HttpConfiguration httpConfig, HttpTransportOverHTTP2 transport)
+    {
+        return new ServerHttpChannelOverHTTP2(connector, httpConfig, getEndPoint(), transport);
     }
 
     private void offerChannel(HttpChannelOverHTTP2 channel)
@@ -271,7 +276,7 @@ public class HTTP2ServerConnection extends HTTP2Connection implements Connection
         return true;
     }
 
-    private class ServerHttpChannelOverHTTP2 extends HttpChannelOverHTTP2 implements ExecutionStrategy.Rejectable
+    protected class ServerHttpChannelOverHTTP2 extends HttpChannelOverHTTP2 implements ExecutionStrategy.Rejectable
     {
         public ServerHttpChannelOverHTTP2(Connector connector, HttpConfiguration configuration, EndPoint endPoint, HttpTransportOverHTTP2 transport)
         {
