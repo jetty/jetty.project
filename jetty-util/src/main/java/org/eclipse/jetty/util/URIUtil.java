874662fe4a05954dab1ab77534f745aa322f7441
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2017-04-20 09:00:48 -0700

    Issue #1475 - retain encoded form of path

--- jetty-util/src/main/java/org/eclipse/jetty/util/URIUtil.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/URIUtil.java
@@ -1085,7 +1085,8 @@ public class URIUtil
         {
             if (!"jar".equals(uri.getScheme()))
                 return uri;
-            String s = uri.getSchemeSpecificPart();
+            // Get SSP (retaining encoded form)
+            String s = uri.getRawSchemeSpecificPart();
             int bang_slash = s.indexOf("!/");
             if (bang_slash>=0)
                 s=s.substring(0,bang_slash);
