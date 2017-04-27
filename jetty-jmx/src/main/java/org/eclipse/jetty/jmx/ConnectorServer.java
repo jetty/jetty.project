de16eba903d20da5dd8458ff32c0bd2742a8e348
Simone Bordet <simone.bordet@gmail.com>
2017-04-27 19:08:55 +0200

    Reworked ConnectorServer to honor hosts and ports declared for the RMI server and the RMI registry.

--- jetty-jmx/src/main/java/org/eclipse/jetty/jmx/ConnectorServer.java
+++ jetty-jmx/src/main/java/org/eclipse/jetty/jmx/ConnectorServer.java
@@ -18,12 +18,16 @@
 
 package org.eclipse.jetty.jmx;
 
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.RMIServerSocketFactory;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.management.MBeanServer;
@@ -31,6 +35,7 @@ import javax.management.ObjectName;
 import javax.management.remote.JMXConnectorServer;
 import javax.management.remote.JMXConnectorServerFactory;
 import javax.management.remote.JMXServiceURL;
+import javax.management.remote.rmi.RMIConnectorServer;
 
 import org.eclipse.jetty.util.HostPort;
 import org.eclipse.jetty.util.component.AbstractLifeCycle;
@@ -38,140 +43,122 @@ import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.util.thread.ShutdownThread;
 
-
-/* ------------------------------------------------------------ */
 /**
- * AbstractLifeCycle wrapper for JMXConnector Server
+ * AbstractLifeCycle wrapper for JMXConnectorServer
  */
 public class ConnectorServer extends AbstractLifeCycle
 {
     private static final Logger LOG = Log.getLogger(ConnectorServer.class);
 
-    JMXConnectorServer _connectorServer;
-    Registry _registry;
+    private JMXServiceURL _jmxURL;
+    private final Map<String, Object> _environment;
+    private final String _objectName;
+    private String _registryHost;
+    private int _registryPort;
+    private String _rmiHost;
+    private int _rmiPort;
+    private JMXConnectorServer _connectorServer;
+    private Registry _registry;
 
-    /* ------------------------------------------------------------ */
     /**
-     * Constructs connector server
+     * Constructs a ConnectorServer
      *
-     * @param serviceURL the address of the new connector server.
-     * The actual address of the new connector server, as returned
-     * by its getAddress method, will not necessarily be exactly the same.
-     * @param name object name string to be assigned to connector server bean
-     * @throws Exception if unable to setup connector server
+     * @param serviceURL the address of the new ConnectorServer
+     * @param name       object name string to be assigned to ConnectorServer bean
      */
     public ConnectorServer(JMXServiceURL serviceURL, String name)
-        throws Exception
     {
         this(serviceURL, null, name);
     }
 
-    /* ------------------------------------------------------------ */
     /**
-     * Constructs connector server
+     * Constructs a ConnectorServer
      *
-     * @param svcUrl the address of the new connector server.
-     * The actual address of the new connector server, as returned
-     * by its getAddress method, will not necessarily be exactly the same.
-     * @param environment  a set of attributes to control the new connector
-     * server's behavior. This parameter can be null. Keys in this map must
-     * be Strings. The appropriate type of each associated value depends on
-     * the attribute. The contents of environment are not changed by this call.
-     * @param name object name string to be assigned to connector server bean
-     * @throws Exception if unable to create connector server
+     * @param svcUrl      the address of the new ConnectorServer
+     * @param environment a set of attributes to control the new ConnectorServer's behavior.
+     *                    This parameter can be null. Keys in this map must
+     *                    be Strings. The appropriate type of each associated value depends on
+     *                    the attribute. The contents of environment are not changed by this call.
+     * @param name        object name string to be assigned to ConnectorServer bean
      */
-    public ConnectorServer(JMXServiceURL svcUrl, Map<String,?> environment, String name)
-         throws Exception
+    public ConnectorServer(JMXServiceURL svcUrl, Map<String, ?> environment, String name)
     {
-        String urlPath = svcUrl.getURLPath();
-        int idx = urlPath.indexOf("rmi://");
-        if (idx > 0)
-        {
-            String hostPort = urlPath.substring(idx+6, urlPath.indexOf('/', idx+6));
-            String regHostPort = startRegistry(hostPort);
-            if (regHostPort != null) {
-                urlPath = urlPath.replace(hostPort,regHostPort);
-                svcUrl = new JMXServiceURL(svcUrl.getProtocol(), svcUrl.getHost(), svcUrl.getPort(), urlPath);
-            }
-        }
-        MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
-        _connectorServer = JMXConnectorServerFactory.newJMXConnectorServer(svcUrl, environment, mbeanServer);
-        mbeanServer.registerMBean(_connectorServer,new ObjectName(name));
+        this._jmxURL = svcUrl;
+        this._environment = environment == null ? new HashMap<>() : new HashMap<>(environment);
+        this._objectName = name;
     }
 
-        /* ------------------------------------------------------------ */
-    /**
-     * @see org.eclipse.jetty.util.component.AbstractLifeCycle#doStart()
-     */
-    @Override
-    public void doStart()
-        throws Exception
+    public JMXServiceURL getAddress()
     {
+        return _jmxURL;
+    }
+
+    @Override
+    public void doStart() throws Exception
+    {
+        boolean rmi = "rmi".equals(_jmxURL.getProtocol());
+        if (rmi)
+        {
+            if (!_environment.containsKey(RMIConnectorServer.RMI_SERVER_SOCKET_FACTORY_ATTRIBUTE))
+                _environment.put(RMIConnectorServer.RMI_SERVER_SOCKET_FACTORY_ATTRIBUTE, new JMXRMIServerSocketFactory(false));
+        }
+
+        String urlPath = _jmxURL.getURLPath();
+        String jndiRMI = "/jndi/rmi://";
+        boolean registry = urlPath.startsWith(jndiRMI);
+        if (registry)
+        {
+            int startIndex = jndiRMI.length();
+            int endIndex = urlPath.indexOf('/', startIndex);
+            HostPort hostPort = new HostPort(urlPath.substring(startIndex, endIndex));
+            _registryHost = hostPort.getHost();
+            startRegistry(hostPort);
+            urlPath = jndiRMI + _registryHost + ":" + _registryPort + urlPath.substring(endIndex);
+            // Rebuild JMXServiceURL to use it for the creation of the JMXConnectorServer.
+            _jmxURL = new JMXServiceURL(_jmxURL.getProtocol(), _jmxURL.getHost(), _jmxURL.getPort(), urlPath);
+        }
+
+        MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
+        _connectorServer = JMXConnectorServerFactory.newJMXConnectorServer(_jmxURL, _environment, mbeanServer);
+        mbeanServer.registerMBean(_connectorServer, new ObjectName(_objectName));
         _connectorServer.start();
         ShutdownThread.register(0, this);
 
-        LOG.info("JMX Remote URL: {}", _connectorServer.getAddress().toString());
+        _jmxURL = new JMXServiceURL(_jmxURL.getProtocol(),
+                _rmiHost != null ? _rmiHost : _jmxURL.getHost(),
+                _rmiPort > 0 ? _rmiPort : _jmxURL.getPort(),
+                urlPath);
+
+        LOG.info("JMX Remote URL: {}", _jmxURL);
     }
 
-    /* ------------------------------------------------------------ */
-    /**
-     * @see org.eclipse.jetty.util.component.AbstractLifeCycle#doStop()
-     */
     @Override
-    public void doStop()
-        throws Exception
+    public void doStop() throws Exception
     {
         ShutdownThread.deregister(this);
         _connectorServer.stop();
+        MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
+        mbeanServer.unregisterMBean(new ObjectName(_objectName));
         stopRegistry();
     }
 
-    /**
-     * Check that local RMI registry is used, and ensure it is started. If local RMI registry is being used and not started, start it.
-     *
-     * @param hostPath
-     *            hostname and port number of RMI registry
-     * @throws Exception
-     */
-    private String startRegistry(String hostPath) throws Exception
+    private void startRegistry(HostPort hostPort) throws Exception
     {
-        HostPort hostPort = new HostPort(hostPath);
+        String host = hostPort.getHost();
+        int port = hostPort.getPort(1099);
 
-        String rmiHost = hostPort.getHost();
-        int rmiPort = hostPort.getPort(1099);
-
-        // Verify that local registry is being used
-        InetAddress hostAddress = InetAddress.getByName(rmiHost);
-        if(hostAddress.isLoopbackAddress())
+        try
         {
-            if (rmiPort == 0)
-            {
-                ServerSocket socket = new ServerSocket(0);
-                rmiPort = socket.getLocalPort();
-                socket.close();
-            }
-            else
-            {
-                try
-                {
-                    // Check if a local registry is already running
-                    LocateRegistry.getRegistry(rmiPort).list();
-                    return null;
-                }
-                catch (Exception ex)
-                {
-                    LOG.ignore(ex);
-                }
-            }
-
-            _registry = LocateRegistry.createRegistry(rmiPort);
-            Thread.sleep(1000);
-
-            rmiHost = HostPort.normalizeHost(InetAddress.getLocalHost().getCanonicalHostName());
-            return rmiHost + ':' + Integer.toString(rmiPort);
+            // Check if a local registry is already running.
+            LocateRegistry.getRegistry(host, port).list();
+            return;
+        }
+        catch (Throwable ex)
+        {
+            LOG.ignore(ex);
         }
 
-        return null;
+        _registry = LocateRegistry.createRegistry(port, null, new JMXRMIServerSocketFactory(true));
     }
 
     private void stopRegistry()
@@ -180,11 +167,66 @@ public class ConnectorServer extends AbstractLifeCycle
         {
             try
             {
-                UnicastRemoteObject.unexportObject(_registry,true);
+                UnicastRemoteObject.unexportObject(_registry, true);
             }
             catch (Exception ex)
             {
                 LOG.ignore(ex);
+            }
+            finally
+            {
+                _registry = null;
+            }
+        }
+    }
+
+
+    private class JMXRMIServerSocketFactory implements RMIServerSocketFactory
+    {
+        private boolean registry;
+
+        private JMXRMIServerSocketFactory(boolean registry)
+        {
+            this.registry = registry;
+        }
+
+        @Override
+        public ServerSocket createServerSocket(int port) throws IOException
+        {
+            if (registry)
+            {
+                InetAddress address;
+                if (_registryHost == null || _registryHost.isEmpty())
+                {
+                    _registryHost = InetAddress.getLocalHost().getHostName();
+                    address = null;
+                }
+                else
+                {
+                    address = InetAddress.getByName(_registryHost);
+                }
+                ServerSocket server = new ServerSocket();
+                server.bind(new InetSocketAddress(address, port));
+                _registryPort = server.getLocalPort();
+                return server;
+            }
+            else
+            {
+                InetAddress address;
+                _rmiHost = _jmxURL.getHost();
+                if (_rmiHost == null || _rmiHost.isEmpty())
+                {
+                    _rmiHost = InetAddress.getLocalHost().getHostName();
+                    address = null;
+                }
+                else
+                {
+                    address = InetAddress.getByName(_rmiHost);
+                }
+                ServerSocket server = new ServerSocket();
+                server.bind(new InetSocketAddress(address, port));
+                _rmiPort = server.getLocalPort();
+                return server;
             }
         }
     }
--- jetty-jmx/src/test/java/org/eclipse/jetty/jmx/ConnectorServerTest.java
+++ jetty-jmx/src/test/java/org/eclipse/jetty/jmx/ConnectorServerTest.java
@@ -18,95 +18,146 @@
 
 package org.eclipse.jetty.jmx;
 
+import java.net.ConnectException;
 import java.net.InetAddress;
-import java.rmi.registry.LocateRegistry;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import javax.management.remote.JMXServiceURL;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.AnyOf.anyOf;
 
+/**
+ * Running the tests of this class in the same JVM results often in
+ * <pre>
+ * Caused by: java.rmi.NoSuchObjectException: no such object in table
+ *     at sun.rmi.transport.StreamRemoteCall.exceptionReceivedFromServer(StreamRemoteCall.java:276)
+ *     at sun.rmi.transport.StreamRemoteCall.executeCall(StreamRemoteCall.java:253)
+ *     at sun.rmi.server.UnicastRef.invoke(UnicastRef.java:379)
+ *     at sun.rmi.registry.RegistryImpl_Stub.bind(Unknown Source)
+ * </pre>
+ * Running each test method in a forked JVM makes these tests all pass,
+ * therefore the issue is likely caused by use of stale stubs cached by the JDK.
+ */
+@Ignore
 public class ConnectorServerTest
 {
-
+    private String objectName = "org.eclipse.jetty:name=rmiconnectorserver";
     private ConnectorServer connectorServer;
 
     @After
     public void tearDown() throws Exception
     {
         if (connectorServer != null)
-        {
-            connectorServer.doStop();
-        }
+            connectorServer.stop();
     }
 
     @Test
-    public void randomPortTest() throws Exception
+    public void testAddressAfterStart() throws Exception
     {
-        // given
-        connectorServer = new ConnectorServer(new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:0/jettytest"),
-                "org.eclipse.jetty:name=rmiconnectorserver");
-        // if port is not available then the server value is null
-        if (connectorServer != null)
-        {
-            // when
-            connectorServer.start();
-
-            // then
-            assertThat("Server status must be in started or starting",connectorServer.getState(),
-                    anyOf(is(ConnectorServer.STARTED),is(ConnectorServer.STARTING)));
-        }
-    }
-
-    @Test
-    @Ignore // collides on ci server
-    public void testConnServerWithRmiDefaultPort() throws Exception
-    {
-        // given
-        LocateRegistry.createRegistry(1099);
-        JMXServiceURL serviceURLWithOutPort = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:1099/jmxrmi");
-        connectorServer = new ConnectorServer(serviceURLWithOutPort," domain: key3 = value3");
-
-        // when
+        connectorServer = new ConnectorServer(new JMXServiceURL("service:jmx:rmi:///jndi/rmi:///jmxrmi"), objectName);
         connectorServer.start();
 
-        // then
-        assertThat("Server status must be in started or starting",connectorServer.getState(),anyOf(is(ConnectorServer.STARTED),is(ConnectorServer.STARTING)));
+        JMXServiceURL address = connectorServer.getAddress();
+        Assert.assertTrue(address.toString().matches("service:jmx:rmi://[^:]+:\\d+/jndi/rmi://[^:]+:\\d+/jmxrmi"));
     }
 
     @Test
-    public void testConnServerWithRmiRandomPort() throws Exception
+    public void testNoRegistryHostBindsToAny() throws Exception
     {
-        // given
-        JMXServiceURL serviceURLWithOutPort = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:1199/jmxrmi");
-        connectorServer = new ConnectorServer(serviceURLWithOutPort," domain: key4 = value4");
-        // if port is not available then the server value is null
-        if (connectorServer != null)
-        {
-            // when
-            connectorServer.start();
-            connectorServer.stop();
+        connectorServer = new ConnectorServer(new JMXServiceURL("service:jmx:rmi:///jndi/rmi:///jmxrmi"), objectName);
+        connectorServer.start();
 
-            // then
-            assertThat("Server status must be in started or starting",connectorServer.getState(),
-                    anyOf(is(ConnectorServer.STOPPING),is(ConnectorServer.STOPPED)));
+        InetAddress localHost = InetAddress.getLocalHost();
+        if (!localHost.isLoopbackAddress())
+        {
+            // Verify that I can connect to the RMIRegistry using a non-loopback address.
+            new Socket(localHost, 1099).close();
         }
     }
 
     @Test
-    @Ignore
-    public void testIsLoopbackAddressWithWrongValue() throws Exception
+    public void testNoRMIHostBindsToAny() throws Exception
     {
-        // given
-        JMXServiceURL serviceURLWithOutPort = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + InetAddress.getLocalHost() + ":1199/jmxrmi");
+        connectorServer = new ConnectorServer(new JMXServiceURL("service:jmx:rmi:///jndi/rmi:///jmxrmi"), objectName);
+        connectorServer.start();
 
-        // when
-        connectorServer = new ConnectorServer(serviceURLWithOutPort," domain: key5 = value5");
+        InetAddress localHost = InetAddress.getLocalHost();
+        if (!localHost.isLoopbackAddress())
+        {
+            // Verify that I can connect to the RMI server using a non-loopback address.
+            new Socket(localHost, connectorServer.getAddress().getPort()).close();
+        }
+    }
 
-        // then
-        assertNull("As loopback address returns false...registry must be null",connectorServer._registry);
+    @Test
+    public void testLocalhostRegistryBindsToLoopback() throws Exception
+    {
+        connectorServer = new ConnectorServer(new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:1099/jmxrmi"), objectName);
+        connectorServer.start();
+
+        InetAddress localHost = InetAddress.getLocalHost();
+        if (!localHost.isLoopbackAddress())
+        {
+            try
+            {
+                // Verify that I cannot connect to the RMIRegistry using a non-loopback address.
+                new Socket(localHost, 1099);
+                Assert.fail();
+            }
+            catch (ConnectException ignored)
+            {
+                // Ignored.
+            }
+        }
+
+        InetAddress loopback = InetAddress.getLoopbackAddress();
+        new Socket(loopback, 1099).close();
+    }
+
+    @Test
+    public void testLocalhostRMIBindsToLoopback() throws Exception
+    {
+        connectorServer = new ConnectorServer(new JMXServiceURL("service:jmx:rmi://localhost/jndi/rmi://localhost:1099/jmxrmi"), objectName);
+        connectorServer.start();
+        JMXServiceURL address = connectorServer.getAddress();
+
+        InetAddress localHost = InetAddress.getLocalHost();
+        if (!localHost.isLoopbackAddress())
+        {
+            try
+            {
+                // Verify that I cannot connect to the RMIRegistry using a non-loopback address.
+                new Socket(localHost, address.getPort());
+                Assert.fail();
+            }
+            catch (ConnectException ignored)
+            {
+                // Ignored.
+            }
+        }
+
+        InetAddress loopback = InetAddress.getLoopbackAddress();
+        new Socket(loopback, address.getPort()).close();
+    }
+
+    @Test
+    public void testRMIServerPort() throws Exception
+    {
+        ServerSocket server = new ServerSocket(0);
+        int port = server.getLocalPort();
+        server.close();
+
+        connectorServer = new ConnectorServer(new JMXServiceURL("service:jmx:rmi://localhost:" + port + "/jndi/rmi:///jmxrmi"), objectName);
+        connectorServer.start();
+
+        JMXServiceURL address = connectorServer.getAddress();
+        Assert.assertEquals(port, address.getPort());
+
+        InetAddress loopback = InetAddress.getLoopbackAddress();
+        new Socket(loopback, port).close();
     }
 }
