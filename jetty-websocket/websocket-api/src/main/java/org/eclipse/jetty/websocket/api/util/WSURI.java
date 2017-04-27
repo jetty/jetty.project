d216206fcd6c7f16326f91a8c2503dca8171caff
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2017-04-27 13:18:14 -0700

    Issue #207 - Adding missing JSR356 Server @PathParam ArgIdentifier

--- jetty-websocket/websocket-api/src/main/java/org/eclipse/jetty/websocket/api/util/WSURI.java
+++ jetty-websocket/websocket-api/src/main/java/org/eclipse/jetty/websocket/api/util/WSURI.java
@@ -139,25 +139,21 @@ public final class WSURI
         {
             // keep as-is
             wsScheme = httpScheme;
-            port = 80;
         }
         else if ("wss".equalsIgnoreCase(httpScheme))
         {
             // keep as-is
             wsScheme = httpScheme;
-            port = 443;
         }
         else if ("http".equalsIgnoreCase(httpScheme))
         {
             // convert to ws
             wsScheme = "ws";
-            port = 80;
         }
         else if ("https".equalsIgnoreCase(httpScheme))
         {
             // convert to wss
             wsScheme = "wss";
-            port = 443;
         }
         else
         {
