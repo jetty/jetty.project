58492b3ebc5c6044cec7a19e066cca55e727bfff
Greg Wilkins <gregw@webtide.com>
2017-05-02 12:14:00 +0200

    fixed merge

--- jetty-util/src/test/java/org/eclipse/jetty/util/RolloverFileOutputStreamTest.java
+++ jetty-util/src/test/java/org/eclipse/jetty/util/RolloverFileOutputStreamTest.java
@@ -83,13 +83,14 @@ public class RolloverFileOutputStreamTest
     public void testMidnightRolloverCalc_PDT_Issue1507()
     {
         ZoneId zone = toZoneId("PST");
-        ZonedDateTime initialDate = toDateTime("2017.04.27-08:00:00.0 PM PDT", zone);
+        ZonedDateTime initialDate = toDateTime("2017.04.26-08:00:00.0 PM PDT", zone);
         
         ZonedDateTime midnight = RolloverFileOutputStream.toMidnight(initialDate);
         assertThat("Midnight", toString(midnight), is("2017.04.27-12:00:00.0 AM PDT"));
         
         Object expected[][] = {
-                {"2017.04.28-12:00:00.0 AM PDT", 14_400_000L}, // Ensure not negative
+                {"2017.04.27-12:00:00.0 AM PDT", 14_400_000L}, 
+                {"2017.04.28-12:00:00.0 AM PDT", 86_400_000L}, 
                 {"2017.04.29-12:00:00.0 AM PDT", 86_400_000L},
                 {"2017.04.30-12:00:00.0 AM PDT", 86_400_000L},
                 {"2017.05.01-12:00:00.0 AM PDT", 86_400_000L},
