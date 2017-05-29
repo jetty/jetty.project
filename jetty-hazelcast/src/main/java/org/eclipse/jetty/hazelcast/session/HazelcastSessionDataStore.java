b8e4f72e38412c106e7d7f54f034c938fdfcc7f7
olivier lamy <olamy@webtide.com>
2017-05-29 14:47:18 +1000

    add Hazelcast session management #1571
    Signed-off-by: olivier lamy <olamy@webtide.com>

---
+++ jetty-hazelcast/src/main/java/org/eclipse/jetty/hazelcast/session/HazelcastSessionDataStore.java
@@ -0,0 +1,211 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2017 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.hazelcast.session;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import org.eclipse.jetty.server.session.AbstractSessionDataStore;
+import org.eclipse.jetty.server.session.SessionContext;
+import org.eclipse.jetty.server.session.SessionData;
+import org.eclipse.jetty.server.session.SessionDataMap;
+import org.eclipse.jetty.server.session.SessionDataStore;
+import org.eclipse.jetty.util.annotation.ManagedObject;
+import org.eclipse.jetty.util.component.AbstractLifeCycle;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+/**
+ * Session data stored in Hazelcast
+ */
+@ManagedObject
+public class HazelcastSessionDataStore
+    extends AbstractSessionDataStore
+    implements SessionDataStore
+{
+
+    private  final static Logger LOG = Log.getLogger( "org.eclipse.jetty.server.session");
+
+    private HazelcastInstance hazelcastInstance;
+
+    private String jettySessionMapName;
+
+    private IMap<String, SessionData> sessionDataMap;
+
+    public HazelcastSessionDataStore()
+    {
+        // no op
+    }
+
+    public HazelcastSessionDataStore( HazelcastInstance hazelcastInstance, String jettySessionMapName )
+    {
+        this.hazelcastInstance = hazelcastInstance;
+        this.jettySessionMapName = jettySessionMapName;
+        this.sessionDataMap = hazelcastInstance.getMap( getJettySessionMapName() );
+    }
+
+    public HazelcastInstance getHazelcastInstance()
+    {
+        return hazelcastInstance;
+    }
+
+    public void setHazelcastInstance( HazelcastInstance hazelcastInstance )
+    {
+        this.hazelcastInstance = hazelcastInstance;
+    }
+
+    public String getJettySessionMapName()
+    {
+        return jettySessionMapName;
+    }
+
+    public void setJettySessionMapName( String jettySessionMapName )
+    {
+        this.jettySessionMapName = jettySessionMapName;
+    }
+
+
+    @Override
+    public SessionData load( String id )
+        throws Exception
+    {
+        return sessionDataMap == null ? null : sessionDataMap.get( id );
+    }
+
+    @Override
+    public boolean delete( String id )
+        throws Exception
+    {
+        return sessionDataMap == null ? false : sessionDataMap.remove( id ) != null;
+    }
+
+    @Override
+    public void initialize( SessionContext context )
+        throws Exception
+    {
+        _context = context;
+        if (this.sessionDataMap == null)
+        {
+            this.sessionDataMap = getHazelcastInstance().getMap( getJettySessionMapName() );
+        }
+    }
+
+    @Override
+    public void store( String id, SessionData data )
+        throws Exception
+    {
+        this.sessionDataMap.put( id, data );
+    }
+
+    @Override
+    public void doStore( String id, SessionData data, long lastSaveTime )
+        throws Exception
+    {
+        this.sessionDataMap.put( id, data);
+    }
+
+    @Override
+    public boolean isPassivating()
+    {
+        return false;
+    }
+
+    @Override
+    public Set<String> doGetExpired( Set<String> candidates )
+    {
+        if (candidates == null || candidates.isEmpty())
+        {
+            return Collections.emptySet();
+        }
+        long now = System.currentTimeMillis();
+        return candidates.stream().filter( candidate -> {
+            if (LOG.isDebugEnabled())
+            {
+                LOG.debug( "Checking expiry for candidate {}", candidate );
+            }
+            try
+            {
+                SessionData sd = load(candidate);
+
+                //if the session no longer exists
+                if (sd == null)
+                {
+                    if (LOG.isDebugEnabled())
+                    {
+                        LOG.debug( "Session {} does not exist in infinispan", candidate );
+                    }
+                    return true;
+                }
+                else
+                {
+                    if (_context.getWorkerName().equals(sd.getLastNode()))
+                    {
+                        //we are its manager, add it to the expired set if it is expired now
+                        if ((sd.getExpiry() > 0 ) && sd.getExpiry() <= now)
+                        {
+                            if (LOG.isDebugEnabled())
+                            {
+                                LOG.debug( "Session {} managed by {} is expired", candidate, _context.getWorkerName() );
+                            }
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        //if we are not the session's manager, only expire it iff:
+                        // this is our first expiryCheck and the session expired a long time ago
+                        //or
+                        //the session expired at least one graceperiod ago
+                        if (_lastExpiryCheckTime <=0)
+                        {
+                            if ((sd.getExpiry() > 0 ) && sd.getExpiry() < (now - (1000L * (3 * _gracePeriodSec))))
+                            {
+                                return true;
+                            }
+                        }
+                        else
+                        {
+                            if ((sd.getExpiry() > 0 ) && sd.getExpiry() < (now - (1000L * _gracePeriodSec)))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                LOG.warn("Error checking if candidate {} is expired so expire it", candidate, e);
+                return true;
+            }
+            return false;
+        } ).collect( Collectors.toSet() );
+    }
+
+    @Override
+    public boolean exists( String id )
+        throws Exception
+    {
+        return this.sessionDataMap.containsKey( id );
+    }
+}
