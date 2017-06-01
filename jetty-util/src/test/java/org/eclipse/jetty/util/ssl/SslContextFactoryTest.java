189cc4e369a0238828bfe71c11a87ee15e3e7280
Greg Wilkins <gregw@webtide.com>
2017-06-01 13:57:20 +0200

    Issue #1468 Configure PKIX Revocation Checker for SslContextFactory

--- jetty-util/src/test/java/org/eclipse/jetty/util/ssl/SslContextFactoryTest.java
+++ jetty-util/src/test/java/org/eclipse/jetty/util/ssl/SslContextFactoryTest.java
@@ -52,6 +52,15 @@ public class SslContextFactoryTest
     public void setUp() throws Exception
     {
         cf = new SslContextFactory();
+        
+        java.security.cert.CertPathBuilder certPathBuilder = java.security.cert.CertPathBuilder.getInstance("PKIX");
+        java.security.cert.PKIXRevocationChecker revocationChecker = (java.security.cert.PKIXRevocationChecker) certPathBuilder.getRevocationChecker();
+        revocationChecker.setOptions(java.util.EnumSet.of(
+            java.security.cert.PKIXRevocationChecker.Option.valueOf("PREFER_CRLS"),
+            java.security.cert.PKIXRevocationChecker.Option.valueOf("SOFT_FAIL"),
+            java.security.cert.PKIXRevocationChecker.Option.valueOf("NO_FALLBACK")));
+        cf.setPkixCertPathChecker(revocationChecker);
+        
     }
 
     @Test
