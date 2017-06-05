0ac5e569d4e3e620d884bae61cb3b6b064848283
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2017-06-05 10:37:41 -0700

    Fixing merge problems with issue-207 -> master

--- jetty-websocket/websocket-server/src/main/java/org/eclipse/jetty/websocket/server/WebSocketServerFactory.java
+++ jetty-websocket/websocket-server/src/main/java/org/eclipse/jetty/websocket/server/WebSocketServerFactory.java
@@ -634,7 +634,7 @@ public class WebSocketServerFactory extends ContainerLifeCycle implements WebSoc
     {
         final StringBuilder sb = new StringBuilder(this.getClass().getSimpleName());
         sb.append('@').append(Integer.toHexString(hashCode()));
-        sb.append("[defaultPolicy=").append(defaultPolicy);
+        sb.append("[defaultPolicy=").append(containerPolicy);
         sb.append(",creator=").append(creator.getClass().getName());
         sb.append("]");
         return sb.toString();
