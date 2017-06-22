a7e4f920546f5cab83c4fcd874600bd614a866bf
S K <xz64@users.noreply.github.com>
2017-06-21 23:18:30 -0700

    Issue #1622 - HeaderFilter does not work if response has been committed (#1628)
    Signed-off-by: S K (xz64) <xz64.xyz@gmail.com>

--- jetty-servlets/src/main/java/org/eclipse/jetty/servlets/HeaderFilter.java
+++ jetty-servlets/src/main/java/org/eclipse/jetty/servlets/HeaderFilter.java
@@ -80,42 +80,40 @@ public class HeaderFilter extends IncludeExcludeBasedFilter
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
     {
-        chain.doFilter(request,response);
-
         HttpServletRequest http_request = (HttpServletRequest)request;
         HttpServletResponse http_response = (HttpServletResponse)response;
 
-        if (!super.shouldFilter(http_request,http_response))
+        if (super.shouldFilter(http_request,http_response))
         {
-            return;
+            for (ConfiguredHeader header : _configuredHeaders)
+            {
+                if (header.isDate())
+                {
+                    long header_value = System.currentTimeMillis() + header.getMsOffset();
+                    if (header.isAdd())
+                    {
+                        http_response.addDateHeader(header.getName(),header_value);
+                    }
+                    else
+                    {
+                        http_response.setDateHeader(header.getName(),header_value);
+                    }
+                }
+                else // constant header value
+                {
+                    if (header.isAdd())
+                    {
+                        http_response.addHeader(header.getName(),header.getValue());
+                    }
+                    else
+                    {
+                        http_response.setHeader(header.getName(),header.getValue());
+                    }
+                }
+            }
         }
 
-        for (ConfiguredHeader header : _configuredHeaders)
-        {
-            if (header.isDate())
-            {
-                long header_value = System.currentTimeMillis() + header.getMsOffset();
-                if (header.isAdd())
-                {
-                    http_response.addDateHeader(header.getName(),header_value);
-                }
-                else
-                {
-                    http_response.setDateHeader(header.getName(),header_value);
-                }
-            }
-            else // constant header value
-            {
-                if (header.isAdd())
-                {
-                    http_response.addHeader(header.getName(),header.getValue());
-                }
-                else
-                {
-                    http_response.setHeader(header.getName(),header.getValue());
-                }
-            }
-        }
+        chain.doFilter(request,response);
     }
 
     @Override
