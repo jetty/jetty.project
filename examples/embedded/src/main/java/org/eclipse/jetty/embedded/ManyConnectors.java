22bc4f79e200b9131546bd69339490f10df26190
Greg Wilkins <gregw@webtide.com>
2017-09-06 12:39:32 +1000

    remove conscrypt testing

--- examples/embedded/src/main/java/org/eclipse/jetty/embedded/ManyConnectors.java
+++ examples/embedded/src/main/java/org/eclipse/jetty/embedded/ManyConnectors.java
@@ -20,12 +20,8 @@ package org.eclipse.jetty.embedded;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Method;
 import java.security.Provider;
 import java.security.Security;
-import java.util.function.BiFunction;
-
-import javax.net.ssl.SSLEngine;
 
 import org.eclipse.jetty.http.HttpVersion;
 import org.eclipse.jetty.server.Connector;
@@ -43,7 +39,7 @@ import org.eclipse.jetty.util.ssl.SslContextFactory;
 public class ManyConnectors
 {
     public static void main( String[] args ) throws Exception
-    {
+    {        
         // Since this example shows off SSL configuration, we need a keystore
         // with the appropriate key. These lookup of jetty.home is purely a hack
         // to get access to a keystore that we use in many unit tests and should
@@ -97,7 +93,6 @@ public class ManyConnectors
         sslContextFactory.setKeyStorePath(keystoreFile.getAbsolutePath());
         sslContextFactory.setKeyStorePassword("OBF:1vny1zlo1x8e1vnw1vn61x8g1zlu1vn4");
         sslContextFactory.setKeyManagerPassword("OBF:1u2u1wml1z7s1z7a1wnl1u2g");
-        sslContextFactory.setProvider("Conscrypt");
 
         // HTTPS Configuration
         // A new HttpConfiguration object is needed for the next connector and
@@ -136,18 +131,6 @@ public class ManyConnectors
 
         // Start the server
         server.start();
-        
-        SSLEngine engine = sslContextFactory.newSSLEngine();
-        System.err.println(engine);
-        System.err.println(engine.getClass());
-        for (Method m : engine.getClass().getMethods())
-        {
-            System.err.println(m);
-        }
-        
-        
-        
-        
         
         server.join();
     }
