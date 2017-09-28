d166881a5a6bfbcb40e5d2734898eec3c74b8050
Greg Wilkins <gregw@webtide.com>
2017-09-28 12:44:23 +1000

    Issue #1806 Concurrent ReservedThreadExcecutor
    Squashed commit of the following:
    
    commit ab7f711c30ad1408ecdceffdca51cf167dc11a9c
    Author: Greg Wilkins <gregw@webtide.com>
    Date:   Thu Sep 28 10:20:08 2017 +1000
    
        Fixed format
    
    commit 062b051b56efb9924b81cf755297f538e4691851
    Merge: d1e27d4 7d98cbb
    Author: Greg Wilkins <gregw@webtide.com>
    Date:   Thu Sep 28 09:19:40 2017 +1000
    
        Merge branch 'jetty-9.4.x' into jetty-9.4.x-1806-ConcurrentReservedThreadExecutor
    
    commit d1e27d42983487941c6dd7cb0ea1068f36ff92f0
    Author: Greg Wilkins <gregw@webtide.com>
    Date:   Tue Sep 26 11:28:21 2017 +1000
    
        improvements after review
    
    commit 477e3ac05610825cc834d3195e39e8e8620a44cb
    Merge: 51a3ac3 35d0b59
    Author: Greg Wilkins <gregw@webtide.com>
    Date:   Tue Sep 26 10:10:30 2017 +1000
    
        Merge branch 'jetty-9.4.x' into jetty-9.4.x-1806-ConcurrentReservedThreadExecutor
    
    commit 51a3ac37e63108ca93fe2fdd9e02e7f8161072ba
    Author: Greg Wilkins <gregw@webtide.com>
    Date:   Tue Sep 26 10:08:55 2017 +1000
    
        improvements after review
    
    commit 23df855bf457fe609576a35327db3fbd4c82b491
    Merge: b52c6de 5764afc
    Author: Greg Wilkins <gregw@webtide.com>
    Date:   Tue Sep 26 09:30:42 2017 +1000
    
        Merge branch 'jetty-9.4.x' into jetty-9.4.x-1806-ConcurrentReservedThreadExecutor
    
    commit b52c6de6573433a960fdb6f2692e7bbd9effb48c
    Author: Greg Wilkins <gregw@webtide.com>
    Date:   Fri Sep 22 15:36:53 2017 +1000
    
        Issue #1806
    
        Added a concurrent stack class
        Converted ReservedThreadExcecutor to use the concurrent stack
        Added support for idle

--- jetty-util/src/test/java/org/eclipse/jetty/util/thread/ReservedThreadExecutorTest.java
+++ jetty-util/src/test/java/org/eclipse/jetty/util/thread/ReservedThreadExecutorTest.java
@@ -18,24 +18,44 @@
 
 package org.eclipse.jetty.util.thread;
 
+import java.security.SecureRandom;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.StreamSupport;
 
+import org.eclipse.jetty.toolchain.test.annotation.Stress;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class ReservedThreadExecutorTest
 {
     private static final int SIZE = 2;
-    private static final Runnable NOOP = () -> {};
+    private static final Runnable NOOP = new Runnable()
+    {
+        @Override
+        public void run()
+        {}
+
+        @Override
+        public String toString()
+        {
+            return "NOOP!";
+        }
+    };
 
     private TestExecutor _executor;
     private ReservedThreadExecutor _reservedExecutor;
@@ -127,17 +147,87 @@ public class ReservedThreadExecutorTest
         waitForAllAvailable();
     }
 
-    protected void waitForAllAvailable() throws InterruptedException
+
+    @Test
+    public void testShrink() throws Exception
+    {
+        final long IDLE = 1000;
+
+        _reservedExecutor.stop();
+        _reservedExecutor.setIdleTimeout(IDLE,TimeUnit.MILLISECONDS);
+        _reservedExecutor.start();
+
+        // Reserved threads are lazily started.
+        assertThat(_executor._queue.size(), is(0));
+
+        assertThat(_reservedExecutor.tryExecute(NOOP),is(false));
+        _executor.execute();
+        waitForNoPending();
+
+        CountDownLatch latch = new CountDownLatch(1);
+        Runnable waitForLatch = ()->{try {latch.await();} catch(Exception e){}};
+        assertThat(_reservedExecutor.tryExecute(waitForLatch),is(true));
+        _executor.execute();
+
+        assertThat(_reservedExecutor.tryExecute(NOOP),is(false));
+        _executor.execute();
+        waitForNoPending();
+
+        latch.countDown();
+        waitForAvailable(2);
+
+        // Check that regular moderate activity keeps the pool a moderate size
+        TimeUnit.MILLISECONDS.sleep(IDLE/2);
+        assertThat(_reservedExecutor.tryExecute(NOOP),is(true));
+        waitForAvailable(2);
+        TimeUnit.MILLISECONDS.sleep(IDLE/2);
+        assertThat(_reservedExecutor.tryExecute(NOOP),is(true));
+        waitForAvailable(1);
+        TimeUnit.MILLISECONDS.sleep(IDLE/2);
+        assertThat(_reservedExecutor.tryExecute(NOOP),is(true));
+        waitForAvailable(1);
+        TimeUnit.MILLISECONDS.sleep(IDLE/2);
+        assertThat(_reservedExecutor.tryExecute(NOOP),is(true));
+        waitForAvailable(1);
+        TimeUnit.MILLISECONDS.sleep(IDLE/2);
+        assertThat(_reservedExecutor.tryExecute(NOOP),is(true));
+        waitForAvailable(1);
+
+        // check fully idle goes to zero
+        TimeUnit.MILLISECONDS.sleep(IDLE);
+        assertThat(_reservedExecutor.getAvailable(),is(0));
+
+    }
+
+    protected void waitForNoPending() throws InterruptedException
     {
         long started = System.nanoTime();
-        while (_reservedExecutor.getAvailable() < SIZE)
+        while (_reservedExecutor.getPending() > 0)
+        {
+            long elapsed = System.nanoTime() - started;
+            if (elapsed > TimeUnit.SECONDS.toNanos(10))
+                Assert.fail("pending="+_reservedExecutor.getPending());
+            Thread.sleep(10);
+        }
+        assertThat(_reservedExecutor.getPending(), is(0));
+    }
+
+    protected void waitForAvailable(int size) throws InterruptedException
+    {
+        long started = System.nanoTime();
+        while (_reservedExecutor.getAvailable() < size)
         {
             long elapsed = System.nanoTime() - started;
             if (elapsed > TimeUnit.SECONDS.toNanos(10))
                 Assert.fail();
             Thread.sleep(10);
         }
-        assertThat(_reservedExecutor.getAvailable(), is(SIZE));
+        assertThat(_reservedExecutor.getAvailable(), is(size));
+    }
+
+    protected void waitForAllAvailable() throws InterruptedException
+    {
+        waitForAvailable(SIZE);
     }
 
     private static class TestExecutor implements Executor
@@ -176,5 +266,74 @@ public class ReservedThreadExecutorTest
                 e.printStackTrace();
             }
         }
+    }
+
+    @Ignore
+    @Test
+    public void stressTest() throws Exception
+    {
+        QueuedThreadPool pool = new QueuedThreadPool(20);
+        pool.setStopTimeout(10000);
+        pool.start();
+        ReservedThreadExecutor reserved = new ReservedThreadExecutor(pool,10);
+        reserved.setIdleTimeout(0,null);
+        reserved.start();
+
+        final int LOOPS = 1000000;
+        final Random random = new Random();
+        final AtomicInteger executions = new AtomicInteger(LOOPS);
+        final CountDownLatch executed = new CountDownLatch(executions.get());
+        final AtomicInteger usedReserved = new AtomicInteger(0);
+        final AtomicInteger usedPool = new AtomicInteger(0);
+
+        Runnable task = new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
+                    while (true)
+                    {
+                        int loops = executions.get();
+                        if (loops <= 0)
+                            return;
+
+                        if (executions.compareAndSet(loops, loops - 1))
+                        {
+                            if (reserved.tryExecute(this))
+                            {
+                                usedReserved.incrementAndGet();
+                            } else
+                            {
+                                usedPool.incrementAndGet();
+                                pool.execute(this);
+                            }
+                            return;
+                        }
+                    }
+                }
+                finally
+                {
+                    executed.countDown();
+                }
+            }
+        };
+
+        task.run();
+        task.run();
+        task.run();
+        task.run();
+        task.run();
+        task.run();
+        task.run();
+        task.run();
+
+        assertTrue(executed.await(60,TimeUnit.SECONDS));
+
+        reserved.stop();
+        pool.stop();
+
+        assertThat(usedReserved.get()+usedPool.get(),is(LOOPS));
+        System.err.printf("reserved=%d pool=%d total=%d%n",usedReserved.get(),usedPool.get(),LOOPS);
     }
 }
