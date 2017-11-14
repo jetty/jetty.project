0a9d74c1e0323f6e160e60167f471f5ff169e0c1
Greg Wilkins <gregw@webtide.com>
2017-11-14 19:11:27 +1100

    Issue #922 (#1965) bytesIn/Out stats
    Ensure -1 fill is not appended to bytesIn counts
    Don't include discarded bytes from head responses in counts
    refactored gather writes in HttpConnection to be clearer and removed
    redundant buffer checks

--- jetty-client/src/main/java/org/eclipse/jetty/client/http/HttpChannelOverHTTP.java
+++ jetty-client/src/main/java/org/eclipse/jetty/client/http/HttpChannelOverHTTP.java
@@ -84,8 +84,8 @@ public class HttpChannelOverHTTP extends HttpChannel
         HttpExchange exchange = getHttpExchange();
         if (exchange != null)
         {
-            sender.send( exchange );
             outMessages.increment();
+            sender.send( exchange );
         }
     }
 
