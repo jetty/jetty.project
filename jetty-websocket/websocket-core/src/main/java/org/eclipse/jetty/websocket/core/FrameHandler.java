340741506d8c56cf707c661e74c8d0582bb01663
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2018-01-17 10:58:42 -0600

    More javadoc and TODOs for FrameHandler

--- jetty-websocket/websocket-core/src/main/java/org/eclipse/jetty/websocket/core/FrameHandler.java
+++ jetty-websocket/websocket-core/src/main/java/org/eclipse/jetty/websocket/core/FrameHandler.java
@@ -27,11 +27,46 @@ import org.eclipse.jetty.util.Callback;
  */
 public interface FrameHandler
 {
+    /**
+     * Connection is being opened.
+     * <p>
+     *     FrameHandler can write during this call, but will not receive frames until
+     *     the onOpen() completes.
+     * </p>
+     * @param channel the channel associated with this connection.
+     * @throws Exception if unable to open. TODO: will close the connection (optionally choosing close status code based on WebSocketException type)?
+     */
     void onOpen(Channel channel) throws Exception;
+
+    /**
+     * Receiver of all DATA Frames (Text, Binary, Continuation), and all CONTROL Frames (Ping, Pong, Close)
+
+     * @param frame the raw frame
+     * @param callback the callback to indicate success in processing frame (or failure)
+     * @throws Exception if unable to process the frame.  TODO: will close the connection (optionally choosing close status code based on WebSocketException type)?
+     */
     void onFrame(Frame frame, Callback callback) throws Exception;
+
+    /**
+     * This is the Close Handshake Complete event.
+     * <p>
+     *     The connection is now closed, no reading or writing is possible anymore.
+     *     Implementations of FrameHandler can cleanup their resources for this connection now.
+     * </p>
+     *
+     * @param closeStatus the close status received from remote, or in the case of abnormal closure from local.
+     * @throws Exception if unable to complete the closure. TODO: what happens if an exception occurs here?
+     */
     void onClosed(CloseStatus closeStatus) throws Exception;
+
+    /**
+     * An error has occurred or been detected in websocket-core and being reported to FrameHandler.
+     *
+     * @param cause the reason for the error
+     * @throws Exception if unable to process the error. TODO: what hapens if an exception occurs here?  does any error means a connection is (or will be) closed?
+     */
     void onError(Throwable cause) throws Exception;
-    
+
     interface Channel extends OutgoingFrames
     {        
         String getSubprotocol();
