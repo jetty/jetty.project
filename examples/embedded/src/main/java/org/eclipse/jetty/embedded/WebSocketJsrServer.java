617c97c91d5bf6ea8b467cc11c900531a02c3c5f
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2018-01-19 10:54:00 -0600

    Fixing embedded examples

--- examples/embedded/src/main/java/org/eclipse/jetty/embedded/WebSocketJsrServer.java
+++ examples/embedded/src/main/java/org/eclipse/jetty/embedded/WebSocketJsrServer.java
@@ -20,11 +20,11 @@ package org.eclipse.jetty.embedded;
 
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
+import javax.websocket.server.ServerContainer;
 import javax.websocket.server.ServerEndpoint;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.websocket.jsr356.server.ServerContainer;
 import org.eclipse.jetty.websocket.jsr356.server.JavaxWebSocketServerContainerInitializer;
 
 /**
