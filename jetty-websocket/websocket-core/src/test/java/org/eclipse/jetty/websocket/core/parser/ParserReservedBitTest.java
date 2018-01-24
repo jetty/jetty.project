6d930af735d08ef1a25ffd84a52c923a5e1e604e
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2018-01-24 17:35:45 -0600

    Adding RSV bit tests
    + Moving RSV parser tests to websocket-core

--- jetty-websocket/websocket-core/src/test/java/org/eclipse/jetty/websocket/core/parser/ParserReservedBitTest.java
+++ jetty-websocket/websocket-core/src/test/java/org/eclipse/jetty/websocket/core/parser/ParserReservedBitTest.java
@@ -16,50 +16,83 @@
 //  ========================================================================
 //
 
-package org.eclipse.jetty.websocket.tests.server;
+package org.eclipse.jetty.websocket.core.parser;
 
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.eclipse.jetty.io.ByteBufferPool;
+import org.eclipse.jetty.io.MappedByteBufferPool;
+import org.eclipse.jetty.util.BufferUtil;
 import org.eclipse.jetty.util.log.StacklessLogging;
-import org.eclipse.jetty.websocket.api.StatusCode;
+import org.eclipse.jetty.websocket.core.Generator;
+import org.eclipse.jetty.websocket.core.Parser;
+import org.eclipse.jetty.websocket.core.ProtocolException;
+import org.eclipse.jetty.websocket.core.WebSocketBehavior;
+import org.eclipse.jetty.websocket.core.WebSocketPolicy;
 import org.eclipse.jetty.websocket.core.frames.BinaryFrame;
 import org.eclipse.jetty.websocket.core.frames.CloseFrame;
 import org.eclipse.jetty.websocket.core.frames.PingFrame;
 import org.eclipse.jetty.websocket.core.frames.TextFrame;
 import org.eclipse.jetty.websocket.core.frames.WebSocketFrame;
-import org.eclipse.jetty.websocket.tests.LocalFuzzer;
-import org.eclipse.jetty.websocket.tests.server.servlets.EchoSocket;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * Test various RSV violations
  */
-public class ReservedBitTest extends AbstractLocalServerCase
+public class ParserReservedBitTest
 {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    private WebSocketPolicy policy = new WebSocketPolicy(WebSocketBehavior.CLIENT);
+    private ByteBufferPool bufferPool = new MappedByteBufferPool();
+    private boolean validatingGenerator = false;
+
+    private void expectProtocolException(List<WebSocketFrame> frames)
+    {
+        ParserCapture parserCapture = new ParserCapture();
+        Parser parser = new Parser(policy, bufferPool, parserCapture);
+
+        // generate raw bytebuffer of provided frames
+        int size = frames.stream().mapToInt(frame -> frame.getPayloadLength() + Generator.MAX_HEADER_LENGTH).sum();
+        ByteBuffer raw = BufferUtil.allocate(size);
+        BufferUtil.clearToFill(raw);
+        Generator generator = new Generator(policy, bufferPool, validatingGenerator);
+        frames.forEach(frame -> generator.generateWholeFrame(frame, raw));
+        BufferUtil.flipToFlush(raw, 0);
+
+        // parse buffer
+        try (StacklessLogging ignore = new StacklessLogging(Parser.class))
+        {
+            expectedException.expect(ProtocolException.class);
+            expectedException.expectMessage(allOf(containsString("RSV"),containsString("not allowed to be set")));
+            parser.parse(raw);
+        }
+    }
+
     /**
      * Send small text frame, with RSV1 == true, with no extensions defined.
      * <p>
      * From Autobahn WebSocket Server Testcase 3.1
      * </p>
+     *
      * @throws Exception on test failure
      */
     @Test
-    public void testCase3_1() throws Exception
+    public void testCase3_1()
     {
         List<WebSocketFrame> send = new ArrayList<>();
         send.add(new TextFrame().setPayload("small").setRsv1(true)); // intentionally bad
 
-        List<WebSocketFrame> expect = new ArrayList<>();
-        expect.add(new CloseFrame().setPayload(StatusCode.PROTOCOL.getCode()));
-
-        try (StacklessLogging ignored = new StacklessLogging(EchoSocket.class);
-             LocalFuzzer session = server.newLocalFuzzer())
-        {
-            session.sendBulk(send);
-            session.expect(expect);
-        }
+        expectProtocolException(send);
     }
 
     /**
@@ -67,26 +100,18 @@ public class ReservedBitTest extends AbstractLocalServerCase
      * <p>
      * From Autobahn WebSocket Server Testcase 3.2
      * </p>
+     *
      * @throws Exception on test failure
      */
     @Test
-    public void testCase3_2() throws Exception
+    public void testCase3_2()
     {
         List<WebSocketFrame> send = new ArrayList<>();
         send.add(new TextFrame().setPayload("small"));
         send.add(new TextFrame().setPayload("small").setRsv2(true)); // intentionally bad
         send.add(new PingFrame().setPayload("ping"));
 
-        List<WebSocketFrame> expect = new ArrayList<>();
-        expect.add(new TextFrame().setPayload("small")); // echo on good frame
-        expect.add(new CloseFrame().setPayload(StatusCode.PROTOCOL.getCode()));
-
-        try (StacklessLogging ignored = new StacklessLogging(EchoSocket.class);
-             LocalFuzzer session = server.newLocalFuzzer())
-        {
-            session.sendBulk(send);
-            session.expect(expect);
-        }
+        expectProtocolException(send);
     }
 
     /**
@@ -94,26 +119,18 @@ public class ReservedBitTest extends AbstractLocalServerCase
      * <p>
      * From Autobahn WebSocket Server Testcase 3.3
      * </p>
+     *
      * @throws Exception on test failure
      */
     @Test
-    public void testCase3_3() throws Exception
+    public void testCase3_3()
     {
         List<WebSocketFrame> send = new ArrayList<>();
         send.add(new TextFrame().setPayload("small"));
         send.add(new TextFrame().setPayload("small").setRsv1(true).setRsv2(true)); // intentionally bad
         send.add(new PingFrame().setPayload("ping"));
 
-        List<WebSocketFrame> expect = new ArrayList<>();
-        expect.add(new TextFrame().setPayload("small")); // echo on good frame
-        expect.add(new CloseFrame().setPayload(StatusCode.PROTOCOL.getCode()));
-
-        try (StacklessLogging ignored = new StacklessLogging(EchoSocket.class);
-             LocalFuzzer session = server.newLocalFuzzer())
-        {
-            session.sendFrames(send);
-            session.expect(expect);
-        }
+        expectProtocolException(send);
     }
 
     /**
@@ -121,26 +138,18 @@ public class ReservedBitTest extends AbstractLocalServerCase
      * <p>
      * From Autobahn WebSocket Server Testcase 3.4
      * </p>
+     *
      * @throws Exception on test failure
      */
     @Test
-    public void testCase3_4() throws Exception
+    public void testCase3_4()
     {
         List<WebSocketFrame> send = new ArrayList<>();
         send.add(new TextFrame().setPayload("small"));
         send.add(new TextFrame().setPayload("small").setRsv3(true)); // intentionally bad
         send.add(new PingFrame().setPayload("ping"));
 
-        List<WebSocketFrame> expect = new ArrayList<>();
-        expect.add(new TextFrame().setPayload("small")); // echo on good frame
-        expect.add(new CloseFrame().setPayload(StatusCode.PROTOCOL.getCode()));
-
-        try (StacklessLogging ignored = new StacklessLogging(EchoSocket.class);
-             LocalFuzzer session = server.newLocalFuzzer())
-        {
-            session.sendSegmented(send, 1);
-            session.expect(expect);
-        }
+        expectProtocolException(send);
     }
 
     /**
@@ -148,26 +157,19 @@ public class ReservedBitTest extends AbstractLocalServerCase
      * <p>
      * From Autobahn WebSocket Server Testcase 3.5
      * </p>
+     *
      * @throws Exception on test failure
      */
     @Test
-    public void testCase3_5() throws Exception
+    public void testCase3_5()
     {
         byte payload[] = new byte[8];
-        Arrays.fill(payload,(byte)0xFF);
+        Arrays.fill(payload, (byte) 0xFF);
 
         List<WebSocketFrame> send = new ArrayList<>();
         send.add(new BinaryFrame().setPayload(payload).setRsv3(true).setRsv1(true)); // intentionally bad
 
-        List<WebSocketFrame> expect = new ArrayList<>();
-        expect.add(new CloseFrame().setPayload(StatusCode.PROTOCOL.getCode()));
-
-        try (StacklessLogging ignored = new StacklessLogging(EchoSocket.class);
-             LocalFuzzer session = server.newLocalFuzzer())
-        {
-            session.sendBulk(send);
-            session.expect(expect);
-        }
+        expectProtocolException(send);
     }
 
     /**
@@ -175,26 +177,19 @@ public class ReservedBitTest extends AbstractLocalServerCase
      * <p>
      * From Autobahn WebSocket Server Testcase 3.6
      * </p>
+     *
      * @throws Exception on test failure
      */
     @Test
-    public void testCase3_6() throws Exception
+    public void testCase3_6()
     {
         byte payload[] = new byte[8];
-        Arrays.fill(payload,(byte)0xFF);
+        Arrays.fill(payload, (byte) 0xFF);
 
         List<WebSocketFrame> send = new ArrayList<>();
         send.add(new PingFrame().setPayload(payload).setRsv3(true).setRsv2(true)); // intentionally bad
 
-        List<WebSocketFrame> expect = new ArrayList<>();
-        expect.add(new CloseFrame().setPayload(StatusCode.PROTOCOL.getCode()));
-
-        try (StacklessLogging ignored = new StacklessLogging(EchoSocket.class);
-             LocalFuzzer session = server.newLocalFuzzer())
-        {
-            session.sendBulk(send);
-            session.expect(expect);
-        }
+        expectProtocolException(send);
     }
 
     /**
@@ -202,26 +197,19 @@ public class ReservedBitTest extends AbstractLocalServerCase
      * <p>
      * From Autobahn WebSocket Server Testcase 3.7
      * </p>
+     *
      * @throws Exception on test failure
      */
     @Test
-    public void testCase3_7() throws Exception
+    public void testCase3_7()
     {
         List<WebSocketFrame> send = new ArrayList<>();
-        WebSocketFrame frame = new CloseFrame().setPayload(StatusCode.NORMAL.getCode());
+        WebSocketFrame frame = new CloseFrame().setPayload(1000);
         frame.setRsv1(true);
         frame.setRsv2(true);
         frame.setRsv3(true);
         send.add(frame); // intentionally bad
 
-        List<WebSocketFrame> expect = new ArrayList<>();
-        expect.add(new CloseFrame().setPayload(StatusCode.PROTOCOL.getCode()));
-
-        try (StacklessLogging ignored = new StacklessLogging(EchoSocket.class);
-             LocalFuzzer session = server.newLocalFuzzer())
-        {
-            session.sendBulk(send);
-            session.expect(expect);
-        }
+        expectProtocolException(send);
     }
 }
