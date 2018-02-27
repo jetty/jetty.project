e84be792dfb63642a15ad06de5269ca1a244ac32
Simone Bordet <simone.bordet@gmail.com>
2018-02-27 17:35:44 +0100

    Improved implementation.
    Implemented name, thread priorities, thread group and daemon properties.
    Implemented toString(), dump() and using a thread factory.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-util/src/main/java/org/eclipse/jetty/util/thread/ExecutorSizedThreadPool.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/thread/ExecutorSizedThreadPool.java
@@ -18,13 +18,21 @@
 
 package org.eclipse.jetty.util.thread;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import org.eclipse.jetty.util.annotation.ManagedAttribute;
 import org.eclipse.jetty.util.annotation.ManagedObject;
 import org.eclipse.jetty.util.component.ContainerLifeCycle;
+import org.eclipse.jetty.util.component.Dumpable;
+import org.eclipse.jetty.util.component.DumpableCollection;
 
 /**
  * A {@link org.eclipse.jetty.util.thread.ThreadPool.SizedThreadPool} wrapper around {@link ThreadPoolExecutor}.
@@ -33,10 +41,15 @@ import org.eclipse.jetty.util.component.ContainerLifeCycle;
 public class ExecutorSizedThreadPool extends ContainerLifeCycle implements ThreadPool.SizedThreadPool, TryExecutor
 {
     private final ThreadPoolExecutor _executor;
+    private final ThreadPoolBudget _budget;
+    private final ThreadGroup _group;
+    private String _name = "etp" + hashCode();
     private int _minThreads;
     private int _reservedThreads = -1;
     private TryExecutor _tryExecutor = TryExecutor.NO_TRY;
-    private ThreadPoolBudget _budget;
+    private int _priority = Thread.NORM_PRIORITY;
+    private boolean _daemon;
+    private boolean _detailedDump;
 
     public ExecutorSizedThreadPool()
     {
@@ -45,9 +58,9 @@ public class ExecutorSizedThreadPool extends ContainerLifeCycle implements Threa
 
     public ExecutorSizedThreadPool(int maxThreads, int minThreads)
     {
-        this(new ThreadPoolExecutor(maxThreads, maxThreads, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<>()),minThreads,-1);
+        this(new ThreadPoolExecutor(maxThreads, maxThreads, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<>()), minThreads, -1, null);
     }
-    
+
     public ExecutorSizedThreadPool(ThreadPoolExecutor executor)
     {
         this(executor, -1);
@@ -55,14 +68,47 @@ public class ExecutorSizedThreadPool extends ContainerLifeCycle implements Threa
 
     public ExecutorSizedThreadPool(ThreadPoolExecutor executor, int reservedThreads)
     {
-        this(executor, Math.min(Runtime.getRuntime().availableProcessors(),executor.getCorePoolSize()),-1);
+        this(executor, reservedThreads, null);
     }
-    
-    private ExecutorSizedThreadPool(ThreadPoolExecutor executor, int minThreads, int reservedThreads)
+
+    public ExecutorSizedThreadPool(ThreadPoolExecutor executor, int reservedThreads, ThreadGroup group)
     {
+        this(executor, Math.min(Runtime.getRuntime().availableProcessors(), executor.getCorePoolSize()), reservedThreads, group);
+    }
+
+    private ExecutorSizedThreadPool(ThreadPoolExecutor executor, int minThreads, int reservedThreads, ThreadGroup group)
+    {
+        int maxThreads = executor.getMaximumPoolSize();
+        if (maxThreads < minThreads)
+        {
+            executor.shutdownNow();
+            throw new IllegalArgumentException("max threads (" + maxThreads + ") cannot be less than min threads (" + minThreads + ")");
+        }
         _executor = executor;
+        _executor.setThreadFactory(this::newThread);
+        _budget = new ThreadPoolBudget(this);
+        _group = group;
         _minThreads = minThreads;
         _reservedThreads = reservedThreads;
+    }
+
+    /**
+     * @return the name of the this thread pool
+     */
+    @ManagedAttribute("name of this thread pool")
+    public String getName()
+    {
+        return _name;
+    }
+
+    /**
+     * @param name the name of this thread pool, used to name threads
+     */
+    public void setName(String name)
+    {
+        if (isRunning())
+            throw new IllegalStateException(getState());
+        _name = name;
     }
 
     @Override
@@ -73,41 +119,16 @@ public class ExecutorSizedThreadPool extends ContainerLifeCycle implements Threa
     }
 
     @Override
+    public void setMinThreads(int threads)
+    {
+        _minThreads = threads;
+    }
+
+    @Override
     @ManagedAttribute("maximum number of threads in the pool")
     public int getMaxThreads()
     {
         return _executor.getMaximumPoolSize();
-    }
-
-    /**
-     * Get the maximum thread idle time.
-     *
-     * @return Max idle time in ms.
-     * @see #setIdleTimeout
-     */
-    @ManagedAttribute("maximum time a thread may be idle in ms")
-    public int getIdleTimeout()
-    {
-        return (int)_executor.getKeepAliveTime(TimeUnit.MILLISECONDS);
-    }
-    
-    /**
-     * Set the number of reserved threads.
-     *
-     * @param reservedThreads number of reserved threads or -1 for heuristically determined 
-     * @see #getReservedThreads
-     */
-    public void setReservedThreads(int reservedThreads)
-    {
-        if (isRunning())
-            throw new IllegalStateException(getState());
-        _reservedThreads = reservedThreads;
-    }
-    
-    @Override
-    public void setMinThreads(int threads)
-    {
-        _minThreads = threads;
     }
 
     @Override
@@ -118,20 +139,93 @@ public class ExecutorSizedThreadPool extends ContainerLifeCycle implements Threa
     }
 
     /**
-     * Set the maximum thread idle time.
-     * Threads that are idle for longer than this period may be
-     * stopped.
+     * @return the maximum thread idle time in ms.
+     * @see #setIdleTimeout(int)
+     */
+    @ManagedAttribute("maximum time a thread may be idle in ms")
+    public int getIdleTimeout()
+    {
+        return (int)_executor.getKeepAliveTime(TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * <p>Sets the maximum thread idle time in ms.</p>
+     * <p>Threads that are idle for longer than this
+     * period may be stopped.</p>
      *
-     * @param idleTimeout Max idle time in ms.
-     * @see #getIdleTimeout
+     * @param idleTimeout the maximum thread idle time in ms.
+     * @see #getIdleTimeout()
      */
     public void setIdleTimeout(int idleTimeout)
     {
-        _executor.setKeepAliveTime(idleTimeout,TimeUnit.MILLISECONDS);
+        _executor.setKeepAliveTime(idleTimeout, TimeUnit.MILLISECONDS);
     }
-    
-    
-    
+
+    /**
+     * @return number of reserved threads or -1 to indicate that the number is heuristically determined
+     * @see #setReservedThreads(int)
+     */
+    @ManagedAttribute("the number of reserved threads in the pool")
+    public int getReservedThreads()
+    {
+        if (isStarted())
+            return getBean(ReservedThreadExecutor.class).getCapacity();
+        return _reservedThreads;
+    }
+
+    /**
+     * Sets the number of reserved threads.
+     *
+     * @param reservedThreads number of reserved threads or -1 to determine the number heuristically
+     * @see #getReservedThreads()
+     */
+    public void setReservedThreads(int reservedThreads)
+    {
+        if (isRunning())
+            throw new IllegalStateException(getState());
+        _reservedThreads = reservedThreads;
+    }
+
+    public void setThreadsPriority(int priority)
+    {
+        _priority = priority;
+    }
+
+    public int getThreadsPriority()
+    {
+        return _priority;
+    }
+
+    /**
+     * @return whether this thread pool uses daemon threads
+     * @see #setDaemon(boolean)
+     */
+    @ManagedAttribute("whether this thread pool uses daemon threads")
+    public boolean isDaemon()
+    {
+        return _daemon;
+    }
+
+    /**
+     * @param daemon whether this thread pool uses daemon threads
+     * @see Thread#setDaemon(boolean)
+     */
+    public void setDaemon(boolean daemon)
+    {
+        _daemon = daemon;
+    }
+
+    @ManagedAttribute("reports additional details in the dump")
+    public boolean isDetailedDump()
+    {
+        return _detailedDump;
+    }
+
+    public void setDetailedDump(boolean detailedDump)
+    {
+        _detailedDump = detailedDump;
+    }
+
     @Override
     @ManagedAttribute("number of threads in the pool")
     public int getThreads()
@@ -143,21 +237,7 @@ public class ExecutorSizedThreadPool extends ContainerLifeCycle implements Threa
     @ManagedAttribute("number of idle threads in the pool")
     public int getIdleThreads()
     {
-        return _executor.getPoolSize()-_executor.getActiveCount();
-    }
-
-    /**
-     * Get the number of reserved threads.
-     *
-     * @return number of reserved threads or or -1 for heuristically determined
-     * @see #setReservedThreads
-     */
-    @ManagedAttribute("the number of reserved threads in the pool")
-    public int getReservedThreads()
-    {
-        if (isStarted())
-            return getBean(ReservedThreadExecutor.class).getCapacity();
-        return _reservedThreads;
+        return _executor.getPoolSize() - _executor.getActiveCount();
     }
 
     @Override
@@ -170,7 +250,7 @@ public class ExecutorSizedThreadPool extends ContainerLifeCycle implements Threa
     public boolean tryExecute(Runnable task)
     {
         TryExecutor tryExecutor = _tryExecutor;
-        return tryExecutor!=null && tryExecutor.tryExecute(task);
+        return tryExecutor != null && tryExecutor.tryExecute(task);
     }
 
     @Override
@@ -184,15 +264,16 @@ public class ExecutorSizedThreadPool extends ContainerLifeCycle implements Threa
     protected void doStart() throws Exception
     {
         if (_executor.isShutdown())
-            throw new IllegalStateException("Not restartable");
-        for (int i=_minThreads;i-->0;)
+            throw new IllegalStateException("This thread pool is not restartable");
+        for (int i = 0; i < _minThreads; ++i)
             _executor.prestartCoreThread();
-        _tryExecutor = new ReservedThreadExecutor(this,_reservedThreads);
+
+        _tryExecutor = new ReservedThreadExecutor(this, _reservedThreads);
         addBean(_tryExecutor);
-        
+
         super.doStart();
     }
-    
+
     @Override
     protected void doStop() throws Exception
     {
@@ -212,18 +293,105 @@ public class ExecutorSizedThreadPool extends ContainerLifeCycle implements Threa
     @Override
     public ThreadPoolBudget getThreadPoolBudget()
     {
-        if (_budget==null)
-            _budget = new ThreadPoolBudget(this);
         return _budget;
     }
 
-    public void setThreadsPriority(int i)
+    protected Thread newThread(Runnable job)
     {
-        // TODO implement
+        Thread thread = new Thread(_group, job);
+        thread.setDaemon(isDaemon());
+        thread.setPriority(getThreadsPriority());
+        thread.setName(getName() + "-" + thread.getId());
+        return thread;
     }
-    
-    public int getThreadsPriority()
+
+    @Override
+    public void dump(Appendable out, String indent) throws IOException
     {
-        return 0;
+        String prefix = getName() + "-";
+        List<Dumpable> threads = Thread.getAllStackTraces().entrySet().stream()
+                .filter(entry -> entry.getKey().getName().startsWith(prefix))
+                .map(entry ->
+                {
+                    Thread thread = entry.getKey();
+                    StackTraceElement[] frames = entry.getValue();
+                    String knownMethod = null;
+                    for (StackTraceElement frame : frames)
+                    {
+                        if ("getTask".equals(frame.getMethodName()) && frame.getClassName().endsWith("ThreadPoolExecutor"))
+                        {
+                            knownMethod = "IDLE ";
+                            break;
+                        }
+                        else if ("reservedWait".equals(frame.getMethodName()) && frame.getClassName().endsWith("ReservedThread"))
+                        {
+                            knownMethod = "RESERVED ";
+                            break;
+                        }
+                        else if ("select".equals(frame.getMethodName()) && frame.getClassName().endsWith("SelectorProducer"))
+                        {
+                            knownMethod = "SELECTING ";
+                            break;
+                        }
+                        else if ("accept".equals(frame.getMethodName()) && frame.getClassName().contains("ServerConnector"))
+                        {
+                            knownMethod = "ACCEPTING ";
+                            break;
+                        }
+                    }
+                    String known = knownMethod == null ? "" : knownMethod;
+                    return new Dumpable()
+                    {
+                        @Override
+                        public void dump(Appendable out, String indent) throws IOException
+                        {
+                            out.append(String.valueOf(thread.getId()))
+                                    .append(" ")
+                                    .append(thread.getName())
+                                    .append(" p=").append(String.valueOf(thread.getPriority()))
+                                    .append(" ")
+                                    .append(known)
+                                    .append(thread.getState().toString());
+                            if (isDetailedDump())
+                            {
+                                out.append(System.lineSeparator());
+                                if (known.isEmpty())
+                                    ContainerLifeCycle.dump(out, indent, Arrays.asList(frames));
+                            }
+                            else
+                            {
+                                out.append(" @ ").append(frames.length > 0 ? String.valueOf(frames[0]) : "<no_stack_frames>")
+                                        .append(System.lineSeparator());
+                            }
+                        }
+
+                        @Override
+                        public String dump()
+                        {
+                            return null;
+                        }
+                    };
+                })
+                .collect(Collectors.toList());
+
+        List<Runnable> jobs = Collections.emptyList();
+        if (isDetailedDump())
+            jobs = new ArrayList<>(_executor.getQueue());
+        dumpBeans(out, indent, threads, Collections.singletonList(new DumpableCollection("jobs - size=" + jobs.size(), jobs)));
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("%s[%s]@%x{%s,%d<=%d<=%d,i=%d,q=%d}",
+                getClass().getSimpleName(),
+                getName(),
+                hashCode(),
+                getState(),
+                getMinThreads(),
+                getThreads(),
+                getMaxThreads(),
+                getIdleThreads(),
+                _executor.getQueue().size());
     }
 }
