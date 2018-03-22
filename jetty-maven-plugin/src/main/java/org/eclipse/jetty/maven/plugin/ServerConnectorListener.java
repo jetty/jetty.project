5327884a40e3308d6d42122055b9b69878ba99a6
olivier lamy <olamy@webtide.com>
2018-03-23 09:16:41 +1000

    restore extra lines :-)
    Signed-off-by: olivier lamy <olamy@webtide.com>

--- jetty-maven-plugin/src/main/java/org/eclipse/jetty/maven/plugin/ServerConnectorListener.java
+++ jetty-maven-plugin/src/main/java/org/eclipse/jetty/maven/plugin/ServerConnectorListener.java
@@ -40,8 +40,7 @@ public class ServerConnectorListener extends AbstractLifeCycleListener
     private String _fileName;
     private String _sysPropertyName;
 
-
-
+    
     /** 
      * @see org.eclipse.jetty.util.component.AbstractLifeCycle.AbstractLifeCycleListener#lifeCycleStarted(org.eclipse.jetty.util.component.LifeCycle)
      */
@@ -91,7 +90,7 @@ public class ServerConnectorListener extends AbstractLifeCycleListener
      */
     public void setFileName(String name)
     {
-        
+
         _fileName = name;
     }
 
