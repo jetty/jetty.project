a28ec963d15719b32d62baa1c1c771bbf0864fd1
Simone Bordet <simone.bordet@gmail.com>
2018-03-28 11:36:50 +0200

    Issue #2388 - AtomicBiInteger.compareAndSet(long,int,int) not using encoded parameter.
    Using capital 'L' for long literals to avoid confusion with '1'.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-util/src/main/java/org/eclipse/jetty/util/AtomicBiInteger.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/AtomicBiInteger.java
@@ -265,7 +265,7 @@ public class AtomicBiInteger extends AtomicLong
     public static long encodeHi(long encoded, int hi)
     {
         long h = ((long)hi)&0xFFFF_FFFFL;
-        long l = encoded&0xFFFF_FFFFl;
+        long l = encoded&0xFFFF_FFFFL;
         return (h<<32)+l;
     }
     
@@ -278,7 +278,7 @@ public class AtomicBiInteger extends AtomicLong
      */
     public static long encodeLo(long encoded, int lo)
     {
-        long h = (encoded>>32)&0xFFFF_FFFFl;
+        long h = (encoded>>32)&0xFFFF_FFFFL;
         long l = ((long)lo)&0xFFFF_FFFFL;
         return (h<<32)+l;
     }
