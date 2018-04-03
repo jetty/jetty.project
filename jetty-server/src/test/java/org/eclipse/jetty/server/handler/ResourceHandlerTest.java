9397ae2d77170f7621d04bcf5bfa5a6ba21f70e7
Greg Wilkins <gregw@webtide.com>
2018-04-04 09:10:57 +1000

    revert ResourceHandlerTest changes
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- jetty-server/src/test/java/org/eclipse/jetty/server/handler/ResourceHandlerTest.java
+++ jetty-server/src/test/java/org/eclipse/jetty/server/handler/ResourceHandlerTest.java
@@ -170,14 +170,14 @@ public class ResourceHandlerTest
         assertThat(response.getContent(),containsString("big.txt"));
         assertThat(response.getContent(),containsString("bigger.txt"));
         assertThat(response.getContent(),containsString("directory"));
-        assertThat(response.getContent(),containsString("simple.raw"));
+        assertThat(response.getContent(),containsString("simple.txt"));
     }
 
     @Test
     public void testHeaders() throws Exception
     {
         HttpTester.Response response = HttpTester.parseResponse(
-            _local.getResponse("GET /resource/simple.raw HTTP/1.0\r\n\r\n"));
+            _local.getResponse("GET /resource/simple.txt HTTP/1.0\r\n\r\n"));
         assertThat(response.getStatus(),equalTo(200));
         assertThat(response.get(CONTENT_TYPE),equalTo("text/plain"));
         assertThat(response.get(LAST_MODIFIED),Matchers.notNullValue());
