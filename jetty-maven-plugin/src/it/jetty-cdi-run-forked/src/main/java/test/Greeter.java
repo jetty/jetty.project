ce1317f2045fcda584047c118e8d7158825995b1
Olivier Lamy <olamy@apache.org>
2018-04-16 10:22:56 +1000

    add it test for #2430 which works... (#2442)
    * add it test for 2430 which works...
    
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>
    
    * fix missing header
    
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

---
+++ jetty-maven-plugin/src/it/jetty-cdi-run-forked/src/main/java/test/Greeter.java
@@ -0,0 +1,39 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2018 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package test;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet( "/*" )
+public class Greeter
+    extends HttpServlet
+{
+
+    @Override
+    protected void doGet( final HttpServletRequest req, final HttpServletResponse resp )
+        throws ServletException, IOException
+    {
+        resp.getWriter().print( "Hello" );
+    }
+}
