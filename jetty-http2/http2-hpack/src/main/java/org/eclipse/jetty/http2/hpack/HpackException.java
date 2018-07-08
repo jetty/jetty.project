9fd80e8524d62ec2ebf8f8235b50fbf49e571752
Greg Wilkins <gregw@webtide.com>
2018-07-08 15:17:39 +0000

    WIP
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

---
+++ jetty-http2/http2-hpack/src/main/java/org/eclipse/jetty/http2/hpack/HpackException.java
@@ -0,0 +1,54 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2018 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.http2.hpack;
+
+import java.io.IOException;
+
+public abstract class HpackException extends IOException
+{
+    HpackException(String messageFormat, Object... args)
+    {
+        super(String.format(messageFormat, args));
+    }
+    
+    public static class Stream extends HpackException
+    {
+        Stream(String messageFormat, Object... args)
+        {
+            super(messageFormat,args);
+        }
+    }
+    
+    
+    public static class Session extends HpackException
+    {
+        Session(String messageFormat, Object... args)
+        {
+            super(messageFormat,args);
+        }
+    }
+    
+    public static class Compression extends Session
+    {
+        public Compression(String messageFormat, Object... args)
+        {
+            super(messageFormat,args);    
+        }
+    }
+}
