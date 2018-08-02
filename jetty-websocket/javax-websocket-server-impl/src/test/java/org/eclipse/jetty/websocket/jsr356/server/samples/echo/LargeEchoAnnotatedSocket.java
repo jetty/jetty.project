9a4b7807810d884a56a8022f48021a9def75e761
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2018-08-02 11:38:09 -0500

    Issue #2767 - WebSocket Policy on JSR356 ClientContainer not represented correctly
    + Added Client unit tests for large messages
    + Re-enabled Server unit tests for large messages
    + Added more Server unit tests for large messages
    + In case of JSR356 Server with policy (and Behavior of SERVER)
      is controlling javax.websocker.server.ServerContainer
      and a user chooses to use that ServerContainer to
      connect to a remote websocket endpoint (using ServerContainer
      as a client), then the policy is delegated down to the
      Client Container with a different behavior (only).
    
    Signed-off-by: Joakim Erdfelt <joakim.erdfelt@gmail.com>

---
+++ jetty-websocket/javax-websocket-server-impl/src/test/java/org/eclipse/jetty/websocket/jsr356/server/samples/echo/LargeEchoAnnotatedSocket.java
@@ -0,0 +1,46 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2018 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.websocket.jsr356.server.samples.echo;
+
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * Annotated echo socket
+ */
+@ServerEndpoint(value = "/echo/large")
+public class LargeEchoAnnotatedSocket
+{
+    private Session session;
+
+    @OnOpen
+    public void open(Session session)
+    {
+        this.session = session;
+    }
+
+    @OnMessage(maxMessageSize = 128 * 1024)
+    public void echo(String msg)
+    {
+        // reply with echo
+        session.getAsyncRemote().sendText(msg);
+    }
+}
