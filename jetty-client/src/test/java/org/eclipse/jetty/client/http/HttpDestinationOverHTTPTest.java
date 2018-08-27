6211564f797ee2a09b8216c9aa0990f913bfa475
Ivanov Anton <an.ivanov@corp.mail.ru>
2018-08-27 08:28:07 +0300

    Issue #2860 fix after review

--- jetty-client/src/test/java/org/eclipse/jetty/client/http/HttpDestinationOverHTTPTest.java
+++ jetty-client/src/test/java/org/eclipse/jetty/client/http/HttpDestinationOverHTTPTest.java
@@ -294,7 +294,8 @@ public class HttpDestinationOverHTTPTest extends AbstractHttpClientServerTest
         {
             request.send();
             Assert.fail("Request to a closed port must fail");
-        } catch (Exception expected)
+        }
+        catch (Exception expected)
         {
         }
 
