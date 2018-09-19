02f5dfd0428ccc7a5c8b7c3f8519b697edbae0ad
Lachlan Roberts <lachlan@webtide.com>
2018-09-20 08:08:10 +1000

    ValidationExtension Cleanup
    ValidationExtension now uses the Validation methods from its WebSocketChannel
    simplified the parameter passing to the ValidationExtension
    
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-websocket/websocket-core/src/main/java/org/eclipse/jetty/websocket/core/extensions/ValidationExtension.java
+++ jetty-websocket/websocket-core/src/main/java/org/eclipse/jetty/websocket/core/extensions/ValidationExtension.java
@@ -18,18 +18,13 @@
 
 package org.eclipse.jetty.websocket.core.extensions;
 
-import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jetty.io.ByteBufferPool;
 import org.eclipse.jetty.util.Callback;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.websocket.core.CloseException;
-import org.eclipse.jetty.websocket.core.CloseStatus;
-import org.eclipse.jetty.websocket.core.MessageTooLargeException;
-import org.eclipse.jetty.websocket.core.Parser;
-import org.eclipse.jetty.websocket.core.ProtocolException;
-import org.eclipse.jetty.websocket.core.WebSocketBehavior;
 import org.eclipse.jetty.websocket.core.WebSocketPolicy;
 import org.eclipse.jetty.websocket.core.frames.Frame;
 import org.eclipse.jetty.websocket.core.frames.OpCode;
@@ -59,7 +54,7 @@ public class ValidationExtension extends AbstractExtension
                 incomingSequence.check(frame.getOpCode(), frame.isFin());
 
             if (incomingFrameValidation)
-                assertValidIncoming(frame);
+                getWebSocketChannel().assertValidIncoming(frame);
 
             nextIncomingFrame(frame, callback);
         }
@@ -78,7 +73,7 @@ public class ValidationExtension extends AbstractExtension
                 outgoingSequence.check(frame.getOpCode(), frame.isFin());
 
             if (outgoingFrameValidation)
-                assertValidOutgoing(frame);
+                getWebSocketChannel().assertValidOutgoing(frame);
 
             nextOutgoingFrame(frame, callback, batchMode);
         }
@@ -88,132 +83,23 @@ public class ValidationExtension extends AbstractExtension
         }
     }
 
-
-    public void assertValidIncoming(Frame frame)
-    {
-        assertValid(frame);
-
-        // Assert Behavior Required by RFC-6455 / Section 5.1
-        if (getPolicy().getBehavior() == WebSocketBehavior.SERVER)
-        {
-            if (!frame.isMasked())
-                throw new ProtocolException("Client MUST mask all frames (RFC-6455: Section 5.1)");
-        } else if (getPolicy().getBehavior() == WebSocketBehavior.CLIENT)
-        {
-            if (frame.isMasked())
-                throw new ProtocolException("Server MUST NOT mask any frames (RFC-6455: Section 5.1)");
-        }
-    }
-
-    public void assertValidOutgoing(Frame frame) throws CloseException
-    {
-        if (!OpCode.isKnown(frame.getOpCode()))
-            throw new ProtocolException("Unknown opcode: " + frame.getOpCode());
-
-        assertValid(frame);
-
-        int payloadLength = (frame.getPayload() == null) ? 0 : frame.getPayload().remaining();
-
-        // Sane Payload Length
-        if (payloadLength > getPolicy().getMaxAllowedFrameSize())
-            throw new MessageTooLargeException("Cannot handle payload lengths larger than " + getPolicy().getMaxAllowedFrameSize());
-
-        if (frame.isControlFrame())
-        {
-            if (!frame.isFin())
-                throw new ProtocolException("Fragmented Control Frame [" + OpCode.name(frame.getOpCode()) + "]");
-
-            if (payloadLength > Frame.MAX_CONTROL_PAYLOAD)
-                throw new ProtocolException("Invalid control frame payload length, [" + payloadLength + "] cannot exceed [" + Frame.MAX_CONTROL_PAYLOAD + "]");
-        }
-    }
-
-    private void assertValid(Frame frame)
-    {
-
-        // Control Frame Validation
-        if (frame.isControlFrame())
-        {
-            if (frame.isRsv1())
-                throw new ProtocolException("Cannot have RSV1==true on Control frames");
-
-            if (frame.isRsv2())
-                throw new ProtocolException("Cannot have RSV2==true on Control frames");
-
-            if (frame.isRsv3())
-                throw new ProtocolException("Cannot have RSV3==true on Control frames");
-
-
-            /*
-             * RFC 6455 Section 5.5.1
-             * close frame payload is specially formatted which is checked in CloseStatus
-             */
-            if (frame.getOpCode() == OpCode.CLOSE)
-            {
-                if (!(frame instanceof Parser.ParsedFrame)) // already check in parser
-                    new CloseStatus(frame.getPayload());
-            }
-        }
-
-        /*
-         * RFC 6455 Section 5.2
-         *
-         * MUST be 0 unless an extension is negotiated that defines meanings for non-zero values. If a nonzero value is received and none of the negotiated
-         * extensions defines the meaning of such a nonzero value, the receiving endpoint MUST _Fail the WebSocket Connection_.
-         */
-        List<? extends Extension> exts = getWebSocketChannel().getExtensionStack().getExtensions();
-
-        boolean isRsv1InUse = false;
-        boolean isRsv2InUse = false;
-        boolean isRsv3InUse = false;
-        for (Extension ext : exts)
-        {
-            if (ext.isRsv1User())
-                isRsv1InUse = true;
-            if (ext.isRsv2User())
-                isRsv2InUse = true;
-            if (ext.isRsv3User())
-                isRsv3InUse = true;
-        }
-
-        if (frame.isRsv1() && !isRsv1InUse)
-            throw new ProtocolException("RSV1 not allowed to be set");
-
-        if (frame.isRsv2() && !isRsv2InUse)
-            throw new ProtocolException("RSV2 not allowed to be set");
-
-        if (frame.isRsv3() && !isRsv3InUse)
-            throw new ProtocolException("RSV3 not allowed to be set");
-    }
-
-
     @Override
     public void init(ExtensionConfig config, WebSocketPolicy policy, ByteBufferPool bufferPool)
     {
         super.init(config, policy, bufferPool);
 
-        String param = config.getParameter("outgoing-sequence", "false");
-        if ("true".equals(param))
-        {
+        Map<String, String> parameters = config.getParameters();
+
+        if (parameters.containsKey("outgoing-sequence"))
             outgoingSequence = new OpCode.Sequence();
-        }
 
-        param = config.getParameter("incoming-sequence", "false");
-        if ("true".equals(param))
-        {
+        if (parameters.containsKey("incoming-sequence"))
             incomingSequence = new OpCode.Sequence();
-        }
 
-        param = config.getParameter("incoming-frame", "false");
-        if ("true".equals(param))
-        {
+        if (parameters.containsKey("incoming-frame"))
             incomingFrameValidation = true;
-        }
 
-        param = config.getParameter("outgoing-frame", "false");
-        if ("true".equals(param))
-        {
+        if (parameters.containsKey("outgoing-frame"))
             outgoingFrameValidation = true;
-        }
     }
 }
--- jetty-websocket/websocket-core/src/test/java/org/eclipse/jetty/websocket/core/TestWebSocketNegotiator.java
+++ jetty-websocket/websocket-core/src/test/java/org/eclipse/jetty/websocket/core/TestWebSocketNegotiator.java
@@ -50,7 +50,7 @@ public class TestWebSocketNegotiator implements WebSocketNegotiator
         if (!offeredSubprotocols.contains("test"))
             return null;
         negotiation.setSubprotocol("test");
-        negotiation.getResponse().addHeader(HttpHeader.SEC_WEBSOCKET_EXTENSIONS.asString(),"@validation; outgoing-sequence=true; incoming-sequence=true; outgoing-frame=true; incoming-frame=true");
+        negotiation.getResponse().addHeader(HttpHeader.SEC_WEBSOCKET_EXTENSIONS.asString(),"@validation; outgoing-sequence; incoming-sequence; outgoing-frame; incoming-frame");
         return frameHandler;
     }
 
