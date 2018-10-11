8b392547452c7bf64f2ba16d22ff718e16b1b510
Simone Bordet <simone.bordet@gmail.com>
2018-10-11 18:31:36 +0200

    Issue #2978 - Add module-info to relevant Jetty modules.
    Added module-info to jetty-jmx.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

---
+++ jetty-jmx/src/main/java/module-info.java
@@ -0,0 +1,26 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2018 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+module org.eclipse.jetty.jmx {
+    exports org.eclipse.jetty.jmx;
+
+    requires transitive java.management;
+    requires static java.management.rmi;
+    requires static java.rmi;
+    requires org.eclipse.jetty.util;
+}
