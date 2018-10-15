3de54d0557a761e507001c0a00155bf3451d4fbc
Simone Bordet <simone.bordet@gmail.com>
2018-10-15 10:25:49 +0200

    Issue #2978 - Add module-info to relevant Jetty modules.
    Added module-info to jetty-servlet.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

---
+++ jetty-servlet/src/main/java/module-info.java
@@ -0,0 +1,33 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2018 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+module org.eclipse.jetty.servlet {
+    exports org.eclipse.jetty.servlet;
+    exports org.eclipse.jetty.servlet.jmx to org.eclipse.jetty.jmx;
+    exports org.eclipse.jetty.servlet.listener;
+
+    requires javax.servlet.api;
+    requires org.eclipse.jetty.util;
+    requires org.eclipse.jetty.http;
+    requires org.eclipse.jetty.server;
+    requires org.eclipse.jetty.security;
+    requires static java.management;
+    requires static java.desktop;
+    requires static org.eclipse.jetty.io;
+    requires static org.eclipse.jetty.jmx;
+}
