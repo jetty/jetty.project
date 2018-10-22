585af5905378843a3d12600036e28e64dde785bc
Simone Bordet <simone.bordet@gmail.com>
2018-10-22 11:35:20 +0200

    Issue #2978 - Add module-info to relevant Jetty modules.
    Added module-info to jetty-jaspi.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

---
+++ jetty-jaspi/src/main/java/module-info.java
@@ -0,0 +1,30 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2018 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+module org.eclipse.jetty.security.jaspi {
+    exports org.eclipse.jetty.security.jaspi;
+    exports org.eclipse.jetty.security.jaspi.callback;
+    exports org.eclipse.jetty.security.jaspi.modules;
+
+    requires javax.servlet.api;
+    requires javax.security.auth.message;
+    requires org.eclipse.jetty.util;
+    requires org.eclipse.jetty.http;
+    requires org.eclipse.jetty.security;
+    requires org.eclipse.jetty.server;
+}
