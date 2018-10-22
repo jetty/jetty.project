8150ad7d087e63615e7942f6e411bbcb0814eb52
Simone Bordet <simone.bordet@gmail.com>
2018-10-22 15:47:12 +0200

    Issue #2978 - Add module-info to relevant Jetty modules.
    Added module-info to jetty-rewrite.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

---
+++ jetty-rewrite/src/main/java/module-info.java
@@ -0,0 +1,28 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2018 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+module org.eclipse.jetty.rewrite {
+    exports org.eclipse.jetty.rewrite;
+    exports org.eclipse.jetty.rewrite.handler;
+
+    requires javax.servlet.api;
+    requires org.eclipse.jetty.util;
+    requires org.eclipse.jetty.http;
+    requires org.eclipse.jetty.io;
+    requires org.eclipse.jetty.server;
+}
