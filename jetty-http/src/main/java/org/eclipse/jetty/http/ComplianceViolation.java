0fb17bff76258a4d5037cc8d904e3dd3e50d8d67
Greg Wilkins <gregw@webtide.com>
2019-03-13 11:47:11 +1100

    Issue #3012 Compliance modes.
    Updated unit tests for messages and reasons
    
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- jetty-http/src/main/java/org/eclipse/jetty/http/ComplianceViolation.java
+++ jetty-http/src/main/java/org/eclipse/jetty/http/ComplianceViolation.java
@@ -15,6 +15,7 @@
 //  You may elect to redistribute this code under either of these licenses.
 //  ========================================================================
 //
+
 package org.eclipse.jetty.http;
 
 import java.util.Set;
