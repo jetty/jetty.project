2dc02e4946839599d1a2ee395ac8d27da6bf9051
Jan Bartel <janb@webtide.com>
2019-03-28 18:47:16 +1100

    Issue #3395 Add tests for serializing dynamic proxies in sessions.
    Signed-off-by: Jan Bartel <janb@webtide.com>

---
+++ tests/test-sessions/test-sessions-common/src/main/resources/ProxyableInvocationHandler.java
@@ -0,0 +1,46 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2019 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+import java.io.Serializable;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class ProxyableInvocationHandler implements InvocationHandler,Serializable
+{
+    private static final long serialVersionUID = 222222222222L;
+
+    public ProxyableInvocationHandler ()
+    {
+        super();
+    }
+
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
+    {
+        if ("equals".equals(method.getName()))
+        {
+            if (args != null && (args[0] instanceof Proxyable))
+                return true;
+            else
+                return false;
+        }
+        
+        return 5;
+    }
+}
