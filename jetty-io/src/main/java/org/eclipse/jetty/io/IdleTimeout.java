68c6fcf6cdb1bc617f8b0545658c8c47c77eaefe
SLeonhar <stefan.leonhartsberger@frequentis.com>
2019-04-05 15:45:04 +0200

    Added comment
    Signed-off-by: SLeonhar <stefan.leonhartsberger@frequentis.com>

--- jetty-io/src/main/java/org/eclipse/jetty/io/IdleTimeout.java
+++ jetty-io/src/main/java/org/eclipse/jetty/io/IdleTimeout.java
@@ -69,6 +69,9 @@ public abstract class IdleTimeout
         return _scheduler;
     }
 
+	/**
+	* Get the idle for time
+	*/
     public long getIdleFor()
     {
         return TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - _idleTimeout);
