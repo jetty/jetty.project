df6d20a85bb39d8a47e0201b8fbae8789d358d7b
Michael Hausegger <hausegger.michael@googlemail.com>
2019-05-15 19:36:33 +0200

    Added Unit Tests to increase code coverage.
    Signed-off-by: Michael Hausegger <hausegger.michael@googlemail.com>

---
+++ jetty-websocket/websocket-common/src/test/java/org/eclipse/jetty/websocket/common/util/ReflectUtilsTest.java
@@ -0,0 +1,77 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2019 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.websocket.common.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * Unit tests for class {@link ReflectUtils}.
+ *
+ * @see ReflectUtils
+ */
+public class ReflectUtilsTest
+{
+
+    @Test
+    public void testTrimClassName()
+    {
+        assertEquals("+~Z", ReflectUtils.trimClassName("NbcbeUUm$+~Z"));
+    }
+
+    @Test
+    public void testToShortNameWithNull()
+    {
+        assertEquals("<null>", ReflectUtils.toShortName(null));
+    }
+
+    @Test
+    public void testToShortNameWithIntegerClass()
+    {
+        assertEquals("Integer", ReflectUtils.toShortName(Integer.class));
+    }
+
+    @Test
+    public void testFindGenericClassForObjectReturningNull()
+    {
+        assertNull(ReflectUtils.findGenericClassFor(Object.class, Object.class));
+    }
+
+    @Test
+    public void testFindGenericClassForWithNullParameters()
+    {
+        assertNull(ReflectUtils.findGenericClassFor(null, null));
+    }
+
+    @Test
+    public void testIsDefaultConstructableWithIntegerClass()
+    {
+        assertFalse(ReflectUtils.isDefaultConstructable(Integer.class));
+    }
+
+    @Test
+    public void testFindGenericClassForStringClassTwice()
+    {
+        assertNull(ReflectUtils.findGenericClassFor(String.class, String.class));
+    }
+
+}
---
+++ jetty-websocket/websocket-common/src/test/java/org/eclipse/jetty/websocket/common/util/TextUtilTest.java
@@ -0,0 +1,57 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2019 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.websocket.common.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit tests for class {@link TextUtil}.
+ *
+ * @see TextUtil
+ */
+public class TextUtilTest
+{
+
+    @Test
+    public void testMaxStringLengthReturningNonEmptyString()
+    {
+        assertEquals("o,A...q{g", TextUtil.maxStringLength(9, "o,AE`s6y-Wsq{g"));
+    }
+
+    @Test
+    public void testMaxStringLengthReturningEmptyString()
+    {
+        assertEquals("", TextUtil.maxStringLength(0, "\"\""));
+    }
+
+    @Test
+    public void testHintWithNull()
+    {
+        assertEquals("<null>", TextUtil.hint(null));
+    }
+
+    @Test
+    public void testHintWithEmptyString()
+    {
+        assertEquals("\"\"", TextUtil.hint(""));
+    }
+
+}
