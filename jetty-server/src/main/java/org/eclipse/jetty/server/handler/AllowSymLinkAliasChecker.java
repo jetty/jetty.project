80699dfeab5069764f0a080dc3b22a1d6751fc71
sam <sam1287@gmail.com>
2019-07-01 10:32:11 +0900

    Jetty could not running on MacOS Catalina - MacOS 10.15 has different mount path with previous version. So AllowAliasCheck module deny all kind of resources has alias.ˆ

--- jetty-server/src/main/java/org/eclipse/jetty/server/handler/AllowSymLinkAliasChecker.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/handler/AllowSymLinkAliasChecker.java
@@ -61,6 +61,13 @@ public class AllowSymLinkAliasChecker implements AliasCheck
                     LOG.debug("Allow symlink {} --> {}", resource, pathResource.getAliasPath());
                 return true;
             }
+
+            if (containsReservedPathByMacOS(alias) && Files.isSameFile(path, alias))
+            {
+                if (LOG.isDebugEnabled())
+                    LOG.debug("Allow path by MacOS mount location {} --> {}", resource, pathResource.getAliasPath());
+                return true;
+            }
         }
         catch (Exception e)
         {
@@ -68,6 +75,10 @@ public class AllowSymLinkAliasChecker implements AliasCheck
         }
 
         return false;
+    }
+
+    private boolean containsReservedPathByMacOS(Path path) {
+        return path.startsWith("/System/Volumes/Data");
     }
 
     private boolean hasSymbolicLink(Path path)
