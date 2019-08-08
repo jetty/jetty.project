9d497084d435a72ffd3026d97b37d6fab4ea6acc
Greg Wilkins <gregw@webtide.com>
2019-08-08 15:21:06 +1000

    Issue #3804
    Rename attributes and classes to have a more regular pattern.
    The DecoratingListener is now extened by the
    CdiDecoratingListener which is used by the cdi-decorate module
    
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

---
+++ jetty-cdi/src/main/java/org/eclipse/jetty/cdi/CdiDecoratingListener.java
@@ -0,0 +1,36 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2019 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.cdi;
+
+import org.eclipse.jetty.webapp.DecoratingListener;
+
+class CdiDecoratingListener extends DecoratingListener
+{
+    public static final String MODE = "CdiDecoratingListener";
+    public static final String ATTRIBUTE = "org.eclipse.jetty.cdi.decorator";
+
+    public CdiDecoratingListener()
+    {
+        super(ATTRIBUTE);
+    }
+
+    @Override
+    protected void checkAndSetAttributeName()
+    {}
+}
