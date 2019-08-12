5ce5fadbc8beb771e655f5ef312dc6b9638950d7
Jan Bartel <janb@webtide.com>
2019-08-12 15:33:28 +1000

    Issue #3913 Fix the fix of the fix for ClusteredSessionScavengingTest
    Signed-off-by: Jan Bartel <janb@webtide.com>

--- tests/test-sessions/test-sessions-common/src/main/java/org/eclipse/jetty/server/session/AbstractClusteredSessionScavengingTest.java
+++ tests/test-sessions/test-sessions-common/src/main/java/org/eclipse/jetty/server/session/AbstractClusteredSessionScavengingTest.java
@@ -232,13 +232,15 @@ public abstract class AbstractClusteredSessionScavengingTest extends AbstractTes
             {
                 HttpSession session = request.getSession(false);
 
-                // if we node hopped we should get the session and test should already be present
-                sendResult(session, httpServletResponse.getWriter());
 
                 if (session != null)
                 {
                     session.setAttribute("test", "test");
                 }
+                
+                // if we node hopped we should get the session and test should already be present
+                sendResult(session, httpServletResponse.getWriter());
+
             }
         }
 
