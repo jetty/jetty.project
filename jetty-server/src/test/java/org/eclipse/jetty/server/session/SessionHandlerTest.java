502e6d79efc1e1f86dd4d2bd43e4fb72fa259aa4
olivier lamy <oliver.lamy@gmail.com>
2019-08-19 11:18:18 +1000

    fix package order
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

--- jetty-server/src/test/java/org/eclipse/jetty/server/session/SessionHandlerTest.java
+++ jetty-server/src/test/java/org/eclipse/jetty/server/session/SessionHandlerTest.java
@@ -18,13 +18,14 @@
 
 package org.eclipse.jetty.server.session;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import javax.servlet.SessionTrackingMode;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import javax.servlet.SessionTrackingMode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 
 public class SessionHandlerTest
 {
