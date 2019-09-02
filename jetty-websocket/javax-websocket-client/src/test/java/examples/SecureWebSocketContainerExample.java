9f2390a3dbf18147b35e586d24a59c06aafb1f49
Lachlan Roberts <lachlan@webtide.com>
2019-09-02 14:45:55 +1000

    Issue #4035 - remove classloader tricks from example
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-websocket/javax-websocket-client/src/test/java/examples/SecureWebSocketContainerExample.java
+++ jetty-websocket/javax-websocket-client/src/test/java/examples/SecureWebSocketContainerExample.java
@@ -21,14 +21,12 @@ package examples;
 import java.io.FileNotFoundException;
 import java.net.URI;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.concurrent.TimeUnit;
 import javax.websocket.ClientEndpointConfig;
 import javax.websocket.ContainerProvider;
 import javax.websocket.WebSocketContainer;
 
 import org.eclipse.jetty.util.component.LifeCycle;
-import org.eclipse.jetty.util.thread.ThreadClassLoaderScope;
 
 public class SecureWebSocketContainerExample
 {
@@ -69,23 +67,14 @@ public class SecureWebSocketContainerExample
      * Since javax.websocket does not have an API for configuring SSL, each implementation
      * of javax.websocket has to come up with their own SSL configuration mechanism.
      * <p>
-     * When the call to {@link javax.websocket.ContainerProvider}.{@link ContainerProvider#getWebSocketContainer()}
-     * occurs, that needs to have a started and available WebSocket Client.
-     * Jetty's {@code WebSocketClient} must have a Jetty {@code HttpClient} started as well.
-     * If you want SSL, then that configuration has to be passed into the Jetty {@code HttpClient} at initialization.
+     * When the {@link WebSocketContainer} is used it will need to create and start a Jetty WebSocket Client.
+     * The {@code WebSocketClient} must use a Jetty {@code HttpClient} which can be configured for SSL, this
+     * configuration needs to be passed into the Jetty {@code HttpClient} at initialization.
      * </p>
      * <p>
      * How Jetty makes this available, is via the {@code jetty-websocket-httpclient.xml} classloader resource
      * along with the jetty-xml artifact.
      * </p>
-     * <p>
-     * This method will look for the file in the classloader resources, and then
-     * sets up a {@link URLClassLoader} to make that {@code jetty-websocket-httpclient.xml} available
-     * for this specific example.
-     * If we had put the `jetty-websocket-httpclient.xml` in the root of a JAR file loaded by this
-     * project then you can skip all of the classloader trickery this method performs.
-     * </p>
-     *
      * @return the client WebSocketContainer
      * @see <a href="https://github.com/eclipse-ee4j/websocket-api/issues/210">javax.websocket issue #210</a>
      */
@@ -99,17 +88,6 @@ public class SecureWebSocketContainerExample
             throw new FileNotFoundException("Unable to find Jetty HttpClient configuration XML");
         }
 
-        URI jettyConfigDirUri = jettyHttpClientConfigUrl.toURI().resolve("./");
-
-        ClassLoader parentClassLoader = Thread.currentThread().getContextClassLoader();
-        URL[] urls = new URL[]{
-            jettyConfigDirUri.toURL()
-        };
-        URLClassLoader classLoader = new URLClassLoader(urls, parentClassLoader);
-
-        try (ThreadClassLoaderScope ignore = new ThreadClassLoaderScope(classLoader))
-        {
-            return ContainerProvider.getWebSocketContainer();
-        }
+        return ContainerProvider.getWebSocketContainer();
     }
 }
