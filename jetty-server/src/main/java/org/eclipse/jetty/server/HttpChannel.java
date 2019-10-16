c196213aeef2e8cd158d819fe450b91218e8f45c
Greg Wilkins <gregw@webtide.com>
2019-10-16 23:24:30 +1100

    Issue #4208 Content-Length in 304
    Added tests for RFC7230 section 3.3.2 for 304 and HEAD responses with content length.
    Fixed HttpGenerator to set content-length in 304 response
    Fixed insufficient content written check for 304
    
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- jetty-server/src/main/java/org/eclipse/jetty/server/HttpChannel.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/HttpChannel.java
@@ -507,7 +507,9 @@ public class HttpChannel implements Runnable, HttpOutput.Interceptor
                         }
 
                         // RFC 7230, section 3.3.
-                        if (!_request.isHead() && !_response.isContentComplete(_response.getHttpOutput().getWritten()))
+                        if (!_request.isHead() &&
+                            _response.getStatus() != HttpStatus.NOT_MODIFIED_304 &&
+                            !_response.isContentComplete(_response.getHttpOutput().getWritten()))
                         {
                             if (isCommitted())
                                 abort(new IOException("insufficient content written"));
