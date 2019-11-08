c336616c9655b4fba5023d6496d7633461619927
Lachlan <lachlan@webtide.com>
2019-11-08 21:57:49 +1100

    Issue #2709 - reduce default headerCacheSize to 1024 (#4292)
    make headerCacheSize on HttpReceiverOverHTTP configurable
    
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-http/src/test/java/org/eclipse/jetty/http/HttpParserTest.java
+++ jetty-http/src/test/java/org/eclipse/jetty/http/HttpParserTest.java
@@ -2364,7 +2364,7 @@ public class HttpParserTest
         @Override
         public int getHeaderCacheSize()
         {
-            return 4096;
+            return 1024;
         }
 
         @Override
