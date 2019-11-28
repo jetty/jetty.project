e58f570266a6d966442f52d1e5a312501f0eecce
olivier lamy <oliver.lamy@gmail.com>
2019-11-28 13:42:52 +1000

    remove unused import
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

--- jetty-server/src/main/java/org/eclipse/jetty/server/session/JDBCSessionDataStore.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/session/JDBCSessionDataStore.java
@@ -28,7 +28,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Types;
 import java.util.HashSet;
 import java.util.Set;
 
