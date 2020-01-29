fdd27a9f28a66754675cd2db0f11a8b3e3593503
Lachlan Roberts <lachlan@webtide.com>
2020-01-29 15:20:36 +1100

    Issue #4502 - add onClose tests for Jetty and Javax WS APIs
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-websocket/websocket-javax-tests/src/main/java/org/eclipse/jetty/websocket/javax/tests/EventSocket.java
+++ jetty-websocket/websocket-javax-tests/src/main/java/org/eclipse/jetty/websocket/javax/tests/EventSocket.java
@@ -50,6 +50,7 @@ public class EventSocket
 
     public CountDownLatch openLatch = new CountDownLatch(1);
     public CountDownLatch closeLatch = new CountDownLatch(1);
+    public CountDownLatch errorLatch = new CountDownLatch(1);
 
     @OnOpen
     public void onOpen(Session session, EndpointConfig endpointConfig)
@@ -85,5 +86,6 @@ public class EventSocket
         if (LOG.isDebugEnabled())
             LOG.debug("{}  onError(): {}", toString(), cause);
         error = cause;
+        errorLatch.countDown();
     }
 }
