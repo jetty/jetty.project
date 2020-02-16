b0273d460103f68ea833cddeb6ac67c7e5748c37
liudonghua123 <liudonghua123@gmail.com>
2020-02-17 01:24:56 +0800

    try to fix #4577
    Signed-off-by: liudonghua123 <liudonghua123@gmail.com>

--- jetty-server/src/main/java/org/eclipse/jetty/server/handler/IPAccessHandler.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/handler/IPAccessHandler.java
@@ -201,7 +201,7 @@ public class IPAccessHandler extends HandlerWrapper
             if (endp != null)
             {
                 InetSocketAddress address = endp.getRemoteAddress();
-                if (address != null && !isAddrUriAllowed(address.getHostString(), baseRequest.getPathInfo()))
+                if (address != null && !isAddrUriAllowed(address.getHostString(), baseRequest.getContextPath()))
                 {
                     response.sendError(HttpStatus.FORBIDDEN_403);
                     baseRequest.setHandled(true);
