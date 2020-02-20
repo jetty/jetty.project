f0b48ec4abde26533f6a571252203cc50db564b0
Greg Wilkins <gregw@webtide.com>
2020-02-20 18:18:39 +0100

    Tests #4594 ContextListener removed
    Remove ContextListeners from the destroy list when removed.
    
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- jetty-server/src/main/java/org/eclipse/jetty/server/handler/ContextHandler.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/handler/ContextHandler.java
@@ -206,7 +206,7 @@ public class ContextHandler extends ScopedHandler implements Attributes, Gracefu
     private final List<EventListener> _eventListeners = new CopyOnWriteArrayList<>();
     private final List<EventListener> _programmaticListeners = new CopyOnWriteArrayList<>();
     private final List<ServletContextListener> _servletContextListeners = new CopyOnWriteArrayList<>();
-    private final List<ServletContextListener> _destroySerletContextListeners = new ArrayList<>();
+    private final List<ServletContextListener> _destroyServletContextListeners = new ArrayList<>();
     private final List<ServletContextAttributeListener> _servletContextAttributeListeners = new CopyOnWriteArrayList<>();
     private final List<ServletRequestListener> _servletRequestListeners = new CopyOnWriteArrayList<>();
     private final List<ServletRequestAttributeListener> _servletRequestAttributeListeners = new CopyOnWriteArrayList<>();
@@ -696,7 +696,10 @@ public class ContextHandler extends ScopedHandler implements Attributes, Gracefu
             _contextListeners.remove(listener);
 
         if (listener instanceof ServletContextListener)
+        {
             _servletContextListeners.remove(listener);
+            _destroyServletContextListeners.remove(listener);
+        }
 
         if (listener instanceof ServletContextAttributeListener)
             _servletContextAttributeListeners.remove(listener);
@@ -880,14 +883,14 @@ public class ContextHandler extends ScopedHandler implements Attributes, Gracefu
         super.doStart();
 
         // Call context listeners
-        _destroySerletContextListeners.clear();
+        _destroyServletContextListeners.clear();
         if (!_servletContextListeners.isEmpty())
         {
             ServletContextEvent event = new ServletContextEvent(_scontext);
             for (ServletContextListener listener : _servletContextListeners)
             {
                 callContextInitialized(listener, event);
-                _destroySerletContextListeners.add(listener);
+                _destroyServletContextListeners.add(listener);
             }
         }
     }
@@ -896,9 +899,9 @@ public class ContextHandler extends ScopedHandler implements Attributes, Gracefu
     {
         // Call the context listeners
         ServletContextEvent event = new ServletContextEvent(_scontext);
-        Collections.reverse(_destroySerletContextListeners);
+        Collections.reverse(_destroyServletContextListeners);
         MultiException ex = new MultiException();
-        for (ServletContextListener listener : _destroySerletContextListeners)
+        for (ServletContextListener listener : _destroyServletContextListeners)
         {
             try
             {
