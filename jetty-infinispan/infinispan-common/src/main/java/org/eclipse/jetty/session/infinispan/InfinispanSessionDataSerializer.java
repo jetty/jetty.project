e64fee5a486dbd99acdde322950b28efd1746303
Andrej Krota <andrej.krota@gmail.com>
2020-02-25 16:45:53 +0100

    Refactored InfinispanSessionDataSerializer to make use of SessionDataMarshaller
    Signed-off-by: Andrej Krota <andrej.krota@gmail.com>

--- jetty-infinispan/infinispan-common/src/main/java/org/eclipse/jetty/session/infinispan/InfinispanSessionDataSerializer.java
+++ jetty-infinispan/infinispan-common/src/main/java/org/eclipse/jetty/session/infinispan/InfinispanSessionDataSerializer.java
@@ -5,74 +5,79 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
 
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
 import org.infinispan.commons.marshall.Externalizer;
+import org.infinispan.protostream.FileDescriptorSource;
+import org.infinispan.protostream.ProtobufUtil;
+import org.infinispan.protostream.SerializationContext;
 
 public class InfinispanSessionDataSerializer implements Externalizer<InfinispanSessionData>
 {
-    private static final int VERSION = 0;
-
     private static final long serialVersionUID = 1L;
+
+    private static final Logger LOG = Log.getLogger("org.eclipse.jetty.server.session");
+    
+    private static SerializationContext serCtx;
+
+    static
+    {
+        FileDescriptorSource fds = new FileDescriptorSource();
+        try
+        {
+            fds.addProtoFiles("/session.proto");
+            serCtx = ProtobufUtil.newSerializationContext();
+            serCtx.registerProtoFiles(fds);
+            serCtx.registerMarshaller(new SessionDataMarshaller());
+        }
+        catch (IOException e)
+        {
+            serCtx = null;
+            LOG.warn("SerializationContext cannot be initialized", e);
+        }
+
+    }
 
     @Override
     public void writeObject(ObjectOutput output, InfinispanSessionData object) throws IOException
     {
-        output.writeInt(VERSION);
-        output.writeUTF(object.getId()); //session id
-        output.writeUTF(object.getContextPath()); //context path
-        output.writeUTF(object.getVhost()); //first vhost
-        output.writeLong(object.getAccessed());//accessTime
-        output.writeLong(object.getLastAccessed()); //lastAccessTime
-        output.writeLong(object.getCreated()); //time created
-        output.writeLong(object.getCookieSet());//time cookie was set
-        output.writeUTF(object.getLastNode()); //name of last node managing
-        output.writeLong(object.getExpiry());
-        output.writeLong(object.getMaxInactiveMs());
-        object.serializeAttributes();
-        output.write(object.getSerializedAttributes());
-
+        if (serCtx != null) 
+        {
+            byte[] data = ProtobufUtil.toByteArray(serCtx, object);
+            output.write(data);
+        }
+        else
+        {
+            throw new IOException("SerializationContext is not initialized");
+        }
     }
 
     @Override
     public InfinispanSessionData readObject(ObjectInput input) throws IOException, ClassNotFoundException
     {
-        int version = input.readInt();
-        if (version == 0)
+        if (serCtx != null)
         {
-            String id = input.readUTF();
-            String contextPath = input.readUTF();
-            String vhost = input.readUTF();
-            long accessed = input.readLong();
-            long lastAccessed = input.readLong();
-            long created = input.readLong();
-            long cookieSet = input.readLong();
-            String lastNode = input.readUTF();
-            long expiry = input.readLong();
-            long maxInactiveMs = input.readLong();
-        
-            InfinispanSessionData data = new InfinispanSessionData(id, contextPath, vhost, created, accessed, lastAccessed, maxInactiveMs);
-            data.setCookieSet(cookieSet);
-            data.setLastNode(lastNode);
-            data.setExpiry(expiry);
-        
-            try (ByteArrayOutputStream os = new ByteArrayOutputStream())
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            int bufferSize = 4 * 1024;
+            byte[] buffer = new byte[bufferSize];
+            int len = -1;
+            while ((len = input.read(buffer, 0, bufferSize)) != -1)
             {
-                int bufferSize = 4 * 1024;
-                byte[] buffer = new byte[bufferSize];
-                int len = -1;
-                while ((len = input.read(buffer, 0, bufferSize)) != -1)
-                {
-                    os.write(buffer, 0, len);
-                }
-                byte[] serializedAttributes = os.toByteArray();
-                data.setSerializedAttributes(serializedAttributes);
+                os.write(buffer, 0, len);
+            }
+
+            InfinispanSessionData data = ProtobufUtil.fromByteArray(serCtx, os.toByteArray(),
+                    InfinispanSessionData.class);
+            if (data != null)
+            {
                 data.deserializeAttributes();
             }
             return data;
         }
         else
         {
-            throw new IOException("Unrecognized infinispan session version " + version);
+            throw new IOException("SerializationContext is not initialized");
         }
     }
-    
+
 }
