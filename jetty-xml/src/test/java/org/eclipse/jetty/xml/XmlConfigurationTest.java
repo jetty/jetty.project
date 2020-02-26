5ba9411e5c6db7ca805d294e591355107e21a91f
Greg Wilkins <gregw@webtide.com>
2020-02-26 14:37:00 +0100

    cleanup after merge
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- jetty-xml/src/test/java/org/eclipse/jetty/xml/XmlConfigurationTest.java
+++ jetty-xml/src/test/java/org/eclipse/jetty/xml/XmlConfigurationTest.java
@@ -50,7 +50,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.xml.sax.SAXException;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
