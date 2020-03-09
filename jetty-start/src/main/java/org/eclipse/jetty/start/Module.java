7554a3a903500dfb21c6f985265301bdc5f3e2b5
olivier lamy <oliver.lamy@gmail.com>
2020-03-09 16:15:06 +1000

    fix merge
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

--- jetty-start/src/main/java/org/eclipse/jetty/start/Module.java
+++ jetty-start/src/main/java/org/eclipse/jetty/start/Module.java
@@ -20,6 +20,7 @@ package org.eclipse.jetty.start;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
