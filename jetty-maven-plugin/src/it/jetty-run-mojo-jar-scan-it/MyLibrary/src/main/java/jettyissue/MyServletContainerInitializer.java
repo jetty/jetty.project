147770b5f0c4f9a7cd6e5c3cceee256d29c26839
olivier lamy <oliver.lamy@gmail.com>
2020-04-08 10:37:30 +1000

    fix test due to package change
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

--- jetty-maven-plugin/src/it/jetty-run-mojo-jar-scan-it/MyLibrary/src/main/java/jettyissue/MyServletContainerInitializer.java
+++ jetty-maven-plugin/src/it/jetty-run-mojo-jar-scan-it/MyLibrary/src/main/java/jettyissue/MyServletContainerInitializer.java
@@ -18,10 +18,11 @@
 
 package jettyissue;
 
-import javax.servlet.ServletContainerInitializer;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.HandlesTypes;
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HandlesTypes;
+
 import java.util.Set;
 
 @HandlesTypes({MyAnnotation.class})
