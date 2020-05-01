9bc9082ff3a3bd6c84a93e7757b440cc1422e75f
Lachlan Roberts <lachlan@webtide.com>
2020-05-01 13:01:40 +1000

    Fix broken WebSocket UTF-8 continuation test
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-websocket/websocket-util/src/test/java/org/eclipse/jetty/websocket/util/StringMessageSinkTest.java
+++ jetty-websocket/websocket-util/src/test/java/org/eclipse/jetty/websocket/util/StringMessageSinkTest.java
@@ -90,7 +90,7 @@ public class StringMessageSinkTest
         messageSink.accept(new Frame(OpCode.TEXT, continuationUtf8Payload).setFin(true), callback);
         callback.block(5, TimeUnit.SECONDS);
 
-        assertThat(endpoint.messages.poll(5, TimeUnit.SECONDS), is('\uD800' + '\uDF48')); // UTF-8 encoded payload.
+        assertThat(endpoint.messages.poll(5, TimeUnit.SECONDS), is("\uD800\uDF48")); // UTF-8 encoded payload.
     }
 
     @Test
