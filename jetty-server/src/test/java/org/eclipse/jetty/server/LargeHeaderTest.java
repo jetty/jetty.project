9a5dd78004ed0151dc1c4277007d065b6b3e6f55
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2020-06-03 10:21:10 -0500

    Issue #4936 - Updating LargeHeaderTest to use ServerConnector
    Signed-off-by: Joakim Erdfelt <joakim.erdfelt@gmail.com>

--- jetty-server/src/test/java/org/eclipse/jetty/server/LargeHeaderTest.java
+++ jetty-server/src/test/java/org/eclipse/jetty/server/LargeHeaderTest.java
@@ -19,8 +19,10 @@
 package org.eclipse.jetty.server;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
+import java.net.Socket;
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -33,18 +35,21 @@ import org.eclipse.jetty.http.HttpTester;
 import org.eclipse.jetty.http.MimeTypes;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.handler.ErrorHandler;
+import org.eclipse.jetty.util.IO;
 import org.eclipse.jetty.util.component.LifeCycle;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class LargeHeaderTest
 {
     private Server server;
-    private LocalConnector connector;
 
     @BeforeEach
     public void setup() throws Exception
@@ -54,7 +59,8 @@ public class LargeHeaderTest
         HttpConfiguration config = new HttpConfiguration();
         HttpConnectionFactory http = new HttpConnectionFactory(config);
 
-        connector = new LocalConnector(server, http, null);
+        ServerConnector connector = new ServerConnector(server, http);
+        connector.setPort(0);
         connector.setIdleTimeout(5000);
         server.addConnector(connector);
 
@@ -67,7 +73,7 @@ public class LargeHeaderTest
             {
                 byte[] bytes = new byte[8 * 1024];
                 Arrays.fill(bytes, (byte)'X');
-                largeHeaderValue = "LargeHeaderOver8k-" + new String(bytes, StandardCharsets.ISO_8859_1) + "_Z_";
+                largeHeaderValue = "LargeHeaderOver8k-" + new String(bytes, UTF_8) + "_Z_";
             }
 
             @Override
@@ -94,24 +100,34 @@ public class LargeHeaderTest
     @Test
     public void testLargeHeader() throws Exception
     {
+        final Logger CLIENTLOG = Log.getLogger(LargeHeaderTest.class).getLogger(".client");
         ExecutorService executorService = Executors.newFixedThreadPool(8);
 
+        int localPort = server.getURI().getPort();
         String rawRequest = "GET / HTTP/1.1\r\n" +
-            "Host: localhost\r\n" +
+            "Host: localhost:" + localPort + "\r\n" +
             "\r\n";
 
         for (int i = 0; i < 500; ++i)
         {
             executorService.submit(() ->
             {
-                try
+                try (Socket client = new Socket("localhost", localPort);
+                     OutputStream output = client.getOutputStream();
+                     InputStream input = client.getInputStream())
                 {
-                    HttpTester.Response response = HttpTester.parseResponse(connector.getResponse(rawRequest));
+                    output.write(rawRequest.getBytes(UTF_8));
+                    output.flush();
+
+                    String rawResponse = IO.toString(input, UTF_8);
+
+                    CLIENTLOG.info("rawResponse[{}]=[{}]", rawResponse.length(), rawResponse);
+                    HttpTester.Response response = HttpTester.parseResponse(rawResponse);
                     assertThat(response.getStatus(), is(500));
                 }
                 catch (Throwable t)
                 {
-                    t.printStackTrace();
+                    CLIENTLOG.warn("Client Issue", t);
                 }
             });
         }
