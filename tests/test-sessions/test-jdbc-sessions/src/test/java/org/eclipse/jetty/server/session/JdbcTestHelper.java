f745d5d5df07746cc89c73cdcf5a29b421cfd5fe
olivier lamy <oliver.lamy@gmail.com>
2020-06-15 17:40:15 +1000

    remove unused imports and upgrade testcontainers.version
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

--- tests/test-sessions/test-jdbc-sessions/src/test/java/org/eclipse/jetty/server/session/JdbcTestHelper.java
+++ tests/test-sessions/test-jdbc-sessions/src/test/java/org/eclipse/jetty/server/session/JdbcTestHelper.java
@@ -31,11 +31,9 @@ import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.eclipse.jetty.toolchain.test.MavenTestingUtils;
 import org.eclipse.jetty.util.ClassLoadingObjectInputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 
