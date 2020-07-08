35adb003c6ffc72ddcb03274aa9d193e7cd90739
Jan Bartel <janb@webtide.com>
2020-07-08 22:16:04 +0200

    Issue #5013 Classloader.getResources resolves preferentially from osgi bundle loader
    Signed-off-by: Jan Bartel <janb@webtide.com>

---
+++ jetty-osgi/test-jetty-osgi-webapp-resources/src/main/java/com/acme/HelloWorld.java
@@ -0,0 +1,67 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2020 Mort Bay Consulting Pty Ltd and others.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package com.acme;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Set;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Dump Servlet Request.
+ */
+@SuppressWarnings("serial")
+public class HelloWorld extends HttpServlet
+{
+
+    @Override
+    public void init(ServletConfig config) throws ServletException
+    {
+        super.init(config);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        doGet(request, response);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        ServletOutputStream out = response.getOutputStream();
+        out.println("<html>");
+        out.println("<h1>Hello World</h1>");
+       
+        Enumeration<URL> resources = Thread.currentThread().getContextClassLoader().getResources("fake.properties");
+
+        while (resources.hasMoreElements())
+                out.println(resources.nextElement().toString());
+
+        out.println("</html>");
+        out.flush();
+    }
+}
