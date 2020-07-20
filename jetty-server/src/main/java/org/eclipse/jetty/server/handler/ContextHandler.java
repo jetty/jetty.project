65de149f84b68e23fb2a2a849e80b06850c8cd29
Greg Wilkins <gregw@webtide.com>
2020-07-20 17:35:48 +0200

    Issue #5057 - Included root context path (#5058)
    * Fixes #5057 Included root context path
    
    Root context path in include should be empty string.
    
    * Issue #5057
    
    merged context path methods as result of review.
    ServletContent.getContextPath now returns the encoded contextPath (if anybody is silly enough to have one).

--- jetty-server/src/main/java/org/eclipse/jetty/server/handler/ContextHandler.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/handler/ContextHandler.java
@@ -591,6 +591,17 @@ public class ContextHandler extends ScopedHandler implements Attributes, Gracefu
         return _contextPathEncoded;
     }
 
+    /**
+     * Get the context path in a form suitable to be returned from {@link HttpServletRequest#getContextPath()}
+     * or {@link ServletContext#getContextPath()}.
+     * @return Returns the encoded contextPath, or empty string for root context
+     */
+    public String getRequestContextPath()
+    {
+        String contextPathEncoded = getContextPathEncoded();
+        return "/".equals(contextPathEncoded) ? "" : contextPathEncoded;
+    }
+
     /*
      * @see javax.servlet.ServletContext#getInitParameter(java.lang.String)
      */
@@ -1267,10 +1278,7 @@ public class ContextHandler extends ScopedHandler implements Attributes, Gracefu
             __context.set(_scontext);
             if (!DispatcherType.INCLUDE.equals(dispatch) && target.startsWith("/"))
             {
-                if (_contextPath.length() == 1)
-                    baseRequest.setContextPath("");
-                else
-                    baseRequest.setContextPath(getContextPathEncoded());
+                baseRequest.setContextPath(getRequestContextPath());
                 baseRequest.setServletPath(null);
                 baseRequest.setPathInfo(pathInfo);
             }
@@ -2418,10 +2426,7 @@ public class ContextHandler extends ScopedHandler implements Attributes, Gracefu
         @Override
         public String getContextPath()
         {
-            if ((_contextPath != null) && _contextPath.equals(URIUtil.SLASH))
-                return "";
-
-            return _contextPath;
+            return getRequestContextPath();
         }
 
         @Override
