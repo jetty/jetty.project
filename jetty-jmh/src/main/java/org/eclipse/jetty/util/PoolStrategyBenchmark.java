0b0030d9897ce8d95e3167e35b72855cd57db0fd
gregw <gregw@webtide.com>
2020-09-10 09:18:36 +0200

    test
    Signed-off-by: gregw <gregw@webtide.com>

--- jetty-jmh/src/main/java/org/eclipse/jetty/util/PoolStrategyBenchmark.java
+++ jetty-jmh/src/main/java/org/eclipse/jetty/util/PoolStrategyBenchmark.java
@@ -18,8 +18,10 @@
 
 package org.eclipse.jetty.util;
 
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.LongAdder;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Param;
@@ -28,39 +30,72 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.profile.GCProfiler;
+import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 @State(Scope.Benchmark)
-public class PoolBenchmark
+public class PoolStrategyBenchmark
 {
     private Pool<String> pool;
-    private Queue<Pool<String>.Entry> queue = new ConcurrentLinkedQueue<>();
+    private Pool<String> poolA;
+    private Pool<String> poolB;
+    private static final Queue<Pool<String>.Entry> queue = new ConcurrentLinkedQueue<>();
 
     @Param({
         "linear",
-        "random+linear",
+        "OSTRTA(LINEAR)",
         "threadlocal+linear",
-        "threadlocallist+linear",
+        "OSTRTA(THREAD_LOCAL)",
         "random-iteration",
-        "threadlocal-iteration",
-        "threadlocal-iteration-roundrobin",
-        "roundrobin",
-        "roundrobin-iteration",
-        "lru"
+        "OSTRTA(RANDOM)",
+        "roundrobin+linear",
+        "OSTRTA(ROUND_ROBIN)",
     })
     public static String POOL_TYPE;
 
     @Param({
-        "10"
+        "16"
     })
     public static int SIZE;
+
+    private static final LongAdder misses = new LongAdder();
+    private static final LongAdder hits = new LongAdder();
+    private static final LongAdder total = new LongAdder();
 
     @Setup
     public void setUp() throws Exception
     {
+        misses.reset();
+
+        poolA = new Pool<>(SIZE, new Pool.Strategy<String>()
+        {
+            @Override
+            public Pool<String>.Entry tryAcquire(List<Pool<String>.Entry> entries)
+            {
+                int i = (int)Thread.currentThread().getId() % entries.size();
+                Pool<String>.Entry entry = entries.get(i);
+                if (entry != null && entry.tryAcquire())
+                    return entry;
+                return null;
+            }
+        });
+
+        poolB = new Pool<String>(SIZE, new Pool.Strategy<String>()
+        {
+            @Override
+            public Pool<String>.Entry tryAcquire(List<Pool<String>.Entry> entries)
+            {
+                Pool<String>.Entry entry = entries.get(0);
+                if (entry != null && entry.tryAcquire())
+                    return entry;
+                return null;
+            }
+        });
+
         switch (POOL_TYPE)
         {
             case "linear":
@@ -71,6 +106,9 @@ public class PoolBenchmark
                 break;
             case "threadlocal+linear":
                 pool = new Pool<>(SIZE, new Pool.CompositeStrategy<>(new Pool.ThreadLocalStrategy<>(), new Pool.LinearSearchStrategy<>()));
+                break;
+            case "threadid+linear":
+                pool = new Pool<>(SIZE, new Pool.CompositeStrategy<>(new Pool.ThreadIdStrategy<>(), new Pool.LinearSearchStrategy<>()));
                 break;
             case "threadlocallist+linear":
                 pool = new Pool<>(SIZE, new Pool.CompositeStrategy<>(new Pool.ThreadLocalListStrategy<>(2), new Pool.LinearSearchStrategy<>()));
@@ -84,8 +122,11 @@ public class PoolBenchmark
             case "threadlocal-iteration-roundrobin":
                 pool = new Pool<>(SIZE, new Pool.ThreadLocalIteratorStrategy<>(true));
                 break;
-            case "roundrobin":
-                pool = new Pool<>(SIZE, new Pool.RoundRobinStrategy<>());
+            case "retry+roundrobin":
+                pool = new Pool<>(SIZE, new Pool.RetryStategy<>(new Pool.RoundRobinStrategy<>()));
+                break;
+            case "roundrobin+linear":
+                pool = new Pool<>(SIZE, new Pool.CompositeStrategy<>(new Pool.RoundRobinStrategy<>(), new Pool.LinearSearchStrategy<>()));
                 break;
             case "roundrobin-iteration":
                 pool = new Pool<>(SIZE, new Pool.RoundRobinIterationStrategy<>());
@@ -93,17 +134,36 @@ public class PoolBenchmark
             case "lru":
                 pool = new Pool<>(SIZE, new Pool.LeastRecentlyUsedStrategy<>());
                 break;
+            case "OSTRTA(LINEAR)":
+                pool = new Pool<>(SIZE, new Pool.OneStrategyToRuleThemAll<>(Pool.OneStrategyToRuleThemAll.Mode.LINEAR));
+                break;
+            case "OSTRTA(RANDOM)":
+                pool = new Pool<>(SIZE, new Pool.OneStrategyToRuleThemAll<>(Pool.OneStrategyToRuleThemAll.Mode.RANDOM));
+                break;
+            case "OSTRTA(THREAD_LOCAL)":
+                pool = new Pool<>(SIZE, new Pool.OneStrategyToRuleThemAll<>(Pool.OneStrategyToRuleThemAll.Mode.THREAD_LOCAL));
+                break;
+            case "OSTRTA(ROUND_ROBIN)":
+                pool = new Pool<>(SIZE, new Pool.OneStrategyToRuleThemAll<>(Pool.OneStrategyToRuleThemAll.Mode.ROUND_ROBIN));
+                break;
+
             default:
                 throw new IllegalStateException();
         }
 
         for (int i = 0; i < SIZE; i++)
+        {
+            poolA.reserve(1).enable(Integer.toString(i), false);
+            poolB.reserve(1).enable(Integer.toString(i), false);
             pool.reserve(1).enable(Integer.toString(i), false);
+        }
     }
 
     @TearDown
     public void tearDown()
     {
+        System.err.printf("%nMISSES = %d (%d%%)%n", misses.longValue(), 100 * misses.longValue() / (hits.longValue() + misses.longValue()));
+        System.err.printf("AVERAGE = %d%n", total.longValue() / hits.longValue());
         pool.close();
         pool = null;
     }
@@ -111,53 +171,44 @@ public class PoolBenchmark
     @Benchmark
     public void testAcquireReleasePool()
     {
-        // acquire an entry
-        Pool<String>.Entry entry = pool.acquire();
-        if (entry == null || entry.isIdle())
-            throw new IllegalStateException();
+        // force polymorphic pool optimization
+        Pool<String>.Entry entry = poolA.acquire();
+        if (entry != null)
+            entry.release();
+        entry = poolB.acquire();
+        if (entry != null)
+            entry.release();
 
+        // Now really benchmark the strategy we are interested in
+        entry = pool.acquire();
+        if (entry == null || entry.isIdle())
+        {
+            misses.increment();
+            Blackhole.consumeCPU(20);
+            return;
+        }
         // do some work
-        Blackhole.consumeCPU(entry.getPooled().hashCode() % 200);
+        hits.increment();
+        total.add(Long.parseLong(entry.getPooled()));
+        Blackhole.consumeCPU(entry.getPooled().hashCode() % 20);
 
         // release the entry
         entry.release();
     }
 
-    @Benchmark
-    public void testReleaseAcquirePool()
-    {
-        // Release an entry, probably from another thread
-        Pool<String>.Entry entry = queue.poll();
-        if (entry != null)
-        {
-            entry.release();
-
-            // Do some work
-            Blackhole.consumeCPU(entry.getPooled().hashCode() % 100);
-        }
-
-        // Acquire another entry
-        entry = pool.acquire();
-        if (entry == null || entry.isIdle())
-            throw new IllegalStateException();
-        queue.add(entry);
-
-        // Do some more work
-        Blackhole.consumeCPU(entry.getPooled().hashCode() % 100);
-    }
-
     public static void main(String[] args) throws RunnerException
     {
         Options opt = new OptionsBuilder()
-            .include(PoolBenchmark.class.getSimpleName())
-            .warmupIterations(1)
-            .measurementIterations(1)
+            .include(PoolStrategyBenchmark.class.getSimpleName())
+            .warmupIterations(3)
+            .measurementIterations(3)
             .forks(1)
-            .threads(10)
-            //.addProfiler(LinuxPerfProfiler.class)
+            .threads(8)
+            .resultFormat(ResultFormatType.JSON)
+            .result("poolStrategy-" + System.currentTimeMillis() + ".json")
+            .addProfiler(GCProfiler.class)
             .build();
 
         new Runner(opt).run();
     }
-
 }
