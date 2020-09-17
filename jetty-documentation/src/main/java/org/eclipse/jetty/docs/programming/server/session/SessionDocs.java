21539de448f455146e18fea5ddff9d0369716722
Jan Bartel <janb@webtide.com>
2020-09-17 15:27:04 +0200

    Session doc update.
    Signed-off-by: Jan Bartel <janb@webtide.com>

--- jetty-documentation/src/main/java/org/eclipse/jetty/docs/programming/server/session/SessionDocs.java
+++ jetty-documentation/src/main/java/org/eclipse/jetty/docs/programming/server/session/SessionDocs.java
@@ -18,10 +18,15 @@
 
 package org.eclipse.jetty.docs.programming.server.session;
 
+import java.io.File;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.session.DefaultSessionCache;
 import org.eclipse.jetty.server.session.DefaultSessionCacheFactory;
 import org.eclipse.jetty.server.session.DefaultSessionIdManager;
+import org.eclipse.jetty.server.session.FileSessionDataStore;
+import org.eclipse.jetty.server.session.FileSessionDataStoreFactory;
 import org.eclipse.jetty.server.session.HouseKeeper;
 import org.eclipse.jetty.server.session.NullSessionCache;
 import org.eclipse.jetty.server.session.NullSessionCacheFactory;
@@ -37,7 +42,7 @@ public class SessionDocs
         Server server = new Server();
         DefaultSessionIdManager idMgr = new DefaultSessionIdManager(server);
         //you must set the workerName unless you set the env viable JETTY_WORKER_NAME
-        idMgr.setWorkerName("3");
+        idMgr.setWorkerName("server3");
         server.setSessionIdManager(idMgr);
         //end::default[]
     }
@@ -49,7 +54,7 @@ public class SessionDocs
             //tag::housekeeper[]
             Server server = new Server();
             DefaultSessionIdManager idMgr = new DefaultSessionIdManager(server);
-            idMgr.setWorkerName("7");
+            idMgr.setWorkerName("server7");
             server.setSessionIdManager(idMgr);
 
             HouseKeeper houseKeeper = new HouseKeeper();
@@ -138,5 +143,67 @@ public class SessionDocs
         nullSessionCache.setSessionDataStore(new NullSessionDataStore());
         app2.getSessionHandler().setSessionCache(nullSessionCache);
         //end::mixedsessioncache[]
+    }
+    
+    public void fileSessionDataStoreFactory()
+    {
+      //tag::filesessiondatastorefactory[]
+        Server server = new Server();
+
+        //First lets configure a DefaultSessionCacheFactory
+        DefaultSessionCacheFactory cacheFactory = new DefaultSessionCacheFactory();
+        //NEVER_EVICT
+        cacheFactory.setEvictionPolicy(SessionCache.NEVER_EVICT);
+        cacheFactory.setFlushOnResponseCommit(true);
+        cacheFactory.setInvalidateOnShutdown(false);
+        cacheFactory.setRemoveUnloadableSessions(true);
+        cacheFactory.setSaveOnCreate(true);
+
+        //Add the factory as a bean to the server, now whenever a 
+        //SessionHandler starts it will consult the bean to create a new DefaultSessionCache
+        server.addBean(cacheFactory);
+        
+        //Now, lets configure a FileSessionDataStoreFactory
+        FileSessionDataStoreFactory storeFactory = new FileSessionDataStoreFactory();
+        storeFactory.setStoreDir(new File("/tmp/sessions"));
+        storeFactory.setGracePeriodSec(3600);
+        storeFactory.setSavePeriodSec(0);
+        
+        //Add the factory as a bean on the server, now whenever a
+        //SessionHandler starts, it will consult the bean to create a new FileSessionDataStore
+        //for use by the DefaultSessionCache
+        server.addBean(storeFactory);
+      //end::filesessiondatastorefactory[]  
+    }
+    
+    public void fileSessionDataStore()
+    {
+      //tag::filesessiondatastore[]
+
+        //create a context
+        WebAppContext app1 = new WebAppContext();
+        app1.setContextPath("/app1");
+        
+        //First, we create a DefaultSessionCache
+        DefaultSessionCache cache = new DefaultSessionCache(app1.getSessionHandler());
+        cache.setEvictionPolicy(SessionCache.NEVER_EVICT);
+        cache.setFlushOnResponseCommit(true);
+        cache.setInvalidateOnShutdown(false);
+        cache.setRemoveUnloadableSessions(true);
+        cache.setSaveOnCreate(true);
+        
+        //Now, we configure a FileSessionDataStore
+        FileSessionDataStore store = new FileSessionDataStore();
+        store.setStoreDir(new File("/tmp/sessions"));
+        store.setGracePeriodSec(3600);
+        store.setSavePeriodSec(0);
+        
+        //Tell the cache to use the store
+        cache.setSessionDataStore(store);
+        
+        //Tell the contex to use the cache/store combination
+        app1.getSessionHandler().setSessionCache(cache);
+        
+      //end::filesessiondatastore[]  
     }
 }
