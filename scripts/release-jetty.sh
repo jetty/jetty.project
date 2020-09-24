f7dedd8e7d354f3d1c2340ba7aef94ab3bbac260
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2020-09-24 09:04:28 -0500

    Addressing javadoc aggregate issues under JPMS
    Signed-off-by: Joakim Erdfelt <joakim.erdfelt@gmail.com>

--- scripts/release-jetty.sh
+++ scripts/release-jetty.sh
@@ -167,7 +167,7 @@ if proceedyn "Are you sure you want to release using above? (y/N)" n; then
     # This is equivalent to 'mvn release:perform'
     if proceedyn "Build/Deploy from tag $TAG_NAME? (Y/n)" y; then
         git checkout $TAG_NAME
-        mvn clean package javadoc:aggregate-jar deploy \
+        mvn clean package deploy \
             -Peclipse-release $DEPLOY_OPTS
         reportMavenTestFailures
         git checkout $GIT_BRANCH_ID
