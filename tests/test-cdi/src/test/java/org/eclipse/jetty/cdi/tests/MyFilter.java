b9112ce63170e06b375c82d27b6a5047e3ad9d2a
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2020-09-28 09:08:35 -0500

    Moving jetty-cdi tests to /tests/test-cdi to avoid jpms testing hell
    Signed-off-by: Joakim Erdfelt <joakim.erdfelt@gmail.com>

---
+++ tests/test-cdi/src/test/java/org/eclipse/jetty/cdi/tests/MyFilter.java
@@ -0,0 +1,56 @@
+//
+// ========================================================================
+// Copyright (c) 1995-2020 Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under
+// the terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0
+//
+// This Source Code may also be made available under the following
+// Secondary Licenses when the conditions for such availability set
+// forth in the Eclipse Public License, v. 2.0 are satisfied:
+// the Apache License v2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
+
+package org.eclipse.jetty.cdi.tests;
+
+import java.io.IOException;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class MyFilter implements Filter
+{
+    @Inject
+    BeanManager manager;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
+        if (manager == null)
+            throw new IllegalStateException();
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
+    {
+        // copy attribute from MyListener to see if it was decorated.
+        request.setAttribute("filter", manager);
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy()
+    {
+
+    }
+}
