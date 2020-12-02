f00a2c2917b4ce93305271768b3798df4dca263a
gregw <gregw@webtide.com>
2020-12-02 09:33:37 +0100

    WIP

--- jetty-start/src/test/java/org/eclipse/jetty/start/util/CorrectMavenCentralRefs.java
+++ jetty-start/src/test/java/org/eclipse/jetty/start/util/CorrectMavenCentralRefs.java
@@ -53,8 +53,8 @@ public class CorrectMavenCentralRefs
 
         // Test to make sure we are in right directory
         Path rootPomXml = buildRoot.resolve("pom.xml");
-        Path distPomXml = buildRoot.resolve("jetty-home/pom.xml");
-        if (!Files.exists(rootPomXml) || !Files.exists(distPomXml))
+        Path homePomXml = buildRoot.resolve("jetty-home/pom.xml");
+        if (!Files.exists(rootPomXml) || !Files.exists(homePomXml))
         {
             System.err.println("Not build root directory: " + buildRoot);
             System.exit(-1);
