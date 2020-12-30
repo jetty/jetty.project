6535f2d6e77b5db2432895fdbc2edd915e85be71
Simone Bordet <simone.bordet@gmail.com>
2020-12-30 12:08:13 +0100

    Fixes #5845 - Use UTF-8 encoding for client basic auth if requested.
    Disable CheckStyle check so that the unicode sequence in the test matches that in realm.properties.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-client/src/test/java/org/eclipse/jetty/client/HttpClientAuthenticationTest.java
+++ jetty-client/src/test/java/org/eclipse/jetty/client/HttpClientAuthenticationTest.java
@@ -157,7 +157,8 @@ public class HttpClientAuthenticationTest extends AbstractHttpClientServerTest
     {
         startBasic(scenario, new EmptyServerHandler(), StandardCharsets.UTF_8);
         URI uri = URI.create(scenario.getScheme() + "://localhost:" + connector.getLocalPort());
-        testAuthentication(scenario, new BasicAuthentication(uri, realm, "basic_utf8", "€"));
+        // @checkstyle-disable-check : AvoidEscapedUnicodeCharactersCheck
+        testAuthentication(scenario, new BasicAuthentication(uri, realm, "basic_utf8", "\u20AC"));
     }
 
     @ParameterizedTest
