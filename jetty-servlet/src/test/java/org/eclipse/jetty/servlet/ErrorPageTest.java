3321ae71bec1102da821cab05905a849addccfe0
Greg Wilkins <gregw@webtide.com>
2021-02-09 10:03:00 +0100

    Fix #5939 unwrap ServletException (#5958)
    * Fix #5939 unwrap ServletException
    
    Fix #5939 unwrap ServletException
    
    Signed-off-by: Greg Wilkins <gregw@webtide.com>
    
    * Fix #5939 unwrap ServletException
    
    reverted default to false
    
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- jetty-servlet/src/test/java/org/eclipse/jetty/servlet/ErrorPageTest.java
+++ jetty-servlet/src/test/java/org/eclipse/jetty/servlet/ErrorPageTest.java
@@ -296,14 +296,28 @@ public class ErrorPageTest
     @Test
     public void testErrorException() throws Exception
     {
+        _errorPageErrorHandler.setUnwrapServletException(false);
         try (StacklessLogging stackless = new StacklessLogging(HttpChannel.class))
         {
             String response = _connector.getResponse("GET /fail/exception HTTP/1.0\r\n\r\n");
             assertThat(response, Matchers.containsString("HTTP/1.1 500 Server Error"));
             assertThat(response, Matchers.containsString("ERROR_PAGE: /TestException"));
             assertThat(response, Matchers.containsString("ERROR_CODE: 500"));
-            assertThat(response, Matchers.containsString("ERROR_EXCEPTION: javax.servlet.ServletException: java.lang.IllegalStateException"));
+            assertThat(response, Matchers.containsString("ERROR_EXCEPTION: javax.servlet.ServletException: java.lang.IllegalStateException: Test Exception"));
             assertThat(response, Matchers.containsString("ERROR_EXCEPTION_TYPE: class javax.servlet.ServletException"));
+            assertThat(response, Matchers.containsString("ERROR_SERVLET: org.eclipse.jetty.servlet.ErrorPageTest$FailServlet-"));
+            assertThat(response, Matchers.containsString("ERROR_REQUEST_URI: /fail/exception"));
+        }
+
+        _errorPageErrorHandler.setUnwrapServletException(true);
+        try (StacklessLogging stackless = new StacklessLogging(HttpChannel.class))
+        {
+            String response = _connector.getResponse("GET /fail/exception HTTP/1.0\r\n\r\n");
+            assertThat(response, Matchers.containsString("HTTP/1.1 500 Server Error"));
+            assertThat(response, Matchers.containsString("ERROR_PAGE: /TestException"));
+            assertThat(response, Matchers.containsString("ERROR_CODE: 500"));
+            assertThat(response, Matchers.containsString("ERROR_EXCEPTION: java.lang.IllegalStateException: Test Exception"));
+            assertThat(response, Matchers.containsString("ERROR_EXCEPTION_TYPE: class java.lang.IllegalStateException"));
             assertThat(response, Matchers.containsString("ERROR_SERVLET: org.eclipse.jetty.servlet.ErrorPageTest$FailServlet-"));
             assertThat(response, Matchers.containsString("ERROR_REQUEST_URI: /fail/exception"));
         }
@@ -619,7 +633,7 @@ public class ErrorPageTest
             if (code != null)
                 response.sendError(Integer.parseInt(code));
             else
-                throw new ServletException(new IllegalStateException());
+                throw new ServletException(new IllegalStateException("Test Exception"));
         }
     }
 
