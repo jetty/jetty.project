19c32d608719c9789d389d9335e37b1034775e29
olivier lamy <oliver.lamy@gmail.com>
2021-02-16 21:01:13 +1000

    fix compilation
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

--- jetty-runner/src/main/java/org/eclipse/jetty/runner/Runner.java
+++ jetty-runner/src/main/java/org/eclipse/jetty/runner/Runner.java
@@ -30,6 +30,7 @@ import org.eclipse.jetty.security.ConstraintMapping;
 import org.eclipse.jetty.security.ConstraintSecurityHandler;
 import org.eclipse.jetty.security.HashLoginService;
 import org.eclipse.jetty.security.authentication.BasicAuthenticator;
+import org.eclipse.jetty.server.AbstractConnector;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.CustomRequestLog;
 import org.eclipse.jetty.server.Handler;
