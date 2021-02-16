071584668cfdae1222deb53eefc2520ce4b00412
gregw <gregw@webtide.com>
2021-02-16 18:42:28 +0100

    updates from review

--- jetty-server/src/main/java/org/eclipse/jetty/server/HttpConnection.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/HttpConnection.java
@@ -380,7 +380,7 @@ public class HttpConnection extends AbstractConnection implements Runnable, Http
         if (isFillInterested())
         {
             LOG.warn("Pending read in onCompleted {} {}", this, getEndPoint());
-            abort(new IllegalStateException("Pending read in onCompleted"));
+            _channel.abort(new IOException("Pending read in onCompleted"));
         }
 
         // Handle connection upgrades
