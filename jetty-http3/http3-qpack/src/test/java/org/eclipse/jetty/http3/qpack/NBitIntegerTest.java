ae4f33ed9e3d4f40a2f5d9f2aee46b07aeb58d19
Lachlan Roberts <lachlan@webtide.com>
2021-02-24 00:40:33 +1100

    Remove the QPACK tests which only apply to HTTP/2 HPACK.
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-http3/http3-qpack/src/test/java/org/eclipse/jetty/http3/qpack/NBitIntegerTest.java
+++ jetty-http3/http3-qpack/src/test/java/org/eclipse/jetty/http3/qpack/NBitIntegerTest.java
@@ -21,6 +21,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SuppressWarnings("PointlessArithmeticExpression")
 public class NBitIntegerTest
 {
 
