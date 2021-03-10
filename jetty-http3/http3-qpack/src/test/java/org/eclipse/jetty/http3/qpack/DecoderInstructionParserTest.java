68e712d6bff7667c09e9ffe3e0db1aa1071c7078
Lachlan Roberts <lachlan@webtide.com>
2021-03-10 12:37:51 +1100

    Fix broken test case EncodeDecodeTest.
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-http3/http3-qpack/src/test/java/org/eclipse/jetty/http3/qpack/DecoderInstructionParserTest.java
+++ jetty-http3/http3-qpack/src/test/java/org/eclipse/jetty/http3/qpack/DecoderInstructionParserTest.java
@@ -14,8 +14,6 @@
 package org.eclipse.jetty.http3.qpack;
 
 import java.nio.ByteBuffer;
-import java.util.LinkedList;
-import java.util.Queue;
 
 import org.eclipse.jetty.http3.qpack.internal.parser.DecoderInstructionParser;
 import org.eclipse.jetty.util.BufferUtil;
@@ -30,76 +28,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DecoderInstructionParserTest
 {
-    public static class DebugHandler implements DecoderInstructionParser.Handler
-    {
-        public Queue<Integer> setCapacities = new LinkedList<>();
-        public Queue<Integer> duplicates = new LinkedList<>();
-        public Queue<Entry> literalNameEntries = new LinkedList<>();
-        public Queue<ReferencedEntry> referencedNameEntries = new LinkedList<>();
-
-        public static class Entry
-        {
-            public Entry(String name, String value)
-            {
-                this.value = value;
-                this.name = name;
-            }
-
-            String name;
-            String value;
-        }
-
-        public static class ReferencedEntry
-        {
-            public ReferencedEntry(int index, boolean dynamic, String value)
-            {
-                this.index = index;
-                this.dynamic = dynamic;
-                this.value = value;
-            }
-
-            int index;
-            boolean dynamic;
-            String value;
-        }
-
-        @Override
-        public void onSetDynamicTableCapacity(int capacity)
-        {
-            setCapacities.add(capacity);
-        }
-
-        @Override
-        public void onDuplicate(int index)
-        {
-            duplicates.add(index);
-        }
-
-        @Override
-        public void onInsertNameWithReference(int nameIndex, boolean isDynamicTableIndex, String value)
-        {
-            referencedNameEntries.add(new ReferencedEntry(nameIndex, isDynamicTableIndex, value));
-        }
-
-        @Override
-        public void onInsertWithLiteralName(String name, String value)
-        {
-            literalNameEntries.add(new Entry(name, value));
-        }
-
-        public boolean isEmpty()
-        {
-            return setCapacities.isEmpty() && duplicates.isEmpty() && literalNameEntries.isEmpty() && referencedNameEntries.isEmpty();
-        }
-    }
-
     private DecoderInstructionParser _instructionParser;
-    private DebugHandler _handler;
+    private DecoderParserDebugHandler _handler;
 
     @BeforeEach
     public void before()
     {
-        _handler = new DebugHandler();
+        _handler = new DecoderParserDebugHandler();
         _instructionParser = new DecoderInstructionParser(_handler);
     }
 
@@ -109,7 +44,7 @@ public class DecoderInstructionParserTest
         String insertAuthorityEntry = "c00f7777772e6578616d706c652e636f6d";
         ByteBuffer buffer = BufferUtil.toBuffer(TypeUtil.fromHexString(insertAuthorityEntry));
         _instructionParser.parse(buffer);
-        DebugHandler.ReferencedEntry entry = _handler.referencedNameEntries.poll();
+        DecoderParserDebugHandler.ReferencedEntry entry = _handler.referencedNameEntries.poll();
         assertNotNull(entry);
         assertThat(entry.index, is(0));
         assertThat(entry.dynamic, is(false));
