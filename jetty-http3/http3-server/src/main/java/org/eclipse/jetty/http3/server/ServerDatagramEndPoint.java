aaf0239911690e2061c0b450df18c78476f73943
Ludovic Orban <lorban@bitronix.be>
2021-03-17 14:35:33 +0100

    handle filling and writing with proper NIO
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-http3/http3-server/src/main/java/org/eclipse/jetty/http3/server/ServerDatagramEndPoint.java
+++ jetty-http3/http3-server/src/main/java/org/eclipse/jetty/http3/server/ServerDatagramEndPoint.java
@@ -13,18 +13,16 @@
 
 package org.eclipse.jetty.http3.server;
 
+import java.io.Closeable;
 import java.io.IOException;
-import java.net.Inet4Address;
-import java.net.Inet6Address;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.CancelledKeyException;
 import java.nio.channels.DatagramChannel;
 import java.nio.channels.ReadPendingException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.WritePendingException;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.eclipse.jetty.io.Connection;
 import org.eclipse.jetty.io.EndPoint;
@@ -34,6 +32,8 @@ import org.eclipse.jetty.io.ManagedSelector;
 import org.eclipse.jetty.io.WriteFlusher;
 import org.eclipse.jetty.util.BufferUtil;
 import org.eclipse.jetty.util.Callback;
+import org.eclipse.jetty.util.thread.AutoLock;
+import org.eclipse.jetty.util.thread.Invocable;
 import org.eclipse.jetty.util.thread.Scheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,15 +43,76 @@ public class ServerDatagramEndPoint extends IdleTimeout implements EndPoint, Man
     private static final Logger LOG = LoggerFactory.getLogger(ServerDatagramEndPoint.class);
 
     private final long createdTimeStamp = System.currentTimeMillis();
-    private final AtomicBoolean fillable = new AtomicBoolean();
+    private final AutoLock _lock = new AutoLock();
+    private final Runnable _runFillable = new ServerDatagramEndPoint.RunnableCloseable("runFillable")
+    {
+        @Override
+        public InvocationType getInvocationType()
+        {
+            return getFillInterest().getCallbackInvocationType();
+        }
+
+        @Override
+        public void run()
+        {
+            getFillInterest().fillable();
+        }
+    };
+
+    private final Runnable _runCompleteWrite = new ServerDatagramEndPoint.RunnableCloseable("runCompleteWrite")
+    {
+        @Override
+        public InvocationType getInvocationType()
+        {
+            return getWriteFlusher().getCallbackInvocationType();
+        }
+
+        @Override
+        public void run()
+        {
+            getWriteFlusher().completeWrite();
+        }
+
+        @Override
+        public String toString()
+        {
+            return String.format("%s:%s:%s->%s", ServerDatagramEndPoint.this, _operation, getInvocationType(), getWriteFlusher());
+        }
+    };
+
+    private final Runnable _runCompleteWriteFillable = new ServerDatagramEndPoint.RunnableCloseable("runCompleteWriteFillable")
+    {
+        @Override
+        public InvocationType getInvocationType()
+        {
+            InvocationType fillT = getFillInterest().getCallbackInvocationType();
+            InvocationType flushT = getWriteFlusher().getCallbackInvocationType();
+            if (fillT == flushT)
+                return fillT;
+
+            if (fillT == InvocationType.EITHER && flushT == InvocationType.NON_BLOCKING)
+                return InvocationType.EITHER;
+
+            if (fillT == InvocationType.NON_BLOCKING && flushT == InvocationType.EITHER)
+                return InvocationType.EITHER;
+
+            return InvocationType.BLOCKING;
+        }
+
+        @Override
+        public void run()
+        {
+            getWriteFlusher().completeWrite();
+            getFillInterest().fillable();
+        }
+    };
+
     private final FillInterest fillInterest = new FillInterest()
     {
         @Override
         protected void needsFillInterest()
         {
-            // TODO: delete code below, needs to interact with NIO.
-            if (fillable.getAndSet(false))
-                fillInterest.fillable();
+            changeInterests(SelectionKey.OP_READ);
         }
     };
     private final WriteFlusher writeFlusher = new WriteFlusher(this)
@@ -59,21 +120,38 @@ public class ServerDatagramEndPoint extends IdleTimeout implements EndPoint, Man
         @Override
         protected void onIncompleteFlush()
         {
-            // TODO: see SocketChannelEndPoint: must interact with NIO.
+            changeInterests(SelectionKey.OP_WRITE);
         }
     };
+
+    public FillInterest getFillInterest()
+    {
+        return fillInterest;
+    }
+
+    public WriteFlusher getWriteFlusher()
+    {
+        return writeFlusher;
+    }
+
+    private final ManagedSelector.SelectorUpdate _updateKeyAction = s -> updateKey();
+
     private final DatagramChannel channel;
-    private final ManagedSelector selector;
-    private final SelectionKey selectionKey;
+    private final ManagedSelector _selector;
     private Connection connection;
     private boolean open;
+
+    private SelectionKey _key;
+    private boolean _updatePending;
+    private int _currentInterestOps;
+    private int _desiredInterestOps;
 
     public ServerDatagramEndPoint(Scheduler scheduler, DatagramChannel channel, ManagedSelector selector, SelectionKey selectionKey)
     {
         super(scheduler);
         this.channel = channel;
-        this.selector = selector;
-        this.selectionKey = selectionKey;
+        this._selector = selector;
+        this._key = selectionKey;
     }
 
     @Override
@@ -185,28 +263,107 @@ public class ServerDatagramEndPoint extends IdleTimeout implements EndPoint, Man
     @Override
     public void updateKey()
     {
-        // TODO: change interest?
+        // This method runs from the selector thread,
+        // possibly concurrently with changeInterests(int).
+
+        try
+        {
+            int oldInterestOps;
+            int newInterestOps;
+            try (AutoLock l = _lock.lock())
+            {
+                _updatePending = false;
+                oldInterestOps = _currentInterestOps;
+                newInterestOps = _desiredInterestOps;
+                if (oldInterestOps != newInterestOps)
+                {
+                    _currentInterestOps = newInterestOps;
+                    _key.interestOps(newInterestOps);
+                }
+            }
+
+            if (LOG.isDebugEnabled())
+                LOG.debug("Key interests updated {} -> {} on {}", oldInterestOps, newInterestOps, this);
+        }
+        catch (CancelledKeyException x)
+        {
+            if (LOG.isDebugEnabled())
+                LOG.debug("Ignoring key update for cancelled key {}", this, x);
+            close();
+        }
+        catch (Throwable x)
+        {
+            LOG.warn("Ignoring key update for {}", this, x);
+            close();
+        }
     }
 
     @Override
     public void replaceKey(SelectionKey newKey)
     {
-        throw new UnsupportedOperationException();
+        this._key = newKey;
+    }
+
+    private void changeInterests(int operation)
+    {
+        // This method runs from any thread, possibly
+        // concurrently with updateKey() and onSelected().
+
+        int oldInterestOps;
+        int newInterestOps;
+        boolean pending;
+        try (AutoLock l = _lock.lock())
+        {
+            pending = _updatePending;
+            oldInterestOps = _desiredInterestOps;
+            newInterestOps = oldInterestOps | operation;
+            if (newInterestOps != oldInterestOps)
+                _desiredInterestOps = newInterestOps;
+        }
+
+        if (LOG.isDebugEnabled())
+            LOG.debug("changeInterests p={} {}->{} for {}", pending, oldInterestOps, newInterestOps, this);
+
+        if (!pending && _selector != null)
+            _selector.submit(_updateKeyAction);
     }
 
     @Override
     public Runnable onSelected()
     {
-        // TODO: need to handle both read and write events.
-        //selectionKey.interestOps(~SelectionKey.OP_READ);
+        // This method runs from the selector thread,
+        // possibly concurrently with changeInterests(int).
 
-
-        return () ->
+        int readyOps = _key.readyOps();
+        int oldInterestOps;
+        int newInterestOps;
+        try (AutoLock l = _lock.lock())
         {
-            //TODO: this is racy
-            if (!fillInterest.fillable())
-                fillable.set(true);
-        };
+            _updatePending = true;
+            // Remove the readyOps, that here can only be OP_READ or OP_WRITE (or both).
+            oldInterestOps = _desiredInterestOps;
+            newInterestOps = oldInterestOps & ~readyOps;
+            _desiredInterestOps = newInterestOps;
+        }
+
+        boolean fillable = (readyOps & SelectionKey.OP_READ) != 0;
+        boolean flushable = (readyOps & SelectionKey.OP_WRITE) != 0;
+
+        if (LOG.isDebugEnabled())
+            LOG.debug("onSelected {}->{} r={} w={} for {}", oldInterestOps, newInterestOps, fillable, flushable, this);
+
+        // return task to complete the job
+        Runnable task = fillable
+            ? (flushable
+            ? _runCompleteWriteFillable
+            : _runFillable)
+            : (flushable
+            ? _runCompleteWrite
+            : null);
+
+        if (LOG.isDebugEnabled())
+            LOG.debug("task {}", task);
+        return task;
     }
 
     @Override
@@ -273,5 +430,42 @@ public class ServerDatagramEndPoint extends IdleTimeout implements EndPoint, Man
     public void upgrade(Connection newConnection)
     {
         throw new UnsupportedOperationException();
+    }
+
+    private abstract class RunnableTask implements Runnable, Invocable
+    {
+        final String _operation;
+
+        protected RunnableTask(String op)
+        {
+            _operation = op;
+        }
+
+        @Override
+        public String toString()
+        {
+            return String.format("%s:%s:%s", ServerDatagramEndPoint.this, _operation, getInvocationType());
+        }
+    }
+
+    private abstract class RunnableCloseable extends ServerDatagramEndPoint.RunnableTask implements Closeable
+    {
+        protected RunnableCloseable(String op)
+        {
+            super(op);
+        }
+
+        @Override
+        public void close()
+        {
+            try
+            {
+                ServerDatagramEndPoint.this.close();
+            }
+            catch (Throwable x)
+            {
+                LOG.warn("Unable to close {}", ServerDatagramEndPoint.this, x);
+            }
+        }
     }
 }
