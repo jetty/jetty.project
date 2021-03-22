4cd5f08cd681aaa081bbc91236900126cea8a13b
Ludovic Orban <lorban@bitronix.be>
2021-03-22 10:12:56 +0100

    add big file download test
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-http3/http3-server/src/test/java/org/eclipse/jetty/http3/server/ServerDatagramConnectorTest.java
+++ jetty-http3/http3-server/src/test/java/org/eclipse/jetty/http3/server/ServerDatagramConnectorTest.java
@@ -13,9 +13,12 @@
 
 package org.eclipse.jetty.http3.server;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -30,7 +33,7 @@ import org.junit.jupiter.api.Test;
 public class ServerDatagramConnectorTest
 {
     @Test
-    public void name() throws Exception
+    public void testSmall() throws Exception
     {
         Server server = new Server();
 
@@ -54,6 +57,51 @@ public class ServerDatagramConnectorTest
                     "\t\tRequest served\n" +
                     "\t</body>\n" +
                     "</html>");
+            }
+        });
+
+        server.start();
+
+        System.out.println("Started.");
+        System.in.read();
+
+        server.stop();
+    }
+
+    @Test
+    public void testBig() throws Exception
+    {
+        Server server = new Server();
+
+        HttpConfiguration config = new HttpConfiguration();
+        config.setHttpCompliance(HttpCompliance.LEGACY); // enable HTTP/0.9
+        HttpConnectionFactory connectionFactory = new HttpConnectionFactory(config);
+
+        ServerDatagramConnector serverDatagramConnector = new ServerDatagramConnector(server, connectionFactory);
+        serverDatagramConnector.setPort(8443);
+        server.addConnector(serverDatagramConnector);
+
+        server.setHandler(new AbstractHandler()
+        {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+            {
+                baseRequest.setHandled(true);
+                ServletOutputStream outputStream = response.getOutputStream();
+
+                File file = new File("./src/test/resources/zfs-intro.pdf");
+                response.setContentLengthLong(file.length());
+                byte[] buffer = new byte[1024];
+                try (FileInputStream fis = new FileInputStream(file))
+                {
+                    while (true)
+                    {
+                        int read = fis.read(buffer);
+                        if (read == -1)
+                            break;
+                        outputStream.write(buffer, 0, read);
+                    }
+                }
             }
         });
 
