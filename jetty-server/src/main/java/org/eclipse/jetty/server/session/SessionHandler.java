2cd5990095b93db2ff9c041c8f5991358c130754
Simone Bordet <simone.bordet@gmail.com>
2021-03-25 11:32:51 +0100

    Issue #6085 Fix duplicate valid session cookies to pick first valid.
    Cosmetic changes.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-server/src/main/java/org/eclipse/jetty/server/session/SessionHandler.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/session/SessionHandler.java
@@ -1674,8 +1674,7 @@ public class SessionHandler extends ScopedHandler
                             if (s != null && isValid(s))
                             {
                                 //associate it with the request so its reference count is decremented as the
-                                //session exits
-                                //try this session id
+                                //request exits
                                 requestedSessionId = id;
                                 session = s;
                                 baseRequest.enterSession(session);
@@ -1713,8 +1712,11 @@ public class SessionHandler extends ScopedHandler
                                     throw new BadMessageException("Duplicate valid session cookies: " + requestedSessionId + " ," + id);
                                 }
                             }
-                            else if (LOG.isDebugEnabled())
-                                LOG.debug("Duplicate valid session cookie id: {}", id);
+                            else
+                            {
+                                if (LOG.isDebugEnabled())
+                                    LOG.debug("Duplicate valid session cookie id: {}", id);
+                            }
                         }
                     }
                 }
