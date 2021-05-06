68fa1475d1e1f11176e1d5a6514ad2c9b8e26215
Simone Bordet <simone.bordet@gmail.com>
2021-05-06 11:58:16 +0200

    Issue #5229 - WebSocket documentation.
    WebSocket server documentation.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-websocket/websocket-jetty-tests/src/test/java/org/eclipse/jetty/websocket/tests/JettyClientClassLoaderTest.java
+++ jetty-websocket/websocket-jetty-tests/src/test/java/org/eclipse/jetty/websocket/tests/JettyClientClassLoaderTest.java
@@ -149,6 +149,11 @@ public class JettyClientClassLoaderTest
     @WebSocket
     public static class EchoSocket
     {
+        public EchoSocket()
+        {
+            System.err.println("this = " + this);
+        }
+
         @OnWebSocketMessage
         public void onMessage(Session session, String message) throws Exception
         {
@@ -191,7 +196,7 @@ public class JettyClientClassLoaderTest
             WebAppTester.WebApp app2 = webAppTester.createWebApp("/echo");
             app2.addConfiguration(new JettyWebSocketConfiguration());
             app2.createWebInf();
-            app2.copyClass(EchoServlet.class);
+//            app2.copyClass(EchoServlet.class);
             app2.copyClass(EchoSocket.class);
             app2.deploy();
         });
