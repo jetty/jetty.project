d9fe0e5476ab63a9fc03523fe9df7b2274f2121b
Greg Wilkins <gregw@webtide.com>
2021-05-25 11:38:48 +1000

    Added doco on compliance modes
    This is a DRAFT!
    
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- jetty-http/src/main/java/org/eclipse/jetty/http/HttpCompliance.java
+++ jetty-http/src/main/java/org/eclipse/jetty/http/HttpCompliance.java
@@ -43,13 +43,66 @@ public final class HttpCompliance implements ComplianceViolation.Mode
     // the relevant section of the RFC is not strictly adhered to.
     public enum Violation implements ComplianceViolation
     {
+        /**
+         * The HTTP RFC(s) require that field names are case-insensitive, so for example the fields "{@code Content-Type: text/xml}"
+         * and "{@code content-type: text/xml}" are considered equivalent.  Jetty has been optimized to take advantage of this by
+         * looking up field names in a case insensitive cache and will by default provide the standard capitalisation of a field
+         * name rather than create a new string with the actual capitalisation received.   However, some applications have been
+         * written to expect a specific capitalisation of field, so deployments of such applications must include this violation
+         * in their {@link HttpCompliance} mode to prevent Jetty altering the case of the fields received. Jetty itself will still
+         * match and handle fields names insensitively and this violation only affects how the names are reported to the application.
+         * There is a small performance and garbage impact of using this mode.
+         */
         CASE_SENSITIVE_FIELD_NAME("https://tools.ietf.org/html/rfc7230#section-3.2", "Field name is case-insensitive"),
+
+        /**
+         * The HTTP RFC(s) require that method names are case sensitive, so that "{@code Get}" and "{@code GET}" are considered
+         * different methods.   Jetty releases prior to 9.4 used a case insensitive cache to match method names, thus this requirement
+         * was violated.  Deployments which wish to retain this legacy violation can include this violation in the
+         * {@link HttpCompliance} mode.
+         */
         CASE_INSENSITIVE_METHOD("https://tools.ietf.org/html/rfc7230#section-3.1.1", "Method is case-sensitive"),
+
+        /**
+         * Since RFC 7230, the expectation that HTTP/0.9 is supported has been removed from the specification.  If a deployment
+         * wished to accept HTTP/0.9 requests, then it can include this violation in it's {@link HttpCompliance} mode.
+         */
         HTTP_0_9("https://tools.ietf.org/html/rfc7230#appendix-A.2", "HTTP/0.9 not supported"),
+
+        /**
+         * Since <a href="https://tools.ietf.org/html/rfc7230#section-3.2.4">RFC 7230</a>, the HTTP protocol no longer supports
+         * line folding, which allows a field value to be provided over several lines.  Deployments that wish to receive folder
+         * field values may include this violation in their {@link HttpCompliance} mode.
+         */
         MULTILINE_FIELD_VALUE("https://tools.ietf.org/html/rfc7230#section-3.2.4", "Line Folding not supported"),
-        MULTIPLE_CONTENT_LENGTHS("https://tools.ietf.org/html/rfc7230#section-3.3.1", "Multiple Content-Lengths"),
+
+        /**
+         * Since <a href="https://tools.ietf.org/html/rfc7230#section-3.3.2">RFC 7230</a>, the HTTP protocol has required that
+         * a request is invalid if it contains multiple {@code Content-Length} fields or values.  The request may be treated
+         * as invalid even if the multiple values are the same. A deployment may include this violation to allow multiple
+         * {@code Content-Length} values to be received, but only if they are identical.
+         */
+        MULTIPLE_CONTENT_LENGTHS("https://tools.ietf.org/html/rfc7230#section-3.3.2", "Multiple Content-Lengths"),
+
+        /**
+         * Since <a href="https://tools.ietf.org/html/rfc7230#section-3.3.1">RFC 7230</a>, the HTTP protocol has required that
+         * a request is invalid if it contains both a {@code Transfer-Encoding} field and  {@code Content-Length} field.
+         * A deployment may include this violation to allow both fields to be in a received request.
+         */
         TRANSFER_ENCODING_WITH_CONTENT_LENGTH("https://tools.ietf.org/html/rfc7230#section-3.3.1", "Transfer-Encoding and Content-Length"),
+
+        /**
+         * Since <a href="https://tools.ietf.org/html/rfc7230#section-3.2.4">RFC 7230</a>, the HTTP protocol has required that
+         * a request header field has no white space after the field name and before the ':'.
+         * A deployment may include this violation to allow such fields to be in a received request.
+         */
         WHITESPACE_AFTER_FIELD_NAME("https://tools.ietf.org/html/rfc7230#section-3.2.4", "Whitespace not allowed after field name"),
+
+        /**
+         * Prior to <a href="https://tools.ietf.org/html/rfc7230#section-3.2">RFC 7230</a>, the HTTP protocol allowed a header
+         * line of a single token with neither a colon nor value following, to be interpreted as a field name with no value.
+         * A deployment may include this violation to allow such fields to be in a received request.
+         */
         NO_COLON_AFTER_FIELD_NAME("https://tools.ietf.org/html/rfc7230#section-3.2", "Fields must have a Colon");
 
         private final String url;
@@ -80,21 +133,51 @@ public final class HttpCompliance implements ComplianceViolation.Mode
         }
     }
 
+    /**
+     * The request attribute which may be set to record any allowed HTTP violations.
+     */
     public static final String VIOLATIONS_ATTR = "org.eclipse.jetty.http.compliance.violations";
 
+    /**
+     * The HttpCompliance mode that supports <a href="https://tools.ietf.org/html/rfc7230">RFC 7230</a>
+     * with no known violations.
+     */
     public static final HttpCompliance RFC7230 = new HttpCompliance("RFC7230", noneOf(Violation.class));
+
+    /**
+     * The HttpCompliance mode that supports <a href="https://tools.ietf.org/html/rfc2616">RFC 7230</a>
+     * with only the violations that differ from {@link #RFC7230}.
+     */
     public static final HttpCompliance RFC2616 = new HttpCompliance("RFC2616", of(Violation.HTTP_0_9, Violation.MULTILINE_FIELD_VALUE));
+
+    /**
+     * A legacy HttpCompliance mode that allows all violations except case-insensitive methods.
+     */
     public static final HttpCompliance LEGACY = new HttpCompliance("LEGACY", complementOf(of(Violation.CASE_INSENSITIVE_METHOD)));
+
+    /**
+     * A legacy HttpCompliance mode that supports {@link #RFC2616}, but that also allows: case-insensitive methods;
+     * colons after field names; {@code Transfer-Encoding} with {@code Content-Length} fields; and multiple {@code Content-Length} values.
+     */
     public static final HttpCompliance RFC2616_LEGACY = RFC2616.with("RFC2616_LEGACY",
         Violation.CASE_INSENSITIVE_METHOD,
         Violation.NO_COLON_AFTER_FIELD_NAME,
         Violation.TRANSFER_ENCODING_WITH_CONTENT_LENGTH,
         Violation.MULTIPLE_CONTENT_LENGTHS);
+
+    /**
+     * A legacy HttpCompliance mode that supports {@link #RFC7230}, but with case-insensitive methods allowed.
+     */
     public static final HttpCompliance RFC7230_LEGACY = RFC7230.with("RFC7230_LEGACY", Violation.CASE_INSENSITIVE_METHOD);
 
     private static final List<HttpCompliance> KNOWN_MODES = Arrays.asList(RFC7230, RFC2616, LEGACY, RFC2616_LEGACY, RFC7230_LEGACY);
     private static final AtomicInteger __custom = new AtomicInteger();
 
+    /**
+     * Get a known compliance mode by name.
+     * @param name The name of a known {@link HttpCompliance} mode.
+     * @return The mode matching the name.
+     */
     public static HttpCompliance valueOf(String name)
     {
         for (HttpCompliance compliance : KNOWN_MODES)
@@ -107,7 +190,7 @@ public final class HttpCompliance implements ComplianceViolation.Mode
     }
 
     /**
-     * Create compliance set from string.
+     * Create compliance mode from a String description.
      * <p>
      * Format:
      * </p>
@@ -186,11 +269,6 @@ public final class HttpCompliance implements ComplianceViolation.Mode
         return _name;
     }
 
-    /**
-     * Get the set of {@link Violation}s allowed by this compliance mode.
-     *
-     * @return The immutable set of {@link Violation}s allowed by this compliance mode.
-     */
     @Override
     public Set<Violation> getAllowed()
     {
