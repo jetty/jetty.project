395e19e838a9691b30a17620f34a911d41e5010c
Greg Wilkins <gregw@webtide.com>
2021-06-02 09:21:53 +1000

    Explain naming
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- jetty-util/src/main/java/org/eclipse/jetty/util/thread/strategy/EatWhatYouKill.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/thread/strategy/EatWhatYouKill.java
@@ -52,6 +52,10 @@ import org.eclipse.jetty.util.thread.TryExecutor;
  * indicated it is non-blocking, then this strategy will dispatch the execution of
  * the task and immediately continue production. When operating in this pattern, the
  * sub-strategy is called ProduceExecuteConsume (PEC).</p>
+ * <p>The EatWhatYouKill strategy is named after a hunting proverb, in the
+ * sense that one should kill(produce) only to eat(consume).
+ * The use of this phrase is not an endorsement of hunting nor killing of
+ * wildlife for food or sport.</p>
  */
 @ManagedObject("eat what you kill execution strategy")
 public class EatWhatYouKill extends ContainerLifeCycle implements ExecutionStrategy, Runnable
