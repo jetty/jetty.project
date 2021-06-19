281d9615516496fc04765758589b6f0c51e4d119
olivier lamy <oliver.lamy@gmail.com>
2021-06-19 10:35:29 +1000

    Issue #6434 test generation of webfrag.xml and his content
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

--- jetty-jspc-maven-plugin/src/it/simple-jsp/postbuild.groovy
+++ jetty-jspc-maven-plugin/src/it/simple-jsp/postbuild.groovy
@@ -1,3 +1,11 @@
-
-
 System.out.println( "running postbuild.groovy" )
+
+File webfrag = new File(basedir, 'target/webfrag.xml')
+assert webfrag.exists()
+assert webfrag.text.contains("<servlet-name>org.apache.jsp.foo_jsp</servlet-name>")
+assert webfrag.text.contains("<servlet-class>org.apache.jsp.foo_jsp</servlet-class>")
+assert webfrag.text.contains("<url-pattern>/foo.jsp</url-pattern>")
+
+// cannot use such parsing as it is not real xml
+//def rootXml = new XmlSlurper().parse(new File(basedir, 'target/webfrag.xml'))
+
