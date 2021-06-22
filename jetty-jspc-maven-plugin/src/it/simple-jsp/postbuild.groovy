39572effdaff535221d276a2f16e72cd8f16a526
Olivier Lamy <oliver.lamy@gmail.com>
2021-06-22 17:38:43 +1000

    Issue #6434 add integration for testing generated webfrag.xml (#6444)
    * Issue #6434 test generation of webfrag.xml and his content
    
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

--- jetty-jspc-maven-plugin/src/it/simple-jsp/postbuild.groovy
+++ jetty-jspc-maven-plugin/src/it/simple-jsp/postbuild.groovy
@@ -1,3 +1,18 @@
-
-
 System.out.println( "running postbuild.groovy" )
+
+File webfrag = new File(basedir, 'target/webfrag.xml')
+assert webfrag.exists()
+assert webfrag.text.contains("<servlet-name>org.apache.jsp.foo_jsp</servlet-name>")
+assert webfrag.text.contains("<servlet-class>org.apache.jsp.foo_jsp</servlet-class>")
+assert webfrag.text.contains("<url-pattern>/foo.jsp</url-pattern>")
+
+// cannot use such parsing as it is not real xml
+//def rootXml = new XmlSlurper().parse(new File(basedir, 'target/webfrag.xml'))
+// so fake it
+def rootXml = new XmlSlurper().parseText("<root>"+webfrag.text+"</root>")
+
+assert rootXml.servlet.'servlet-name'.text() == "org.apache.jsp.foo_jsp"
+assert rootXml.servlet.'servlet-class'.text() == "org.apache.jsp.foo_jsp"
+
+assert rootXml.'servlet-mapping'.'servlet-name'.text() == "org.apache.jsp.foo_jsp"
+assert rootXml.'servlet-mapping'.'url-pattern'.text() == "/foo.jsp"
