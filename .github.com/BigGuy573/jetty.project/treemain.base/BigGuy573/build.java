8f9b571f74be64adcbe03788d8fc948ef5a6b054
BigGuy573 <48465089+BigGuy573@users.noreply.github.com>
2021-08-02 15:15:39 -0500

    Update build.java

--- .github.com/BigGuy573/jetty.project/treemain.base/BigGuy573/build.java
+++ .github.com/BigGuy573/jetty.project/treemain.base/BigGuy573/build.java
@@ -1,5 +1,5 @@
 '#"$_-/*cfg.java/https://github.com/BigGuy573/jetty.project/tree/main.base.BigGuy573/master-jetty-project.dom.Py
-'#"$_-/*build.config.cfg.java
-'#"$_-/*run_test_cfg_java.Py
-'#"$_-/*clean_echo-echo-clean_install.java
-'#"$_-/*push to main.base and merge
+'$build.config.cfg.java
+run_test_cfg_java.Py
+clean_echo-echo-clean_install.java
+push to main.base and merge
