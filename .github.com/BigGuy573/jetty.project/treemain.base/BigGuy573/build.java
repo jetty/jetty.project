8c9e345ede0d91863f521e0d8f278acf60d76a98
BigGuy573 <48465089+BigGuy573@users.noreply.github.com>
2021-08-02 15:14:36 -0500

    Update and rename master-jetty-project.dom.Py to build.java

---
+++ .github.com/BigGuy573/jetty.project/treemain.base/BigGuy573/build.java
@@ -0,0 +1,5 @@
+'#"$_-/*cfg.java/https://github.com/BigGuy573/jetty.project/tree/main.base.BigGuy573/master-jetty-project.dom.Py
+'#"$_-/*build.config.cfg.java
+'#"$_-/*run_test_cfg_java.Py
+'#"$_-/*clean_echo-echo-clean_install.java
+'#"$_-/*push to main.base and merge
