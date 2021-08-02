d35ff03e61cccdfaf311e237c89a89a490ccedfe
Lachlan Roberts <lachlan@webtide.com>
2021-08-02 15:23:28 +1000

    Issue #6554 - DefaultAuthenticatorFactory should not create BasicAuthenticator for null AuthMethod
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-security/src/main/java/org/eclipse/jetty/security/SecurityHandler.java
+++ jetty-security/src/main/java/org/eclipse/jetty/security/SecurityHandler.java
@@ -293,9 +293,6 @@ public abstract class SecurityHandler extends HandlerWrapper implements Authenti
         return getServer().getBean(IdentityService.class);
     }
 
-    /**
-     *
-     */
     @Override
     protected void doStart()
         throws Exception
@@ -349,7 +346,7 @@ public abstract class SecurityHandler extends HandlerWrapper implements Authenti
                 throw new IllegalStateException("LoginService has different IdentityService to " + this);
         }
 
-        if (_authenticator == null && _identityService != null)
+        if (_authenticator == null)
         {
             // If someone has set an authenticator factory only use that, otherwise try the list of discovered factories.
             if (_authenticatorFactory != null)
@@ -396,7 +393,6 @@ public abstract class SecurityHandler extends HandlerWrapper implements Authenti
     }
 
     @Override
-
     protected void doStop() throws Exception
     {
         //if we discovered the services (rather than had them explicitly configured), remove them.
