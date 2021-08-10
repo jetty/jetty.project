1641053b32aaf44f34c1bee1ae432e3160cc7ad8
Simone Bordet <simone.bordet@gmail.com>
2021-08-10 22:56:14 +0200

    Issue #6476 - Show message if JVM args are present but new JVM is spawned
    * Added WARN message.
    * Updated documentation.
    * Added test case.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-start/src/main/java/org/eclipse/jetty/start/Main.java
+++ jetty-start/src/main/java/org/eclipse/jetty/start/Main.java
@@ -30,6 +30,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 import org.eclipse.jetty.start.Props.Prop;
 import org.eclipse.jetty.start.config.CommandLineConfigSource;
@@ -467,6 +468,17 @@ public class Main
         {
             CommandLineBuilder cmd = args.getMainArgs(StartArgs.ALL_PARTS);
             cmd.debug();
+
+            List<String> execModules = args.getEnabledModules().stream()
+                .map(name -> args.getAllModules().get(name))
+                // Keep only the forking modules.
+                .filter(module -> !module.getJvmArgs().isEmpty())
+                .map(Module::getName)
+                .collect(Collectors.toList());
+            StartLog.warn("Forking second JVM due to forking module(s): %s.", execModules);
+            StartLog.warn("Consider using option --dry-run to generate the command line:");
+            StartLog.warn("$(java -jar $JETTY_HOME/start.jar --dry-run)");
+
             ProcessBuilder pbuilder = new ProcessBuilder(cmd.getArgs());
             StartLog.endStartLog();
             final Process process = pbuilder.start();
