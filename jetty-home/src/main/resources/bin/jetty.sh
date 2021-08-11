4cced35e0c9229a7abaa8eb8e8130282d9443d11
Greg Wilkins <gregw@webtide.com>
2021-08-12 09:03:51 +1000

    Fix #6597 Use dry run in jetty.sh
    update from review
    
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- jetty-home/src/main/resources/bin/jetty.sh
+++ jetty-home/src/main/resources/bin/jetty.sh
@@ -433,8 +433,8 @@ case "`uname`" in
 CYGWIN*) JETTY_START="`cygpath -w $JETTY_START`";;
 esac
 
-RUN_ARGS=$("$JAVA" -jar "$JETTY_START" --dry-run=opts,path,main,args ${JETTY_ARGS[*]})
-RUN_CMD=("$JAVA" ${JAVA_OPTIONS[@]} ${RUN_ARGS[@]})
+RUN_ARGS=$(echo $JAVA_OPTIONS ; "$JAVA" -jar "$JETTY_START" --dry-run=opts,path,main,args ${JETTY_ARGS[*]} | tail -1 )
+RUN_CMD=("$JAVA" ${RUN_ARGS[@]})
 
 #####################################################
 # Comment these out after you're happy with what
