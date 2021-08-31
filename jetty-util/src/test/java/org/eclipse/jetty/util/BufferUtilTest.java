cc65f2b750ee5c0fb73fabdda9a4e7bd0186a93f
Olivier Lamy <oliver.lamy@gmail.com>
2021-08-31 11:44:03 +1000

    remove test removed in 9.4.x
    Signed-off-by: Olivier Lamy <oliver.lamy@gmail.com>

--- jetty-util/src/test/java/org/eclipse/jetty/util/BufferUtilTest.java
+++ jetty-util/src/test/java/org/eclipse/jetty/util/BufferUtilTest.java
@@ -340,38 +340,4 @@ public class BufferUtilTest
         assertThat("Bytes in out equal bytes in buffer", Arrays.equals(bytes, out.toByteArray()), is(true));
     }
 
-    @Test
-    public void testMappedFile() throws Exception
-    {
-        String data="Now is the time for all good men to come to the aid of the party";
-        File file = File.createTempFile("test",".txt");
-        file.deleteOnExit();
-        try(FileWriter out = new FileWriter(file);)
-        {
-            out.write(data);
-        }
-
-        ByteBuffer mapped = BufferUtil.toMappedBuffer(file);
-        assertEquals(data,BufferUtil.toString(mapped));
-        assertTrue(BufferUtil.isMappedBuffer(mapped));
-
-        ByteBuffer direct = BufferUtil.allocateDirect(data.length());
-        BufferUtil.clearToFill(direct);
-        direct.put(data.getBytes(StandardCharsets.ISO_8859_1));
-        BufferUtil.flipToFlush(direct, 0);
-        assertEquals(data,BufferUtil.toString(direct));
-        assertFalse(BufferUtil.isMappedBuffer(direct));
-
-        ByteBuffer slice = direct.slice();
-        assertEquals(data,BufferUtil.toString(slice));
-        assertFalse(BufferUtil.isMappedBuffer(slice));
-
-        ByteBuffer duplicate = direct.duplicate();
-        assertEquals(data,BufferUtil.toString(duplicate));
-        assertFalse(BufferUtil.isMappedBuffer(duplicate));
-
-        ByteBuffer readonly = direct.asReadOnlyBuffer();
-        assertEquals(data,BufferUtil.toString(readonly));
-        assertFalse(BufferUtil.isMappedBuffer(readonly));
-    }
 }
