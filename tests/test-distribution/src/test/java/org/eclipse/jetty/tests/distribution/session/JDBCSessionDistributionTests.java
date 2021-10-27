ba7a27915d9c6b519c845ca0ee8d1d497f780d47
Olivier Lamy <oliver.lamy@gmail.com>
2021-10-27 10:13:42 +1000

    cleanup comment and unused imports
    Signed-off-by: Olivier Lamy <oliver.lamy@gmail.com>

--- tests/test-distribution/src/test/java/org/eclipse/jetty/tests/distribution/session/JDBCSessionDistributionTests.java
+++ tests/test-distribution/src/test/java/org/eclipse/jetty/tests/distribution/session/JDBCSessionDistributionTests.java
@@ -22,7 +22,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.MariaDBContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
 
 /**
  *
