cd1343fd6c76aecd292de16b86a31c761dfd7065
Simone Bordet <simone.bordet@gmail.com>
2021-11-03 22:11:11 +0100

    Issue #6728 - QUIC and HTTP/3
    - More test fixes.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-http3/http3-server/src/main/java/org/eclipse/jetty/http3/server/AbstractHTTP3ServerConnectionFactory.java
+++ jetty-http3/http3-server/src/main/java/org/eclipse/jetty/http3/server/AbstractHTTP3ServerConnectionFactory.java
@@ -73,6 +73,7 @@ public abstract class AbstractHTTP3ServerConnectionFactory extends AbstractConne
         long streamId = streamEndPoint.getStreamId();
         ServerHTTP3Session http3Session = (ServerHTTP3Session)streamEndPoint.getQuicSession().getProtocolSession();
         MessageParser parser = new MessageParser(http3Session.getSessionServer(), http3Session.getQpackDecoder(), streamId, streamEndPoint::isStreamFinished);
-        return new ServerHTTP3StreamConnection(connector, getHttpConfiguration(), streamEndPoint, http3Session, parser);
+        ServerHTTP3StreamConnection connection = new ServerHTTP3StreamConnection(connector, getHttpConfiguration(), streamEndPoint, http3Session, parser);
+        return configure(connection, connector, endPoint);
     }
 }
