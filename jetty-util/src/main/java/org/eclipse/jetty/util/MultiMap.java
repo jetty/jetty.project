291f277cee69cb4179b406cda1b707f7fbabc05e
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2022-02-16 11:55:19 -0400

    Removing unused import

--- jetty-util/src/main/java/org/eclipse/jetty/util/MultiMap.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/MultiMap.java
@@ -15,7 +15,6 @@ package org.eclipse.jetty.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
