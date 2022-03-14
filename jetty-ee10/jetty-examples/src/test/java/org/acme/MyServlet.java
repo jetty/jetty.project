7a876c19440890ac481f8ace2ddf1646f453499b
Ludovic Orban <lorban@bitronix.be>
2022-03-14 11:49:43 +0100

    add missing headers
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-ee10/jetty-examples/src/test/java/org/acme/MyServlet.java
+++ jetty-ee10/jetty-examples/src/test/java/org/acme/MyServlet.java
@@ -10,6 +10,7 @@
 // SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
 // ========================================================================
 //
+
 package org.acme;
 
 import java.io.IOException;
