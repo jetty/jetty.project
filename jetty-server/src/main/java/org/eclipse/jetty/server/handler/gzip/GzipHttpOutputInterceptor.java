d5bae95ed817f34a25617c360c86bbdf82094cad
Jaroslaw Rzeszotko <jrzeszotko@gmail.com>
2022-03-21 19:54:50 +0100

    Use direct buffers for gzip input/output, if configured - fixes.

--- jetty-server/src/main/java/org/eclipse/jetty/server/handler/gzip/GzipHttpOutputInterceptor.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/handler/gzip/GzipHttpOutputInterceptor.java
@@ -266,6 +266,16 @@ public class GzipHttpOutputInterceptor implements HttpOutput.Interceptor
             super(callback);
             _content = content;
             _last = complete;
+
+            if (_channel.isUseOutputDirectByteBuffers())
+            {
+                _crc.update(_content.slice());
+
+                Deflater deflater = _deflaterEntry.get();
+                deflater.setInput(_content);
+                if (_last)
+                    deflater.finish();
+            }
         }
 
         @Override
@@ -318,66 +328,64 @@ public class GzipHttpOutputInterceptor implements HttpOutput.Interceptor
             Deflater deflater = _deflaterEntry.get();
             if (!deflater.finished())
             {
-                if (deflater.needsInput())
-                {
-                    // if there is no more content available to compress
-                    // then we are either finished all content or just the current write.
-                    if (BufferUtil.isEmpty(_content))
-                    {
-                        if (_last)
-                            deflater.finish();
-                        else
-                            return Action.SUCCEEDED;
-                    }
-                    else if (_channel.isUseOutputDirectByteBuffers())
-                    {
-                        _crc.update(_content.slice());
-                        deflater.setInput(_content);
-                        if (_last)
-                            deflater.finish();
-                    }
-                    else
-                    {
-                        // If there is more content available to compress, we have to make sure
-                        // it is available in an array for the current deflator API, maybe slicing
-                        // of content.
-                        ByteBuffer slice;
-                        if (_content.hasArray())
-                            slice = _content;
-                        else
-                        {
-                            if (_copy == null)
-                                _copy = _channel.getByteBufferPool().acquire(_bufferSize, false);
-                            else
-                                BufferUtil.clear(_copy);
-                            slice = _copy;
-                            BufferUtil.append(_copy, _content);
-                        }
-
-                        // transfer the data from the slice to the the deflator
-                        byte[] array = slice.array();
-                        int off = slice.arrayOffset() + slice.position();
-                        int len = slice.remaining();
-                        _crc.update(array, off, len);
-                        // Ideally we would want to use the ByteBuffer API for Deflaters. However due the the ByteBuffer implementation
-                        // of the CRC32.update() it is less efficient for us to use this rather than to convert to array ourselves,
-                        // unless the ByteBuffer is a DirectByteBuffer.
-                        _deflaterEntry.get().setInput(array, off, len);
-                        slice.position(slice.position() + len);
-                        if (_last && BufferUtil.isEmpty(_content))
-                            deflater.finish();
-                    }
-                }
-
-                // deflate the content into the available space in the buffer
                 if (_channel.isUseOutputDirectByteBuffers())
                 {
+                    if (deflater.needsInput() && !_last)
+                    {
+                        return Action.SUCCEEDED;
+                    }
+
                     int pos = BufferUtil.flipToFill(_buffer);
-                    deflater.deflate(_buffer, _syncFlush ? Deflater.SYNC_FLUSH : Deflater.NO_FLUSH);
+                    int produced = deflater.deflate(_buffer, _syncFlush ? Deflater.SYNC_FLUSH : Deflater.NO_FLUSH);
                     BufferUtil.flipToFlush(_buffer, pos);
                 }
                 else
                 {
+                    if (deflater.needsInput())
+                    {
+                        // if there is no more content available to compress
+                        // then we are either finished all content or just the current write.
+                        if (BufferUtil.isEmpty(_content))
+                        {
+                            if (_last)
+                                deflater.finish();
+                            else
+                                return Action.SUCCEEDED;
+                        }
+                        else
+                        {
+                            // If there is more content available to compress, we have to make sure
+                            // it is available in an array for the current deflator API, maybe slicing
+                            // of content.
+                            ByteBuffer slice;
+                            if (_content.hasArray())
+                                slice = _content;
+                            else
+                            {
+                                if (_copy == null)
+                                    _copy = _channel.getByteBufferPool().acquire(_bufferSize, false);
+                                else
+                                    BufferUtil.clear(_copy);
+                                slice = _copy;
+                                BufferUtil.append(_copy, _content);
+                            }
+
+                            // transfer the data from the slice to the the deflator
+                            byte[] array = slice.array();
+                            int off = slice.arrayOffset() + slice.position();
+                            int len = slice.remaining();
+                            _crc.update(array, off, len);
+                            // Ideally we would want to use the ByteBuffer API for Deflaters. However due the the ByteBuffer implementation
+                            // of the CRC32.update() it is less efficient for us to use this rather than to convert to array ourselves,
+                            // unless the ByteBuffer is a DirectByteBuffer.
+                            _deflaterEntry.get().setInput(array, off, len);
+                            slice.position(slice.position() + len);
+                            if (_last && BufferUtil.isEmpty(_content))
+                                deflater.finish();
+                        }
+                    }
+
+                    // deflate the content into the available space in the buffer
                     int off = _buffer.arrayOffset() + _buffer.limit();
                     int len = BufferUtil.space(_buffer);
                     int produced = deflater.deflate(_buffer.array(), off, len, _syncFlush ? Deflater.SYNC_FLUSH : Deflater.NO_FLUSH);
