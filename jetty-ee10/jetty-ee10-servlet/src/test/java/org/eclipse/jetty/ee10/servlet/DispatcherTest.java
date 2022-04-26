e24bc85371a976769e094cb49996da34e17724aa
Lachlan Roberts <lachlan@webtide.com>
2022-04-26 18:42:28 +1000

    Fix test expectation for DispatcherTest.testForwardWithParam
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-ee10/jetty-ee10-servlet/src/test/java/org/eclipse/jetty/ee10/servlet/DispatcherTest.java
+++ jetty-ee10/jetty-ee10-servlet/src/test/java/org/eclipse/jetty/ee10/servlet/DispatcherTest.java
@@ -147,7 +147,6 @@ public class DispatcherTest
     }
 
     @Test
-    @Disabled // TODO
     public void testForwardWithParam() throws Exception
     {
         _contextHandler.addServlet(ForwardServlet.class, "/ForwardServlet/*");
@@ -156,11 +155,11 @@ public class DispatcherTest
         String expected =
             "HTTP/1.1 200 OK\r\n" +
                 "Content-Type: text/plain\r\n" +
-                "Content-Length: 54\r\n" +
+                "Content-Length: 56\r\n" +
                 "\r\n" +
                 "/context\r\n" +
                 "/EchoURI\r\n" +
-                "/x x\r\n" +
+                "/x%20x\r\n" +
                 "/context/EchoURI/x%20x;a=1\r\n";
 
         String responses = _connector.getResponse("GET /context/ForwardServlet;ignore=true?do=req.echo&uri=EchoURI%2Fx%2520x%3Ba=1%3Fb=2 HTTP/1.0\n\n");
