98392d870a12fe5a50decc3414129b61da9e05c1
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2022-05-11 12:06:33 -0500

    Fixing Copyright Header for jetty-9.4.x

--- jetty-http2/http2-server/src/test/java/org/eclipse/jetty/http2/server/BadURITest.java
+++ jetty-http2/http2-server/src/test/java/org/eclipse/jetty/http2/server/BadURITest.java
@@ -1,14 +1,19 @@
 //
-// ========================================================================
-// Copyright (c) 1995-2022 Mort Bay Consulting Pty Ltd and others.
+//  ========================================================================
+//  Copyright (c) 1995-2022 Mort Bay Consulting Pty Ltd and others.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
 //
-// This program and the accompanying materials are made available under the
-// terms of the Eclipse Public License v. 2.0 which is available at
-// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
-// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
 //
-// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
-// ========================================================================
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
 //
 
 package org.eclipse.jetty.http2.server;
