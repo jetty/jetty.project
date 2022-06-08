7448461c1a8b555964d35a0dd5fe02c7ff3136df
Ludovic Orban <lorban@bitronix.be>
2022-06-08 10:09:44 +0200

    fix NPE when Path is /
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-ee9/jetty-ee9-webapp/src/main/java/org/eclipse/jetty/ee9/webapp/WebInfConfiguration.java
+++ jetty-ee9/jetty-ee9-webapp/src/main/java/org/eclipse/jetty/ee9/webapp/WebInfConfiguration.java
@@ -570,7 +570,8 @@ public class WebInfConfiguration extends AbstractConfiguration
 
         if (resourceFile != null)
         {
-            return resourceFile.getFileName().toString();
+            Path fileName = resourceFile.getFileName();
+            return fileName == null ? "" : fileName.toString();
         }
 
         // Use URI itself.
