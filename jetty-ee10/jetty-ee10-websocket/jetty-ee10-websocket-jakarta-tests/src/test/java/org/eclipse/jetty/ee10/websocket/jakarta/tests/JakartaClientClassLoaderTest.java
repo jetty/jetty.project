10ad518da9e15298f31f3cc86e0b520285f7cec4
Lachlan Roberts <lachlan@webtide.com>
2022-06-22 16:59:41 +1000

    fixed websocket JakartaClientClassLoaderTest for ee9 and ee10
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-ee10/jetty-ee10-websocket/jetty-ee10-websocket-jakarta-tests/src/test/java/org/eclipse/jetty/ee10/websocket/jakarta/tests/JakartaClientClassLoaderTest.java
+++ jetty-ee10/jetty-ee10-websocket/jetty-ee10-websocket-jakarta-tests/src/test/java/org/eclipse/jetty/ee10/websocket/jakarta/tests/JakartaClientClassLoaderTest.java
@@ -44,7 +44,6 @@ import org.eclipse.jetty.websocket.core.WebSocketComponents;
 import org.eclipse.jetty.websocket.core.client.CoreClientUpgradeRequest;
 import org.eclipse.jetty.xml.XmlConfiguration;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -158,7 +157,6 @@ public class JakartaClientClassLoaderTest
         return app;
     }
 
-    @Disabled
     @Test
     public void websocketProvidedByServer() throws Exception
     {
@@ -187,7 +185,6 @@ public class JakartaClientClassLoaderTest
         ClassLoader serverClassLoader = server.getServer().getClass().getClassLoader();
         assertThat(response.getContentAsString(), containsString("ClientClassLoader: " + serverClassLoader));    }
 
-    @Disabled
     @Test
     public void websocketProvidedByWebApp() throws Exception
     {
