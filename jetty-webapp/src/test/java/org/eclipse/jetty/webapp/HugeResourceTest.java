3d8d88f3587d1c8de3c74d4fa441d0ecfb1c0a0f
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2022-06-29 18:56:30 -0500

    Issue #7833 - Chunked Transfer-Encoding and HEAD response test (#8219)
    * Issue #7833 - Chunked Transfer-Encoding and HEAD response test
    
    Signed-off-by: Joakim Erdfelt <joakim.erdfelt@gmail.com>

--- jetty-webapp/src/test/java/org/eclipse/jetty/webapp/HugeResourceTest.java
+++ jetty-webapp/src/test/java/org/eclipse/jetty/webapp/HugeResourceTest.java
@@ -23,6 +23,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.URI;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.channels.SeekableByteChannel;
 import java.nio.file.FileStore;
@@ -185,6 +186,7 @@ public class HugeResourceTest
         context.setBaseResource(new PathResource(staticBase));
 
         context.addServlet(PostServlet.class, "/post");
+        context.addServlet(ChunkedServlet.class, "/chunked/*");
 
         String location = multipartTempDir.toString();
         long maxFileSize = Long.MAX_VALUE;
@@ -224,7 +226,7 @@ public class HugeResourceTest
 
     @ParameterizedTest
     @MethodSource("staticFiles")
-    public void testDownload(String filename, long expectedSize) throws Exception
+    public void testDownloadStatic(String filename, long expectedSize) throws Exception
     {
         URI destUri = server.getURI().resolve("/" + filename);
         InputStreamResponseListener responseListener = new InputStreamResponseListener();
@@ -251,7 +253,33 @@ public class HugeResourceTest
 
     @ParameterizedTest
     @MethodSource("staticFiles")
-    public void testHead(String filename, long expectedSize) throws Exception
+    public void testDownloadChunked(String filename, long expectedSize) throws Exception
+    {
+        URI destUri = server.getURI().resolve("/chunked/" + filename);
+        InputStreamResponseListener responseListener = new InputStreamResponseListener();
+
+        Request request = client.newRequest(destUri)
+            .method(HttpMethod.GET);
+        request.send(responseListener);
+        Response response = responseListener.get(5, TimeUnit.SECONDS);
+
+        assertThat("HTTP Response Code", response.getStatus(), is(200));
+        // dumpResponse(response);
+
+        String transferEncoding = response.getHeaders().get(HttpHeader.TRANSFER_ENCODING);
+        assertThat("Http Response Header: \"Transfer-Encoding\"", transferEncoding, is("chunked"));
+
+        try (ByteCountingOutputStream out = new ByteCountingOutputStream();
+             InputStream in = responseListener.getInputStream())
+        {
+            IO.copy(in, out);
+            assertThat("Downloaded Files Size: " + filename, out.getCount(), is(expectedSize));
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("staticFiles")
+    public void testHeadStatic(String filename, long expectedSize) throws Exception
     {
         URI destUri = server.getURI().resolve("/" + filename);
         InputStreamResponseListener responseListener = new InputStreamResponseListener();
@@ -272,6 +300,30 @@ public class HugeResourceTest
         String contentLength = response.getHeaders().get(HttpHeader.CONTENT_LENGTH);
         long contentLengthLong = Long.parseLong(contentLength);
         assertThat("Http Response Header: \"Content-Length: " + contentLength + "\"", contentLengthLong, is(expectedSize));
+    }
+
+    @ParameterizedTest
+    @MethodSource("staticFiles")
+    public void testHeadChunked(String filename, long expectedSize) throws Exception
+    {
+        URI destUri = server.getURI().resolve("/chunked/" + filename);
+        InputStreamResponseListener responseListener = new InputStreamResponseListener();
+
+        Request request = client.newRequest(destUri)
+            .method(HttpMethod.HEAD);
+        request.send(responseListener);
+        Response response = responseListener.get(5, TimeUnit.SECONDS);
+
+        try (InputStream in = responseListener.getInputStream())
+        {
+            assertThat(in.read(), is(-1));
+        }
+
+        assertThat("HTTP Response Code", response.getStatus(), is(200));
+        // dumpResponse(response);
+
+        String transferEncoding = response.getHeaders().get(HttpHeader.TRANSFER_ENCODING);
+        assertThat("Http Response Header: \"Transfer-Encoding\"", transferEncoding, is("chunked"));
     }
 
     @ParameterizedTest
@@ -356,6 +408,22 @@ public class HugeResourceTest
             resp.setContentType("text/plain");
             resp.setCharacterEncoding("utf-8");
             resp.getWriter().printf("bytes-received=%d%n", byteCounting.getCount());
+        }
+    }
+
+    public static class ChunkedServlet extends HttpServlet
+    {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
+        {
+            URL resource = req.getServletContext().getResource(req.getPathInfo());
+            OutputStream output = resp.getOutputStream();
+            try (InputStream input = resource.openStream())
+            {
+                resp.setContentType("application/octet-stream");
+                resp.flushBuffer();
+                IO.copy(input, output);
+            }
         }
     }
 
