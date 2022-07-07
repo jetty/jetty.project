273bb35840ce1282d2de1e419fa111301b171854
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2022-07-07 14:06:07 -0500

    Adding build/scripts/ci.sh

---
+++ build/scripts/ci.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Represents the command line version of the CI build in Jenkinsfile
+
+mvn clean install -f build
+mvn clean install -N
+mvn clean install -f jetty-core
+mvn clean install -f jetty-integrations
+mvn clean install -f jetty-ee10
+mvn clean install -f jetty-ee9
+mvn clean install -f jetty-ee8
+mvn clean install -f jetty-home
+mvn clean install -f tests
+# mvn clean install -f jetty-p2
+# mvn clean install -f documentation
+
