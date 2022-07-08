3ec002e4470c97b1195138b72bf2b29196becb4c
Olivier Lamy <oliver.lamy@gmail.com>
2022-07-08 16:29:24 +1000

    update script ci.sh with -pl :jetty-home

--- build/scripts/ci.sh
+++ build/scripts/ci.sh
@@ -9,7 +9,7 @@ mvn clean install -f jetty-integrations
 mvn clean install -f jetty-ee10
 mvn clean install -f jetty-ee9
 mvn clean install -f jetty-ee8
-mvn clean install -f jetty-home
+mvn clean install -pl :jetty-home
 mvn clean install -f tests
 # mvn clean install -f jetty-p2
 # mvn clean install -f documentation
