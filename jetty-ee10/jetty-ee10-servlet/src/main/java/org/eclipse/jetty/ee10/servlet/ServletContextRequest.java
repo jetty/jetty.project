76d7bf7c7a97969e85ab9151745d9cf538285e7c
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2022-07-18 13:44:36 -0500

    NYI: ServletContextRequest.ServletApiRequest.newPushBuilder

--- jetty-ee10/jetty-ee10-servlet/src/main/java/org/eclipse/jetty/ee10/servlet/ServletContextRequest.java
+++ jetty-ee10/jetty-ee10-servlet/src/main/java/org/eclipse/jetty/ee10/servlet/ServletContextRequest.java
@@ -52,6 +52,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.servlet.http.HttpUpgradeHandler;
 import jakarta.servlet.http.Part;
+import jakarta.servlet.http.PushBuilder;
 import org.eclipse.jetty.ee10.servlet.security.Authentication;
 import org.eclipse.jetty.ee10.servlet.security.UserIdentity;
 import org.eclipse.jetty.http.BadMessageException;
@@ -738,14 +739,21 @@ public class ServletContextRequest extends ContextRequest implements Runnable
         @Override
         public Part getPart(String name) throws IOException, ServletException
         {
-            // TODO
+            // TODO NYI
             return null;
         }
 
         @Override
         public <T extends HttpUpgradeHandler> T upgrade(Class<T> handlerClass) throws IOException, ServletException
         {
-            // TODO
+            // TODO NYI
+            return null;
+        }
+
+        @Override
+        public PushBuilder newPushBuilder()
+        {
+            // TODO NYI
             return null;
         }
 
