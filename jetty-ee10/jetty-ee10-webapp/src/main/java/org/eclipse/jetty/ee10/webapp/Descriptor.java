7b4eacf5869c8201fbb87415d83510bcbc3f9d04
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2022-07-26 07:25:26 -0500

    Adding logging to get a hint at what's wrong on CI

--- jetty-ee10/jetty-ee10-webapp/src/main/java/org/eclipse/jetty/ee10/webapp/Descriptor.java
+++ jetty-ee10/jetty-ee10-webapp/src/main/java/org/eclipse/jetty/ee10/webapp/Descriptor.java
@@ -13,6 +13,7 @@
 
 package org.eclipse.jetty.ee10.webapp;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
@@ -20,9 +21,13 @@ import java.util.Objects;
 
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.xml.XmlParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class Descriptor
 {
+    private static final Logger LOG = LoggerFactory.getLogger(Descriptor.class);
+
     protected Resource _xml;
     protected XmlParser.Node _root;
     protected String _dtd;
@@ -35,7 +40,6 @@ public abstract class Descriptor
     public void parse(XmlParser parser)
         throws Exception
     {
-
         if (_root == null)
         {
             Objects.requireNonNull(parser);
@@ -43,6 +47,11 @@ public abstract class Descriptor
             {
                 _root = parser.parse(is);
                 _dtd = parser.getDTD();
+            }
+            catch (IOException e)
+            {
+                LOG.warn("Unable to parse {}", _xml, e);
+                throw e;
             }
         }
     }
