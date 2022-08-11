be2d4ef0694e144d52454352424e74ebb17a75ae
Simone Bordet <simone.bordet@gmail.com>
2022-08-11 11:09:56 +0200

    Jetty 12 module renaming - jetty-quic.
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-core/jetty-quic/jetty-quic-quiche/jetty-quic-quiche-jna/src/test/java/org/eclipse/jetty/quic/quiche/jna/LowLevelQuicheTest.java
+++ jetty-core/jetty-quic/jetty-quic-quiche/jetty-quic-quiche-jna/src/test/java/org/eclipse/jetty/quic/quiche/jna/LowLevelQuicheTest.java
@@ -24,6 +24,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+
 import org.eclipse.jetty.quic.quiche.QuicheConfig;
 import org.eclipse.jetty.quic.quiche.QuicheConnection;
 import org.eclipse.jetty.quic.quiche.SSLKeyPair;
