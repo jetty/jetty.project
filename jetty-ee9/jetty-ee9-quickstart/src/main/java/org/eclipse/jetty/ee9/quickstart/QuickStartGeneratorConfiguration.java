c2380c909fe4810a09a434982d11b9ccbc5fc1c5
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2022-09-22 15:05:36 -0500

    Fixing imports

--- jetty-ee9/jetty-ee9-quickstart/src/main/java/org/eclipse/jetty/ee9/quickstart/QuickStartGeneratorConfiguration.java
+++ jetty-ee9/jetty-ee9-quickstart/src/main/java/org/eclipse/jetty/ee9/quickstart/QuickStartGeneratorConfiguration.java
@@ -16,7 +16,6 @@ package org.eclipse.jetty.ee9.quickstart;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Collection;
 import java.util.Collections;
