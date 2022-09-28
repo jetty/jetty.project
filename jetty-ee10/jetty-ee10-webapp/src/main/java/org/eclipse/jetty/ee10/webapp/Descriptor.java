cb795ab9ecc58c60cfdaaa614e9849cc5d0b7c7e
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2022-09-28 09:10:15 -0500

    Minor tweak to show full URI of resource in logging (helps if it's a jar:file: based URI)

--- jetty-ee10/jetty-ee10-webapp/src/main/java/org/eclipse/jetty/ee10/webapp/Descriptor.java
+++ jetty-ee10/jetty-ee10-webapp/src/main/java/org/eclipse/jetty/ee10/webapp/Descriptor.java
@@ -53,7 +53,7 @@ public abstract class Descriptor
             }
             catch (SAXException | IOException e)
             {
-                LOG.warn("Unable to parse {}", _xml, e);
+                LOG.warn("Unable to parse {}", _xml.getURI(), e);
                 throw e;
             }
         }
