ab555429d9040ce5a785401ff230b9f141bbca53
Ludovic Orban <lorban@bitronix.be>
2022-10-10 16:31:54 +0200

    start of H3 impl
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-core/jetty-http3/jetty-http3-client-transport/src/main/java/org/eclipse/jetty/http3/client/transport/internal/HttpReceiverOverHTTP3.java
+++ jetty-core/jetty-http3/jetty-http3-client-transport/src/main/java/org/eclipse/jetty/http3/client/transport/internal/HttpReceiverOverHTTP3.java
@@ -14,6 +14,7 @@
 package org.eclipse.jetty.http3.client.transport.internal;
 
 import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.eclipse.jetty.client.HttpExchange;
 import org.eclipse.jetty.client.HttpReceiver;
@@ -28,12 +29,17 @@ import org.eclipse.jetty.http3.internal.HTTP3ErrorCode;
 import org.eclipse.jetty.io.Content;
 import org.eclipse.jetty.util.Callback;
 import org.eclipse.jetty.util.thread.Invocable;
+import org.eclipse.jetty.util.thread.SerializedInvoker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HttpReceiverOverHTTP3 extends HttpReceiver implements Stream.Client.Listener
 {
     private static final Logger LOG = LoggerFactory.getLogger(HttpReceiverOverHTTP3.class);
+
+    private ContentSource contentSource;
+    private Content.Chunk contentGenerated;
+    private final AtomicBoolean firstContent = new AtomicBoolean(true);
 
     protected HttpReceiverOverHTTP3(HttpChannelOverHTTP3 channel)
     {
@@ -43,7 +49,148 @@ public class HttpReceiverOverHTTP3 extends HttpReceiver implements Stream.Client
     @Override
     protected Content.Source newContentSource()
     {
-        return null;
+        contentSource = new ContentSource();
+        return contentSource;
+    }
+
+    private class ContentSource implements Content.Source
+    {
+        private final SerializedInvoker invoker = new SerializedInvoker();
+        private volatile Content.Chunk currentChunk;
+        private volatile Runnable demandCallback;
+
+        @Override
+        public Content.Chunk read()
+        {
+            Content.Chunk chunk = consumeCurrentChunk();
+            if (chunk != null)
+                return chunk;
+            currentChunk = HttpReceiverOverHTTP3.this.read(false);
+            return consumeCurrentChunk();
+        }
+
+        public void onDataAvailable()
+        {
+            if (demandCallback != null)
+                invoker.run(this::invokeDemandCallback);
+        }
+
+        private Content.Chunk consumeCurrentChunk()
+        {
+            if (currentChunk != null)
+            {
+                Content.Chunk rc = currentChunk;
+                if (!(rc instanceof Content.Chunk.Error))
+                    currentChunk = currentChunk.isLast() ? Content.Chunk.EOF : null;
+                return rc;
+            }
+            return null;
+        }
+
+        @Override
+        public void demand(Runnable demandCallback)
+        {
+            if (demandCallback == null)
+                throw new IllegalArgumentException();
+            if (this.demandCallback != null)
+                throw new IllegalStateException();
+            this.demandCallback = demandCallback;
+
+            invoker.run(this::meetDemand);
+        }
+
+        private void meetDemand()
+        {
+            while (true)
+            {
+                if (currentChunk != null)
+                {
+                    invoker.run(this::invokeDemandCallback);
+                    break;
+                }
+                else
+                {
+                    currentChunk = HttpReceiverOverHTTP3.this.read(true);
+                    if (currentChunk == null)
+                        return;
+                }
+            }
+        }
+
+        private void invokeDemandCallback()
+        {
+            Runnable demandCallback = this.demandCallback;
+            this.demandCallback = null;
+            if (demandCallback != null)
+            {
+                try
+                {
+                    demandCallback.run();
+                }
+                catch (Throwable x)
+                {
+                    fail(x);
+                }
+            }
+        }
+
+        @Override
+        public void fail(Throwable failure)
+        {
+            if (currentChunk != null)
+            {
+                currentChunk.release();
+                failAndClose(failure);
+            }
+            currentChunk = Content.Chunk.from(failure);
+        }
+    }
+
+    private Content.Chunk read(boolean fillInterestIfNeeded)
+    {
+        Content.Chunk chunk = consumeContentGenerated();
+        if (chunk != null)
+            return chunk;
+        HttpConnectionOverHTTP3 httpConnection = getHttpChannel().getHttpConnection();
+//        boolean contentGenerated = httpConnection.parseAndFill();
+//        if (!contentGenerated && fillInterestIfNeeded)
+//        {
+//            if (!httpConnection.isFillInterested())
+//                httpConnection.fillInterested();
+//        }
+        return consumeContentGenerated();
+    }
+
+    private Content.Chunk consumeContentGenerated()
+    {
+        Content.Chunk chunk = this.contentGenerated;
+        this.contentGenerated = null;
+        return chunk;
+    }
+
+
+    @Override
+    protected void reset()
+    {
+        firstContent.set(true);
+        super.reset();
+    }
+
+    void content(Content.Chunk chunk)
+    {
+        if (contentGenerated != null)
+            throw new IllegalStateException();
+        contentGenerated = chunk;
+
+        if (firstContent.compareAndSet(true, false))
+        {
+            Runnable r = firstResponseContent(getHttpExchange());
+            r.run();
+        }
+        else
+        {
+            contentSource.onDataAvailable();
+        }
     }
 
     @Override
@@ -180,5 +327,14 @@ public class HttpReceiverOverHTTP3 extends HttpReceiver implements Stream.Client
     public void onFailure(Stream.Client stream, long error, Throwable failure)
     {
         responseFailure(failure);
+    }
+
+    private void failAndClose(Throwable failure)
+    {
+        if (responseFailure(failure))
+        {
+            HttpChannelOverHTTP3 httpChannel = getHttpChannel();
+            httpChannel.getHttpConnection().close(failure);
+        }
     }
 }
