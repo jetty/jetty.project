6016106442e2b367b300d38511c23a88ec15e3f9
Lachlan Roberts <lachlan@webtide.com>
2022-10-19 15:58:53 +1100

    re-enable DeploymentTest#testDifferentWebAppsWithSameClassInSignature for ee10 websocket
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-ee10/jetty-ee10-websocket/jetty-ee10-websocket-jakarta-tests/src/test/java/org/eclipse/jetty/ee10/websocket/jakarta/tests/server/DeploymentTest.java
+++ jetty-ee10/jetty-ee10-websocket/jetty-ee10-websocket-jakarta-tests/src/test/java/org/eclipse/jetty/ee10/websocket/jakarta/tests/server/DeploymentTest.java
@@ -36,7 +36,6 @@ import org.eclipse.jetty.toolchain.test.MavenTestingUtils;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnJre;
 import org.junit.jupiter.api.condition.JRE;
@@ -94,7 +93,6 @@ public class DeploymentTest
      * @throws Exception if there is an error during the test.
      */
     @Test
-    @Disabled
     @DisabledOnJre({JRE.JAVA_14, JRE.JAVA_15})
     public void testDifferentWebAppsWithSameClassInSignature() throws Exception
     {
