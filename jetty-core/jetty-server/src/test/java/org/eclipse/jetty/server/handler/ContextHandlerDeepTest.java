3174cfec89773d56327d745503d9bb71d894cfad
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2022-10-20 11:58:44 -0500

    Adding ContextHandlerDeepTest

---
+++ jetty-core/jetty-server/src/test/java/org/eclipse/jetty/server/handler/ContextHandlerDeepTest.java
@@ -0,0 +1,75 @@
+package org.eclipse.jetty.server.handler;
+
+import org.eclipse.jetty.http.HttpHeader;
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.http.HttpTester;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.LocalConnector;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.BufferUtil;
+import org.eclipse.jetty.util.Callback;
+import org.eclipse.jetty.util.component.LifeCycle;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ContextHandlerDeepTest
+{
+    private Server server;
+    private LocalConnector connector;
+
+    private void startServer(Handler handler) throws Exception
+    {
+        server = new Server();
+        connector = new LocalConnector(server);
+        server.addConnector(connector);
+
+        server.setHandler(handler);
+        server.start();
+    }
+
+    @AfterEach
+    public void stopServer()
+    {
+        LifeCycle.stop(server);
+    }
+
+    @Test
+    public void testNestedThreeDeepContextHandler() throws Exception
+    {
+        ContextHandler contextHandlerA = new ContextHandler();
+        contextHandlerA.setContextPath("/a");
+        ContextHandler contextHandlerB = new ContextHandler();
+        contextHandlerB.setContextPath("/b");
+        ContextHandler contextHandlerC = new ContextHandler();
+        contextHandlerC.setContextPath("/c");
+
+        contextHandlerA.setHandler(contextHandlerB);
+        contextHandlerB.setHandler(contextHandlerC);
+        contextHandlerC.setHandler(new Handler.Processor()
+        {
+            @Override
+            public void process(Request request, Response response, Callback callback)
+            {
+                response.getHeaders().put(HttpHeader.CONTENT_TYPE, "text/plain; charset=utf-8");
+                String msg = "contextPath=" + request.getContext().getContextPath();
+                response.write(true, BufferUtil.toBuffer(msg), callback);
+            }
+        });
+
+        startServer(contextHandlerA);
+
+        String rawRequest = """
+            GET /a/b/c/d HTTP/1.1\r
+            Host: local\r
+            Connection: close\r
+                        
+            """;
+        HttpTester.Response response = HttpTester.parseResponse(connector.getResponse(rawRequest));
+        assertEquals(HttpStatus.OK_200, response.getStatus());
+        assertEquals("contextPath=/a/b/c", response.getContent());
+    }
+}
