aff4c96d7cacf092bc1d0848bfd6d5bd6cccaf6d
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2022-10-27 11:44:37 -0500

    Fixing checkstyle

--- jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/handler/TryPathsHandler.java
+++ jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/handler/TryPathsHandler.java
@@ -89,7 +89,7 @@ public class TryPathsHandler extends Handler.Wrapper
                 String path = interpolated.substring(0, queryIdx);
                 _uri = HttpURI.build(wrapped.getHttpURI())
                     .path(URIUtil.addPaths(Request.getContextPath(wrapped), path))
-                    .query(interpolated.substring(queryIdx+1))
+                    .query(interpolated.substring(queryIdx + 1))
                     .asImmutable();
             }
             else
