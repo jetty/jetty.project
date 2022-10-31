650a2ea09810457c302b0d661623a5fe88c620d7
Greg Wilkins <gregw@webtide.com>
2022-11-01 10:32:53 +1100

    Added SizeLimitHandler (#8774)
    Added SizeLimitHandler to limit request and/or response body size.

---
+++ jetty-server/src/main/java/org/eclipse/jetty/server/handler/SizeLimitHandler.java
@@ -0,0 +1,209 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2022 Mort Bay Consulting Pty Ltd and others.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.server.handler;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+import org.eclipse.jetty.http.BadMessageException;
+import org.eclipse.jetty.http.HttpHeader;
+import org.eclipse.jetty.server.HttpInput;
+import org.eclipse.jetty.server.HttpOutput;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.util.Callback;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
+
+/**
+ * <p>A handler that can limit the size of message bodies in requests and responses.</p>
+ * <p>The optional request and response limits are imposed by checking the {@code Content-Length}
+ * header or observing the actual bytes seen by the handler. Handler order is important, in as
+ * much as if this handler is before a the {@link org.eclipse.jetty.server.handler.gzip.GzipHandler},
+ * then it will limit compressed sized, if it as after the {@link org.eclipse.jetty.server.handler.gzip.GzipHandler}
+ * then the limit is applied to uncompressed bytes.
+ * If a size limit is exceeded then {@link BadMessageException} is thrown with a
+ * {@link org.eclipse.jetty.http.HttpStatus#PAYLOAD_TOO_LARGE_413} status.</p>
+ */
+public class SizeLimitHandler extends HandlerWrapper
+{
+    private static final Logger LOG = Log.getLogger(SizeLimitHandler.class);
+
+    private final long _requestLimit;
+    private final long _responseLimit;
+
+    /**
+     * @param requestLimit The request body size limit in bytes or -1 for no limit
+     * @param responseLimit The response body size limit in bytes or -1 for no limit
+     */
+    public SizeLimitHandler(long requestLimit, long responseLimit)
+    {
+        _requestLimit = requestLimit;
+        _responseLimit = responseLimit;
+    }
+
+    protected void checkRequestLimit(long size)
+    {
+        if (_requestLimit >= 0 && size > _requestLimit)
+            throw new BadMessageException(413, "Request body is too large: " + size + ">" + _requestLimit);
+    }
+
+    protected void checkResponseLimit(long size)
+    {
+        if (_responseLimit >= 0 && size > _responseLimit)
+            throw new BadMessageException(500, "Response body is too large: " + size + ">" + _responseLimit);
+    }
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        if (_requestLimit >= 0 || _responseLimit >= 0)
+        {
+            HttpOutput httpOutput = baseRequest.getResponse().getHttpOutput();
+            HttpOutput.Interceptor interceptor = httpOutput.getInterceptor();
+            LimitInterceptor limit = new LimitInterceptor(interceptor);
+
+            if (_requestLimit >= 0)
+            {
+                long contentLength = baseRequest.getContentLengthLong();
+                checkRequestLimit(contentLength);
+                if (contentLength < 0)
+                    baseRequest.getHttpInput().addInterceptor(limit);
+            }
+
+            if (_responseLimit > 0)
+            {
+                httpOutput.setInterceptor(limit);
+                response = new LimitResponse(response);
+            }
+        }
+
+        super.handle(target, baseRequest, request, response);
+    }
+
+    private class LimitInterceptor implements HttpOutput.Interceptor, HttpInput.Interceptor
+    {
+        private final HttpOutput.Interceptor _nextOutput;
+        long _read;
+        long _written;
+
+        public LimitInterceptor(HttpOutput.Interceptor nextOutput)
+        {
+            _nextOutput = nextOutput;
+        }
+
+        @Override
+        public HttpOutput.Interceptor getNextInterceptor()
+        {
+            return _nextOutput;
+        }
+
+        @Override
+        public boolean isOptimizedForDirectBuffers()
+        {
+            return _nextOutput.isOptimizedForDirectBuffers();
+        }
+
+        @Override
+        public HttpInput.Content readFrom(HttpInput.Content content)
+        {
+            if (content.hasContent())
+            {
+                _read += content.remaining();
+                checkResponseLimit(_read);
+            }
+            return content;
+        }
+
+        @Override
+        public void write(ByteBuffer content, boolean last, Callback callback)
+        {
+            if (content.hasRemaining())
+            {
+                _written += content.remaining();
+                checkResponseLimit(_written);
+            }
+            getNextInterceptor().write(content, last, callback);
+        }
+
+        @Override
+        public void resetBuffer()
+        {
+            _written = 0;
+            getNextInterceptor().resetBuffer();
+        }
+    }
+
+    private class LimitResponse extends HttpServletResponseWrapper
+    {
+        public LimitResponse(HttpServletResponse response)
+        {
+            super(response);
+        }
+
+        @Override
+        public void setContentLength(int len)
+        {
+            checkResponseLimit(len);
+            super.setContentLength(len);
+        }
+
+        @Override
+        public void setContentLengthLong(long len)
+        {
+            checkResponseLimit(len);
+            super.setContentLengthLong(len);
+        }
+
+        @Override
+        public void setHeader(String name, String value)
+        {
+            if (HttpHeader.CONTENT_LENGTH.is(name))
+                checkResponseLimit(Long.parseLong(value));
+            super.setHeader(name, value);
+        }
+
+        @Override
+        public void addHeader(String name, String value)
+        {
+            if (HttpHeader.CONTENT_LENGTH.is(name))
+                checkResponseLimit(Long.parseLong(value));
+            super.addHeader(name, value);
+        }
+
+        @Override
+        public void setIntHeader(String name, int value)
+        {
+            if (HttpHeader.CONTENT_LENGTH.is(name))
+                checkResponseLimit(value);
+            super.setIntHeader(name, value);
+        }
+
+        @Override
+        public void addIntHeader(String name, int value)
+        {
+            if (HttpHeader.CONTENT_LENGTH.is(name))
+                checkResponseLimit(value);
+            super.addIntHeader(name, value);
+        }
+    }
+}
---
+++ jetty-server/src/test/java/org/eclipse/jetty/server/handler/SizeLimitHandlerTest.java
@@ -0,0 +1,260 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2022 Mort Bay Consulting Pty Ltd and others.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.server.handler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.http.BadMessageException;
+import org.eclipse.jetty.http.HttpTester;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.LocalConnector;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.IO;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class SizeLimitHandlerTest
+{
+    private Server _server;
+    private LocalConnector _local;
+    private ContextHandler _contextHandler;
+
+    @BeforeEach
+    public void setUp() throws Exception
+    {
+        _server = new Server();
+        HttpConfiguration config = new HttpConfiguration();
+        config.setOutputBufferSize(1024);
+        _local = new LocalConnector(_server, new HttpConnectionFactory(config));
+        _server.setConnectors(new Connector[]{_local});
+        SizeLimitHandler sizeLimitHandler = new SizeLimitHandler(8192, 8192);
+        _contextHandler = new ContextHandler("/ctx");
+        _server.setHandler(sizeLimitHandler);
+        sizeLimitHandler.setHandler(_contextHandler);
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception
+    {
+        _server.stop();
+    }
+
+    @Test
+    public void testSmallGET() throws Exception
+    {
+        _contextHandler.setHandler(new AbstractHandler()
+        {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException
+            {
+                baseRequest.setHandled(true);
+                response.getOutputStream().println("Hello World");
+            }
+        });
+        _server.start();
+        HttpTester.Response response = HttpTester.parseResponse(
+            _local.getResponse("GET /ctx/hello HTTP/1.0\r\n\r\n"));
+        assertThat(response.getStatus(), equalTo(200));
+        assertThat(response.getContent(), containsString("Hello World"));
+    }
+
+    @Test
+    public void testLargeGETContentLengthKnown() throws Exception
+    {
+        _contextHandler.setHandler(new AbstractHandler()
+        {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException
+            {
+                baseRequest.setHandled(true);
+                response.setContentLength(8 * 1024 + 1);
+                fail();
+            }
+        });
+        _server.start();
+        HttpTester.Response response = HttpTester.parseResponse(
+            _local.getResponse("GET /ctx/hello HTTP/1.0\r\n\r\n"));
+        assertThat(response.getStatus(), equalTo(500));
+        assertThat(response.getContent(), containsString("8193&gt;8192"));
+    }
+
+    @Test
+    public void testLargeGETSingleByteBuffer() throws Exception
+    {
+        _contextHandler.setHandler(new AbstractHandler()
+        {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException
+            {
+                baseRequest.setHandled(true);
+                baseRequest.getResponse().getHttpOutput().sendContent(ByteBuffer.wrap(new byte[8 * 1024 + 1]));
+            }
+        });
+        _server.start();
+        HttpTester.Response response = HttpTester.parseResponse(
+            _local.getResponse("GET /ctx/hello HTTP/1.0\r\n\r\n"));
+        assertThat(response.getStatus(), equalTo(500));
+        assertThat(response.getContent(), containsString("8193&gt;8192"));
+    }
+
+    @Test
+    public void testLargeGETManyWrites() throws Exception
+    {
+        AtomicBoolean error = new AtomicBoolean();
+        _contextHandler.setHandler(new AbstractHandler()
+        {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException
+            {
+                baseRequest.setHandled(true);
+                byte[] data = new byte[1024];
+                Arrays.fill(data, (byte)'X');
+                data[1023] = (byte)'\n';
+                String text = new String(data, 0, 1024, Charset.defaultCharset());
+                PrintWriter out = response.getWriter();
+
+                try
+                {
+                    for (int i = 0; i <= 10; i++)
+                    {
+                        out.println(i);
+                        out.write(text);
+                        out.flush();
+                    }
+                }
+                catch (BadMessageException e)
+                {
+                    error.set(true);
+                    throw e;
+                }
+            }
+        });
+        _server.start();
+        HttpTester.Response response = HttpTester.parseResponse(
+            _local.getResponse("GET /ctx/hello HTTP/1.0\r\n\r\n"));
+        assertThat(response.getStatus(), equalTo(200));
+        assertTrue(error.get());
+
+        assertThat(response.getContent(), not(containsString("8")));
+        assertThat(response.getContent(), not(containsString("9")));
+        assertThat(response.getContent(), not(containsString("10")));
+        assertFalse(response.isComplete());
+    }
+
+    @Test
+    public void testSmallPOST() throws Exception
+    {
+        _contextHandler.setHandler(new AbstractHandler()
+        {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException
+            {
+                baseRequest.setHandled(true);
+                String content = IO.toString(request.getInputStream());
+                response.getOutputStream().println("OK " + content.length());
+            }
+        });
+        _server.start();
+        HttpTester.Response response = HttpTester.parseResponse(
+            _local.getResponse("POST /ctx/hello HTTP/1.0\r\n" +
+                "Content-Length: 8\r\n" +
+                "\r\n" +
+                "123456\r\n"));
+        assertThat(response.getStatus(), equalTo(200));
+        assertThat(response.getContent(), containsString("OK 8"));
+    }
+
+    @Test
+    public void testLargePOSTKnownSize() throws Exception
+    {
+        _contextHandler.setHandler(new AbstractHandler()
+        {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException
+            {
+                baseRequest.setHandled(true);
+                String content = IO.toString(request.getInputStream());
+                response.getOutputStream().println("OK " + content.length());
+            }
+        });
+        _server.start();
+        HttpTester.Response response = HttpTester.parseResponse(
+            _local.getResponse("POST /ctx/hello HTTP/1.0\r\n" +
+                "Content-Length: 32768\r\n" +
+                "\r\n" +
+                "123456..."));
+        assertThat(response.getStatus(), equalTo(413));
+        assertThat(response.getContent(), containsString("32768&gt;8192"));
+    }
+
+    @Test
+    public void testLargePOSTUnknownSize() throws Exception
+    {
+        _contextHandler.setHandler(new AbstractHandler()
+        {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException
+            {
+                baseRequest.setHandled(true);
+                String content = IO.toString(request.getInputStream());
+                response.getOutputStream().println("OK " + content.length());
+            }
+        });
+        _server.start();
+
+        try (LocalConnector.LocalEndPoint endp = _local.executeRequest(
+            "POST /ctx/hello HTTP/1.1\r\n" +
+                "Host: localhost\r\n" +
+                "Transfer-Encoding: chunked\r\n" +
+                "\r\n"))
+        {
+            byte[] data = new byte[1024];
+            Arrays.fill(data, (byte)'X');
+            data[1023] = (byte)'\n';
+            String text = new String(data, 0, 1024, Charset.defaultCharset());
+
+            for (int i = 0; i < 9; i++)
+                endp.addInput("400\r\n" + text + "\r\n");
+
+            HttpTester.Response response = HttpTester.parseResponse(endp.getResponse());
+
+            assertThat(response.getStatus(), equalTo(500));
+            assertThat(response.getContent(), containsString("&gt;8192"));
+        }
+    }
+}
