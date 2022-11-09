c280e31b46bb125661b3c2a3a5f2aa7585cbc6f0
Simone Bordet <simone.bordet@gmail.com>
2022-11-09 12:16:42 +0100

    Fixes #7117 - Timeout with Expect 100 continue when using ProxyServlet. (#8873)
    * Fixes #7117 - Timeout with Expect 100 continue when using ProxyServlet.
    
    Now getReader() tests whether it has to send a 100 continue in case getInputStream() 
    is not called because the reader is reused.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-server/src/main/java/org/eclipse/jetty/server/Request.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/Request.java
@@ -1161,16 +1161,22 @@ public class Request implements HttpServletRequest
 
         if (_reader == null || !encoding.equalsIgnoreCase(_readerEncoding))
         {
-            final ServletInputStream in = getInputStream();
+            ServletInputStream in = getInputStream();
             _readerEncoding = encoding;
             _reader = new BufferedReader(new InputStreamReader(in, encoding))
             {
                 @Override
                 public void close() throws IOException
                 {
+                    // Do not call super to avoid marking this reader as closed,
+                    // but do close the ServletInputStream that can be reopened.
                     in.close();
                 }
             };
+        }
+        else if (_channel.isExpecting100Continue())
+        {
+            _channel.continue100(_input.available());
         }
         _inputState = INPUT_READER;
         return _reader;
