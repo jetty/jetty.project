2f37dd30cc66a3dd53ba6a52e417eb3577b2a6cc
Eddú Meléndez <eddu.melendez@gmail.com>
2022-12-22 16:57:59 -0600

    Improve DatastoreEmulatorContainer usage
    Update to testcontainers 1.17.6 and take advantage of
    `DatastoreEmulatorContainer#withFlags()`

--- tests/test-sessions/test-gcloud-sessions/src/test/java/org/eclipse/jetty/gcloud/session/GCloudSessionTestSupport.java
+++ tests/test-sessions/test-gcloud-sessions/src/test/java/org/eclipse/jetty/gcloud/session/GCloudSessionTestSupport.java
@@ -65,28 +65,10 @@ public class GCloudSessionTestSupport
     private static final Logger LOGGER = LoggerFactory.getLogger(GCloudSessionTestSupport.class);
     private static final Logger GCLOUD_LOG = LoggerFactory.getLogger("org.eclipse.jetty.gcloud.session.gcloudLogs");
 
-    public DatastoreEmulatorContainer emulator = new CustomDatastoreEmulatorContainer(
+    public DatastoreEmulatorContainer emulator = new DatastoreEmulatorContainer(
         DockerImageName.parse("gcr.io/google.com/cloudsdktool/cloud-sdk:316.0.0-emulators")
-    ).withLogConsumer(new Slf4jLogConsumer(GCLOUD_LOG));
-
-    private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("gcr.io/google.com/cloudsdktool/cloud-sdk");
-
-    private static final String CMD = "gcloud beta emulators datastore start --project test-project --host-port 0.0.0.0:8081 --consistency=1.0";
-    private static final int HTTP_PORT = 8081;
-
-    public static class CustomDatastoreEmulatorContainer extends DatastoreEmulatorContainer
-    {
-        public CustomDatastoreEmulatorContainer(DockerImageName dockerImageName)
-        {
-            super(dockerImageName);
-
-            dockerImageName.assertCompatibleWith(DEFAULT_IMAGE_NAME);
-
-            withExposedPorts(HTTP_PORT);
-            setWaitStrategy(Wait.forHttp("/").forStatusCode(200));
-            withCommand("/bin/sh", "-c", CMD);
-        }
-    }
+    ).withLogConsumer(new Slf4jLogConsumer(GCLOUD_LOG))
+            .withFlags("--consistency=1.0");
 
     public static class TestGCloudSessionDataStoreFactory extends GCloudSessionDataStoreFactory
     {
