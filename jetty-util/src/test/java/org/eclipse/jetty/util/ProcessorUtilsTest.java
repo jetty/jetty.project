2c4d3dc605aac8c659f8bc825887a72e5914642a
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2023-01-18 16:54:35 -0600

    Script based update of creation years

--- jetty-util/src/test/java/org/eclipse/jetty/util/ProcessorUtilsTest.java
+++ jetty-util/src/test/java/org/eclipse/jetty/util/ProcessorUtilsTest.java
@@ -1,6 +1,6 @@
 //
 // ========================================================================
-// Copyright (c) 2018 Mort Bay Consulting Pty Ltd and others.
+// Copyright (c) 2012 Mort Bay Consulting Pty Ltd and others.
 //
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Public License v. 2.0 which is available at
--- jetty-xml/src/main/java/org/eclipse/jetty/xml/ConfigurationProcessorFactory.java
+++ jetty-xml/src/main/java/org/eclipse/jetty/xml/ConfigurationProcessorFactory.java
@@ -1,6 +1,6 @@
 //
 // ========================================================================
-// Copyright (c) 2014 Mort Bay Consulting Pty Ltd and others.
+// Copyright (c) 2009 Mort Bay Consulting Pty Ltd and others.
 //
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Public License v. 2.0 which is available at
