be61306bf4ac25d998cc8f21291db4aca75bf880
Lachlan <lachlan@webtide.com>
2023-02-14 09:34:26 +1100

    #9345 changes from review
    Co-authored-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-http/src/main/java/org/eclipse/jetty/http/MultiPartFormInputStream.java
+++ jetty-http/src/main/java/org/eclipse/jetty/http/MultiPartFormInputStream.java
@@ -769,7 +769,7 @@ public class MultiPartFormInputStream
             reset();
             _numParts++;
             if (_maxParts >= 0 && _numParts > _maxParts)
-                throw new IllegalStateException(String.format("Form with too many keys [%d > %d]", _numParts, _maxParts));
+                throw new IllegalStateException(String.format("Form with too many parts [%d > %d]", _numParts, _maxParts));
         }
 
         @Override
