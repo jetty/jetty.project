a4adc6206b977f83cf71e9bc45c8c1a7683cea7c
gregw <gregw@webtide.com>
2023-04-12 18:07:45 +0200

    rename Server env to JVM

--- jetty-core/jetty-start/src/test/java/org/eclipse/jetty/start/usecases/AbstractUseCase.java
+++ jetty-core/jetty-start/src/test/java/org/eclipse/jetty/start/usecases/AbstractUseCase.java
@@ -157,7 +157,7 @@ public abstract class AbstractUseCase
 
         public List<String> getXmls()
         {
-            return startArgs.getServerEnvironment().getXmlFiles().stream()
+            return startArgs.getJvmEnvironment().getXmlFiles().stream()
                 .map(p -> baseHome.toShortForm(p))
                 .collect(Collectors.toList());
         }
@@ -165,7 +165,7 @@ public abstract class AbstractUseCase
         public List<String> getLibs()
         {
             return StreamSupport.stream(
-                Spliterators.spliteratorUnknownSize(startArgs.getServerEnvironment().getClasspath().iterator(), Spliterator.ORDERED), false)
+                Spliterators.spliteratorUnknownSize(startArgs.getJvmEnvironment().getClasspath().iterator(), Spliterator.ORDERED), false)
                 .map(f -> baseHome.toShortForm(f))
                 .collect(Collectors.toList());
         }
@@ -182,7 +182,7 @@ public abstract class AbstractUseCase
 
         public List<String> getProperties()
         {
-            Props props = startArgs.getServerEnvironment().getProperties();
+            Props props = startArgs.getJvmEnvironment().getProperties();
 
             Predicate<Props.Prop> propPredicate = (p) ->
             {
