10a4d158cbf5b1260aee4ea237d3857676220d24
Olivier Lamy <olamy@apache.org>
2023-04-25 16:59:43 +1000

    fix retrieving demos when cache is used as they could be in local repo only
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- jetty-ee9/jetty-ee9-demos/jetty-ee9-demo-embedded/src/main/java/org/eclipse/jetty/ee9/demos/JettyDemos.java
+++ jetty-ee9/jetty-ee9-demos/jetty-ee9-demo-embedded/src/main/java/org/eclipse/jetty/ee9/demos/JettyDemos.java
@@ -34,7 +34,7 @@ public class JettyDemos
 {
     private static final Logger LOG = LoggerFactory.getLogger(JettyDemos.class);
     private static final Path JETTY_DEMOS_DIR;
-    private static final String VERSION;
+    private static String VERSION = System.getProperty("jettyVersion", "unknown");
 
     static
     {
@@ -71,29 +71,27 @@ public class JettyDemos
                 LOG.warn("Unable to resolve Jetty Demos location", th);
             }
         }
-
         JETTY_DEMOS_DIR = demosDir;
-
         String version = "unknown";
-        Path pomFile = demosDir.resolve("pom.xml");
-        try (Stream<String> lineStream = Files.lines(pomFile))
+        if (version.equals(VERSION))
         {
-            String versionLine = lineStream
-                .filter((line) -> line.contains("<version>"))
-                .findFirst()
-                .orElseThrow(() ->
-                {
-                    throw new RuntimeException("Unable to find <version> in " + pomFile);
-                });
+            Path pomFile = demosDir.resolve("pom.xml");
+            try (Stream<String> lineStream = Files.lines(pomFile))
+            {
+                String versionLine = lineStream
+                        .filter((line) -> line.contains("<version>"))
+                        .findFirst()
+                        .orElseThrow(() -> new RuntimeException("Unable to find <version> in " + pomFile));
 
-            version = versionLine.replaceAll("<[^>]*>", "").trim();
-        }
-        catch (IOException e)
-        {
-            LOG.warn("Unable to find <version> in " + pomFile, e);
-        }
+                version = versionLine.replaceAll("<[^>]*>", "").trim();
+            }
+            catch (IOException e)
+            {
+                LOG.warn("Unable to find <version> in " + pomFile, e);
+            }
 
-        VERSION = version;
+            VERSION = version;
+        }
     }
 
     private static Path asDirectory(String path)
@@ -141,6 +139,32 @@ public class JettyDemos
         return JETTY_DEMOS_DIR;
     }
 
+    public static Path find(String path, MavenCoordinate mavenCoordinate) throws FileNotFoundException
+    {
+        try
+        {
+            return find(path);
+        }
+        catch (FileNotFoundException e)
+        {
+            //could be not in the target directory if restored from build cache
+            // but as the build always runs install the artifact should be there in local repository
+        }
+        //.repository/org/eclipse/jetty/ee9/demos/jetty-ee9-demo-jndi-webapp/12.0.0-SNAPSHOT/jetty-ee9-demo-jndi-webapp-12.0.0-SNAPSHOT.war
+        String version = mavenCoordinate.version().isEmpty() ? VERSION : mavenCoordinate.version();
+        Path result = Paths.get(System.getProperty("mavenRepoPath", System.getProperty("user.home") + "/.m2/repository"),
+                mavenCoordinate.groupId().replaceAll("\\.", "/"),
+                mavenCoordinate.artifactId(),
+                version,
+                mavenCoordinate.artifactId() + "-" + version + "." + mavenCoordinate.packaging()
+                );
+        if (!Files.exists(result))
+        {
+            throw new FileNotFoundException(result.toString());
+        }
+        return result;
+    }
+
     public static Path find(String path) throws FileNotFoundException
     {
         String expandedPath = path.replaceAll("@VER@", VERSION);
@@ -151,6 +175,8 @@ public class JettyDemos
         }
         return result;
     }
+
+    public record MavenCoordinate(String groupId, String artifactId, String version, String packaging){}
 
     public static void main(String... arg)
     {
