ab4c3dfb08d59a50bc6aaeeab8c2fd6a1e2bd49f
Lachlan Roberts <lachlan@webtide.com>
2023-04-26 12:11:55 +1000

    fix checkstyle errors
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-ee10/jetty-ee10-servlet/src/test/java/org/eclipse/jetty/ee10/servlet/ServletRequestListenerTest.java
+++ jetty-ee10/jetty-ee10-servlet/src/test/java/org/eclipse/jetty/ee10/servlet/ServletRequestListenerTest.java
@@ -1,3 +1,16 @@
+//
+// ========================================================================
+// Copyright (c) 1995 Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
+
 package org.eclipse.jetty.ee10.servlet;
 
 import java.io.IOException;
@@ -152,7 +165,6 @@ public class ServletRequestListenerTest
         assertEvents("requestInitialized /", "doFilter /", "REQUEST /", "requestDestroyed /",
             "requestInitialized /", "doFilter /", "ASYNC /", "requestDestroyed /");
     }
-
 
     @Test
     public void testError() throws Exception
