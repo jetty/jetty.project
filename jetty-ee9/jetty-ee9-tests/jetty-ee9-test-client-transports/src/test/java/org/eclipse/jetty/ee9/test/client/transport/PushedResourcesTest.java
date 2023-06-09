2080c24d06dd5ec2558d5a35fd16b4b766f1c645
Jan Bartel <janb@webtide.com>
2023-06-09 14:47:52 +0200

    Add more test cases more like the tck test

--- jetty-ee9/jetty-ee9-tests/jetty-ee9-test-client-transports/src/test/java/org/eclipse/jetty/ee9/test/client/transport/PushedResourcesTest.java
+++ jetty-ee9/jetty-ee9-tests/jetty-ee9-test-client-transports/src/test/java/org/eclipse/jetty/ee9/test/client/transport/PushedResourcesTest.java
@@ -14,6 +14,8 @@
 package org.eclipse.jetty.ee9.test.client.transport;
 
 import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Path;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -25,12 +27,16 @@ import jakarta.servlet.http.PushBuilder;
 import org.eclipse.jetty.client.BufferingResponseListener;
 import org.eclipse.jetty.client.ContentResponse;
 import org.eclipse.jetty.client.Result;
+import org.eclipse.jetty.ee9.servlet.DefaultServlet;
 import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.server.NetworkConnector;
+import org.eclipse.jetty.toolchain.test.MavenTestingUtils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PushedResourcesTest extends AbstractTest
@@ -65,10 +71,81 @@ public class PushedResourcesTest extends AbstractTest
                 }
                 else
                 {
+                    request.newPushBuilder()
+                        .path(path1)
+                        .push();
+                    request.newPushBuilder()
+                        .path(path2)
+                        .push();
+                    response.getOutputStream().write(bytes);
+                }
+            }
+        });
+
+        CountDownLatch latch1 = new CountDownLatch(1);
+        CountDownLatch latch2 = new CountDownLatch(1);
+        ContentResponse response = client.newRequest(newURI(transport))
+            .onPush((mainRequest, pushedRequest) -> new BufferingResponseListener()
+            {
+                @Override
+                public void onComplete(Result result)
+                {
+                    assertTrue(result.isSucceeded());
+                    if (pushedRequest.getPath().equals(path1))
+                    {
+                        assertArrayEquals(pushBytes1, getContent());
+                        latch1.countDown();
+                    }
+                    else if (pushedRequest.getPath().equals(path2))
+                    {
+                        assertArrayEquals(pushBytes2, getContent());
+                        latch2.countDown();
+                    }
+                }
+            })
+            .timeout(5, TimeUnit.SECONDS)
+            .send();
+
+        assertEquals(HttpStatus.OK_200, response.getStatus());
+        assertArrayEquals(bytes, response.getContent());
+        assertTrue(latch1.await(5, TimeUnit.SECONDS));
+        assertTrue(latch2.await(5, TimeUnit.SECONDS));
+    }
+
+    @ParameterizedTest
+    @MethodSource("transportsWithPushSupport")
+    public void testPushedResourcesSomewhatLikeTCK(Transport transport) throws Exception
+    {
+        Random random = new Random();
+        byte[] bytes = new byte[512];
+        random.nextBytes(bytes);
+        byte[] pushBytes1 = new byte[1024];
+        random.nextBytes(pushBytes1);
+        byte[] pushBytes2 = new byte[2048];
+        random.nextBytes(pushBytes2);
+
+        String path1 = "/secondary1";
+        String path2 = "/secondary2";
+        start(transport, new HttpServlet()
+        {
+            @Override
+            protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException
+            {
+                String target = request.getRequestURI();
+                if (target.equals(path1))
+                {
+                    response.getOutputStream().write(pushBytes1);
+                }
+                else if (target.equals(path2))
+                {
+                    response.getOutputStream().write(pushBytes2);
+                }
+                else
+                {
                     try
                     {
-                            PushBuilder pb = request.newPushBuilder();
-                            pb.push();
+                        PushBuilder pb = request.newPushBuilder();
+                        pb.push();
                     }
                     catch (Exception e)
                     {
@@ -101,10 +178,12 @@ public class PushedResourcesTest extends AbstractTest
                         System.err.println("Expected error null method");
                     }
 
-                    String[] METHODS = { "", "POST", "PUT", "DELETE",
-      "CONNECT", "OPTIONS", "TRACE" };
+                    String[] methods = {
+                        "", "POST", "PUT", "DELETE",
+                        "CONNECT", "OPTIONS", "TRACE"
+                    };
 
-                    for (String m : METHODS)
+                    for (String m : methods)
                     {
                         try
                         {
@@ -153,6 +232,109 @@ public class PushedResourcesTest extends AbstractTest
 
     @ParameterizedTest
     @MethodSource("transportsWithPushSupport")
+    public void testPushedResourcesLikeTCK(Transport transport) throws Exception
+    {
+        String path1 = "/secondary1.html";
+
+        prepareServer(transport, new DefaultServlet());
+        Path staticDir = MavenTestingUtils.getTestResourcePath("serverpushtck");
+        assertNotNull(staticDir);
+        servletContextHandler.setBaseResourceAsPath(staticDir);
+        addServlet(
+            new HttpServlet()
+            {
+                @Override
+                protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException
+                {
+                    try
+                    {
+                        PushBuilder pb = request.newPushBuilder();
+                        pb.push();
+                    }
+                    catch (Exception e)
+                    {
+                        System.err.println("Expected error empty push builder");
+                    }
+
+                    PushBuilder pb1 = request.newPushBuilder();
+                    pb1.path(path1);
+                    pb1.push();
+
+                    try
+                    {
+                        pb1.push();
+                    }
+                    catch (Exception e)
+                    {
+                        System.err.println("Expected error no path reset");
+                    }
+
+                    PushBuilder pb3 = request.newPushBuilder();
+                    try
+                    {
+                        pb3.method(null);
+                    }
+                    catch (Exception e)
+                    {
+                        System.err.println("Expected error null method");
+                    }
+
+                    String[] methods = {
+                        "", "POST", "PUT", "DELETE",
+                        "CONNECT", "OPTIONS", "TRACE"
+                    };
+
+                    for (String m : methods)
+                    {
+                        try
+                        {
+                            pb3.method(m);
+                            System.err.println("Fail " + m);
+                        }
+                        catch (Exception e)
+                        {
+                            System.err.println("Pass " + m);
+                        }
+                    }
+                    response.getWriter().println("TEST FINISHED");
+                }
+            },
+            "/serverpushtck/*");
+
+        server.start();
+        startClient(transport);
+        CountDownLatch latch1 = new CountDownLatch(1);
+
+        String scheme = transport.isSecure() ? "https" : "http";
+        String uri = scheme + "://localhost";
+        if (connector instanceof NetworkConnector networkConnector)
+            uri += ":" + networkConnector.getLocalPort();
+        URI theURI = URI.create(uri + "/serverpushtck/foo");
+
+        ContentResponse response = client.newRequest(theURI)
+            .onPush((mainRequest, pushedRequest) -> new BufferingResponseListener()
+            {
+                @Override
+                public void onComplete(Result result)
+                {
+                    assertTrue(result.isSucceeded());
+                    if (pushedRequest.getPath().equals(path1))
+                    {
+                        assertTrue(getContentAsString().contains("SECONDARY 1"));
+                        latch1.countDown();
+                    }
+                }
+            })
+            .timeout(5, TimeUnit.SECONDS)
+            .send();
+
+        assertEquals(HttpStatus.OK_200, response.getStatus());
+        assertTrue(response.getContentAsString().contains("TEST FINISHED"));
+        assertTrue(latch1.await(5, TimeUnit.SECONDS));
+    }
+
+    @ParameterizedTest
+    @MethodSource("transportsWithPushSupport")
     public void testPushedResourceRedirect(Transport transport) throws Exception
     {
         Random random = new Random();
@@ -178,7 +360,7 @@ public class PushedResourcesTest extends AbstractTest
         });
 
         CountDownLatch latch = new CountDownLatch(1);
-        ;
+
         ContentResponse response = client.newRequest(newURI(transport))
             .onPush((mainRequest, pushedRequest) -> new BufferingResponseListener()
             {
