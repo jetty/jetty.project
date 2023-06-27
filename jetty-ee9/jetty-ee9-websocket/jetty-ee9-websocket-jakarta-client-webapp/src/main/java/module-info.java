e2cc47b0f8df708fb77fd0b7dd4813906fd9f4ae
Lachlan Roberts <lachlan@webtide.com>
2023-06-27 16:17:44 +1000

    split a webapp module for jetty-ee9-websocket-jakarta-client
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-ee9/jetty-ee9-websocket/jetty-ee9-websocket-jakarta-client-webapp/src/main/java/module-info.java
+++ jetty-ee9/jetty-ee9-websocket/jetty-ee9-websocket-jakarta-client-webapp/src/main/java/module-info.java
@@ -11,14 +11,17 @@
 // ========================================================================
 //
 
-module org.eclipse.jetty.ee9.websocket.jakarta.client
+import org.eclipse.jetty.ee9.websocket.jakarta.client.webapp.JakartaWebSocketShutdownContainer;
+
+module org.eclipse.jetty.ee9.websocket.jakarta.client.webapp
 {
     requires org.slf4j;
 
-    requires transitive org.eclipse.jetty.ee9.websocket.jakarta.common;
+    requires transitive org.eclipse.jetty.ee9.websocket.jakarta.client;
+    requires transitive jetty.servlet.api;
 
-    exports org.eclipse.jetty.ee9.websocket.jakarta.client;
+    exports org.eclipse.jetty.ee9.websocket.jakarta.client.webapp;
 
-    provides jakarta.websocket.ContainerProvider with
-        org.eclipse.jetty.ee9.websocket.jakarta.client.JakartaWebSocketClientContainerProvider;
+    provides jakarta.servlet.ServletContainerInitializer with
+        JakartaWebSocketShutdownContainer;
 }
--- jetty-ee9/jetty-ee9-websocket/jetty-ee9-websocket-jakarta-client-webapp/src/main/java/org/eclipse/jetty/ee9/websocket/jakarta/client/webapp/JakartaWebSocketShutdownContainer.java
+++ jetty-ee9/jetty-ee9-websocket/jetty-ee9-websocket-jakarta-client-webapp/src/main/java/org/eclipse/jetty/ee9/websocket/jakarta/client/webapp/JakartaWebSocketShutdownContainer.java
@@ -11,7 +11,7 @@
 // ========================================================================
 //
 
-package org.eclipse.jetty.ee9.websocket.jakarta.client;
+package org.eclipse.jetty.ee9.websocket.jakarta.client.webapp;
 
 import java.util.Set;
 
@@ -20,6 +20,7 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.ServletException;
+import org.eclipse.jetty.ee9.websocket.jakarta.client.JakartaWebSocketClientContainer;
 import org.eclipse.jetty.util.component.ContainerLifeCycle;
 import org.eclipse.jetty.util.component.LifeCycle;
 import org.slf4j.Logger;
