627f9f7321a87d784920c4351e717c521542f589
Lachlan Roberts <lachlan@webtide.com>
2023-07-31 12:15:41 +1000

    fix additional javax references from review
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-ee9/jetty-ee9-annotations/src/test/java/org/eclipse/jetty/ee9/annotations/TestAnnotationIntrospector.java
+++ jetty-ee9/jetty-ee9-annotations/src/test/java/org/eclipse/jetty/ee9/annotations/TestAnnotationIntrospector.java
@@ -51,7 +51,7 @@ public class TestAnnotationIntrospector
             holder.setHeldClass(ServletE.class);
             assertTrue(introspector.isIntrospectable(new ServletE(), holder));
 
-            //a JAVAX API sourced servlet can be introspected
+            //a JAKARTA API sourced servlet can be introspected
             holder = new ServletHolder(Source.JAKARTA_API);
             holder.setHeldClass(ServletE.class);
             assertTrue(introspector.isIntrospectable(new ServletE(), holder));
