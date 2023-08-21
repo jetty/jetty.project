e91a68923a72faac04cdfe9884cfb9a5be34cda8
Simone Bordet <simone.bordet@gmail.com>
2023-08-21 08:58:22 +0200

    Fixes #8926 - HttpClient GZIPContentDecoder should remove Content-Len… (#10326)
    Now Content-Length and Content-Encoding are removed/modified by the decoder.
    In this way, applications have a correct sets of headers to decide whether to decode the content themselves.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-client/src/main/java/org/eclipse/jetty/client/GZIPContentDecoder.java
+++ jetty-client/src/main/java/org/eclipse/jetty/client/GZIPContentDecoder.java
@@ -14,7 +14,10 @@
 package org.eclipse.jetty.client;
 
 import java.nio.ByteBuffer;
+import java.util.ListIterator;
 
+import org.eclipse.jetty.http.HttpField;
+import org.eclipse.jetty.http.HttpHeader;
 import org.eclipse.jetty.io.ByteBufferPool;
 
 /**
@@ -23,6 +26,8 @@ import org.eclipse.jetty.io.ByteBufferPool;
 public class GZIPContentDecoder extends org.eclipse.jetty.http.GZIPContentDecoder implements ContentDecoder
 {
     public static final int DEFAULT_BUFFER_SIZE = 8192;
+
+    private long decodedLength;
 
     public GZIPContentDecoder()
     {
@@ -40,10 +45,51 @@ public class GZIPContentDecoder extends org.eclipse.jetty.http.GZIPContentDecode
     }
 
     @Override
+    public void beforeDecoding(HttpExchange exchange)
+    {
+        exchange.getResponse().headers(headers ->
+        {
+            ListIterator<HttpField> iterator = headers.listIterator();
+            while (iterator.hasNext())
+            {
+                HttpField field = iterator.next();
+                HttpHeader header = field.getHeader();
+                if (header == HttpHeader.CONTENT_LENGTH)
+                {
+                    // Content-Length is not valid anymore while we are decoding.
+                    iterator.remove();
+                }
+                else if (header == HttpHeader.CONTENT_ENCODING)
+                {
+                    // Content-Encoding should be removed/modified as the content will be decoded.
+                    String value = field.getValue();
+                    int comma = value.lastIndexOf(",");
+                    if (comma < 0)
+                        iterator.remove();
+                    else
+                        iterator.set(new HttpField(HttpHeader.CONTENT_ENCODING, value.substring(0, comma)));
+                    break;
+                }
+            }
+        });
+    }
+
+    @Override
     protected boolean decodedChunk(ByteBuffer chunk)
     {
+        decodedLength += chunk.remaining();
         super.decodedChunk(chunk);
         return true;
+    }
+
+    @Override
+    public void afterDecoding(HttpExchange exchange)
+    {
+        exchange.getResponse().headers(headers ->
+        {
+            headers.remove(HttpHeader.TRANSFER_ENCODING);
+            headers.putLongField(HttpHeader.CONTENT_LENGTH, decodedLength);
+        });
     }
 
     /**
