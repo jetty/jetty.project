1a9a4218041bc4f0806f0832db65d5b6274217a9
Ludovic Orban <lorban@bitronix.be>
2023-09-06 17:04:47 +0200

    #10476 add BufferedContentSink
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

---
+++ jetty-core/jetty-io/src/test/java/org/eclipse/jetty/io/BufferedContentSinkTest.java
@@ -0,0 +1,212 @@
+//
+// ========================================================================
+// Copyright (c) 1995 Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
+
+package org.eclipse.jetty.io;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.eclipse.jetty.io.content.AsyncContent;
+import org.eclipse.jetty.io.content.BufferedContentSink;
+import org.eclipse.jetty.util.BufferUtil;
+import org.eclipse.jetty.util.Callback;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BufferedContentSinkTest
+{
+    private ArrayByteBufferPool.Tracking _bufferPool;
+
+    @BeforeEach
+    public void setUp()
+    {
+        _bufferPool = new ArrayByteBufferPool.Tracking();
+    }
+
+    @AfterEach
+    public void tearDown()
+    {
+        assertThat("Leaks: " + _bufferPool.dumpLeaks(), _bufferPool.getLeaks().size(), is(0));
+    }
+
+    @Test
+    public void testWriteInvokesDemandCallback() throws Exception
+    {
+        try (AsyncContent async = new AsyncContent())
+        {
+            BufferedContentSink buffered = new BufferedContentSink(async, _bufferPool, true, 4096);
+
+            CountDownLatch latch = new CountDownLatch(1);
+            async.demand(latch::countDown);
+            assertFalse(latch.await(250, TimeUnit.MILLISECONDS));
+
+            buffered.write(true, UTF_8.encode("one"), Callback.NOOP);
+
+            assertTrue(latch.await(5, TimeUnit.SECONDS));
+
+            Content.Chunk chunk = async.read();
+            assertNotNull(chunk);
+            chunk.release();
+        }
+    }
+
+    @Test
+    public void testChunkReleaseSucceedsWriteCallback()
+    {
+        try (AsyncContent async = new AsyncContent())
+        {
+            BufferedContentSink buffered = new BufferedContentSink(async, _bufferPool, true, 4096);
+
+            AtomicInteger successCounter = new AtomicInteger();
+            AtomicReference<Throwable> failureRef = new AtomicReference<>();
+
+            buffered.write(true, ByteBuffer.wrap(new byte[1]), Callback.from(successCounter::incrementAndGet, failureRef::set));
+
+            Content.Chunk chunk = async.read();
+            assertThat(successCounter.get(), is(0));
+            chunk.retain();
+            assertThat(chunk.release(), is(false));
+            assertThat(successCounter.get(), is(0));
+            assertThat(chunk.release(), is(true));
+            assertThat(successCounter.get(), is(1));
+            assertThat(failureRef.get(), is(nullValue()));
+        }
+    }
+
+    @Test
+    public void testEmptyChunkReadSucceedsWriteCallback()
+    {
+        try (AsyncContent async = new AsyncContent())
+        {
+            BufferedContentSink buffered = new BufferedContentSink(async, _bufferPool, true, 4096);
+
+            AtomicInteger successCounter = new AtomicInteger();
+            AtomicReference<Throwable> failureRef = new AtomicReference<>();
+
+            buffered.write(true, ByteBuffer.wrap(new byte[0]), Callback.from(successCounter::incrementAndGet, failureRef::set));
+
+            Content.Chunk chunk = async.read();
+            assertThat(successCounter.get(), is(1));
+            assertThat(chunk.isLast(), is(true));
+            assertThat(chunk.hasRemaining(), is(false));
+            assertThat(chunk.release(), is(true));
+            assertThat(successCounter.get(), is(1));
+            assertThat(failureRef.get(), is(nullValue()));
+        }
+    }
+
+    @Test
+    public void testWriteAfterWriteLast()
+    {
+        try (AsyncContent async = new AsyncContent())
+        {
+            BufferedContentSink buffered = new BufferedContentSink(async, _bufferPool, true, 4096);
+
+            AtomicInteger successCounter = new AtomicInteger();
+            AtomicReference<Throwable> failureRef = new AtomicReference<>();
+
+            buffered.write(true, ByteBuffer.wrap(new byte[0]), Callback.from(successCounter::incrementAndGet, failureRef::set));
+
+            Content.Chunk chunk = async.read();
+            assertThat(chunk.isLast(), is(true));
+            assertThat(chunk.release(), is(true));
+
+            assertThat(successCounter.get(), is(1));
+            assertThat(failureRef.get(), is(nullValue()));
+
+            buffered.write(false, ByteBuffer.wrap(new byte[0]), Callback.from(successCounter::incrementAndGet, failureRef::set));
+            assertThat(successCounter.get(), is(1));
+            assertThat(failureRef.get(), instanceOf(IOException.class));
+        }
+    }
+
+    @Test
+    public void testLargeBuffer()
+    {
+        try (AsyncContent async = new AsyncContent())
+        {
+            BufferedContentSink buffered = new BufferedContentSink(async, _bufferPool, true, 4096);
+
+            buffered.write(false, ByteBuffer.wrap("one\n".getBytes(UTF_8)), Callback.NOOP);
+            Content.Chunk chunk = async.read();
+            assertThat(chunk, nullValue());
+
+            buffered.write(false, ByteBuffer.wrap("two\n".getBytes(UTF_8)), Callback.NOOP);
+            chunk = async.read();
+            assertThat(chunk, nullValue());
+
+            buffered.write(true, null, Callback.NOOP);
+            chunk = async.read();
+            assertThat(chunk.isLast(), is(true));
+            assertThat(BufferUtil.toString(chunk.getByteBuffer(), UTF_8), is("one\ntwo\n"));
+            assertThat(chunk.release(), is(true));
+        }
+    }
+
+    @Test
+    public void testSmallBuffer()
+    {
+        byte[] input1 = new byte[1024];
+        Arrays.fill(input1, (byte)'1');
+        byte[] input2 = new byte[1023];
+        Arrays.fill(input2, (byte)'2');
+
+        ByteBuffer accumulatingBuffer = BufferUtil.allocate(4096);
+        BufferUtil.flipToFill(accumulatingBuffer);
+
+        try (AsyncContent async = new AsyncContent())
+        {
+            BufferedContentSink buffered = new BufferedContentSink(async, _bufferPool, true, 16);
+
+            buffered.write(false, ByteBuffer.wrap(input1), Callback.from(() ->
+                buffered.write(true, ByteBuffer.wrap(input2), Callback.NOOP)));
+
+            while (true)
+            {
+                Content.Chunk chunk = async.read();
+                assertThat(chunk, notNullValue());
+                accumulatingBuffer.put(chunk.getByteBuffer());
+                assertThat(chunk.release(), is(true));
+                if (chunk.isLast())
+                    break;
+            }
+
+            BufferUtil.flipToFlush(accumulatingBuffer, 0);
+            assertThat(accumulatingBuffer.remaining(), is(input1.length + input2.length));
+            for (byte b : input1)
+            {
+                assertThat(accumulatingBuffer.get(), is(b));
+            }
+            for (byte b : input2)
+            {
+                assertThat(accumulatingBuffer.get(), is(b));
+            }
+        }
+    }
+}
