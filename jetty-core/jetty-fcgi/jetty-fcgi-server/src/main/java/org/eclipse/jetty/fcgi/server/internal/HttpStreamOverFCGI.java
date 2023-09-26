ffb00fd7f1529d6e235eca32e3be64ca2e2bcd1b
Ludovic Orban <lorban@bitronix.be>
2023-09-26 18:14:34 +0200

    #10543 handle review comments
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-core/jetty-fcgi/jetty-fcgi-server/src/main/java/org/eclipse/jetty/fcgi/server/internal/HttpStreamOverFCGI.java
+++ jetty-core/jetty-fcgi/jetty-fcgi-server/src/main/java/org/eclipse/jetty/fcgi/server/internal/HttpStreamOverFCGI.java
@@ -317,15 +317,9 @@ public class HttpStreamOverFCGI implements HttpStream
         Throwable result = HttpStream.consumeAvailable(this, _httpChannel.getConnectionMetaData().getHttpConfiguration());
         if (result != null)
         {
-            // If HttpStream.consumeAvailable() returns an error, there may be unconsumed content left,
-            // so we must make sure the buffer is released and that the next chunk indicates the end of the stream.
             if (_chunk != null)
-            {
                 _chunk.release();
-                _chunk = Content.Chunk.next(_chunk);
-            }
-            if (_chunk == null)
-                _chunk = Content.Chunk.from(result, true);
+            _chunk = Content.Chunk.from(result, true);
         }
         return result;
     }
