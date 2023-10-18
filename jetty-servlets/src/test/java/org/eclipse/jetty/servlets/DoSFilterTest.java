de862c558b1b2cccf03d1f17f72a811ef8e18734
Greg Wilkins <gregw@webtide.com>
2023-10-18 22:34:54 +0200

    Simplify the DosFilter for #1256 (#10748)
    Use only IP tracking for the DosFilter to fix #1256
    
    Signed-off-by: gregw <gregw@webtide.com>

--- jetty-servlets/src/test/java/org/eclipse/jetty/servlets/DoSFilterTest.java
+++ jetty-servlets/src/test/java/org/eclipse/jetty/servlets/DoSFilterTest.java
@@ -173,7 +173,7 @@ public class DoSFilterTest extends AbstractDoSFilterTest
     {
         boolean exceeded = false;
         ServletContext context = new ContextHandler.StaticContext();
-        RateTracker rateTracker = new RateTracker(context, doSFilter.getName(), "test2", DoSFilter.RateType.UNKNOWN, 4);
+        RateTracker rateTracker = new RateTracker(context, doSFilter.getName(), "test2", 4);
 
         for (int i = 0; i < 5; i++)
         {
