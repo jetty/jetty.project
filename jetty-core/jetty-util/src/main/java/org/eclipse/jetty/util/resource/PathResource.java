91b94c7eccc09c075ca785fd400f0845061ac968
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2023-10-24 16:04:14 -0500

    Fixes #10760 - Remove PathResource.TreeCopyFileVisitor (no longer used)

--- jetty-core/jetty-util/src/main/java/org/eclipse/jetty/util/resource/PathResource.java
+++ jetty-core/jetty-util/src/main/java/org/eclipse/jetty/util/resource/PathResource.java
@@ -17,14 +17,11 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.DirectoryIteratorException;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -614,43 +611,5 @@ public class PathResource extends Resource
     public String toString()
     {
         return this.uri.toASCIIString();
-    }
-
-    private static class TreeCopyFileVisitor extends SimpleFileVisitor<Path>
-    {
-        private final String relativeTo;
-        private final Path target;
-
-        public TreeCopyFileVisitor(Path relativeTo, Path target)
-        {
-            this.relativeTo = relativeTo.getRoot().relativize(relativeTo).toString();
-            this.target = target;
-        }
-
-        @Override
-        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException
-        {
-            Path resolvedTarget = target.resolve(dir.getRoot().resolve(relativeTo).relativize(dir).toString());
-            if (Files.notExists(resolvedTarget))
-                Files.createDirectories(resolvedTarget);
-            return FileVisitResult.CONTINUE;
-        }
-
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
-        {
-            Path resolvedTarget = target.resolve(file.getRoot().resolve(relativeTo).relativize(file).toString());
-            Path parentDir = resolvedTarget.getParent();
-            if (Files.notExists(parentDir))
-                Files.createDirectory(parentDir);
-            Files.copy(file, resolvedTarget, StandardCopyOption.COPY_ATTRIBUTES, StandardCopyOption.REPLACE_EXISTING);
-            return FileVisitResult.CONTINUE;
-        }
-
-        @Override
-        public FileVisitResult visitFileFailed(Path file, IOException exc)
-        {
-            return FileVisitResult.CONTINUE;
-        }
     }
 }
