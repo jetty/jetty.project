8875b83f82792a0bc3faddda967a04590bbc9c3e
gregw <gregw@webtide.com>
2023-10-30 13:26:02 +1100

    Recycle ServletChannel at ServletContextHandler completion.

--- jetty-ee10/jetty-ee10-servlet/src/main/java/org/eclipse/jetty/ee10/servlet/ServletChannel.java
+++ jetty-ee10/jetty-ee10-servlet/src/main/java/org/eclipse/jetty/ee10/servlet/ServletChannel.java
@@ -59,7 +59,7 @@ import static org.eclipse.jetty.util.thread.Invocable.InvocationType.NON_BLOCKIN
  * {@link jakarta.servlet.ServletInputStream}.
  * <p>
  * This class is reusable over multiple requests for the same {@link ServletContextHandler}
- * and is {@link #recycle() recycled} after each use before being
+ * and is {@link #recycle(Throwable) recycled} after each use before being
  * {@link #associate(ServletContextRequest) associated} with a new {@link ServletContextRequest}
  * and then {@link #associate(Request, Response, Callback) associated} with possibly wrapped
  * request, response and callback.
@@ -112,10 +112,10 @@ public class ServletChannel
     /**
      * Associate this channel with a specific request.
      * This method is called by the {@link ServletContextHandler} when a core {@link Request} is accepted and associated with
-     * a servlet mapping. The association remains functional until {@link #recycle()} is called,
-     * and it remains readable until a call to {@link #recycle()} or a subsequent call to {@code associate}.
+     * a servlet mapping. The association remains functional until {@link #recycle(Throwable)} is called,
+     * and it remains readable until a call to {@link #recycle(Throwable)} or a subsequent call to {@code associate}.
      * @param servletContextRequest The servlet context request to associate
-     * @see #recycle()
+     * @see #recycle(Throwable)
      */
     public void associate(ServletContextRequest servletContextRequest)
     {
@@ -437,6 +437,8 @@ public class ServletChannel
     }
 
     /**
+     * Prepare to be reused.
+     * @param x Any completion exception, or null for successful completion.
      * @see #associate(ServletContextRequest)
      */
     void recycle(Throwable x)
