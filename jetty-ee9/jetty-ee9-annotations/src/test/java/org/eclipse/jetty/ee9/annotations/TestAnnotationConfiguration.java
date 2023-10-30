f20425fd0f21d20d0d94be5ce95676f161ec3604
Olivier Lamy <olamy@apache.org>
2023-10-30 12:44:20 +1000

    fix path
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- jetty-ee9/jetty-ee9-annotations/src/test/java/org/eclipse/jetty/ee9/annotations/TestAnnotationConfiguration.java
+++ jetty-ee9/jetty-ee9-annotations/src/test/java/org/eclipse/jetty/ee9/annotations/TestAnnotationConfiguration.java
@@ -84,7 +84,7 @@ public class TestAnnotationConfiguration
         assertTrue(Files.exists(web31true), web31true + " should exist");
 
         // prepare an sci that will be on the webapp's classpath
-        jarDir = MavenPaths.targetTestDir("jar");
+        jarDir = MavenPaths.findTestResourceDir("jar");
         testSciJar = jarDir.resolve("test-sci.jar");
         assertTrue(Files.exists(testSciJar));
 
