7f2859ab49490ffdb1d01619c20f1c7e1830f60f
Greg Wilkins <gregw@webtide.com>
2023-12-06 20:12:00 +0000

    Issue #11014 - Improve Relative Redirect Handling in Jetty 10/11 (#11018)
    * Improve relative redirect handling #11014
    * Common relative redirect method.
    * Use the relative redirect method from redirection rules.
    
    Signed-off-by: gregw <gregw@webtide.com>

--- jetty-rewrite/src/main/java/org/eclipse/jetty/rewrite/handler/RedirectUtil.java
+++ jetty-rewrite/src/main/java/org/eclipse/jetty/rewrite/handler/RedirectUtil.java
@@ -15,7 +15,7 @@ package org.eclipse.jetty.rewrite.handler;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.eclipse.jetty.util.URIUtil;
+import org.eclipse.jetty.server.Response;
 
 /**
  * Utility for managing redirect based rules
@@ -32,33 +32,6 @@ public final class RedirectUtil
      */
     public static String toRedirectURL(final HttpServletRequest request, String location)
     {
-        if (!URIUtil.hasScheme(location))
-        {
-            StringBuilder url = new StringBuilder(128);
-            URIUtil.appendSchemeHostPort(url, request.getScheme(), request.getServerName(), request.getServerPort());
-
-            if (location.startsWith("/"))
-            {
-                // absolute in context
-                location = URIUtil.canonicalURI(location);
-            }
-            else
-            {
-                // relative to request
-                String path = request.getRequestURI();
-                String parent = (path.endsWith("/")) ? path : URIUtil.parentPath(path);
-                location = URIUtil.canonicalURI(URIUtil.addEncodedPaths(parent, location));
-                if (location != null && !location.startsWith("/"))
-                    url.append('/');
-            }
-
-            if (location == null)
-                throw new IllegalStateException("redirect path cannot be above root");
-            url.append(location);
-
-            location = url.toString();
-        }
-
-        return location;
+        return Response.toRedirectURI(request, location);
     }
 }
