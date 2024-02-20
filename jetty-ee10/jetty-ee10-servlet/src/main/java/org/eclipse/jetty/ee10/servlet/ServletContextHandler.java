0c0886f646354b3ea7ea7743ab11c3782705922b
Ludovic Orban <lorban@bitronix.be>
2024-02-20 12:23:32 +0100

    Revert "#11396 restore previous setTempDirectory impl"
    This reverts commit 9ed0a58c5c2032c4e216ec506653732fa20b220b.

--- jetty-ee10/jetty-ee10-servlet/src/main/java/org/eclipse/jetty/ee10/servlet/ServletContextHandler.java
+++ jetty-ee10/jetty-ee10-servlet/src/main/java/org/eclipse/jetty/ee10/servlet/ServletContextHandler.java
@@ -13,7 +13,6 @@
 
 package org.eclipse.jetty.ee10.servlet;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -45,6 +44,7 @@ import jakarta.servlet.FilterRegistration;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.Servlet;
 import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextAttributeEvent;
 import jakarta.servlet.ServletContextAttributeListener;
 import jakarta.servlet.ServletContextEvent;
@@ -110,8 +110,6 @@ import org.eclipse.jetty.util.resource.Resources;
 import org.eclipse.jetty.util.security.SecurityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static jakarta.servlet.ServletContext.TEMPDIR;
 
 /**
  * Servlet Context.
@@ -288,13 +286,6 @@ public class ServletContextHandler extends ContextHandler
 
         // Link the handlers
         relinkHandlers();
-    }
-
-    @Override
-    public void setTempDirectory(File tempDirectory)
-    {
-        super.setTempDirectory(tempDirectory);
-        setAttribute(TEMPDIR, getTempDirectory());
     }
 
     public ServletContextApi newServletContextApi()
@@ -1988,6 +1979,22 @@ public class ServletContextHandler extends ContextHandler
 
     public class ServletScopedContext extends ScopedContext
     {
+        @Override
+        public Object getAttribute(String name)
+        {
+            if (ServletContext.TEMPDIR.equals(name))
+                return getTempDirectory();
+            return super.getAttribute(name);
+        }
+
+        @Override
+        public Set<String> getAttributeNameSet()
+        {
+            Set<String> attributeNameSet = new HashSet<>(super.getAttributeNameSet());
+            attributeNameSet.add(ServletContext.TEMPDIR);
+            return Collections.unmodifiableSet(attributeNameSet);
+        }
+
         public ServletContextApi getServletContext()
         {
             return _servletContext;
