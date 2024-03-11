2a5363a607d389a3853cf886c0e15e0ba983acde
Olivier Lamy <olamy@apache.org>
2024-03-11 11:25:04 +1000

    fixing dependencies and code with new hibernate search
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- tests/jetty-test-session-common/src/main/java/org/eclipse/jetty/session/test/tools/InfinispanTestSupport.java
+++ tests/jetty-test-session-common/src/main/java/org/eclipse/jetty/session/test/tools/InfinispanTestSupport.java
@@ -22,14 +22,12 @@ import org.eclipse.jetty.session.SessionData;
 import org.eclipse.jetty.session.infinispan.InfinispanSerializationContextInitializer;
 import org.eclipse.jetty.session.infinispan.InfinispanSessionData;
 import org.eclipse.jetty.toolchain.test.FS;
-import org.hibernate.search.cfg.Environment;
-import org.hibernate.search.cfg.SearchMapping;
 import org.infinispan.Cache;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.cache.ConfigurationChildBuilder;
+import org.infinispan.configuration.cache.IndexStorage;
 import org.infinispan.configuration.cache.StorageType;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
-import org.infinispan.eviction.EvictionStrategy;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
 
@@ -92,12 +90,6 @@ public class InfinispanTestSupport
         Path tmpdir = root.resolve("tmp");
         FS.ensureDirExists(indexesDir);
 
-        SearchMapping mapping = new SearchMapping();
-        mapping.entity(SessionData.class).indexed().providedId().property("expiry", ElementType.FIELD).field();
-        Properties properties = new Properties();
-        properties.put(Environment.MODEL_MAPPING, mapping);
-        properties.put("hibernate.search.default.indexBase", indexesDir.toString());
-        properties.put("hibernate.cache.infinispan.entity.eviction.strategy", "NONE");
 
         if (_manager.cacheExists(_name))
         {
@@ -106,31 +98,30 @@ public class InfinispanTestSupport
 
         if (_useFileStore)
         {
+
             ConfigurationChildBuilder b = _builder
                 .indexing()
-                .addIndexedEntity(SessionData.class)
-                // FIXME
-                //.withProperties(properties)
-                .memory()
-                .whenFull(EvictionStrategy.NONE)
-                .persistence()
-                .addSoftIndexFileStore()
-                .segmented(false)
-                .indexLocation(tmpdir.toFile().getAbsolutePath());
-            if (_serializeSessionData)
-            {
-                b = b.memory().storage(StorageType.HEAP)
-                    .encoding()
-                    .mediaType("application/x-protostream");
-            }
+                .enable()
+                .addIndexedEntity(InfinispanSessionData.class)
+                .storage(IndexStorage.FILESYSTEM)
+                .path(tmpdir.toFile().getAbsolutePath());
+                //.memory()
+                //.whenFull(EvictionStrategy.NONE)
+                //.persistence();
+//            if (_serializeSessionData)
+//            {
+//                b = b.memory().storage(StorageType.HEAP)
+//                    .encoding()
+//                    .mediaType("application/x-protostream");
+//            }
             _manager.defineConfiguration(_name, b.build());
         }
         else
         {
             ConfigurationChildBuilder b = _builder.indexing()
-                    // FIXME
-                    //.withProperties(properties)
-                .addIndexedEntity(SessionData.class);
+                    .enable()
+                    .storage(IndexStorage.LOCAL_HEAP)
+                    .addIndexedEntity(InfinispanSessionData.class);
         
             if (_serializeSessionData)
             {
