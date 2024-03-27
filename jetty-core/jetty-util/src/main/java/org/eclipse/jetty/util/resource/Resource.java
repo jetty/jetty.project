69ef33eda0296d71b8703472ca4bfb703efb40df
Jan Bartel <janb@webtide.com>
2024-03-27 10:00:56 +0100

    Remove unused import

--- jetty-core/jetty-util/src/main/java/org/eclipse/jetty/util/resource/Resource.java
+++ jetty-core/jetty-util/src/main/java/org/eclipse/jetty/util/resource/Resource.java
@@ -26,7 +26,6 @@ import java.nio.file.StandardOpenOption;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
