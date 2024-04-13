ea8139e94a6918b6edb2a3a1e8e83af45f0057ab
Olivier Lamy <olamy@apache.org>
2024-04-13 10:33:30 +0200

    Issue #11642 restore Jetty ee8 runner (#11650)
    * Issue #11642 restore Jetty ee8 runner
    ---------
    
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- jetty-ee8/jetty-ee8-runner/src/test/java/org/eclipse/jetty/ee9/maven/jettyrunner/it/IntegrationTestJettyRunner.java
+++ jetty-ee8/jetty-ee8-runner/src/test/java/org/eclipse/jetty/ee9/maven/jettyrunner/it/IntegrationTestJettyRunner.java
@@ -11,7 +11,7 @@
 // ========================================================================
 //
 
-package org.eclipse.jetty.ee10.maven.jettyrunner.it;
+package org.eclipse.jetty.ee9.maven.jettyrunner.it;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -38,7 +38,7 @@ public class IntegrationTestJettyRunner
             httpClient.start();
             ContentResponse response = httpClient.newRequest(serverUri).send();
             String res = response.getContentAsString();
-            assertThat(res, Matchers.containsString("Hello World EE10!"));
+            assertThat(res, Matchers.containsString("Hello World EE8!"));
         }
         finally
         {
@@ -62,5 +62,4 @@ public class IntegrationTestJettyRunner
 
         throw new Exception("cannot find started Jetty");
     }
-
 }
