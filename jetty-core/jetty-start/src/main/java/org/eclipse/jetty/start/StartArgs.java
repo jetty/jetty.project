e3e474cbca62fe7ff81ddb343045dee2eb4fca2a
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2024-04-18 11:58:41 -0500

    Changes from review

--- jetty-core/jetty-start/src/main/java/org/eclipse/jetty/start/StartArgs.java
+++ jetty-core/jetty-start/src/main/java/org/eclipse/jetty/start/StartArgs.java
@@ -1078,7 +1078,7 @@ public class StartArgs
             return environment;
         }
 
-        if (arg.startsWith("--include-jetty-dir=") || arg.startsWith("--add-base-dir="))
+        if (arg.startsWith("--include-jetty-dir=") || arg.startsWith("--add-config-dir="))
         {
             // valid, but handled in ConfigSources instead
             return environment;
