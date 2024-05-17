241cbca46d61d25f4718ba01a387ea61cb533474
Jan Bartel <janb@webtide.com>
2024-05-17 18:12:10 +1000

    WIP

--- jetty-ee11/jetty-ee11-tests/jetty-ee11-test-integration/src/test/java/org/eclipse/jetty/ee11/test/websocket/JakartaWebSocketTest.java
+++ jetty-ee11/jetty-ee11-tests/jetty-ee11-test-integration/src/test/java/org/eclipse/jetty/ee11/test/websocket/JakartaWebSocketTest.java
@@ -58,7 +58,7 @@ public class JakartaWebSocketTest
     @Test
     public void testChatEndpoint() throws Exception
     {
-        URI uri = WSURI.toWebsocket(server.getServerURI().resolve("/ee11-demo-jetty/jakarta.websocket"));
+        URI uri = WSURI.toWebsocket(server.getServerURI().resolve("/servlet5-demo-jakarta/jakarta.websocket"));
 
         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
 
