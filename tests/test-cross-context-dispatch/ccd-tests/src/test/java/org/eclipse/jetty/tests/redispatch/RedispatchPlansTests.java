ff3ce62eb4bb84370fc5d07362a9b98dae715e41
Jan Bartel <janb@webtide.com>
2024-06-13 09:32:30 +1000

    Comment out disabling test.

--- tests/test-cross-context-dispatch/ccd-tests/src/test/java/org/eclipse/jetty/tests/redispatch/RedispatchPlansTests.java
+++ tests/test-cross-context-dispatch/ccd-tests/src/test/java/org/eclipse/jetty/tests/redispatch/RedispatchPlansTests.java
@@ -75,7 +75,7 @@ public class RedispatchPlansTests extends AbstractRedispatchTest
         List<Arguments> plans = new ArrayList<>();
 
         List<String> disabledTests = new ArrayList<>();
-        disabledTests.add("ee10-session-ee8-ee9-ee8.txt"); // causes an ISE
+        //disabledTests.add("ee10-session-ee8-ee9-ee8.txt");
 
         Path testPlansDir = MavenPaths.findTestResourceDir("plans");
         try (Stream<Path> plansStream = Files.list(testPlansDir))
