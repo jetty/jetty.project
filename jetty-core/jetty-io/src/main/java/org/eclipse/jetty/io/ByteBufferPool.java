5e8cc2243ebe54a113e529df76c980346d528ada
Greg Wilkins <gregw@webtide.com>
2024-06-28 09:37:09 +1000

    Remove buffer from pool on write failure (#11951)
    * Experiment with removable buffer from pool
    * Changed remove return to be release boolean
    * Fixes to avoid double release
    * Tracking ByteBufferPool handles remove
    * Adding assert on _channelState.isLockHeldByCurrentThread()
    
    ---------
    
    Co-authored-by: Joakim Erdfelt <joakim.erdfelt@gmail.com>

--- jetty-core/jetty-io/src/main/java/org/eclipse/jetty/io/ByteBufferPool.java
+++ jetty-core/jetty-io/src/main/java/org/eclipse/jetty/io/ByteBufferPool.java
@@ -59,6 +59,20 @@ public interface ByteBufferPool
     RetainableByteBuffer acquire(int size, boolean direct);
 
     /**
+     * {@link RetainableByteBuffer#release() Release} the buffer in a way that will remove it from any pool that it may be in.
+     * If the buffer is not in a pool, calling this method is equivalent to calling {@link RetainableByteBuffer#release()}.
+     * Calling this method satisfies any contract that requires a call to {@link RetainableByteBuffer#release()}.
+     * @return {@code true} if a call to {@link RetainableByteBuffer#release()} would have returned {@code true}.
+     * @see RetainableByteBuffer#release()
+     * @deprecated This API is experimental and may be removed in future releases
+     */
+    @Deprecated
+    default boolean removeAndRelease(RetainableByteBuffer buffer)
+    {
+        return buffer != null && buffer.release();
+    }
+
+    /**
      * <p>Removes all {@link RetainableByteBuffer#isRetained() non-retained}
      * pooled instances from this pool.</p>
      */
