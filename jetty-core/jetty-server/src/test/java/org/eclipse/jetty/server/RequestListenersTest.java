bad4df0e71fc1ca3a0167e3e376d4a7186ed3099
Simone Bordet <simone.bordet@gmail.com>
2024-09-04 22:15:41 +0200

    Fixed test expectation.
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-core/jetty-server/src/test/java/org/eclipse/jetty/server/RequestListenersTest.java
+++ jetty-core/jetty-server/src/test/java/org/eclipse/jetty/server/RequestListenersTest.java
@@ -195,7 +195,9 @@ public class RequestListenersTest
 
         int expectedStatus = succeedCallback ? HttpStatus.OK_200 : HttpStatus.INTERNAL_SERVER_ERROR_500;
         assertEquals(expectedStatus, response.getStatus());
-        assertThat(failureLatch.await(idleTimeout + 500, TimeUnit.MILLISECONDS), is(failIdleTimeout && !succeedCallback));
+        // The failure listener is never invoked because completing the callback
+        // produces a response that completes the stream so the failure is ignored.
+        assertThat(failureLatch.await(idleTimeout + 500, TimeUnit.MILLISECONDS), is(false));
     }
 
     @ParameterizedTest
