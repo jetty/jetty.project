2da18a0b870619c8faa785333f8b4fe7496f3b9f
Ludovic Orban <lorban@bitronix.be>
2024-09-13 13:26:21 +0200

    #12268 reset _iterate flag when another processing is scheduled
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-util/src/main/java/org/eclipse/jetty/util/IteratingCallback.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/IteratingCallback.java
@@ -275,6 +275,7 @@ public abstract class IteratingCallback implements Callback
                                 }
                                 case SCHEDULED:
                                 {
+                                    _iterate = false;
                                     // we won the race against the callback, so the callback has to process and we can break processing
                                     _state = State.PENDING;
                                     break processing;
@@ -300,6 +301,7 @@ public abstract class IteratingCallback implements Callback
                     {
                         if (action != Action.SCHEDULED)
                             throw new IllegalStateException(String.format("%s[action=%s]", this, action));
+                        _iterate = false;
                         // we lost the race, so we have to keep processing
                         _state = State.PROCESSING;
                         continue;
@@ -457,6 +459,14 @@ public abstract class IteratingCallback implements Callback
 
         if (failure != null)
             onCompleteFailure(new IOException(failure));
+    }
+
+    boolean isPending()
+    {
+        try (AutoLock ignored = _lock.lock())
+        {
+            return _state == State.PENDING;
+        }
     }
 
     /**
