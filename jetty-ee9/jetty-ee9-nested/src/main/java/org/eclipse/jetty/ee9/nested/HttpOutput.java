e75b294a15d5d06431cc3ecca64388002c21c392
Simone Bordet <simone.bordet@gmail.com>
2024-10-10 11:36:13 +0200

    Fixes #12313 - Jetty 12 ee9/ee10 doesn't invoke callbacks when h2 client sends RST_STREAM.
    * Fixed invocation of AsyncListener.onError(), now called even if the response is already committed, in both EE9 and EE10.
    * Reworked EE9 HttpChannel state machine in case of failures to be like EE10's.
      In particular, calling abort now is a state change, rather than a failure of the Handler callback.
      In this way, the handle() loop continues, enters case TERMINATED, and the callback is completed in onCompleted().
    * Fixed EE9 handling of idle timeout in HttpChannel.onRequest(), that was missing.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-ee9/jetty-ee9-nested/src/main/java/org/eclipse/jetty/ee9/nested/HttpOutput.java
+++ jetty-ee9/jetty-ee9-nested/src/main/java/org/eclipse/jetty/ee9/nested/HttpOutput.java
@@ -763,7 +763,7 @@ public class HttpOutput extends ServletOutputStream implements Runnable
     private void checkWritable() throws EofException
     {
         if (_softClose)
-                throw new EofException("Closed");
+            throw new EofException("Closed");
 
         switch (_state)
         {
