8031d7c78a5037c2a057a0e5ec54a2f2d83d2f4d
Ludovic Orban <lorban@bitronix.be>
2024-10-18 11:21:26 +0200

    remove redundant test
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-core/jetty-server/src/test/java/org/eclipse/jetty/server/ThreadStarvationTest.java
+++ jetty-core/jetty-server/src/test/java/org/eclipse/jetty/server/ThreadStarvationTest.java
@@ -163,34 +163,6 @@ public class ThreadStarvationTest
 
     @ParameterizedTest
     @MethodSource("scenarios")
-    public void testReadInput(Scenario scenario) throws Exception
-    {
-        prepareServer(scenario, new ReadHandler()).start();
-
-        try (Socket client = scenario.clientSocketProvider.newSocket("localhost", _connector.getLocalPort()))
-        {
-            client.setSoTimeout(10000);
-            OutputStream os = client.getOutputStream();
-            InputStream is = client.getInputStream();
-
-            String request =
-                "GET / HTTP/1.0\r\n" +
-                    "Host: localhost\r\n" +
-                    "Content-Length: 10\r\n" +
-                    "\r\n" +
-                    "0123456789\r\n";
-            os.write(request.getBytes(StandardCharsets.UTF_8));
-            os.flush();
-
-            String response = IO.toString(is);
-            assertEquals(-1, is.read());
-            assertThat(response, containsString("200 OK"));
-            assertThat(response, containsString("Read Input 10"));
-        }
-    }
-
-    @ParameterizedTest
-    @MethodSource("scenarios")
     public void testReadStarvation(Scenario scenario) throws Exception
     {
         prepareServer(scenario, new ReadHandler());
