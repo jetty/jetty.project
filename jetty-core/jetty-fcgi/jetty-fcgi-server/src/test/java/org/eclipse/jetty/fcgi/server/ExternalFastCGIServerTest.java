2eae801e40dac24e790f0d58bc10c29f2bc493c1
Jan Bartel <janb@webtide.com>
2024-11-06 16:30:20 +1100

    Improve disabled test in ExternalFastCGIServerTest

--- jetty-core/jetty-fcgi/jetty-fcgi-server/src/test/java/org/eclipse/jetty/fcgi/server/ExternalFastCGIServerTest.java
+++ jetty-core/jetty-fcgi/jetty-fcgi-server/src/test/java/org/eclipse/jetty/fcgi/server/ExternalFastCGIServerTest.java
@@ -22,15 +22,15 @@ import java.util.concurrent.TimeUnit;
 import org.eclipse.jetty.client.ContentResponse;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.fcgi.client.transport.HttpClientTransportOverFCGI;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExternalFastCGIServerTest
 {
     @Test
-    @Disabled("Relies on an external server")
+    @EnabledIfSystemProperty(named = "external.fastcgi.port", matches = "[0-9][0-9][0-9][0-9]")
     public void testExternalFastCGIServer() throws Exception
     {
         // Assume a FastCGI server is listening on localhost:9000
