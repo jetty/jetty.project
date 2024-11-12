2f06d2fd9c905c99d77a98c81a1117ac6f4562d8
gregw <gregw@webtide.com>
2024-11-12 16:15:38 +1100

    deprecated DelayedHandler

---
+++ jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/handler/DelayedHandler.java
@@ -0,0 +1,33 @@
+//
+// ========================================================================
+// Copyright (c) 1995 Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
+
+package org.eclipse.jetty.server.handler;
+
+import org.eclipse.jetty.server.Handler;
+
+/**
+ * @deprecated Use {@link EagerContentHandler}
+ */
+@Deprecated(forRemoval = true; since="12.1.0")
+public class DelayedHandler extends EagerContentHandler
+{
+    DelayedHandler()
+    {
+        this(null);
+    }
+
+    DelayedHandler(Handler handler)
+    {
+        super(handler);
+    }
+}
