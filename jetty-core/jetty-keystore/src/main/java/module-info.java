d278a1949a7d99b8f4f476a1482e9559f170f68f
Olivier Lamy <olamy@apache.org>
2024-11-19 16:32:52 +1000

    WIP aggregated javadoc and some missing module-info.java
    Signed-off-by: Olivier Lamy <olamy@apache.org>

---
+++ jetty-core/jetty-keystore/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+//
+// ========================================================================
+// Copyright (c) 1995 Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
+
+module org.eclipse.jetty.keystore
+{
+    requires org.slf4j;
+    requires org.bouncycastle.provider;
+    requires org.bouncycastle.pkix;
+    requires org.bouncycastle.util;
+    requires org.eclipse.jetty.util;
+
+    exports org.eclipse.jetty.keystore;
+}
