af07a1d2bcaec0ccfa971ecf297690f3f8e2c537
Simone Bordet <simone.bordet@gmail.com>
2024-11-22 16:50:48 +0100

    Updates from review.
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-core/jetty-client/src/main/java/org/eclipse/jetty/client/ProxyProtocolClientConnectionFactory.java
+++ jetty-core/jetty-client/src/main/java/org/eclipse/jetty/client/ProxyProtocolClientConnectionFactory.java
@@ -460,7 +460,7 @@ public abstract class ProxyProtocolClientConnectionFactory implements ClientConn
 
     protected abstract ProxyProtocolConnection newProxyProtocolConnection(EndPoint endPoint, Map<String, Object> context);
 
-    protected abstract static class ProxyProtocolConnection extends AbstractConnection implements Callback
+    protected abstract static class ProxyProtocolConnection extends AbstractConnection.NonBlocking implements Callback
     {
         static final Logger LOG = LoggerFactory.getLogger(ProxyProtocolConnection.class);
 
