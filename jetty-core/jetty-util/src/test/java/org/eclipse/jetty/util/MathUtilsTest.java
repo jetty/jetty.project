0db8cd5fdcfca687f0aed8be7a1c0234e789da8b
Ludovic Orban <lorban@bitronix.be>
2025-01-24 16:23:40 +0100

    use correct name for test
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-core/jetty-util/src/test/java/org/eclipse/jetty/util/MathUtilsTest.java
+++ jetty-core/jetty-util/src/test/java/org/eclipse/jetty/util/MathUtilsTest.java
@@ -19,7 +19,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class MathUtilTest
+public class MathUtilsTest
 {
     @Test
     public void testCeilNextPowerOfTwo()
