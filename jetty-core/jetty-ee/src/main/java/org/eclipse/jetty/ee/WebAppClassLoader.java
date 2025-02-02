183ddf590d4cddada1b8231aeed02b6839beaeed
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2025-02-02 03:07:21 -0600

    Moving jetty-ee11 WebAppClassLoader to jetty-ee

--- jetty-core/jetty-ee/src/main/java/org/eclipse/jetty/ee/WebAppClassLoader.java
+++ jetty-core/jetty-ee/src/main/java/org/eclipse/jetty/ee/WebAppClassLoader.java
@@ -11,7 +11,7 @@
 // ========================================================================
 //
 
-package org.eclipse.jetty.ee11.webapp;
+package org.eclipse.jetty.ee;
 
 import java.io.IOException;
 import java.io.InputStream;
