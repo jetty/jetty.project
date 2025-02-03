56fdc2964ae58dc5bd9b41844bcc67a6659760f3
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2025-02-03 14:30:29 -0600

    Fix sibling check in WebInfConfiguration.unpack

--- jetty-ee10/jetty-ee10-webapp/src/main/java/org/eclipse/jetty/ee10/webapp/WebInfConfiguration.java
+++ jetty-ee10/jetty-ee10-webapp/src/main/java/org/eclipse/jetty/ee10/webapp/WebInfConfiguration.java
@@ -208,16 +208,23 @@ public class WebInfConfiguration extends AbstractConfiguration
                 if (war != null)
                 {
                     // We have obtained the webApp from the war string, so it
-                    // cannot be a CombinedResource, therefore safe to use it's Path
+                    // cannot be a CombinedResource, therefore safe to use its Path
                     Path warPath = webApp.getPath();
-                    if (warPath != null)
+
+                    if (Files.isDirectory(warPath))
                     {
-                        // look for a sibling like "foo/" to a "foo.war"
-                        if (FileID.isWebArchive(warPath) && Files.exists(warPath))
+                        // only working with a directory, nothing else to check.
+                        extractedWebAppDir = warPath;
+                    }
+                    else if (FileID.isWebArchive(warPath))
+                    {
+                        // we are working with a war file, look for a directory with the same basename
+                        // in the same directory as the war, that would be our extracted webapp directory.
+                        String basename = FileID.getBasename(warPath);
+                        Path sibling = warPath.getParent().resolve(basename);
+                        if (Files.isDirectory(sibling) && Files.isWritable(sibling))
                         {
-                            Path sibling = warPath.getParent().resolve(FileID.getBasename(warPath));
-                            if (Files.exists(sibling) && Files.isDirectory(sibling) && Files.isWritable(sibling))
-                                extractedWebAppDir = sibling;
+                            extractedWebAppDir = sibling;
                         }
                     }
                 }
