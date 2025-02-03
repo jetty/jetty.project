b0edf97a2d72a99f1f12ca0245fb6441ca5f93ff
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2025-02-03 16:58:06 -0600

    Sort test args coming from disk, for repeatable results.

--- tests/test-cross-context-dispatch/ccd-tests/src/test/java/org/eclipse/jetty/tests/redispatch/RedispatchPlansTests.java
+++ tests/test-cross-context-dispatch/ccd-tests/src/test/java/org/eclipse/jetty/tests/redispatch/RedispatchPlansTests.java
@@ -31,6 +31,7 @@ import org.eclipse.jetty.tests.ccd.common.HttpRequest;
 import org.eclipse.jetty.tests.ccd.common.Property;
 import org.eclipse.jetty.tests.testers.JettyHomeTester;
 import org.eclipse.jetty.toolchain.test.MavenPaths;
+import org.eclipse.jetty.util.resource.PathCollators;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
@@ -85,6 +86,7 @@ public class RedispatchPlansTests extends AbstractRedispatchTest
                 .filter(Files::isRegularFile)
                 .filter((file) -> file.getFileName().toString().endsWith(".txt"))
                 .filter((file) -> !disabledTests.contains(file.getFileName().toString()))
+                .sorted(PathCollators.byName(true))
                 .toList();
 
             for (Path plansText : testPlans)
