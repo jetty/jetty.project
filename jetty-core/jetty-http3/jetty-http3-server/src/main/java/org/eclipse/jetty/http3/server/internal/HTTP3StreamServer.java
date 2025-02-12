0c5711f86c39efca324db60d970f7b40e04978ae
Simone Bordet <simone.bordet@gmail.com>
2025-02-12 12:16:00 +0100

    Removed HTTP3StreamServer.dataReady, as it is not necessary.
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-core/jetty-http3/jetty-http3-server/src/main/java/org/eclipse/jetty/http3/server/internal/HTTP3StreamServer.java
+++ jetty-core/jetty-http3/jetty-http3-server/src/main/java/org/eclipse/jetty/http3/server/internal/HTTP3StreamServer.java
@@ -34,7 +34,6 @@ public class HTTP3StreamServer extends HTTP3Stream implements Stream.Server, Inv
     private static final Listener DEFAULT_LISTENER = new Listener() {};
 
     private Stream.Server.Listener listener;
-    private volatile boolean dataReady;
 
     public HTTP3StreamServer(HTTP3Session session, StreamEndPoint endPoint, boolean local)
     {
@@ -53,19 +52,7 @@ public class HTTP3StreamServer extends HTTP3Stream implements Stream.Server, Inv
             onHeaders(frame);
             updateClose(frame.isLast(), false);
             listener = notifyRequest(frame);
-            dataReady = true;
         }
-    }
-
-    @Override
-    public Data readData()
-    {
-        boolean ready = dataReady;
-        if (ready)
-            return super.readData();
-        if (LOG.isDebugEnabled())
-            LOG.debug("data not ready on {}", this);
-        return null;
     }
 
     private Listener notifyRequest(HeadersFrame frame)
