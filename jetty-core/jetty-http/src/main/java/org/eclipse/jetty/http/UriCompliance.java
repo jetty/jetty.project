7df4bcc83d65aaefaad5c606057f727a5e101316
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2025-02-28 08:05:22 -0600

    Fixing import syntax

--- jetty-core/jetty-http/src/main/java/org/eclipse/jetty/http/UriCompliance.java
+++ jetty-core/jetty-http/src/main/java/org/eclipse/jetty/http/UriCompliance.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
