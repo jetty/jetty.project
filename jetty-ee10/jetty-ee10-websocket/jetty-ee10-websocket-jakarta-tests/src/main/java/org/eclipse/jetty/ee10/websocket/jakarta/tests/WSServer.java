bdae57ae70f69f97e1631dc9c3838098d87a707c
Lachlan Roberts <lachlan.p.roberts@gmail.com>
2025-03-05 20:36:26 +1100

    PR #12830 - changes from review
    Signed-off-by: Lachlan Roberts <lachlan.p.roberts@gmail.com>

--- jetty-ee10/jetty-ee10-websocket/jetty-ee10-websocket-jakarta-tests/src/main/java/org/eclipse/jetty/ee10/websocket/jakarta/tests/WSServer.java
+++ jetty-ee10/jetty-ee10-websocket/jetty-ee10-websocket-jakarta-tests/src/main/java/org/eclipse/jetty/ee10/websocket/jakarta/tests/WSServer.java
@@ -15,13 +15,10 @@ package org.eclipse.jetty.ee10.websocket.jakarta.tests;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.util.Random;
 
 import org.eclipse.jetty.ee10.webapp.WebAppContext;
@@ -34,6 +31,7 @@ import org.eclipse.jetty.toolchain.test.IO;
 import org.eclipse.jetty.toolchain.test.JAR;
 import org.eclipse.jetty.toolchain.test.MavenTestingUtils;
 import org.eclipse.jetty.util.TypeUtil;
+import org.eclipse.jetty.util.resource.ResourceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -80,6 +78,7 @@ public class WSServer extends LocalServer implements LocalFuzzer.Provider
     public class WebApp
     {
         private final WebAppContext context;
+        private final ResourceFactory resourceFactory;
         private final Path contextDir;
         private final Path webInf;
         private final Path classesDir;
@@ -106,6 +105,8 @@ public class WSServer extends LocalServer implements LocalFuzzer.Provider
             context.setBaseResourceAsPath(contextDir);
             context.setAttribute("org.eclipse.jetty.websocket.jakarta", Boolean.TRUE);
             context.addConfiguration(new JakartaWebSocketConfiguration());
+
+            resourceFactory = ResourceFactory.of(context);
         }
 
         public WebAppContext getWebAppContext()
@@ -144,20 +145,7 @@ public class WSServer extends LocalServer implements LocalFuzzer.Provider
             assertThat("Class URL for: " + clazz, classUrl, notNullValue());
             Path destFile = classesDir.resolve(endpointPath);
             FS.ensureDirExists(destFile.getParent());
-            URI uri = classUrl.toURI();
-
-            if ("jar".equalsIgnoreCase(uri.getScheme()))
-            {
-                try (InputStream in = classUrl.openStream())
-                {
-                    Files.copy(in, destFile, StandardCopyOption.REPLACE_EXISTING);
-                }
-            }
-            else
-            {
-                File srcFile = new File(uri);
-                IO.copy(srcFile.toPath(), destFile);
-            }
+            resourceFactory.newResource(classUrl).copyTo(destFile);
         }
 
         public void copyLib(Class<?> clazz, String jarFileName) throws URISyntaxException, IOException
