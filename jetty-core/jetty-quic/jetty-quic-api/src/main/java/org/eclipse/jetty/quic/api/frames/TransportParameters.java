206fcb305e5e24fd3c66d978ee2bf98e80e21603
Simone Bordet <simone.bordet@gmail.com>
2025-04-07 18:09:39 +0200

    Updated TransportParameters for future usage.
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-core/jetty-quic/jetty-quic-api/src/main/java/org/eclipse/jetty/quic/api/frames/TransportParameters.java
+++ jetty-core/jetty-quic/jetty-quic-api/src/main/java/org/eclipse/jetty/quic/api/frames/TransportParameters.java
@@ -17,6 +17,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.LongFunction;
 
 import org.eclipse.jetty.util.TypeUtil;
 
@@ -29,50 +30,6 @@ import org.eclipse.jetty.util.TypeUtil;
  */
 public class TransportParameters implements Iterable<Map.Entry<TransportParameters.Id<?>, Object>>
 {
-    // The max N that can produce a grease parameter id that fits a VarLenInt.
-    private static final long MAX_N = 148764065110560899L;
-
-    /**
-     * <p>The session max idle timeout in milliseconds.</p>
-     */
-    public static final Id<Long> MAX_IDLE_TIMEOUT = Id.from(0x01);
-    /**
-     * <p>The initial session max data.</p>
-     * <p>A local peer sends this parameter to inform the remote peer about
-     * the max data the local peer is willing to receive on the session.</p>
-     */
-    public static final Id<Long> INITIAL_MAX_DATA = Id.from(0x04);
-    /**
-     * <p>The initial local bidirectional stream max data.</p>
-     * <p>A local peer sends this parameter to inform the remote peer about
-     * the max data the local peer is willing to receive on local bidirectional streams.</p>
-     */
-    public static final Id<Long> INITIAL_MAX_STREAM_DATA_BIDIRECTIONAL_LOCAL = Id.from(0x05);
-    /**
-     * <p>The initial remote bidirectional stream max data.</p>
-     * <p>A local peer sends this parameter to inform the remote peer about
-     * the max data the local peer is willing to receive on remote bidirectional streams.</p>
-     */
-    public static final Id<Long> INITIAL_MAX_STREAM_DATA_BIDIRECTIONAL_REMOTE = Id.from(0x06);
-    /**
-     * <p>The initial unidirectional stream max data.</p>
-     * <p>A local peer sends this parameter to inform the remote peer about
-     * the max data the local peer is willing to receive on unidirectional streams.</p>
-     */
-    public static final Id<Long> INITIAL_MAX_STREAM_DATA_UNIDIRECTIONAL = Id.from(0x07);
-    /**
-     * <p>The initial bidirectional max streams.</p>
-     * <p>A local peer sends this parameter to inform the remote peer about
-     * the max number of streams the local peer is willing to receive.</p>
-     */
-    public static final Id<Long> INITIAL_MAX_STREAMS_BIDIRECTIONAL = Id.from(0x08);
-    /**
-     * <p>The initial unidirectional max streams.</p>
-     * <p>A local peer sends this parameter to inform the remote peer about
-     * the max number of streams the local peer is willing to receive.</p>
-     */
-    public static final Id<Long> INITIAL_MAX_STREAMS_UNIDIRECTIONAL = Id.from(0x09);
-
     private final Map<Id<?>, Object> parameters;
 
     public TransportParameters()
@@ -107,11 +64,10 @@ public class TransportParameters implements Iterable<Map.Entry<TransportParamete
 
     public void putGreaseParameter()
     {
-        ThreadLocalRandom random = ThreadLocalRandom.current();
-        long id = 31 * random.nextLong(MAX_N + 1) + 27;
-        byte[] bytes = new byte[8];
-        random.nextBytes(bytes);
-        put(Id.from(id), bytes);
+        long id = Ids.newGrease();
+        byte[] bytes = new byte[16];
+        ThreadLocalRandom.current().nextBytes(bytes);
+        put(Ids.create(id, BytesId::new), bytes);
     }
 
     public int size()
@@ -126,24 +82,121 @@ public class TransportParameters implements Iterable<Map.Entry<TransportParamete
     }
 
     /**
-     * <p>The parameter ID with the type of the value.</p>
+     * <p>The collection of known QUIC transport parameter IDs.</p>
+     */
+    public static class Ids
+    {
+        // The max N that can produce a grease parameter id that fits a VarLenInt.
+        private static final long MAX_N = 148764065110560899L;
+        private static final Map<Long, Id<?>> ids = new HashMap<>();
+
+        /**
+         * <p>The session max idle timeout in milliseconds.</p>
+         */
+        public static final Id<Long> MAX_IDLE_TIMEOUT = Ids.create(0x01, LongId::new);
+        /**
+         * <p>The initial session max data.</p>
+         * <p>A local peer sends this parameter to inform the remote peer about
+         * the max data the local peer is willing to receive on the session.</p>
+         */
+        public static final Id<Long> INITIAL_MAX_DATA = Ids.create(0x04, LongId::new);
+        /**
+         * <p>The initial local bidirectional stream max data.</p>
+         * <p>A local peer sends this parameter to inform the remote peer about
+         * the max data the local peer is willing to receive on local bidirectional streams.</p>
+         */
+        public static final Id<Long> INITIAL_MAX_STREAM_DATA_BIDIRECTIONAL_LOCAL = Ids.create(0x05, LongId::new);
+        /**
+         * <p>The initial remote bidirectional stream max data.</p>
+         * <p>A local peer sends this parameter to inform the remote peer about
+         * the max data the local peer is willing to receive on remote bidirectional streams.</p>
+         */
+        public static final Id<Long> INITIAL_MAX_STREAM_DATA_BIDIRECTIONAL_REMOTE = Ids.create(0x06, LongId::new);
+        /**
+         * <p>The initial unidirectional stream max data.</p>
+         * <p>A local peer sends this parameter to inform the remote peer about
+         * the max data the local peer is willing to receive on unidirectional streams.</p>
+         */
+        public static final Id<Long> INITIAL_MAX_STREAM_DATA_UNIDIRECTIONAL = Ids.create(0x07, LongId::new);
+        /**
+         * <p>The initial bidirectional max streams.</p>
+         * <p>A local peer sends this parameter to inform the remote peer about
+         * the max number of streams the local peer is willing to receive.</p>
+         */
+        public static final Id<Long> INITIAL_MAX_STREAMS_BIDIRECTIONAL = Ids.create(0x08, LongId::new);
+        /**
+         * <p>The initial unidirectional max streams.</p>
+         * <p>A local peer sends this parameter to inform the remote peer about
+         * the max number of streams the local peer is willing to receive.</p>
+         */
+        public static final Id<Long> INITIAL_MAX_STREAMS_UNIDIRECTIONAL = Ids.create(0x09, LongId::new);
+
+        /**
+         * <p>Creates a new {@link TransportParameters.Id} with the given id and type.</p>
+         *
+         * @param id the transport parameter ID
+         * @param creator the function that creates the specific {@link TransportParameters.Id}
+         * @return a new {@link TransportParameters.Id}
+         * @param <R> the type of the {@link TransportParameters.Id}
+         * @see #get(long)
+         */
+        @SuppressWarnings("unchecked")
+        public static <R> Id<R> create(long id, LongFunction<Id<R>> creator)
+        {
+            return (Id<R>)ids.computeIfAbsent(id, creator::apply);
+        }
+
+        /**
+         * <p>Returns a {@link TransportParameters.Id} with the given id.</p>
+         *
+         * @param id the transport parameter ID
+         * @return the {@link TransportParameters.Id} with the given id, or {@code null}
+         * @param <R> the type of the {@link TransportParameters.Id}
+         */
+        @SuppressWarnings("unchecked")
+        public static <R> Id<R> get(long id)
+        {
+            return (Id<R>)ids.get(id);
+        }
+
+        /**
+         * @return a new grease transport parameter ID
+         * @see #isGrease(long)
+         */
+        public static long newGrease()
+        {
+            return 31 * ThreadLocalRandom.current().nextLong(MAX_N + 1) + 27;
+        }
+
+        /**
+         * <p>Returns whether a transport parameter ID is a <em>grease</em> one, as defined by
+         * <a href="https://datatracker.ietf.org/doc/html/rfc9000#name-reserved-transport-paramete">
+         *     RFC 9000, section 18.1
+     *     </a>.</p>
+         *
+         * @param id the transport parameter ID to test
+         * @return whether the given transport parameter ID is a grease one
+         * @see #newGrease()
+         */
+        public static boolean isGrease(long id)
+        {
+            return (id - 27) % 31 == 0;
+        }
+    }
+
+    /**
+     * <p>The parameter ID, with the type of the value.</p>
      * <p>For most parameter IDs, the value type is {@code long},
      * but for other parameters, such as tokens or connection IDs,
      * the value type is a {@code byte[]}.</p>
      *
      * @param <T> the type of the value
      */
-    public static class Id<T>
+    public abstract static class Id<T>
     {
-        @SuppressWarnings("unchecked")
-        public static <R> Id<R> from(long id)
-        {
-            return (Id<R>)Ids.ids.computeIfAbsent(id, Id::new);
-        }
-
         private final long id;
 
-        private Id(long id)
+        protected Id(long id)
         {
             this.id = id;
         }
@@ -175,9 +228,21 @@ public class TransportParameters implements Iterable<Map.Entry<TransportParamete
             return "%s[%d]".formatted(TypeUtil.toShortName(getClass()), id);
         }
 
-        private static class Ids
+    }
+
+    public static class LongId extends Id<Long>
+    {
+        public LongId(long id)
         {
-            private static final Map<Long, Id<?>> ids = new HashMap<>();
+            super(id);
+        }
+    }
+
+    public static class BytesId extends Id<byte[]>
+    {
+        public BytesId(long id)
+        {
+            super(id);
         }
     }
 }
