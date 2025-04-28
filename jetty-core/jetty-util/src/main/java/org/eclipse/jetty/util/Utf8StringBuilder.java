247d21619c34756f18fca8751969d60db811f10e
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2025-04-28 14:24:55 -0500

    Checkstyle fix

--- jetty-core/jetty-util/src/main/java/org/eclipse/jetty/util/Utf8StringBuilder.java
+++ jetty-core/jetty-util/src/main/java/org/eclipse/jetty/util/Utf8StringBuilder.java
@@ -409,7 +409,7 @@ public class Utf8StringBuilder implements CharsetStringBuilder
     public String build(boolean allowPartialString) throws CharacterCodingException
     {
         complete();
-        return takePartialString(allowPartialString ? () -> null: Utf8IllegalArgumentException::new);
+        return takePartialString(allowPartialString ? () -> null : Utf8IllegalArgumentException::new);
     }
 
     /**
