028cdd49304c8b583716b267413aa046c7dc3065
Olivier Lamy <olamy@apache.org>
2025-04-30 11:14:25 +1000

    HugeResourceTest need to be marked as @Isolated
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- jetty-ee11/jetty-ee11-webapp/src/test/java/org/eclipse/jetty/ee11/webapp/HugeResourceTest.java
+++ jetty-ee11/jetty-ee11-webapp/src/test/java/org/eclipse/jetty/ee11/webapp/HugeResourceTest.java
@@ -68,6 +68,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.parallel.Isolated;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -78,6 +79,7 @@ import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Isolated
 public class HugeResourceTest
 {
     private static final long KB = 1024;
