37afaa8a6fe073c066008c0b97110483822ec930
Ludovic Orban <lorban@bitronix.be>
2025-05-12 14:17:37 +0200

    Case 1
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/internal/HttpConnection.java
+++ jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/internal/HttpConnection.java
@@ -1092,7 +1092,8 @@ public class HttpConnection extends AbstractMetaDataConnection implements Runnab
                     stream._chunk = Content.Chunk.from(bad);
                 }
 
-                ThreadPool.executeImmediately(getServer().getThreadPool(), _httpChannel.onFailure(bad));
+//              Case 1
+//                ThreadPool.executeImmediately(getServer().getThreadPool(), _httpChannel.onFailure(bad));
             }
         }
     }
