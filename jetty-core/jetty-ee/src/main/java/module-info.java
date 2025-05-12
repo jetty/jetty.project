4e8295cf847e214ce34a9c8d3446cb211123450f
gregw <gregw@webtide.com>
2025-05-12 13:29:05 +1000

    Remove EE10/11 examples

--- jetty-core/jetty-ee/src/main/java/module-info.java
+++ jetty-core/jetty-ee/src/main/java/module-info.java
@@ -18,7 +18,6 @@ module org.eclipse.jetty.ee
     requires transitive org.eclipse.jetty.util;
     requires transitive org.eclipse.jetty.server;
     requires java.instrument;
-    requires org.objectweb.asm;
 
     exports org.eclipse.jetty.ee;
 }
