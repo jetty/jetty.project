edd57efc3a5e228d5cd9cce1ee38b18da7178aed
Ludovic Orban <lorban@bitronix.be>
2025-05-19 14:00:56 +0200

    add comment
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/internal/HttpConnection.java
+++ jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/internal/HttpConnection.java
@@ -529,12 +529,13 @@ public class HttpConnection extends AbstractMetaDataConnection implements Runnab
             int filled = fillRequestBuffer();
             if (filled <= 0)
             {
-                // TODO comment and write test for this change
+                // If fillRequestBuffer() read -1, it may be because of an early EOF;
+                // force the parser check its state again so its handler can generate
+                // an error chunk if appropriate. Doing this saves returning a null
+                // chunk followed by an immediately served demand where the next read()
+                // actually makes the parser generate the error chunk.
                 if (filled < 0)
-                {
-                    _parser.atEOF();
                     _parser.parseNext(BufferUtil.EMPTY_BUFFER);
-                }
                 releaseRequestBuffer();
                 break;
             }
