6baeafb23b8fe55f8838787b4ef88c99e0d879ee
Jan Bartel <janb@webtide.com>
2025-06-25 12:04:54 +1000

    Issue #13269 Fix differences between ee10 and ee11

--- jetty-ee11/jetty-ee11-plus/src/test/java/org/eclipse/jetty/ee11/plus/webapp/PlusDescriptorProcessorTest.java
+++ jetty-ee11/jetty-ee11-plus/src/test/java/org/eclipse/jetty/ee11/plus/webapp/PlusDescriptorProcessorTest.java
@@ -19,11 +19,11 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.Name;
 
+import org.eclipse.jetty.ee.webapp.WebAppClassLoader;
 import org.eclipse.jetty.ee11.webapp.Configuration;
 import org.eclipse.jetty.ee11.webapp.Descriptor;
 import org.eclipse.jetty.ee11.webapp.FragmentDescriptor;
 import org.eclipse.jetty.ee11.webapp.Origin;
-import org.eclipse.jetty.ee11.webapp.WebAppClassLoader;
 import org.eclipse.jetty.ee11.webapp.WebAppContext;
 import org.eclipse.jetty.ee11.webapp.WebDescriptor;
 import org.eclipse.jetty.plus.annotation.Injection;
