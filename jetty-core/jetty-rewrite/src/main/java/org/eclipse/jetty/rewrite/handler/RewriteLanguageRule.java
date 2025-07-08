9a7e912186d52df6ec39064bfd639559d4171892
Greg Wilkins <gregw@webtide.com>
2025-07-08 11:41:10 +1000

    Implement accept-language rewrite rule
    Fix #13330 by implementing an accept-language rewrite rule.

---
+++ jetty-core/jetty-rewrite/src/main/java/org/eclipse/jetty/rewrite/handler/RewriteLanguageRule.java
@@ -0,0 +1,258 @@
+//
+// ========================================================================
+// Copyright (c) 1995 Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
+
+package org.eclipse.jetty.rewrite.handler;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiFunction;
+
+import org.eclipse.jetty.http.HttpField;
+import org.eclipse.jetty.http.HttpFields;
+import org.eclipse.jetty.http.HttpHeader;
+import org.eclipse.jetty.http.HttpURI;
+import org.eclipse.jetty.http.PreEncodedHttpField;
+import org.eclipse.jetty.server.Context;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+import org.eclipse.jetty.util.Callback;
+import org.eclipse.jetty.util.TypeUtil;
+import org.eclipse.jetty.util.URIUtil;
+import org.eclipse.jetty.util.resource.Resource;
+
+/**
+ * Rule that can rewrite a request based on the {@code Accept-Language} header.
+ */
+public class RewriteLanguageRule extends Rule
+{
+    public static String localizeWithSuffix(String pathInContext, String language)
+    {
+        if (pathInContext.isEmpty())
+            return "/" + language + "/";
+        if (pathInContext.endsWith("/"))
+            return pathInContext + language + "/";
+        return pathInContext + "." + language;
+    }
+
+    public static String localizeWithPrefix(String pathInContext, String language)
+    {
+        return "/" + language + pathInContext;
+    }
+
+    public static Resource getResource(Context context, String pathInContext)
+    {
+        return context.getBaseResource().resolve(pathInContext);
+    }
+
+    private static final HttpField VARY_ACCEPT_LANGUAGE = new PreEncodedHttpField(HttpHeader.VARY, HttpHeader.ACCEPT_LANGUAGE.asString());
+    private final BiFunction<String, String, String> _localizePath;
+    private final BiFunction<Context, String, Resource> _getResource;
+    private final List<String> _wildCardLanguages;
+
+    protected RewriteLanguageRule()
+    {
+        this(true);
+    }
+
+    protected RewriteLanguageRule(List<String> wildCardLanguages)
+    {
+        this(RewriteLanguageRule::localizeWithPrefix, RewriteLanguageRule::getResource, wildCardLanguages);
+    }
+
+    protected RewriteLanguageRule(boolean prefix)
+    {
+        this(prefix ? RewriteLanguageRule::localizeWithPrefix : RewriteLanguageRule::localizeWithSuffix, RewriteLanguageRule::getResource, null);
+    }
+
+    protected RewriteLanguageRule(BiFunction<String, String, String> localizePath, BiFunction<Context, String, Resource> getResource)
+    {
+        this(localizePath, getResource, null);
+    }
+
+    protected RewriteLanguageRule(BiFunction<String, String, String> localizePath, BiFunction<Context, String, Resource> getResource, List<String> wildCardLanguages)
+    {
+        _localizePath = localizePath;
+        _getResource = getResource;
+        _wildCardLanguages = wildCardLanguages == null ? List.of() : Collections.unmodifiableList(wildCardLanguages);
+    }
+
+    @Override
+    public Handler matchAndApply(Handler input) throws IOException
+    {
+        List<String> languages = input.getHeaders().getQualityCSV(HttpHeader.ACCEPT_LANGUAGE);
+        if (languages != null && !languages.isEmpty())
+        {
+            for (String language : languages)
+            {
+                if (language == null)
+                    continue;
+                if ("*".equals(language))
+                {
+                    for (String wildCardLanguage : _wildCardLanguages)
+                    {
+                        String pathInContext = Request.getPathInContext(input);
+                        String languagePathInContext = _localizePath.apply(pathInContext, wildCardLanguage);
+                        Resource resource = _getResource.apply(input.getContext(), languagePathInContext);
+                        if (resource.exists())
+                            return newLanguageHandler(input, languagePathInContext, wildCardLanguage);
+                    }
+                }
+                else
+                {
+                    String pathInContext = Request.getPathInContext(input);
+                    String languagePathInContext = _localizePath.apply(pathInContext, language);
+                    Resource resource = _getResource.apply(input.getContext(), languagePathInContext);
+                    if (resource.exists())
+                        return newLanguageHandler(input, languagePathInContext, language);
+                }
+            }
+        }
+
+        return new VaryHandler(input);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "%s@%x".formatted(TypeUtil.toShortName(this.getClass()), hashCode());
+    }
+
+    protected List<String> getWildCardLanguages()
+    {
+        return _wildCardLanguages;
+    }
+
+    protected Handler newLanguageHandler(Handler input, String languagePathInContext, String language)
+    {
+        return new LanguageHandler(input, languagePathInContext, language);
+    }
+
+    private void ensureVaryAcceptLanguages(Response response)
+    {
+        response.getHeaders().computeField(HttpHeader.VARY, (h, l) ->
+        {
+            if (l == null || l.isEmpty())
+                return VARY_ACCEPT_LANGUAGE;
+
+            boolean acceptLanguage = false;
+            StringBuilder vary = new StringBuilder();
+            loop:
+            for (HttpField field : l)
+            {
+                for (String value : field.getValues())
+                {
+                    if (HttpHeader.ACCEPT_LANGUAGE.asString().equalsIgnoreCase(value))
+                    {
+                        acceptLanguage = true;
+                        break loop;
+                    }
+                    if (!vary.isEmpty())
+                        vary.append(", ");
+                    vary.append(value);
+                }
+            }
+
+            if (!acceptLanguage)
+                vary.append(", ").append(HttpHeader.ACCEPT_LANGUAGE.asString());
+
+            return new HttpField(HttpHeader.VARY, vary.toString());
+        });
+    }
+
+    protected class VaryHandler extends Handler
+    {
+        public VaryHandler(Rule.Handler input)
+        {
+            super(input);
+        }
+
+        @Override
+        protected boolean handle(Response response, Callback callback) throws Exception
+        {
+            // Add the vary header
+            ensureVaryAcceptLanguages(response);
+            return super.handle(response, callback);
+        }
+    }
+
+    protected class LanguageHandler extends Handler
+    {
+        private static final HttpField VARY_ACCEPT_LANGUAGE = new PreEncodedHttpField(HttpHeader.VARY, HttpHeader.ACCEPT_LANGUAGE.asString());
+        private final String _language;
+        private final HttpURI _languageURI;
+        private final HttpField _languageField;
+
+        public LanguageHandler(Rule.Handler input, String languagePathInContext, String language)
+        {
+            super(input);
+            _language = language;
+            _languageURI = HttpURI.build(input.getHttpURI()).path(URIUtil.addPaths(input.getContext().getContextPath(), languagePathInContext)).asImmutable();
+            _languageField = new HttpField(HttpHeader.CONTENT_LANGUAGE, language);
+        }
+
+        @Override
+        public HttpURI getHttpURI()
+        {
+            return _languageURI;
+        }
+
+        @Override
+        protected boolean handle(Response response, Callback callback) throws Exception
+        {
+            // Add the language field if not already set
+            response.getHeaders().computeField(HttpHeader.CONTENT_LANGUAGE, (h, l) ->
+            {
+                if (l == null || l.isEmpty())
+                    return _languageField;
+                return l.get(0);
+            });
+
+            // Add the vary header
+            ensureVaryAcceptLanguages(response);
+
+            // intercept etags
+            final HttpFields.Mutable responseFields = new HttpFields.Mutable.Wrapper(response.getHeaders())
+            {
+                @Override
+                public HttpField onAddField(HttpField field)
+                {
+                    if (field.getHeader() == HttpHeader.ETAG)
+                    {
+                        String etag = field.getValue();
+                        if (etag.endsWith("\""))
+                            return new HttpField(HttpHeader.ETAG, etag.substring(0, etag.length() - 1) + '-' + _language + "\"");
+                    }
+
+                    return field;
+                }
+
+                @Override
+                public HttpField onReplaceField(HttpField oldField, HttpField newField)
+                {
+                    return onAddField(newField);
+                }
+            };
+
+            Response wrappedResponse = new Response.Wrapper(response.getRequest(), response)
+            {
+                @Override
+                public HttpFields.Mutable getHeaders()
+                {
+                    return responseFields;
+                }
+            };
+            return super.handle(wrappedResponse, callback);
+        }
+    }
+}
---
+++ jetty-core/jetty-rewrite/src/test/java/org/eclipse/jetty/rewrite/handler/RewriteLanguageRuleTest.java
@@ -0,0 +1,311 @@
+//
+// ========================================================================
+// Copyright (c) 1995 Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
+
+package org.eclipse.jetty.rewrite.handler;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.eclipse.jetty.http.HttpHeader;
+import org.eclipse.jetty.http.HttpTester;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.toolchain.test.jupiter.WorkDir;
+import org.eclipse.jetty.toolchain.test.jupiter.WorkDirExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(WorkDirExtension.class)
+public class RewriteLanguageRuleTest extends AbstractRuleTest
+{
+    public WorkDir _workDir;
+    protected ContextHandler _contextHandler;
+    Path _docRoot;
+
+    public static Stream<String> languages()
+    {
+        return Stream.of(
+            "en",
+            "en-UK",
+            "en-AU",
+            "fr");
+    }
+
+    @BeforeEach
+    public void beforeEach() throws Exception
+    {
+        _docRoot = _workDir.getEmptyPathDir();
+        Files.writeString(Files.createFile(_docRoot.resolve("all.html")), "all", StandardCharsets.UTF_8);
+        Files.writeString(Files.createFile(_docRoot.resolve("only.html")), "only", StandardCharsets.UTF_8);
+        for (String language : languages().toArray(String[]::new))
+        {
+            Path languageDir = _docRoot.resolve(language);
+            Files.createDirectory(languageDir);
+            Files.writeString(Files.createFile(languageDir.resolve("all.html")), language, StandardCharsets.UTF_8);
+        }
+        Files.writeString(_docRoot.resolve("en-AU/en-AU-only.html"), "en-AU-only", StandardCharsets.UTF_8);
+
+        _contextHandler = new ContextHandler();
+        _contextHandler.setBaseResourceAsPath(_docRoot);
+        _contextHandler.setContextPath("/ctx");
+        _server.setHandler(_contextHandler);
+        _rewriteHandler.addRule(new RewriteLanguageRule(List.of("xx", "en-AU", "en-UK")));
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setEtags(true);
+        start(resourceHandler);
+    }
+
+    @Test
+    public void testNoLanguage() throws Exception
+    {
+        HttpTester.Response response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/unknown.html HTTP/1.0\r
+            \r
+            """));
+
+        assertEquals(404, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/only.html HTTP/1.0\r
+            \r
+            """));
+
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("only", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), nullValue());
+        assertThat(response.get(HttpHeader.ETAG), not(nullValue()));
+        assertThat(response.get(HttpHeader.ETAG), not(containsString("-")));
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/all.html HTTP/1.0\r
+            \r
+            """));
+
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("all", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), nullValue());
+        assertThat(response.get(HttpHeader.ETAG), not(nullValue()));
+        assertThat(response.get(HttpHeader.ETAG), not(containsString("-")));
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/en-AU-only.html HTTP/1.0\r
+            \r
+            """));
+
+        assertEquals(404, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+    }
+
+    @Test
+    public void testOneLanguage() throws Exception
+    {
+        HttpTester.Response response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/unknown.html HTTP/1.0\r
+            Accept-Language: en-AU\r
+            \r
+            """));
+
+        assertEquals(404, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/only.html HTTP/1.0\r
+            Accept-Language: en-AU\r
+            \r
+            """));
+
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("only", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), nullValue());
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/all.html HTTP/1.0\r
+            Accept-Language: en-AU\r
+            \r
+            """));
+
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("en-AU", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), is("en-AU"));
+        assertThat(response.get(HttpHeader.ETAG), containsString("-en-AU\""));
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/en-AU-only.html HTTP/1.0\r
+            Accept-Language: en-AU\r
+            \r
+            """));
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("en-AU-only", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), is("en-AU"));
+        assertThat(response.get(HttpHeader.ETAG), containsString("-en-AU\""));
+    }
+
+    @Test
+    public void testQualityLanguage() throws Exception
+    {
+        HttpTester.Response response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/unknown.html HTTP/1.0\r
+            Accept-Language: fr,en-AU;q=0.5\r
+            \r
+            """));
+
+        assertEquals(404, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/only.html HTTP/1.0\r
+            Accept-Language: fr,en-AU;q=0.5
+            \r
+            """));
+
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("only", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), nullValue());
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/all.html HTTP/1.0\r
+            Accept-Language: fr,en-AU;q=0.5
+            \r
+            """));
+
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("fr", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), is("fr"));
+        assertThat(response.get(HttpHeader.ETAG), containsString("-fr\""));
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/en-AU-only.html HTTP/1.0\r
+            Accept-Language: fr,en-AU;q=0.5\r
+            \r
+            """));
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("en-AU-only", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), is("en-AU"));
+        assertThat(response.get(HttpHeader.ETAG), containsString("-en-AU\""));
+    }
+
+    @Test
+    public void testUnknownQualityLanguage() throws Exception
+    {
+        HttpTester.Response response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/unknown.html HTTP/1.0\r
+            Accept-Language: xx,fr,en-AU;q=0.5\r
+            \r
+            """));
+
+        assertEquals(404, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/only.html HTTP/1.0\r
+            Accept-Language: xx,fr,en-AU;q=0.5
+            \r
+            """));
+
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("only", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), nullValue());
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/all.html HTTP/1.0\r
+            Accept-Language: xx,fr,en-AU;q=0.5
+            \r
+            """));
+
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("fr", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), is("fr"));
+        assertThat(response.get(HttpHeader.ETAG), containsString("-fr\""));
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/en-AU-only.html HTTP/1.0\r
+            Accept-Language: xx,fr,en-AU;q=0.5\r
+            \r
+            """));
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("en-AU-only", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), is("en-AU"));
+        assertThat(response.get(HttpHeader.ETAG), containsString("-en-AU\""));
+    }
+
+    @Test
+    public void testWildLanguage() throws Exception
+    {
+        HttpTester.Response response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/unknown.html HTTP/1.0\r
+            Accept-Language: fr,*;q=0.5\r
+            \r
+            """));
+
+        assertEquals(404, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/only.html HTTP/1.0\r
+            Accept-Language: fr,*;q=0.5\r
+            \r
+            """));
+
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("only", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), nullValue());
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/all.html HTTP/1.0\r
+            Accept-Language: fr,*;q=0.5\r
+            \r
+            """));
+
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("fr", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), is("fr"));
+        assertThat(response.get(HttpHeader.ETAG), containsString("-fr\""));
+
+        response = HttpTester.parseResponse(_connector.getResponse("""
+            GET /ctx/en-AU-only.html HTTP/1.0\r
+            Accept-Language: fr,*;q=0.5\r
+            \r
+            """));
+        assertEquals(200, response.getStatus());
+        assertThat(response.get(HttpHeader.VARY), containsString("Accept-Language"));
+        assertEquals("en-AU-only", response.getContent());
+        assertThat(response.get(HttpHeader.CONTENT_LANGUAGE), is("en-AU"));
+        assertThat(response.get(HttpHeader.ETAG), containsString("-en-AU\""));
+    }
+}
