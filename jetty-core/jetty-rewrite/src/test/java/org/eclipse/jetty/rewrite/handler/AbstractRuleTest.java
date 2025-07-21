241afbd37aa998d34ab27a90a3e378c00569ace1
Greg Wilkins <gregw@webtide.com>
2025-07-22 08:26:38 +1000

    Implement accept-language and accept-encoding rewrite rules (#13332)
    Fix #13330 by implementing an accept-language rewrite rule.
    * Implement accept-language rewrite rule.
    * Added RewriteEncodingRule.java
    * Do not strip embedded quotes

--- jetty-core/jetty-rewrite/src/test/java/org/eclipse/jetty/rewrite/handler/AbstractRuleTest.java
+++ jetty-core/jetty-rewrite/src/test/java/org/eclipse/jetty/rewrite/handler/AbstractRuleTest.java
@@ -27,17 +27,23 @@ public abstract class AbstractRuleTest
     protected HttpConfiguration _httpConfig = new HttpConfiguration();
     protected LocalConnector _connector = new LocalConnector(_server, new HttpConnectionFactory(_httpConfig));
     protected RewriteHandler _rewriteHandler = new RewriteHandler();
+    Handler.Singleton _next;
 
     @AfterEach
     public void stop() throws Exception
     {
         LifeCycle.stop(_server);
+        if (_next != null)
+            _next.setHandler((Handler)null);
     }
 
     protected void start(Handler handler) throws Exception
     {
         _server.addConnector(_connector);
-        _server.setHandler(_rewriteHandler);
+        _next = _server;
+        while (_next.getHandlers() != null && _next.getHandlers().size() == 1 && _next.getHandlers().get(0) instanceof Handler.Singleton singleton)
+            _next = singleton;
+        _next.setHandler(_rewriteHandler);
         _rewriteHandler.setHandler(handler);
         _server.start();
     }
