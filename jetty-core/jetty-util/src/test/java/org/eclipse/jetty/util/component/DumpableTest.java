9d0157969c13b56265fe655701ee487486b1223d
Greg Wilkins <gregw@webtide.com>
2025-08-06 11:21:22 +1000

    Dump invocationType
    added test

--- jetty-core/jetty-util/src/test/java/org/eclipse/jetty/util/component/DumpableTest.java
+++ jetty-core/jetty-util/src/test/java/org/eclipse/jetty/util/component/DumpableTest.java
@@ -16,13 +16,16 @@ package org.eclipse.jetty.util.component;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import org.eclipse.jetty.util.thread.Invocable;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DumpableTest
 {
@@ -81,4 +84,66 @@ public class DumpableTest
         a.dump(new PrintStream(baos), "");
         assertThat(baos.toString(), Matchers.startsWith(EXPECTED));
     }
+
+    @Test
+    public void testDumpState() throws Exception
+    {
+        Invocable invocable0 = Invocable.from(Invocable.InvocationType.EITHER, Invocable.NOOP);
+        Invocable invocable1 = Invocable.from(Invocable.InvocationType.NON_BLOCKING, Invocable.NOOP);
+        Invocable invocable2 = Invocable.from(Invocable.InvocationType.BLOCKING, Invocable.NOOP);
+
+        LifeCycle lifeCycle0 = new AbstractLifeCycle() {};
+        LifeCycle lifeCycle1 = new AbstractLifeCycle() {};
+        LifeCycle lifeCycle2 = new AbstractLifeCycle()
+        {
+            @Override
+            protected void doStart() throws Exception
+            {
+                throw new IllegalStateException("start");
+            }
+        };
+        lifeCycle1.start();
+        assertThrows(IllegalStateException.class, lifeCycle2::start);
+
+        InvocableContainer container0 = new InvocableContainer();
+        container0.start();
+        container0.addBean(invocable0);
+        container0.addBean(lifeCycle0);
+        container0.addBean(lifeCycle1);
+        container0.addBean(lifeCycle2);
+
+        InvocableContainer container1 = new InvocableContainer();
+        container1.addBean(invocable1);
+        container1.addBean(invocable2);
+        container0.addBean(container1);
+
+        List<String> dump = Arrays.asList(container0.dump().split("\n"));
+        dump = dump.stream()
+            .filter(s -> !s.startsWith("UTC:"))
+            .filter(s -> !s.startsWith("JVM:"))
+            .map(s -> s.replaceAll("@[a-z0-9]*", "@xxx"))
+            .map(s -> s.replaceAll("0x[a-z0-9]*", "0x"))
+            .toList();
+        assertThat(dump, Matchers.contains(
+            "oejuc.DumpableTest$InvocableContainer@xxx{STARTED} ~ BLOCKING - STARTED",
+            "+- oejut.Invocable$ReadyTask@xxx[EITHER][org.eclipse.jetty.util.thread.Invocable$$Lambda/0x@xxx] ~ EITHER",
+            "+~ oejuc.DumpableTest$@xxx{STOPPED} - STOPPED",
+            "+~ oejuc.DumpableTest$@xxx{STARTED} - STARTED",
+            "+~ oejuc.DumpableTest$@xxx{FAILED} - FAILED",
+            "+~ oejuc.DumpableTest$InvocableContainer@xxx{STOPPED} ~ BLOCKING - STOPPED",
+            "   +- oejut.Invocable$ReadyTask@xxx[NON_BLOCKING][org.eclipse.jetty.util.thread.Invocable$$Lambda/0x@xxx] ~ NON_BLOCKING",
+            "   +- oejut.Invocable$ReadyTask@xxx[BLOCKING][org.eclipse.jetty.util.thread.Invocable$$Lambda/0x@xxx] ~ BLOCKING",
+            "legend: +- bean, += managed, +~ unmanaged, +? auto, +: iterable, +] array, +} map, +> pojo; @xxx visited"
+        ));
+    }
+
+    private static class InvocableContainer extends ContainerLifeCycle implements Invocable
+    {
+        @Override
+        public InvocationType getInvocationType()
+        {
+            return Invocable.combineTypes(getBeans().stream().map(Invocable::getInvocationType).toArray(InvocationType[]::new));
+        }
+    }
+
 }
