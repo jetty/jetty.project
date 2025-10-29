336eeac8a3134d6abc9bad6282cb663707c54b16
Simone Bordet <simone.bordet@gmail.com>
2025-10-29 16:34:57 +0100

    Fixes #13915 - You can not have both Frame and Text handler in 12.1.x Jetty websocket. (#13917)
    * Updated migration guide.
    * Updated annotation javadocs.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-core/jetty-websocket/jetty-websocket-jetty-api/src/main/java/org/eclipse/jetty/websocket/api/annotations/OnWebSocketMessage.java
+++ jetty-core/jetty-websocket/jetty-websocket-jetty-api/src/main/java/org/eclipse/jetty/websocket/api/annotations/OnWebSocketMessage.java
@@ -23,14 +23,12 @@ import java.lang.annotation.Target;
 
 /**
  * <p>Annotation for methods to receive BINARY or TEXT WebSocket events.</p>
- * <p>Acceptable method patterns:</p>
- * <u>Text Message Versions</u>
+ * <p>Acceptable method signatures for text messages:</p>
  * <ol>
- * <li>{@code public void methodName(String text)}</li>
- * <li>{@code public void methodName(Session session, String text)}</li>
- * <li>{@code public void methodName(Reader reader)}</li>
- * <li>{@code public void methodName(Session session, Reader reader)}</li>
+ * <li>{@code public void methodName(Session session, *String text)}</li>
+ * <li>{@code public void methodName(Session session, *Reader reader)}</li>
  * </ol>
+ * <p>The {@code *} before the parameter type means that the parameter is mandatory.</p>
  * <p>NOTE</p>
  * <p>Method that takes a {@link Reader} must have
  * {@link WebSocket#autoDemand()} set to {@code true}.</p>
@@ -38,27 +36,25 @@ import java.lang.annotation.Target;
  * <p>The {@link Reader} argument will always use the UTF-8 charset,
  * (as dictated by RFC 6455). If you need to use a different charset,
  * you must use BINARY messages.</p>
- * <u>Binary Message Versions</u>
+ * <p>Acceptable method signatures for binary messages:</p>
  * <ol>
- * <li>{@code public void methodName(ByteBuffer message, Callback callback)}</li>
- * <li>{@code public void methodName(Session session, ByteBuffer message, Callback callback)}</li>
- * <li>{@code public void methodName(InputStream stream)}</li>
- * <li>{@code public void methodName(Session session, InputStream stream)}</li>
+ * <li>{@code public void methodName(Session session, *ByteBuffer message, *Callback callback)}</li>
+ * <li>{@code public void methodName(Session session, *InputStream stream)}</li>
  * </ol>
+ * <p>The {@code *} before the parameter type means that the parameter is mandatory.</p>
  * <p>NOTE</p>
  * <p>Method that takes a {@link InputStream} must have
  * {@link WebSocket#autoDemand()} set to {@code true}.</p>
- * <u>Partial Message Variations</u>
- * <p>These are used to receive individual frames (and therefore partial
+ * <p>Partial messages are used to receive individual frames (and therefore partial
  * messages) without aggregating the frames into a complete WebSocket message.
  * A {@code boolean} parameter is supplied to indicate whether the frame is
  * the last segment of data of the message.</p>
+ * <p>Acceptable method signatures for partial messages:</p>
  * <ol>
- * <li>{@code public void methodName(ByteBuffer payload, boolean last, Callback callback)}</li>
- * <li>{@code public void methodName(Session session, ByteBuffer payload, boolean last, Callback callback)}</li>
- * <li>{@code public void methodName(String payload, boolean last)}</li>
- * <li>{@code public void methodName(Session session, String payload, boolean last)}</li>
+ * <li>{@code public void methodName(Session session, *String payload, *boolean last)}</li>
+ * <li>{@code public void methodName(Session session, *ByteBuffer payload, *boolean last, *Callback callback)}</li>
  * </ol>
+ * <p>The {@code *} before the parameter type means that the parameter is mandatory.</p>
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
