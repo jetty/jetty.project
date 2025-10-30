cdfd78039c4f9262ddb6aeb3c1557349470629b4
Ludovic Orban <lorban@bitronix.be>
2025-10-30 16:28:05 +0100

    #13933 rename test
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-core/jetty-server/src/test/java/org/eclipse/jetty/server/PartialRFC2616Test.java
+++ jetty-core/jetty-server/src/test/java/org/eclipse/jetty/server/PartialRFC2616Test.java
@@ -550,7 +550,7 @@ public class PartialRFC2616Test
     }
 
     @Test
-    public void test824() throws Exception
+    public void test823No100Response() throws Exception
     {
         // Expect 100 not sent
         int offset = 0;
