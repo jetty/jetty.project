e9aa0c6e2f832778a7af31d2d937509840deb785
Ludovic Orban <lorban@bitronix.be>
2025-12-01 15:45:10 +0100

    add h2 test + make useAlpn parameterized
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-core/jetty-http2/jetty-http2-tests/src/test/java/org/eclipse/jetty/http2/tests/BrokenServerNegotiationTest.java
+++ jetty-core/jetty-http2/jetty-http2-tests/src/test/java/org/eclipse/jetty/http2/tests/BrokenServerNegotiationTest.java
@@ -17,8 +17,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
 import javax.net.ssl.SSLHandshakeException;
 
 import org.eclipse.jetty.client.HttpClient;
@@ -26,30 +24,32 @@ import org.eclipse.jetty.client.transport.HttpClientConnectionFactory;
 import org.eclipse.jetty.client.transport.HttpClientTransportDynamic;
 import org.eclipse.jetty.http2.client.HTTP2Client;
 import org.eclipse.jetty.http2.client.transport.ClientConnectionFactoryOverHTTP2;
+import org.eclipse.jetty.http2.client.transport.HttpClientTransportOverHTTP2;
 import org.eclipse.jetty.io.ClientConnectionFactory;
 import org.eclipse.jetty.io.ClientConnector;
 import org.eclipse.jetty.util.component.LifeCycle;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.awaitility.Awaitility.await;
-import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BrokenServerNegotiationTest
 {
-    @Test
-    public void testAlpnNegociationWithBrokenServer() throws Exception
+    private volatile ServerSocket serverSocket;
+
+    public int startServer() throws Exception
     {
-        AtomicReference<ServerSocket> serverSocketRef = new AtomicReference<>();
-        AtomicInteger port = new AtomicInteger();
+        if (serverSocket != null)
+            return serverSocket.getLocalPort();
         Thread server = new Thread(() ->
         {
             try
             {
-                ServerSocket serverSocket = new ServerSocket(0);
-                serverSocketRef.set(serverSocket);
-                port.set(serverSocket.getLocalPort());
+                serverSocket = new ServerSocket(0);
                 while (true)
                 {
                     Socket accept = serverSocket.accept();
@@ -62,20 +62,51 @@ public class BrokenServerNegotiationTest
             }
         });
         server.start();
-        await().atMost(5, TimeUnit.SECONDS).until(port::get, greaterThan(0));
+        await().atMost(5, TimeUnit.SECONDS).until(() -> serverSocket, notNullValue());
+        return serverSocket.getLocalPort();
+    }
 
+    @AfterEach
+    public void stopServer() throws Exception
+    {
+        if (serverSocket != null)
+            serverSocket.close();
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {false, true})
+    public void testAlpnNegotiation(boolean useAlpn) throws Exception
+    {
+        int port = startServer();
         ClientConnector clientConnector = new ClientConnector();
         HTTP2Client h2Client = new HTTP2Client();
+        h2Client.setUseALPN(useAlpn);
         ClientConnectionFactoryOverHTTP2.HTTP2 http2 = new ClientConnectionFactoryOverHTTP2.HTTP2(h2Client);
         ClientConnectionFactory.Info http1 = HttpClientConnectionFactory.HTTP11;
         HttpClientTransportDynamic transportDynamic = new HttpClientTransportDynamic(clientConnector, http1,  http2);
         HttpClient client = new HttpClient(transportDynamic);
         client.start();
 
-        ExecutionException ee = assertThrows(ExecutionException.class, () -> client.GET("https://localhost:" + port.get() + "/hello"));
+        ExecutionException ee = assertThrows(ExecutionException.class, () -> client.GET("https://localhost:" + port + "/hello"));
         assertInstanceOf(SSLHandshakeException.class, ee.getCause());
 
         LifeCycle.stop(client);
-        serverSocketRef.get().close();
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {false, true})
+    public void testH2Only(boolean useAlpn) throws Exception
+    {
+        int port = startServer();
+        HTTP2Client h2Client = new HTTP2Client();
+        h2Client.setUseALPN(useAlpn);
+        HttpClientTransportOverHTTP2 httpClientTransportOverHTTP2 = new HttpClientTransportOverHTTP2(h2Client);
+        HttpClient client = new HttpClient(httpClientTransportOverHTTP2);
+        client.start();
+
+        ExecutionException ee = assertThrows(ExecutionException.class, () -> client.GET("https://localhost:" + port + "/hello"));
+        assertInstanceOf(SSLHandshakeException.class, ee.getCause());
+
+        LifeCycle.stop(client);
     }
 }
