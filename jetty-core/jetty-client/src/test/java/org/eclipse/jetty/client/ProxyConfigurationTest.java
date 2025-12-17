bc0f4e75cc3c030e3d73375d27ac17c52eb332dc
Alexander Farber <farber72@outlook.de>
2025-12-17 17:01:10 +0100

    Add wildcard and CIDR pattern support to ProxyConfiguration include/exclude addresses

--- jetty-core/jetty-client/src/test/java/org/eclipse/jetty/client/ProxyConfigurationTest.java
+++ jetty-core/jetty-client/src/test/java/org/eclipse/jetty/client/ProxyConfigurationTest.java
@@ -75,4 +75,105 @@ public class ProxyConfigurationTest
         assertTrue(proxy.matches(new Origin("http", "[1::2:3:4]", 0)));
         assertFalse(proxy.matches(new Origin("http", "[1::2:3:4]", 5)));
     }
+
+    @Test
+    public void testProxyMatchesWithWildcardPrefix()
+    {
+        HttpProxy proxy = new HttpProxy("host", 0);
+        proxy.getExcludedAddresses().add("*.internal.corp");
+
+        assertTrue(proxy.matches(new Origin("http", "example.com", 80)));
+        assertFalse(proxy.matches(new Origin("http", "api.internal.corp", 80)));
+        assertFalse(proxy.matches(new Origin("http", "db.internal.corp", 443)));
+        // Also matches the domain itself
+        assertFalse(proxy.matches(new Origin("http", "internal.corp", 80)));
+    }
+
+    @Test
+    public void testProxyMatchesWithWildcardSuffix()
+    {
+        HttpProxy proxy = new HttpProxy("host", 0);
+        proxy.getExcludedAddresses().add("localhost.*");
+
+        assertTrue(proxy.matches(new Origin("http", "example.com", 80)));
+        assertFalse(proxy.matches(new Origin("http", "localhost.local", 80)));
+        assertFalse(proxy.matches(new Origin("http", "localhost.corp", 443)));
+    }
+
+    @Test
+    public void testProxyMatchesWithCidr()
+    {
+        HttpProxy proxy = new HttpProxy("host", 0);
+        proxy.getExcludedAddresses().add("192.168.0.0/16");
+
+        assertTrue(proxy.matches(new Origin("http", "10.0.0.1", 80)));
+        assertFalse(proxy.matches(new Origin("http", "192.168.1.1", 80)));
+        assertFalse(proxy.matches(new Origin("http", "192.168.255.255", 443)));
+    }
+
+    @Test
+    public void testProxyMatchesWithIpRange()
+    {
+        HttpProxy proxy = new HttpProxy("host", 0);
+        proxy.getExcludedAddresses().add("10.0.0.1-10.0.0.10");
+
+        assertTrue(proxy.matches(new Origin("http", "10.0.0.11", 80)));
+        assertFalse(proxy.matches(new Origin("http", "10.0.0.1", 80)));
+        assertFalse(proxy.matches(new Origin("http", "10.0.0.5", 80)));
+        assertFalse(proxy.matches(new Origin("http", "10.0.0.10", 80)));
+    }
+
+    @Test
+    public void testProxyMatchesWithWildcardAndPort()
+    {
+        HttpProxy proxy = new HttpProxy("host", 0);
+        proxy.getExcludedAddresses().add("*.internal.corp:8080");
+
+        assertTrue(proxy.matches(new Origin("http", "api.internal.corp", 80)));
+        assertTrue(proxy.matches(new Origin("http", "api.internal.corp", 443)));
+        assertFalse(proxy.matches(new Origin("http", "api.internal.corp", 8080)));
+    }
+
+    @Test
+    public void testProxyMatchesWithMultiplePatterns()
+    {
+        HttpProxy proxy = new HttpProxy("host", 0);
+        proxy.getExcludedAddresses().add("*.internal.corp");
+        proxy.getExcludedAddresses().add("192.168.0.0/16");
+        proxy.getExcludedAddresses().add("localhost");
+
+        assertTrue(proxy.matches(new Origin("http", "example.com", 80)));
+        assertTrue(proxy.matches(new Origin("http", "10.0.0.1", 80)));
+
+        assertFalse(proxy.matches(new Origin("http", "api.internal.corp", 80)));
+        assertFalse(proxy.matches(new Origin("http", "192.168.1.1", 80)));
+        assertFalse(proxy.matches(new Origin("http", "localhost", 8080)));
+    }
+
+    @Test
+    public void testProxyMatchesWithIncludeWildcardExcludeSpecific()
+    {
+        HttpProxy proxy = new HttpProxy("host", 0);
+        proxy.getIncludedAddresses().add("*.example.com");
+        proxy.getExcludedAddresses().add("admin.example.com");
+
+        assertFalse(proxy.matches(new Origin("http", "other.com", 80)));
+        assertTrue(proxy.matches(new Origin("http", "www.example.com", 80)));
+        assertTrue(proxy.matches(new Origin("http", "api.example.com", 443)));
+        assertFalse(proxy.matches(new Origin("http", "admin.example.com", 80)));
+    }
+
+    @Test
+    public void testProxyMatchesWithCidrIncludeAndExclude()
+    {
+        HttpProxy proxy = new HttpProxy("host", 0);
+        proxy.getIncludedAddresses().add("10.0.0.0/8");
+        proxy.getExcludedAddresses().add("10.10.0.0/16");
+
+        assertFalse(proxy.matches(new Origin("http", "192.168.1.1", 80)));
+        assertTrue(proxy.matches(new Origin("http", "10.0.0.1", 80)));
+        assertTrue(proxy.matches(new Origin("http", "10.255.255.255", 80)));
+        assertFalse(proxy.matches(new Origin("http", "10.10.0.1", 80)));
+        assertFalse(proxy.matches(new Origin("http", "10.10.255.255", 80)));
+    }
 }
