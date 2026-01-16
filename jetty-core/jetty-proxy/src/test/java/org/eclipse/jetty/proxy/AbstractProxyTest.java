ea21ff178aa222d76e409402225841541408ca74
Alexander Farber <farber72@outlook.de>
2026-01-16 12:30:04 +0100

    Improve comments

--- jetty-core/jetty-proxy/src/test/java/org/eclipse/jetty/proxy/AbstractProxyTest.java
+++ jetty-core/jetty-proxy/src/test/java/org/eclipse/jetty/proxy/AbstractProxyTest.java
@@ -70,8 +70,6 @@ public class AbstractProxyTest
         QueuedThreadPool proxyPool = new QueuedThreadPool();
         proxyPool.setName("proxy");
         proxy = new Server(proxyPool);
-        // Note: Individual tests can configure proxyHttpConfig.setSendDateHeader()
-        // and proxyHttpConfig.setSendServerVersion() before calling startProxy().
         proxyConnector = new ServerConnector(proxy, 1, 1, new HttpConnectionFactory(proxyHttpConfig), new HTTP2CServerConnectionFactory(proxyHttpConfig));
         proxy.addConnector(proxyConnector);
         proxy.setHandler(handler);
