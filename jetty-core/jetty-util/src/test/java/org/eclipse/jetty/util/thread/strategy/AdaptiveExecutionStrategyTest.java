386bbd27eb037d25016c192bf2c6b7cf1245885c
Ludovic Orban <lorban@bitronix.be>
2026-01-19 17:04:09 +0100

    revert Invocable.ReadyTask replacements
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-core/jetty-util/src/test/java/org/eclipse/jetty/util/thread/strategy/AdaptiveExecutionStrategyTest.java
+++ jetty-core/jetty-util/src/test/java/org/eclipse/jetty/util/thread/strategy/AdaptiveExecutionStrategyTest.java
@@ -63,7 +63,7 @@ public class AdaptiveExecutionStrategyTest
         {
             AtomicReference<Throwable> detector = new AtomicReference<>();
             CountDownLatch latch = new CountDownLatch(2);
-            BlockingQueue<Invocable.Task> tasks = new LinkedBlockingQueue<>();
+            BlockingQueue<Invocable.ReadyTask> tasks = new LinkedBlockingQueue<>();
             startAES(() ->
             {
                 boolean proceed = detector.compareAndSet(null, new Throwable());
@@ -92,7 +92,7 @@ public class AdaptiveExecutionStrategyTest
             // Start production in another thread.
             aes.dispatch();
 
-            tasks.offer(Invocable.from(Invocable.InvocationType.BLOCKING, () ->
+            tasks.offer(new Invocable.ReadyTask(Invocable.InvocationType.BLOCKING, () ->
             {
                 try
                 {
