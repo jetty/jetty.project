b8b298d44547c6d8de8419f8d2bf90ae8eefe9a7
Ludovic Orban <lorban@bitronix.be>
2026-01-21 22:05:29 +0100

    fix checkstyle
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-core/jetty-util/src/test/java/org/eclipse/jetty/util/ArrayUtilTest.java
+++ jetty-core/jetty-util/src/test/java/org/eclipse/jetty/util/ArrayUtilTest.java
@@ -16,8 +16,11 @@ package org.eclipse.jetty.util;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
