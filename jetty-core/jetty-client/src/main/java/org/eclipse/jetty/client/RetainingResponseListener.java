0a51876a56687f4934144ab1003f9d65688a4ffb
Ludovic Orban <lorban@bitronix.be>
2026-02-12 17:39:11 +0100

    javadoc when streaming happens or not, and its potential to leak buffers
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-core/jetty-client/src/main/java/org/eclipse/jetty/client/RetainingResponseListener.java
+++ jetty-core/jetty-client/src/main/java/org/eclipse/jetty/client/RetainingResponseListener.java
@@ -18,13 +18,18 @@ import org.eclipse.jetty.io.RetainableByteBuffer;
 /**
  * <p>Implementation of {@link Response.Listener} that retains the response
  * content without copying it, up to a configurable number of bytes.</p>
- * <p>The content may be retrieved from {@link #onSuccess(Response)} or {@link #onComplete(Result)}
- * via {@link #getContent()} or {@link #getContentAsString()}.</p>
  * <p>Instances of this class are not reusable, so one must be allocated for each request.</p>
- * <p>The implementation retains the response content chunks, and converts them into a {@code byte[]}
- * upon response success, returned by {@link #getContent()}.</p>
- * <p>Subclasses overriding {@link #onSuccess(Response)} or {@link #onFailure(Response, Throwable)}
- * must always call the {@code super} method to guarantee that the retained chunks are released.</p>
+ * <p>The content may be retrieved from {@link #onSuccess(Response)} or {@link #onComplete(Result)}
+ * via one of the {@code getContent*()} methods.</p>
+ * <p>If {@link #getContent()} or {@link #getContentAsString()} is called first, then the content is copied
+ * into a {@code byte[]} and all further calls will read this {@code byte[]}, while if
+ * {@link #getContentAsInputStream()} or {@link #getContentAsContentSource()} is called first, the content will be
+ * read without copying it, but further {@code getContent*()} calls will see an empty content.</p>
+ * <p>ATTENTION: {@link #onSuccess(Response)} is overridden to avoid copying the contents into a {@code byte[]},
+ * so this means the contents MUST be consumed by calling one of the {@code getContent*()} methods
+ * otherwise the backing buffers will be leaked. If a streaming method like {@link #getContentAsInputStream()}
+ * or {@link #getContentAsContentSource()} is called, the contents MUST be read until the end
+ * (or closed/failed appropriately) otherwise the backing buffers of the unread content will also be leaked.</p>
  */
 public abstract class RetainingResponseListener extends AbstractResponseListener
 {
