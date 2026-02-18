bf7174ea3287a80959d0934725d0ddf74c71a577
Simone Bordet <simone.bordet@gmail.com>
2026-02-18 15:11:30 +0100

    Fixes #14442 - Allow splitting of Transfer-Encoding chunks. (#14443)
    * Introduced `HttpConnection.transferEncodingChunkMaxLength`, defaulting to 1 GiB to allow applications to configure chunking.
    * Introduced `HttpGenerator.chunkMaxLength` to control the chunk-size produced by the generator.
    * Updated `HttpConnection` to slice the content appropriately if it is larger than the chunkMaxLength.
    * Updated `HttpSenderOverHTTP` to slice the content appropriately if it is larger than the chunkMaxLength.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/internal/HttpConnection.java
+++ jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/internal/HttpConnection.java
@@ -295,6 +295,16 @@ public class HttpConnection extends AbstractMetaDataConnection implements Runnab
         getHttpConfiguration().setUseOutputDirectByteBuffers(useOutputDirectByteBuffers);
     }
 
+    public int getTransferEncodingChunkMaxLength()
+    {
+        return _generator.getChunkMaxLength();
+    }
+
+    public void setTransferEncodingChunkMaxLength(int transferEncodingChunkMaxLength)
+    {
+        _generator.setChunkMaxLength(transferEncodingChunkMaxLength);
+    }
+
     @Override
     public ByteBuffer onUpgradeFrom()
     {
@@ -839,6 +849,7 @@ public class HttpConnection extends AbstractMetaDataConnection implements Runnab
             int responseHeadersSize = getHttpConfiguration().getResponseHeaderSize();
             int maxResponseHeadersSize = getHttpConfiguration().getMaxResponseHeaderSize();
             boolean useDirectByteBuffers = isUseOutputDirectByteBuffers();
+            int chunkMaxLength = getTransferEncodingChunkMaxLength();
             while (true)
             {
                 ByteBuffer headerByteBuffer = _header == null ? null : _header.getByteBuffer();
@@ -916,37 +927,29 @@ public class HttpConnection extends AbstractMetaDataConnection implements Runnab
                             gatherWrite += 2;
                             bytes += _chunk.remaining();
                         }
-                        if (BufferUtil.hasContent(_content))
+                        ByteBuffer contentByteBuffer = _content;
+                        if (BufferUtil.hasContent(contentByteBuffer))
                         {
                             gatherWrite += 1;
-                            bytes += _content.remaining();
+                            if (_generator.isChunking() && contentByteBuffer.remaining() > chunkMaxLength)
+                            {
+                                ByteBuffer slice = contentByteBuffer.slice(contentByteBuffer.position(), chunkMaxLength);
+                                contentByteBuffer.position(contentByteBuffer.position() + chunkMaxLength);
+                                contentByteBuffer = slice;
+                            }
+                            bytes += contentByteBuffer.remaining();
                         }
                         _bytesOut.addAndGet(bytes);
                         switch (gatherWrite)
                         {
-                            case 7:
-                                getEndPoint().write(this, headerByteBuffer, chunkByteBuffer, _content);
-                                break;
-                            case 6:
-                                getEndPoint().write(this, headerByteBuffer, chunkByteBuffer);
-                                break;
-                            case 5:
-                                getEndPoint().write(this, headerByteBuffer, _content);
-                                break;
-                            case 4:
-                                getEndPoint().write(this, headerByteBuffer);
-                                break;
-                            case 3:
-                                getEndPoint().write(this, chunkByteBuffer, _content);
-                                break;
-                            case 2:
-                                getEndPoint().write(this, chunkByteBuffer);
-                                break;
-                            case 1:
-                                getEndPoint().write(this, _content);
-                                break;
-                            default:
-                                succeeded();
+                            case 7 -> getEndPoint().write(this, headerByteBuffer, chunkByteBuffer, contentByteBuffer);
+                            case 6 -> getEndPoint().write(this, headerByteBuffer, chunkByteBuffer);
+                            case 5 -> getEndPoint().write(this, headerByteBuffer, contentByteBuffer);
+                            case 4 -> getEndPoint().write(this, headerByteBuffer);
+                            case 3 -> getEndPoint().write(this, chunkByteBuffer, contentByteBuffer);
+                            case 2 -> getEndPoint().write(this, chunkByteBuffer);
+                            case 1 -> getEndPoint().write(this, contentByteBuffer);
+                            default -> succeeded();
                         }
 
                         return Action.SCHEDULED;
