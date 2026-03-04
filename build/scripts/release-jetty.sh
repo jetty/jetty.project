65a631a60cff307a22557c69b884a37a90b133de
Olivier Lamy <olamy@apache.org>
2026-03-04 11:37:30 +1000

    fix comment in release script
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- build/scripts/release-jetty.sh
+++ build/scripts/release-jetty.sh
@@ -203,7 +203,7 @@ if proceedyn "Are you sure you want to release using above? (y/N)" n; then
     fi
 
     # here we need to add something to publish to our staging repo
-    # mvn njord:publish -Ddrop=false -Dpublisher=deploy -DaltDeploymentRepository=jetty-staging::https://repository.webtide.net/repository/release-staging/
+    # mvn njord:publish -Dnjord.drop=false -Dnjord.publisher=deploy -DaltDeploymentRepository=jetty-staging::https://repository.webtide.net/repository/release-staging/
     # need an entry in settings.xml for id jetty-staging
 
 else
