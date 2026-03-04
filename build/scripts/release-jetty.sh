89802249da77463e64925720d22384034bc5f1a0
Olivier Lamy <olamy@apache.org>
2026-03-04 10:08:53 +1000

    fix staging url
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- build/scripts/release-jetty.sh
+++ build/scripts/release-jetty.sh
@@ -204,7 +204,7 @@ if proceedyn "Are you sure you want to release using above? (y/N)" n; then
     fi
 
     # here we need to add something to publish to our staging repo
-    # mvn njord:publish -Ddrop=false -Dpublisher=deploy -DaltDeploymentRepository=jetty-staging::http://localhost:8081/repository/release-staging
+    # mvn njord:publish -Ddrop=false -Dpublisher=deploy -DaltDeploymentRepository=jetty-staging::https://repository.webtide.net/repository/release-staging/
     # need an entry in settings.xml for id jetty-staging
 
 else
