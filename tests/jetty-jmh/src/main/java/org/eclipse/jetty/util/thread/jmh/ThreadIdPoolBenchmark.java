eba58f697dc6136de37676aada6400e8a5a964a0
Ludovic Orban <lorban@bitronix.be>
2026-03-24 16:15:46 +0100

    add ThreadIdPool benchmark
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

---
+++ tests/jetty-jmh/src/main/java/org/eclipse/jetty/util/thread/jmh/ThreadIdPoolBenchmark.java
@@ -0,0 +1,67 @@
+//
+// ========================================================================
+// Copyright (c) 1995 Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
+
+package org.eclipse.jetty.util.thread.jmh;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@State(Scope.Benchmark)
+@Warmup(iterations = 4, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 10, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(1)
+@Threads(200)
+public class ThreadIdPoolBenchmark
+{
+    private final ThreadIdPool<String> threadIdPool = new ThreadIdPool<>(8);
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    public void test()
+    {
+        String s = threadIdPool.take();
+
+        Blackhole.consumeCPU(100);
+
+        threadIdPool.offer(s);
+    }
+
+    public static void main(String[] args) throws RunnerException
+    {
+        Options opt = new OptionsBuilder()
+            .include(ThreadIdPoolBenchmark.class.getSimpleName())
+            // .syncIterations(true) // Don't start all threads at same time
+            // .addProfiler(CompilerProfiler.class)
+            // .addProfiler(LinuxPerfProfiler.class)
+            // .addProfiler(LinuxPerfNormProfiler.class)
+            // .addProfiler(LinuxPerfAsmProfiler.class)
+            // .resultFormat(ResultFormatType.CSV)
+            .build();
+
+        new Runner(opt).run();
+    }
+}
