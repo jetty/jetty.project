17b6d2535fb1faedcbd1f5b48bd2edb1c74e10a9
Jesse Glick <jglick@cloudbees.com>
2026-03-26 08:17:06 -0400

    Clarified error message
    Co-authored-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/internal/HttpConnection.java
+++ jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/internal/HttpConnection.java
@@ -1608,7 +1608,7 @@ public class HttpConnection extends AbstractMetaDataConnection implements Runnab
             @SuppressWarnings("ReferenceEquality")
             boolean isPriorKnowledgeH2C = _upgrade == PREAMBLE_UPGRADE_H2C;
             if (!isPriorKnowledgeH2C  && !_connectionUpgrade)
-                throw new HttpException.RuntimeException(HttpStatus.BAD_REQUEST_400, "Unexpected Upgrade header");
+                throw new HttpException.RuntimeException(HttpStatus.BAD_REQUEST_400, "Invalid H2C upgrade headers");
 
             // Find the upgrade factory.
             ConnectionFactory.Upgrading factory = getConnector().getConnectionFactories().stream()
