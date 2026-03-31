0bc383dd0d6102c334e5f53516ae1f1aaaed556a
Lachlan <lachlan@webtide.com>
2026-04-01 04:53:54 +1100

    Issue #14431 - fixes for ServletContainerInitializer exclusion with absolute-ordering (#14432)
    Change MetaData to lazily order its resources only when getWebInfResources(true) is called, and reset this ordering when metadata.setOrdering() is called.
    
    Signed-off-by: Lachlan Roberts <lachlan.p.roberts@gmail.com>

--- jetty-ee9/jetty-ee9-webapp/src/main/java/org/eclipse/jetty/ee9/webapp/MetaData.java
+++ jetty-ee9/jetty-ee9-webapp/src/main/java/org/eclipse/jetty/ee9/webapp/MetaData.java
@@ -331,6 +331,7 @@ public class MetaData
         }
 
         //recompute the ordering with the new fragment name
+        _orderedWebInfResources.clear();
         orderFragments();
     }
 
@@ -430,8 +431,7 @@ public class MetaData
 
     public void orderFragments()
     {
-        _orderedWebInfResources.clear();
-        if (getOrdering() != null)
+        if (_orderedWebInfResources.isEmpty() && getOrdering() != null && !_webInfJars.isEmpty())
             _orderedWebInfResources.addAll(getOrdering().order(_webInfJars));
     }
 
@@ -488,7 +488,7 @@ public class MetaData
         resources.add(null); //always apply annotations with no resource first
         resources.addAll(_orderedContainerResources); //next all annotations from container path
         resources.addAll(_webInfClasses); //next everything from web-inf classes
-        resources.addAll(getWebInfResources(isOrdered())); //finally annotations (in order) from webinf path 
+        resources.addAll(getWebInfResources(isOrdered())); //finally annotations (in order) from webinf path
 
         for (Resource r : resources)
         {
@@ -533,7 +533,7 @@ public class MetaData
     {
         boolean distributable = (
             (_webDefaultsRoot != null && _webDefaultsRoot.isDistributable()) ||
-                (_webXmlRoot != null && _webXmlRoot.isDistributable()));
+            (_webXmlRoot != null && _webXmlRoot.isDistributable()));
 
         for (WebDescriptor d : _webOverrideRoots)
         {
@@ -581,7 +581,7 @@ public class MetaData
     public void setOrdering(Ordering o)
     {
         _ordering = o;
-        orderFragments();
+        _orderedWebInfResources.clear();
     }
 
     /**
@@ -703,14 +703,16 @@ public class MetaData
     public void addWebInfResource(Resource newResource)
     {
         _webInfJars.add(newResource);
+        _orderedWebInfResources.clear();
     }
 
     public List<Resource> getWebInfResources(boolean withOrdering)
     {
         if (!withOrdering)
             return Collections.unmodifiableList(_webInfJars);
-        else
-            return Collections.unmodifiableList(_orderedWebInfResources);
+
+        orderFragments();
+        return Collections.unmodifiableList(_orderedWebInfResources);
     }
 
     public List<Resource> getContainerResources()
