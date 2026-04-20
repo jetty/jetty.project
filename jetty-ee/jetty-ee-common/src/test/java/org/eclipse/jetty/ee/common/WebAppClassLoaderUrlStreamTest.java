974e68eaea5ed0e7443ae5bb78a4267444db09a4
rickardoberg <rickardoberg@gmail.com>
2026-04-20 12:46:00 +0800

    Rename modules

--- jetty-ee/jetty-ee-common/src/test/java/org/eclipse/jetty/ee/common/WebAppClassLoaderUrlStreamTest.java
+++ jetty-ee/jetty-ee-common/src/test/java/org/eclipse/jetty/ee/common/WebAppClassLoaderUrlStreamTest.java
@@ -11,7 +11,7 @@
 // ========================================================================
 //
 
-package org.eclipse.jetty.ee.webapp;
+package org.eclipse.jetty.ee.common;
 
 import java.net.URLStreamHandler;
 import java.net.URLStreamHandlerFactory;
