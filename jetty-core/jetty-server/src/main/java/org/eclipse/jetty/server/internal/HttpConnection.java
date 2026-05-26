72206b3ea623cf7ed8729b47a83ee628ff10e8eb
Lachlan Roberts <lachlan.p.roberts@gmail.com>
2026-05-26 20:24:11 +1000

    Issue #15161 - optimization for memory footprint of HttpConnection (12.1)
    Signed-off-by: Lachlan Roberts <lachlan.p.roberts@gmail.com>

--- jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/internal/HttpConnection.java
+++ jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/internal/HttpConnection.java
@@ -1102,6 +1102,8 @@ public class HttpConnection extends AbstractMetaDataConnection implements Runnab
             if (!_stream.compareAndSet(null, stream))
                 throw new IllegalStateException("Stream pending");
             _headerBuilder.clear();
+            if (_trailers != null)
+                _trailers.clear();
             _httpChannel.setHttpStream(stream);
         }
 
@@ -1168,9 +1170,15 @@ public class HttpConnection extends AbstractMetaDataConnection implements Runnab
             if (stream._chunk != null)
                 throw new IllegalStateException();
             if (_trailers != null)
-                stream._chunk = new Trailers(_trailers.asImmutable());
+            {
+                if (_trailers.size() > 0)
+                    stream._chunk = new Trailers(_trailers.asImmutable());
+                _trailers = null;
+            }
             else
+            {
                 stream._chunk = Content.Chunk.EOF;
+            }
 
             return false;
         }
@@ -1358,7 +1366,7 @@ public class HttpConnection extends AbstractMetaDataConnection implements Runnab
             if (_uri.getPath() == null)
                 _uri.path("/");
 
-            _request = new MetaData.Request(_parser.getBeginNanoTime(), _method, _uri.asImmutable(), _version, _headerBuilder, _contentLength)
+            _request = new MetaData.Request(_parser.getBeginNanoTime(), _method, _uri.asImmutable(), _version, _headerBuilder.asImmutable(), _contentLength)
             {
                 @Override
                 public boolean is100ContinueExpected()
@@ -1434,6 +1442,7 @@ public class HttpConnection extends AbstractMetaDataConnection implements Runnab
                 }
             }
 
+            _headerBuilder.clear();
             if (!persistent)
                 _generator.setPersistent(false);
 
