8e0c2a66dd2e5c6d43a4d5f903bc3e55a3b77958
Simone Bordet <simone.bordet@gmail.com>
2026-08-06 19:43:20 +0200

    Moved jetty-ee11 test brought in by bad merge into jetty-ee.

--- jetty-ee/jetty-ee-tests/jetty-ee-test-client-transports/src/test/java/org/eclipse/jetty/ee/test/client/transport/Http2Test.java
+++ jetty-ee/jetty-ee-tests/jetty-ee-test-client-transports/src/test/java/org/eclipse/jetty/ee/test/client/transport/Http2Test.java
@@ -11,7 +11,7 @@
 // ========================================================================
 //
 
-package org.eclipse.jetty.ee11.test.client.transport;
+package org.eclipse.jetty.ee.test.client.transport;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -22,8 +22,8 @@ import java.util.concurrent.TimeUnit;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.eclipse.jetty.ee11.servlet.ServletContextHandler;
-import org.eclipse.jetty.ee11.servlet.ServletHolder;
+import org.eclipse.jetty.ee.servlet.ServletContextHandler;
+import org.eclipse.jetty.ee.servlet.ServletHolder;
 import org.eclipse.jetty.http.HttpFields;
 import org.eclipse.jetty.http.HttpURI;
 import org.eclipse.jetty.http.HttpVersion;
