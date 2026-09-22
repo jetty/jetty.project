d147560eab4144bfe25f85d4502d45cd146fad06
Ludovic Orban <lorban@bitronix.be>
2026-09-22 10:11:10 +0200

    bypass HttpOutput completion when HttpOutput is bypassed, this prevents writing a 2nd last buffer to the underlying sink
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-ee10/jetty-ee10-servlet/src/main/java/org/eclipse/jetty/ee10/servlet/ServletContextResponse.java
+++ jetty-ee10/jetty-ee10-servlet/src/main/java/org/eclipse/jetty/ee10/servlet/ServletContextResponse.java
@@ -207,6 +207,12 @@ public class ServletContextResponse extends ContextResponse implements ServletCo
 
     public void completeOutput(Callback callback)
     {
+        if (!isWritingOrStreaming())
+        {
+            callback.succeeded();
+            return;
+        }
+
         if (_outputType == OutputType.WRITER)
             _writer.markAsClosed();
         getHttpOutput().complete(callback);
