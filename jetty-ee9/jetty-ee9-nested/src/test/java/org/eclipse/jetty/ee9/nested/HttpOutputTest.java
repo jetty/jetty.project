540d3fbe531cada43720b9a64b64b7d1478d700f
Ludovic Orban <lorban@bitronix.be>
2026-09-25 10:46:18 +0200

    better align EE9/10/11 tests
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-ee9/jetty-ee9-nested/src/test/java/org/eclipse/jetty/ee9/nested/HttpOutputTest.java
+++ jetty-ee9/jetty-ee9-nested/src/test/java/org/eclipse/jetty/ee9/nested/HttpOutputTest.java
@@ -105,6 +105,60 @@ public class HttpOutputTest
         assertThat(response, containsString("HTTP/1.1 200 OK"));
     }
 
+
+    @Test
+    public void testSendContentHeapByteBuffer() throws Exception
+    {
+        _server.start();
+        byte[] buffer = new byte[16 * 1024];
+        Arrays.fill(buffer, 0, 4 * 1024, (byte)0x99);
+        Arrays.fill(buffer, 4 * 1024, 12 * 1024, (byte)0x58);
+        Arrays.fill(buffer, 12 * 1024, 16 * 1024, (byte)0x66);
+        _handler._content = ByteBuffer.wrap(buffer);
+        _handler._content.limit(12 * 1024);
+        _handler._content.position(4 * 1024);
+        String response = _connector.getResponse("GET / HTTP/1.0\nHost: localhost:80\n\n");
+        assertThat(response, containsString("HTTP/1.1 200 OK"));
+        assertThat(response, containsString("\r\nXXXXXXXXXXXXXXXXXXXXXXXXXXX"));
+
+        for (int i = 0; i < 4 * 1024; i++)
+        {
+            assertEquals((byte)0x99, buffer[i], "i=" + i);
+        }
+        for (int i = 12 * 1024; i < 16 * 1024; i++)
+        {
+            assertEquals((byte)0x66, buffer[i], "i=" + i);
+        }
+    }
+
+    @Test
+    public void testSendContentDirectByteBuffer() throws Exception
+    {
+        _server.start();
+        byte[] buffer = new byte[16 * 1024];
+        Arrays.fill(buffer, 0, 4 * 1024, (byte)0x99);
+        Arrays.fill(buffer, 4 * 1024, 12 * 1024, (byte)0x58);
+        Arrays.fill(buffer, 12 * 1024, 16 * 1024, (byte)0x66);
+        _handler._content = ByteBuffer.allocateDirect(buffer.length);
+        BufferUtil.flipToFill(_handler._content);
+        BufferUtil.append(_handler._content, buffer);
+        BufferUtil.flipToFlush(_handler._content, 0);
+        _handler._content.limit(12 * 1024);
+        _handler._content.position(4 * 1024);
+        String response = _connector.getResponse("GET / HTTP/1.0\nHost: localhost:80\n\n");
+        assertThat(response, containsString("HTTP/1.1 200 OK"));
+        assertThat(response, containsString("\r\nXXXXXXXXXXXXXXXXXXXXXXXXXXX"));
+
+        for (int i = 0; i < 4 * 1024; i++)
+        {
+            assertEquals((byte)0x99, buffer[i], "i=" + i);
+        }
+        for (int i = 12 * 1024; i < 16 * 1024; i++)
+        {
+            assertEquals((byte)0x66, buffer[i], "i=" + i);
+        }
+    }
+
     @Test
     public void testByteUnknown() throws Exception
     {
